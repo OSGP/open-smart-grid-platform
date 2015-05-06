a79e326eb2fba1e5287455bfef9afdeef33655d0
Kevin Smeets <kevin.smeets@cgi.com>
2015-05-06 16:20:10 +0200

    closes #40 Fixes database authorization for read only database users.

--- osgp-core/src/main/resources/db/migration/V2015.001__Added_read_only_role_for_osgp_core_db.sql
+++ osgp-core/src/main/resources/db/migration/V2015.001__Added_read_only_role_for_osgp_core_db.sql
@@ -2,4 +2,4 @@
 
 
 --CREATE USER osgp_core_db_api_user PASSWORD 'osgp_core_db_api_user' NOSUPERUSER;
---GRANT SELECT ON public.device TO osgp_core_db_api_user;
+GRANT SELECT ON public.device TO osgp_core_db_api_user;
