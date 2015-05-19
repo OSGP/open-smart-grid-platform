701d77dcf302e47d0f41cb32bbff52de3930e297
Ruud Lemmers <ruud.lemmers@cgi.com>
2015-05-19 17:32:11 +0200

    Adds Smart Metering to the domain_info table for #16.

---
+++ osgp-core/src/main/resources/db/migration/V2015.005_Added_Smart_Metering_domain_info.sql
@@ -0,0 +1,10 @@
+-- Adds the incoming and outgoing queues for the Smart Metering queues
+INSERT INTO domain_info(creation_time, modification_time, version, domain,
+ domain_version, incoming_domain_requests_queue, outgoing_domain_responses_queue,
+ outgoing_domain_requests_queue, incoming_domain_responses_queue)
+VALUES (current_date, current_date, 0, 'SMART_METERING', '1.0',
+ 'osgp-core.1_0.domain-smartmetering.1_0.requests',
+ 'domain-smartmetering.1_0.osgp-core.1_0.responses',
+ 'domain-smartmetering.1_0.osgp-core.1_0.requests',
+ 'osgp-core.1_0.domain-smartmetering.1_0.responses'
+);
