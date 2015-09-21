9b3cfaad7aad87cfe374296c1a5ddc1a572eb0c9
bjornamkreutz <bjorn.amkreutz@cgi.com>
2015-09-21 17:03:41 +0200

    added messageProcessor for core

---
+++ osgp-adapter-domain-smartmetering/src/main/java/com/alliander/osgp/adapter/domain/smartmetering/infra/jms/core/messageprocessors/SynchronizeTimeReadsresponseMessageProcessor.java
@@ -0,0 +1,88 @@
+package com.alliander.osgp.adapter.domain.smartmetering.infra.jms.core.messageprocessors;
+
+import javax.jms.JMSException;
+import javax.jms.ObjectMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.alliander.osgp.adapter.domain.smartmetering.application.services.AdhocService;
+import com.alliander.osgp.adapter.domain.smartmetering.infra.jms.core.OsgpCoreResponseMessageProcessor;
+import com.alliander.osgp.domain.core.valueobjects.DeviceFunction;
+import com.alliander.osgp.dto.valueobjects.smartmetering.SynchronizeTimeReads;
+import com.alliander.osgp.shared.exceptionhandling.OsgpException;
+import com.alliander.osgp.shared.infra.jms.Constants;
+import com.alliander.osgp.shared.infra.jms.ResponseMessage;
+import com.alliander.osgp.shared.infra.jms.ResponseMessageResultType;
+
+/**
+ * Class for processing smart metering default response messages
+ */
+@Component("domainSmartMeteringSynchronizeTimeReadsResponseMessageProcessor")
+public class SynchronizeTimeReadsresponseMessageProcessor extends OsgpCoreResponseMessageProcessor {
+	
+    /**
+     * Logger for this class
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SynchronizeTimeReadsresponseMessageProcessor.class);
+
+    @Autowired
+    @Qualifier("domainSmartMeteringAdhocService")
+    private AdhocService adhocService;
+
+    protected SynchronizeTimeReadsresponseMessageProcessor() {
+        super(DeviceFunction.REQUEST_SYNCHRONIZE_TIME);
+    }
+
+	@Override
+	public void processMessage(ObjectMessage message) throws JMSException {
+        LOGGER.debug("Processing smart metering default response message");
+
+        String correlationUid = null;
+        String messageType = null;
+        String organisationIdentification = null;
+        String deviceIdentification = null;
+
+        ResponseMessage responseMessage = null;
+        ResponseMessageResultType responseMessageResultType = null;
+        OsgpException osgpException = null;
+        SynchronizeTimeReads synchronizeTimeReads = null;
+
+        try {
+            correlationUid = message.getJMSCorrelationID();
+            messageType = message.getJMSType();
+            organisationIdentification = message.getStringProperty(Constants.ORGANISATION_IDENTIFICATION);
+            deviceIdentification = message.getStringProperty(Constants.DEVICE_IDENTIFICATION);
+
+            responseMessage = (ResponseMessage) message.getObject();
+            responseMessageResultType = responseMessage.getResult();
+            osgpException = responseMessage.getOsgpException();
+            synchronizeTimeReads = (SynchronizeTimeReads) responseMessage.getDataObject();
+        } catch (final JMSException e) {
+            LOGGER.error("UNRECOVERABLE ERROR, unable to read ObjectMessage instance, giving up.", e);
+            LOGGER.debug("correlationUid: {}", correlationUid);
+            LOGGER.debug("messageType: {}", messageType);
+            LOGGER.debug("organisationIdentification: {}", organisationIdentification);
+            LOGGER.debug("deviceIdentification: {}", deviceIdentification);
+            LOGGER.debug("responseMessageResultType: {}", responseMessageResultType);
+            LOGGER.debug("deviceIdentification: {}", deviceIdentification);
+            LOGGER.debug("osgpException: {}", osgpException);
+            return;
+        }
+
+        try {
+            LOGGER.info("Calling application service function to handle response: {}", messageType);
+
+            this.adhocService.handleSynchronizeTimeReadsresponse(deviceIdentification, organisationIdentification,
+                    correlationUid, messageType, responseMessageResultType, osgpException, synchronizeTimeReads);
+
+        } catch (final Exception e) {
+            this.handleError(e, correlationUid, organisationIdentification, deviceIdentification, messageType);
+        }
+ 		
+	}
+
+}
