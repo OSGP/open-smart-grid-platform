8888d5e621e7c49d4f3f0c999865f1726e63c627
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2015-10-13 09:45:06 +0200

    Adds set alarm notification to DLMS protocol adapter.

---
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/infra/messaging/processors/SetAlarmNotificationsRequestMessageProcessor.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.osgp.adapter.protocol.dlms.infra.messaging.processors;
+
+import javax.jms.JMSException;
+import javax.jms.ObjectMessage;
+
+import org.osgp.adapter.protocol.dlms.application.services.InstallationService;
+import org.osgp.adapter.protocol.dlms.infra.messaging.DeviceRequestMessageProcessor;
+import org.osgp.adapter.protocol.dlms.infra.messaging.DeviceRequestMessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.alliander.osgp.dto.valueobjects.smartmetering.AlarmSwitches;
+import com.alliander.osgp.shared.infra.jms.Constants;
+
+/**
+ * Class for processing set alarm notifications request messages
+ */
+@Component("dlmsSetAlarmNotificationsRequestMessageProcessor")
+public class SetAlarmNotificationsRequestMessageProcessor extends DeviceRequestMessageProcessor {
+    /**
+     * Logger for this class
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SetAlarmNotificationsRequestMessageProcessor.class);
+
+    @Autowired
+    private InstallationService installationService;
+
+    public SetAlarmNotificationsRequestMessageProcessor() {
+        super(DeviceRequestMessageType.SET_ALARM_NOTIFICATIONS);
+    }
+
+    @Override
+    public void processMessage(final ObjectMessage message) {
+        LOGGER.debug("Processing set alarm notifications request message");
+
+        String correlationUid = null;
+        String domain = null;
+        String domainVersion = null;
+        String messageType = null;
+        String organisationIdentification = null;
+        String deviceIdentification = null;
+
+        try {
+            correlationUid = message.getJMSCorrelationID();
+            domain = message.getStringProperty(Constants.DOMAIN);
+            domainVersion = message.getStringProperty(Constants.DOMAIN_VERSION);
+            messageType = message.getJMSType();
+            organisationIdentification = message.getStringProperty(Constants.ORGANISATION_IDENTIFICATION);
+            deviceIdentification = message.getStringProperty(Constants.DEVICE_IDENTIFICATION);
+
+            final AlarmSwitches alarmSwitches = (AlarmSwitches) message.getObject();
+
+            this.installationService.setAlarmNotifications(organisationIdentification, deviceIdentification,
+                    correlationUid, alarmSwitches, this.responseMessageSender, domain, domainVersion, messageType);
+
+        } catch (final JMSException e) {
+            LOGGER.error("UNRECOVERABLE ERROR, unable to read ObjectMessage instance, giving up.", e);
+            LOGGER.debug("correlationUid: {}", correlationUid);
+            LOGGER.debug("domain: {}", domain);
+            LOGGER.debug("domainVersion: {}", domainVersion);
+            LOGGER.debug("messageType: {}", messageType);
+            LOGGER.debug("organisationIdentification: {}", organisationIdentification);
+            LOGGER.debug("deviceIdentification: {}", deviceIdentification);
+            return;
+        }
+    }
+}
