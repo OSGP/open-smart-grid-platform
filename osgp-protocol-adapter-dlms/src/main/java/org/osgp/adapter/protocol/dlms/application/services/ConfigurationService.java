6dfe2f21db2ec3133d63352d958221be3d82f18a
bjornamkreutz <bjorn.amkreutz@cgi.com>
2015-10-14 18:02:26 +0200

    added set configuration object functionality

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/services/ConfigurationService.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/services/ConfigurationService.java
@@ -12,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.alliander.osgp.dto.valueobjects.smartmetering.Configurations;
+import com.alliander.osgp.dto.valueobjects.smartmetering.SetConfigurationObjectRequest;
+import com.alliander.osgp.dto.valueobjects.smartmetering.SetConfigurationObjectRequestData;
 import com.alliander.osgp.dto.valueobjects.smartmetering.SpecialDay;
 import com.alliander.osgp.dto.valueobjects.smartmetering.SpecialDaysRequest;
 import com.alliander.osgp.dto.valueobjects.smartmetering.SpecialDaysRequestData;
@@ -45,7 +48,7 @@ public class ConfigurationService {
         try {
             // The Special days towards the Smart Meter
             SpecialDaysRequestData specialDaysRequestData = specialDaysRequest.getSpecialDaysRequestData();
-            
+
             LOGGER.info("SpecialDaysRequest : {}", specialDaysRequest.getSpecialDaysRequestData());
             for (final SpecialDay specialDay : specialDaysRequestData.getSpecialDays()) {
                 LOGGER.info("******************************************************");
@@ -58,9 +61,46 @@ public class ConfigurationService {
                     deviceIdentification, ResponseMessageResultType.OK, null, responseMessageSender);
 
         } catch (final Exception e) {
-            LOGGER.error("Unexpected exception during synchronizeTime", e);
+            LOGGER.error("Unexpected exception during set special days", e);
             final TechnicalException ex = new TechnicalException(ComponentType.UNKNOWN,
-                    "Unexpected exception during synchronizeTime", e);
+                    "Unexpected exception during set special days", e);
+
+            this.sendResponseMessage(domain, domainVersion, messageType, correlationUid, organisationIdentification,
+                    deviceIdentification, ResponseMessageResultType.NOT_OK, ex, responseMessageSender);
+        }
+    }
+
+    // === REQUEST Configuration Object DATA ===
+
+    public void requestSetConfiguration(final String organisationIdentification, final String deviceIdentification,
+            final String correlationUid, final SetConfigurationObjectRequest setConfigurationObjectRequest,
+            final DeviceResponseMessageSender responseMessageSender, final String domain, final String domainVersion,
+            final String messageType) {
+
+        LOGGER.info("requestSetConfiguration called for device: {} for organisation: {}", deviceIdentification,
+                organisationIdentification);
+
+        try {
+            // Configuration Object towards the Smart Meter
+            SetConfigurationObjectRequestData setConfigurationObjectRequestData = setConfigurationObjectRequest
+                    .getSetConfigurationObjectRequestData();
+
+            LOGGER.info("Set Configuration Object : {}",
+                    setConfigurationObjectRequest.getSetConfigurationObjectRequestData());
+
+            Configurations configurations = setConfigurationObjectRequestData.getConfiguration();
+
+            LOGGER.info("******************************************************");
+            LOGGER.info("Configuration Object :{} ", configurations.getConfiguration());
+            LOGGER.info("******************************************************");
+
+            this.sendResponseMessage(domain, domainVersion, messageType, correlationUid, organisationIdentification,
+                    deviceIdentification, ResponseMessageResultType.OK, null, responseMessageSender);
+
+        } catch (final Exception e) {
+            LOGGER.error("Unexpected exception during set Configuration Object", e);
+            final TechnicalException ex = new TechnicalException(ComponentType.UNKNOWN,
+                    "Unexpected exception during set Configuration Object", e);
 
             this.sendResponseMessage(domain, domainVersion, messageType, correlationUid, organisationIdentification,
                     deviceIdentification, ResponseMessageResultType.NOT_OK, ex, responseMessageSender);
