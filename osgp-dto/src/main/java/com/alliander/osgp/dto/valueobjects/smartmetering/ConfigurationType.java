cabc072e95f63f02e97184d95d1b38c6c8a78b74
bjornamkreutz <bjorn.amkreutz@cgi.com>
2015-10-14 17:34:24 +0200

    added dto's for setConfigObject

---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ConfigurationType.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+public enum ConfigurationType {
+    DISCOVER_ON_OPEN_COVER,
+    DISCOVER_ON_POWER_ON,
+    DYNAMIC_MBUS_ADDRESS,
+    PO_ENABLE,
+    HLS_3_ON_P3_ENABLE,
+    HLS_4_ON_P3_ENABLE,
+    HLS_5_ON_P3_ENABLE,
+    HLS_3_ON_PO_ENABLE,
+    HLS_4_ON_PO_ENABLE,
+    HLS_5_ON_PO_ENABLE;
+
+    public String value() {
+        return this.name();
+    }
+
+    public static ConfigurationType fromValue(final String v) {
+        return valueOf(v);
+    }
+}
---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetConfigurationObjectRequest.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+import java.io.Serializable;
+
+public class SetConfigurationObjectRequest implements Serializable {
+
+    private static final long serialVersionUID = -8295596279285780413L;
+
+    private String deviceIdentification;
+
+    private SetConfigurationObjectRequestData setConfigurationObjectRequestData;
+
+    public String getDeviceIdentification() {
+        return deviceIdentification;
+    }
+
+    public void setDeviceIdentification(String deviceIdentification) {
+        this.deviceIdentification = deviceIdentification;
+    }
+
+    public SetConfigurationObjectRequestData getSetConfigurationObjectRequestData() {
+        return setConfigurationObjectRequestData;
+    }
+
+    public void setSetConfigurationObjectRequestData(SetConfigurationObjectRequestData setConfigurationObjectRequestData) {
+        this.setConfigurationObjectRequestData = setConfigurationObjectRequestData;
+    }
+
+}
