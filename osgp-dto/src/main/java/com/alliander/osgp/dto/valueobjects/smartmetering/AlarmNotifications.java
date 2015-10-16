f0ba095c4ef45fabe6d092fe1c7986ef39f66ae8
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2015-10-16 17:02:15 +0200

    Refactors AlarmSwitches to AlarmNotifications.

---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/AlarmNotifications.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Set;
+
+public class AlarmNotifications implements Serializable {
+
+    private static final long serialVersionUID = 2319359505656305783L;
+
+    private Set<AlarmNotification> alarmNotifications;
+
+    public AlarmNotifications() {
+        this.alarmNotifications = Collections.emptySet();
+    }
+
+    public AlarmNotifications(final Set<AlarmNotification> alarmNotifications) {
+        this.alarmNotifications = alarmNotifications;
+    }
+
+    @Override
+    public String toString() {
+        return "AlarmNotifications[" + String.valueOf(this.alarmNotifications) + "]";
+    }
+
+    public Set<AlarmNotification> getAlarmNotifications() {
+        return this.alarmNotifications;
+    }
+
+    public void setAlarmNotifications(final Set<AlarmNotification> alarmNotifications) {
+        this.alarmNotifications = alarmNotifications;
+    }
+}
