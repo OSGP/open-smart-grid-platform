ac31f1db307d64e6f6d508df8fc03fa3a21010a6
thijlboonen <thijl.boonen@cgi.com>
2015-11-02 13:09:34 +0100

    Formats code

--- osgp-adapter-ws-admin/src/main/java/com/alliander/osgp/adapter/ws/admin/application/services/DeviceManagementService.java
+++ osgp-adapter-ws-admin/src/main/java/com/alliander/osgp/adapter/ws/admin/application/services/DeviceManagementService.java
@@ -348,7 +348,7 @@ public class DeviceManagementService {
 
     public Page<DeviceLogItem> findOslpMessages(@Identification final String organisationIdentification,
             @Identification final String deviceIdentification, @Min(value = 0) final int pageNumber)
-                    throws FunctionalException {
+            throws FunctionalException {
 
         LOGGER.debug("findOslpMessage called with organisation {}, device {} and pagenumber {}", new Object[] {
                 organisationIdentification, deviceIdentification, pageNumber });
@@ -574,7 +574,7 @@ public class DeviceManagementService {
      */
     public void setOwner(@Identification final String organisationIdentification,
             @Identification final String deviceIdentification, @Identification final String newOwner)
-                    throws FunctionalException {
+            throws FunctionalException {
         Organisation organisation = this.findOrganisation(organisationIdentification);
         final Device device = this.findDevice(deviceIdentification);
         this.isAllowed(organisation, PlatformFunction.SET_OWNER);
