6205c630a1a209fbc1e93745dced036dd88d14e8
Rudi de Vries <rudi.de.vries@cgi.com>
2015-11-09 09:27:29 +0100

    Added log time for ActualMeterReads

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ActualMeterReads.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ActualMeterReads.java
@@ -1,14 +1,24 @@
 package com.alliander.osgp.dto.valueobjects.smartmetering;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class ActualMeterReads implements Serializable {
     private static final long serialVersionUID = -297320204916085999L;
 
+    private Date logTime;
     private long activeEnergyImportTariffOne;
     private long activeEnergyImportTariffTwo;
     private long activeEnergyExportTariffOne;
     private long activeEnergyExportTariffTwo;
+
+    public Date getLogTime() {
+        return this.logTime;
+    }
+
+    public void setLogTime(final Date logTime) {
+        this.logTime = logTime;
+    }
 
     public long getActiveEnergyImportTariffOne() {
         return this.activeEnergyImportTariffOne;
@@ -42,4 +52,12 @@ public class ActualMeterReads implements Serializable {
         this.activeEnergyExportTariffTwo = activeEnergyExportTariffTwo;
     }
 
+    @Override
+    public String toString() {
+        return "ActualMeterReads [logTime=" + this.logTime + ", activeEnergyImportTariffOne="
+                + this.activeEnergyImportTariffOne + ", activeEnergyImportTariffTwo="
+                + this.activeEnergyImportTariffTwo + ", activeEnergyExportTariffOne="
+                + this.activeEnergyExportTariffOne + ", activeEnergyExportTariffTwo="
+                + this.activeEnergyExportTariffTwo + "]";
+    }
 }
