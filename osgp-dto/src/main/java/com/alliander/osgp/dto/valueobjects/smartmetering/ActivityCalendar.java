eac91257b8372047c343f5b402ca44fd0ed8c620
Hannek Toebast <hannek24@hotmail.com>
2015-11-12 15:01:42 +0100

    set tariff

---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ActivityCalendar.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ */
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public class ActivityCalendar implements Comparable<ActivityCalendar>, Serializable {
+
+    private static final long serialVersionUID = -8278955482889960359L;
+
+    private String logical_name;
+
+    private String calendar_name;
+
+    private Collection<SeasonProfile> seasonProfileCollection;
+
+    public ActivityCalendar() {
+
+    }
+
+    public ActivityCalendar(final String logical_name, final String calendar_name,
+            final Collection<SeasonProfile> seasonProfileCollection) {
+        super();
+        this.logical_name = logical_name;
+        this.logical_name = calendar_name;
+        this.seasonProfileCollection = seasonProfileCollection;
+    }
+
+    public String getLogicalName() {
+        return this.logical_name;
+    }
+
+    public void setLogicalName(final String logical_name) {
+        this.logical_name = logical_name;
+    }
+
+    public String getCalendarName() {
+        return this.calendar_name;
+    }
+
+    public void setCalendarName(final String calendar_name) {
+        this.calendar_name = calendar_name;
+    }
+
+    public Collection<SeasonProfile> getSeasonProfileCollection() {
+        return this.seasonProfileCollection;
+    }
+
+    public void setSeasonProfileCollection(final Collection<SeasonProfile> seasonProfileCollection) {
+        this.seasonProfileCollection = seasonProfileCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "ActivityCalendar [logical_name=" + this.logical_name + ", calendar_name=" + this.calendar_name
+                + ", seasonProfileCollection=" + this.seasonProfileCollection + "]";
+    }
+
+    @Override
+    public int compareTo(final ActivityCalendar o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SeasonProfile.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ */
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SeasonProfile implements Comparable<SeasonProfile>, Serializable {
+
+    private static final long serialVersionUID = -9110599718005128216L;
+
+    private String season_profile_name;
+
+    private Date season_start;
+
+    private WeekProfile weekProfile;
+
+    private ActivityCalendar activityCalendar;
+
+    public SeasonProfile() {
+
+    }
+
+    public SeasonProfile(final String season_profile_name, final Date season_start, final WeekProfile weekProfile,
+            final ActivityCalendar activityCalendar) {
+        super();
+        this.season_profile_name = season_profile_name;
+        this.season_start = season_start;
+        this.weekProfile = weekProfile;
+        this.activityCalendar = activityCalendar;
+    }
+
+    public String getSeasonProfileName() {
+        return this.season_profile_name;
+    }
+
+    public void setSeasonProfileName(final String season_profile_name) {
+        this.season_profile_name = season_profile_name;
+    }
+
+    public Date getSeasonStart() {
+        return this.season_start;
+    }
+
+    public void setSeasonStart(final Date season_start) {
+        this.season_start = season_start;
+    }
+
+    public WeekProfile getWeekProfile() {
+        return this.weekProfile;
+    }
+
+    public void setWeekProfile(final WeekProfile weekProfile) {
+        this.weekProfile = weekProfile;
+    }
+
+    public ActivityCalendar getActivityCalendar() {
+        return this.activityCalendar;
+    }
+
+    public void setActivityCalendar(final ActivityCalendar activityCalendar) {
+        this.activityCalendar = activityCalendar;
+    }
+
+    @Override
+    public String toString() {
+        return "SeasonProfile [season_profile_name=" + this.season_profile_name + ", season_start=" + this.season_start
+                + ", weekProfile=" + this.weekProfile + ", activityCalendar=" + this.activityCalendar + "]";
+    }
+
+    @Override
+    public int compareTo(final SeasonProfile o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
