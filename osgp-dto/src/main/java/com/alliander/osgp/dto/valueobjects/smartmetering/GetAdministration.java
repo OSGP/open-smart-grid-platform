77b61be4849312063fe610f952ecbe9f0675d048
Remco Baans <remco.baans@cgi.com>
2015-12-07 11:49:26 +0100

    Fix after review

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/GetAdministration.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/GetAdministration.java
@@ -6,13 +6,13 @@ public class GetAdministration implements Serializable {
 
     private static final long serialVersionUID = 2969090682230580484L;
 
-    private String deviceIdentification;
+    private final String deviceIdentification;
+
+    public GetAdministration(final String deviceIdentification) {
+        this.deviceIdentification = deviceIdentification;
+    }
 
     public String getDeviceIdentification() {
         return this.deviceIdentification;
-    }
-
-    public void setDeviceIdentification(final String deviceIdentification) {
-        this.deviceIdentification = deviceIdentification;
     }
 }
--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetAdministration.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetAdministration.java
@@ -6,23 +6,21 @@ public class SetAdministration implements Serializable {
 
     private static final long serialVersionUID = 5653168808776680422L;
 
-    private boolean enabled;
+    private final boolean enabled;
 
-    private String deviceIdentification;
+    private final String deviceIdentification;
+
+    public SetAdministration(final boolean enabled, final String deviceIdentification) {
+        this.enabled = enabled;
+        this.deviceIdentification = deviceIdentification;
+    }
 
     public boolean isEnabled() {
         return this.enabled;
-    }
-
-    public void setEnabled(final boolean enabled) {
-        this.enabled = enabled;
     }
 
     public String getDeviceIdentification() {
         return this.deviceIdentification;
     }
 
-    public void setDeviceIdentification(final String deviceIdentification) {
-        this.deviceIdentification = deviceIdentification;
-    }
 }
