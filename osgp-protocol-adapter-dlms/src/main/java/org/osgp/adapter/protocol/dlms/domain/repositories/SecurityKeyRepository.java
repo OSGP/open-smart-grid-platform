5d89f8428b24a1b88fcbdb83ad2bb5789726597b
Rudi de Vries <rudidevries@gmail.com>
2016-01-13 13:07:48 +0100

    Changes security key query. Updates Shared submodule.

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/repositories/SecurityKeyRepository.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/repositories/SecurityKeyRepository.java
@@ -4,10 +4,12 @@ import org.osgp.adapter.protocol.dlms.domain.entities.DlmsDevice;
 import org.osgp.adapter.protocol.dlms.domain.entities.SecurityKey;
 import org.osgp.adapter.protocol.dlms.domain.entities.SecurityKeyType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SecurityKeyRepository extends JpaRepository<SecurityKey, Long> {
-    SecurityKey findByDlmsDeviceAndSecurityKeyTypeAndValidToIsNull(DlmsDevice dlmsDevice,
-            SecurityKeyType securityKeyType);
+
+    @Query("SELECT sk from SecurityKey sk WHERE sk.dlmsDevice = ?1 AND sk.securityKeyType = ?2 AND sk.validFrom <= current_date() AND ( sk.validTo IS NULL OR sk.validTo > current_date() )")
+    SecurityKey findValidSecurityKey(DlmsDevice dlmsDevice, SecurityKeyType securityKeyType);
 }
