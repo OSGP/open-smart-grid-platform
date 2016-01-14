67dca5f0966220159fee1336fcaf56447c06bcb7
Eduard Drenth <eduarddrenth@gmail.com>
2016-01-14 08:38:29 +0100

    name of column with underscore

--- osgp-protocol-adapter-dlms/src/main/resources/db/migration/V2015.003__Added_new_Device_fields.sql
+++ osgp-protocol-adapter-dlms/src/main/resources/db/migration/V2015.003__Added_new_Device_fields.sql
@@ -1,3 +1,3 @@
-ALTER TABLE dlms_device ADD COLUMN dlmsunit character varying(5);
+ALTER TABLE dlms_device ADD COLUMN dlms_unit character varying(5);
 
 ALTER TABLE dlms_device ADD COLUMN scaler smallint;
