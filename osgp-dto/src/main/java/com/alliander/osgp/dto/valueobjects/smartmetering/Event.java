cb1acda71ac7d23f708e6a465a3a6799534f2d52
Hannek Toebast <hannek24@hotmail.com>
2016-01-29 17:36:38 +0100

    Revert "Implementing support for eventCounter"
    This reverts commit bc5b3e78e0db80efa5b22a42312ad40f25f05ed6.

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/Event.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/Event.java
@@ -19,12 +19,10 @@ public class Event implements Serializable {
     private static final long serialVersionUID = 5484936946786037616L;
     private DateTime timestamp;
     private Integer eventCode;
-    private Integer eventCounter;
 
-    public Event(final DateTime timestamp, final Integer eventCode, final Integer eventCounter) {
+    public Event(final DateTime timestamp, final Integer eventCode) {
         this.timestamp = timestamp;
         this.eventCode = eventCode;
-        this.eventCounter = eventCounter;
     }
 
     public DateTime getTimestamp() {
@@ -34,9 +32,4 @@ public class Event implements Serializable {
     public Integer getEventCode() {
         return this.eventCode;
     }
-
-    public Integer getEventCounter() {
-        return this.eventCounter;
-    }
-
 }
