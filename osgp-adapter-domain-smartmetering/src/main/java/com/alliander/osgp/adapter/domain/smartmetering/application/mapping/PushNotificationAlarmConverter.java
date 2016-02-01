1617e69edf0254fa18f161e583df7f31855ae406
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2016-02-01 18:35:19 +0100

    Handles DLMS push notification alarms through the Platform.

---
+++ osgp-adapter-domain-smartmetering/src/main/java/com/alliander/osgp/adapter/domain/smartmetering/application/mapping/PushNotificationAlarmConverter.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.alliander.osgp.adapter.domain.smartmetering.application.mapping;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import ma.glasnost.orika.converter.BidirectionalConverter;
+import ma.glasnost.orika.metadata.Type;
+
+import com.alliander.osgp.domain.core.valueobjects.smartmetering.AlarmType;
+import com.alliander.osgp.domain.core.valueobjects.smartmetering.PushNotificationAlarm;
+
+public class PushNotificationAlarmConverter
+        extends
+BidirectionalConverter<com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationAlarm, PushNotificationAlarm> {
+
+    @Override
+    public PushNotificationAlarm convertTo(
+            final com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationAlarm source,
+            final Type<PushNotificationAlarm> destinationType) {
+        if (source == null) {
+            return null;
+        }
+
+        final Set<AlarmType> alarms = EnumSet.noneOf(AlarmType.class);
+
+        final Set<com.alliander.osgp.dto.valueobjects.smartmetering.AlarmType> sourceAlarms = source.getAlarms();
+        for (final com.alliander.osgp.dto.valueobjects.smartmetering.AlarmType sourceAlarm : sourceAlarms) {
+            alarms.add(AlarmType.valueOf(sourceAlarm.name()));
+        }
+
+        return new PushNotificationAlarm(source.getDeviceIdentification(), alarms);
+    }
+
+    @Override
+    public com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationAlarm convertFrom(
+            final PushNotificationAlarm source,
+            final Type<com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationAlarm> destinationType) {
+        if (source == null) {
+            return null;
+        }
+
+        final Set<com.alliander.osgp.dto.valueobjects.smartmetering.AlarmType> alarms = EnumSet
+                .noneOf(com.alliander.osgp.dto.valueobjects.smartmetering.AlarmType.class);
+
+        final Set<AlarmType> sourceAlarms = source.getAlarms();
+        for (final AlarmType sourceAlarm : sourceAlarms) {
+            alarms.add(com.alliander.osgp.dto.valueobjects.smartmetering.AlarmType.valueOf(sourceAlarm.name()));
+        }
+
+        return new com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationAlarm(
+                source.getDeviceIdentification(), alarms);
+    }
+}
--- osgp-core/src/main/java/com/alliander/osgp/core/infra/jms/protocol/in/messageprocessors/PushNotificationAlarmMessageProcessor.java
+++ osgp-core/src/main/java/com/alliander/osgp/core/infra/jms/protocol/in/messageprocessors/PushNotificationAlarmMessageProcessor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Smart Society Services B.V.
+ * Copyright 2016 Smart Society Services B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
  *
@@ -7,14 +7,20 @@
  */
 package com.alliander.osgp.core.infra.jms.protocol.in.messageprocessors;
 
+import java.util.List;
+
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.alliander.osgp.core.domain.model.domain.DomainRequestService;
 import com.alliander.osgp.core.infra.jms.protocol.in.ProtocolRequestMessageProcessor;
+import com.alliander.osgp.domain.core.entities.DomainInfo;
+import com.alliander.osgp.domain.core.repositories.DomainInfoRepository;
 import com.alliander.osgp.domain.core.valueobjects.DeviceFunction;
 import com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationAlarm;
 import com.alliander.osgp.shared.infra.jms.Constants;
@@ -24,6 +30,12 @@ import com.alliander.osgp.shared.infra.jms.RequestMessage;
 public class PushNotificationAlarmMessageProcessor extends ProtocolRequestMessageProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PushNotificationAlarmMessageProcessor.class);
+
+    @Autowired
+    private DomainRequestService domainRequestService;
+
+    @Autowired
+    private DomainInfoRepository domainInfoRepository;
 
     protected PushNotificationAlarmMessageProcessor() {
         super(DeviceFunction.PUSH_NOTIFICATION_ALARM);
@@ -44,7 +56,32 @@ public class PushNotificationAlarmMessageProcessor extends ProtocolRequestMessag
         try {
             final PushNotificationAlarm pushNotificationAlarm = (PushNotificationAlarm) dataObject;
 
-            LOGGER.info("TODO send push notification alarm as response to domain layer: " + pushNotificationAlarm);
+            /*
+             * This message processor handles messages that came in on the
+             * osgp-core.1_0.protocol-dlms.1_0.requests queue. Therefore lookup
+             * the DomainInfo for DLMS (domain: SMART_METERING) version 1.0.
+             *
+             * At some point in time there may be a cleaner solution, where the
+             * DomainInfo can be derived from information in the message or JMS
+             * metadata, but for now this will have to do.
+             */
+            final List<DomainInfo> domainInfos = this.domainInfoRepository.findAll();
+            DomainInfo smartMeteringDomain = null;
+            for (final DomainInfo di : domainInfos) {
+                if ("SMART_METERING".equals(di.getDomain()) && "1.0".equals(di.getDomainVersion())) {
+                    smartMeteringDomain = di;
+                    break;
+                }
+            }
+
+            if (smartMeteringDomain == null) {
+                LOGGER.error(
+                        "No DomainInfo found for SMART_METERING 1.0, unable to send message of message type: {} to domain adapter. RequestMessage for {} dropped.",
+                        messageType, pushNotificationAlarm);
+            } else {
+                this.domainRequestService.send(requestMessage, DeviceFunction.PUSH_NOTIFICATION_ALARM.name(),
+                        smartMeteringDomain);
+            }
 
         } catch (final Exception e) {
             LOGGER.error("Exception", e);
