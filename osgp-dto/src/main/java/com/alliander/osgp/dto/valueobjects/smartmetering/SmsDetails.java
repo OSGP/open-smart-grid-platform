d0df43daa9912401149c9124f56af76b53d6384c
mvhaasteren <m.v.haasteren@colbus.nl>
2016-02-02 08:23:21 +0100

    processed review comments

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SmsDetails.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SmsDetails.java
@@ -9,7 +9,7 @@ package com.alliander.osgp.dto.valueobjects.smartmetering;
 
 import java.io.Serializable;
 
-public class SMSDetails implements Serializable {
+public class SmsDetails implements Serializable {
 
     private static final long serialVersionUID = -3638851916093320732L;
 
@@ -19,7 +19,7 @@ public class SMSDetails implements Serializable {
     private String smsMsgAttemptStatus;
     private String msgType;
 
-    public SMSDetails(final String deviceIdentification, final Long smsMsgId, final String status,
+    public SmsDetails(final String deviceIdentification, final Long smsMsgId, final String status,
             final String smsMsgAttemptStatus, final String msgType) {
         this.deviceIdentification = deviceIdentification;
         this.smsMsgId = smsMsgId;
