5529c958ee32b3d9d76628b045652ebf5c8858b0
thijlboonen <thijl.boonen@cgi.com>
2016-02-03 15:10:41 +0100

    Adds manufacturer exceptions to FunctionalExceptionType

--- shared/src/main/java/com/alliander/osgp/shared/exceptionhandling/FunctionalExceptionType.java
+++ shared/src/main/java/com/alliander/osgp/shared/exceptionhandling/FunctionalExceptionType.java
@@ -9,27 +9,34 @@ package com.alliander.osgp.shared.exceptionhandling;
 
 public enum FunctionalExceptionType {
     // Organisation exceptions
-    UNKNOWN_ORGANISATION(101, "Organisation does not exist."), EXISTING_ORGANISATION(102,
-            "Organisation already exists."), EXISTING_ORGANISATION_WITH_SAME_IDENTIFICATION(103,
-            "Another organisation with the same identification already exists."),
+    UNKNOWN_ORGANISATION(101, "Organisation does not exist."),
+    EXISTING_ORGANISATION(102, "Organisation already exists."),
+    EXISTING_ORGANISATION_WITH_SAME_IDENTIFICATION(103, "Another organisation with the same identification already exists."),
 
     // Device exceptions
-    UNKNOWN_DEVICE(201, "Device does not exist."), UNREGISTERED_DEVICE(202, "Device is not registered."), UNSCHEDULED_DEVICE(
-            203, "Device does not have a schedule."), EXISTING_DEVICE(204, "Device already exists."), PROTOCOL_UNKNOWN_FOR_DEVICE(
-            205, "Device has no protocol defined."), UNKNOWN_PROTOCOL_NAME_OR_VERSION(206,
-            "Device has an unknown protocol name or version."),
+    UNKNOWN_DEVICE(201, "Device does not exist."),
+    UNREGISTERED_DEVICE(202, "Device is not registered."),
+    UNSCHEDULED_DEVICE(203, "Device does not have a schedule."),
+    EXISTING_DEVICE(204, "Device already exists."),
+    PROTOCOL_UNKNOWN_FOR_DEVICE(205, "Device has no protocol defined."),
+    UNKNOWN_PROTOCOL_NAME_OR_VERSION(206, "Device has an unknown protocol name or version."),
 
     // Authorization exceptions
-    UNAUTHORIZED(301, "Not authorized to perform this action."), EXISTING_DEVICE_AUTHORIZATIONS(302,
-            "Device authorizations are still present for the current organisation."), METHOD_NOT_ALLOWED_FOR_OWNER(303,
-            "Not allowed to set owner via this method."), DEVICE_IN_MAINTENANCE(304,
-            "Device currently disabled for maintenance."),
+    UNAUTHORIZED(301, "Not authorized to perform this action."),
+    EXISTING_DEVICE_AUTHORIZATIONS(302, "Device authorizations are still present for the current organisation."),
+    METHOD_NOT_ALLOWED_FOR_OWNER(303, "Not allowed to set owner via this method."),
+    DEVICE_IN_MAINTENANCE(304, "Device currently disabled for maintenance."),
 
     // Other exceptions
-    VALIDATION_ERROR(401, "Validation error."), TARIFF_SCHEDULE_NOT_ALLOWED_FOR_PSLD(402,
-            "Set tariff schedule is not allowed for PSLD."), ARGUMENT_NULL(403, "Argument null."), JMS_TEMPLATE_NULL(
-            404, "JSM template was null. Restart server."), UNKNOWN_CORRELATION_UID(405,
-            "No results found for the given correlation uid.");
+    VALIDATION_ERROR(401, "Validation error."),
+    TARIFF_SCHEDULE_NOT_ALLOWED_FOR_PSLD(402, "Set tariff schedule is not allowed for PSLD."),
+    ARGUMENT_NULL(403, "Argument null."),
+    JMS_TEMPLATE_NULL(404, "JSM template was null. Restart server."),
+    UNKNOWN_CORRELATION_UID(405, "No results found for the given correlation uid."),
+
+    // Manufacturer exceptions
+    UNKNOWN_MANUFACTURER(501, "Manufacturer does not exist."),
+    EXISTING_MANUFACTURER(502, "Manufacturer already exists.");
 
     private int code;
     private String message;
