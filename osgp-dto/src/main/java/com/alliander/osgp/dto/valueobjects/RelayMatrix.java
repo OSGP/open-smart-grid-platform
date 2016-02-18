4cc8d541dfc1f597bf9aef16887cd4ffdd195859
bjornamkreutz <bjorn.amkreutz@cgi.com>
2016-02-18 16:48:02 +0100

    Enlarged getConfiguration for Elster FLEX-1519

---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/RelayMatrix.java
@@ -0,0 +1,48 @@
+package com.alliander.osgp.dto.valueobjects;
+
+import java.io.Serializable;
+
+public class RelayMatrix implements Serializable {
+
+    /**
+     * Serial Version UID.
+     */
+    private static final long serialVersionUID = 1679416098090362861L;
+
+    private String masterRelayIndex;
+
+    private boolean masterRelayOn;
+
+    private String indicesOfControlledRelaysOn;
+
+    private String indicesOfControlledRelaysOff;
+
+    public RelayMatrix(final String masterRelayIndex, final boolean masterRelayOn) {
+        this.masterRelayIndex = masterRelayIndex;
+        this.masterRelayOn = masterRelayOn;
+    }
+
+    public String getIndicesOfControlledRelaysOn() {
+        return this.indicesOfControlledRelaysOn;
+    }
+
+    public void setIndicesOfControlledRelaysOn(final String indicesOfControlledRelaysOn) {
+        this.indicesOfControlledRelaysOn = indicesOfControlledRelaysOn;
+    }
+
+    public String getIndicesOfControlledRelaysOff() {
+        return this.indicesOfControlledRelaysOff;
+    }
+
+    public void setIndicesOfControlledRelaysOff(final String indicesOfControlledRelaysOff) {
+        this.indicesOfControlledRelaysOff = indicesOfControlledRelaysOff;
+    }
+
+    public String getMasterRelayIndex() {
+        return this.masterRelayIndex;
+    }
+
+    public boolean isMasterRelayOn() {
+        return this.masterRelayOn;
+    }
+}
