d02345165a7e5febe7062d30fef84b5b358c4e39
mvhaasteren <m.v.haasteren@colbus.nl>
2016-02-19 15:15:14 +0100

    update core message processor to add store sms push notification as event

--- osgp-core/src/main/java/com/alliander/osgp/core/infra/jms/protocol/in/messageprocessors/PushNotificationSmsMessageProcessor.java
+++ osgp-core/src/main/java/com/alliander/osgp/core/infra/jms/protocol/in/messageprocessors/PushNotificationSmsMessageProcessor.java
@@ -8,6 +8,7 @@
 package com.alliander.osgp.core.infra.jms.protocol.in.messageprocessors;
 
 import java.net.InetAddress;
+import java.util.List;
 
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
@@ -18,12 +19,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alliander.osgp.core.application.services.EventNotificationMessageService;
+import com.alliander.osgp.core.domain.model.domain.DomainRequestService;
 import com.alliander.osgp.core.infra.jms.protocol.in.ProtocolRequestMessageProcessor;
 import com.alliander.osgp.domain.core.entities.Device;
+import com.alliander.osgp.domain.core.entities.DeviceAuthorization;
+import com.alliander.osgp.domain.core.exceptions.UnknownEntityException;
 import com.alliander.osgp.domain.core.repositories.DeviceAuthorizationRepository;
 import com.alliander.osgp.domain.core.repositories.DeviceRepository;
+import com.alliander.osgp.domain.core.repositories.DomainInfoRepository;
 import com.alliander.osgp.domain.core.valueobjects.DeviceFunction;
+import com.alliander.osgp.domain.core.valueobjects.DeviceFunctionGroup;
 import com.alliander.osgp.dto.valueobjects.smartmetering.PushNotificationSms;
+import com.alliander.osgp.shared.exceptionhandling.ComponentType;
+import com.alliander.osgp.shared.exceptionhandling.FunctionalException;
+import com.alliander.osgp.shared.exceptionhandling.FunctionalExceptionType;
+import com.alliander.osgp.shared.exceptionhandling.OsgpException;
 import com.alliander.osgp.shared.infra.jms.Constants;
 import com.alliander.osgp.shared.infra.jms.RequestMessage;
 
@@ -32,6 +43,15 @@ import com.alliander.osgp.shared.infra.jms.RequestMessage;
 public class PushNotificationSmsMessageProcessor extends ProtocolRequestMessageProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PushNotificationSmsMessageProcessor.class);
+
+    @Autowired
+    private EventNotificationMessageService eventNotificationMessageService;
+
+    @Autowired
+    private DomainRequestService domainRequestService;
+
+    @Autowired
+    private DomainInfoRepository domainInfoRepository;
 
     @Autowired
     private DeviceRepository deviceRepository;
@@ -58,15 +78,22 @@ public class PushNotificationSmsMessageProcessor extends ProtocolRequestMessageP
         try {
             final PushNotificationSms pushNotificationSms = (PushNotificationSms) dataObject;
 
+            this.storeAlarmAsEvent(pushNotificationSms);
+
+            final String ownerIdentification = this.getOrganisationIdentificationOfOwner(deviceIdentification);
+            LOGGER.info("Matching owner {} with device {} handling {} from {}", ownerIdentification,
+                    deviceIdentification, messageType, requestMessage.getIpAddress());
+            // final RequestMessage requestWithUpdatedOrganization = new
+            // RequestMessage(
+            // requestMessage.getCorrelationUid(), ownerIdentification,
+            // requestMessage.getDeviceIdentification(),
+            // requestMessage.getIpAddress(), pushNotificationSms);
+
             /*
              * This message processor handles messages that came in on the
-             * osgp-core.1_0.protocol-dlms.1_0.requests queue. Therefore lookup
-             * the DomainInfo for DLMS (domain: SMART_METERING) version 1.0.
-             *
-             * At some point in time there may be a cleaner solution, where the
-             * DomainInfo can be derived from information in the message or JMS
-             * metadata, but for now this will have to do.
+             * osgp-core.1_0.protocol-dlms.1_0.requests queue.
              */
+
             if (pushNotificationSms.getIpAddress() != null && !"".equals(pushNotificationSms.getIpAddress())) {
                 // Convert the IP address from String to InetAddress.
                 final InetAddress address = InetAddress.getByName(pushNotificationSms.getIpAddress());
@@ -90,5 +117,41 @@ public class PushNotificationSmsMessageProcessor extends ProtocolRequestMessageP
             LOGGER.error("Exception", e);
             throw new JMSException(e.getMessage());
         }
+    }
+
+    private void storeAlarmAsEvent(final PushNotificationSms pushNotificationSms) {
+        try {
+            this.eventNotificationMessageService.handleEvent(pushNotificationSms.getDeviceIdentification(),
+                    com.alliander.osgp.domain.core.valueobjects.EventType.SMS_NOTIFICATION, pushNotificationSms
+                    .getIpAddress().toString(), 0);
+        } catch (final UnknownEntityException uee) {
+            LOGGER.warn("Unable to store event for Push Notification Sms from unknown device: " + pushNotificationSms,
+                    uee);
+        } catch (final Exception e) {
+            LOGGER.error("Error storing event for Push Notification Sms: " + pushNotificationSms, e);
+        }
+    }
+
+    private String getOrganisationIdentificationOfOwner(final String deviceIdentification) throws OsgpException {
+
+        final Device device = this.deviceRepository.findByDeviceIdentification(deviceIdentification);
+
+        if (device == null) {
+            LOGGER.error("No known device for deviceIdentification {} with alarm notification", deviceIdentification);
+            throw new FunctionalException(FunctionalExceptionType.UNKNOWN_DEVICE, ComponentType.OSGP_CORE,
+                    new UnknownEntityException(Device.class, deviceIdentification));
+        }
+
+        final List<DeviceAuthorization> deviceAuthorizations = this.deviceAuthorizationRepository
+                .findByDeviceAndFunctionGroup(device, DeviceFunctionGroup.OWNER);
+
+        if (deviceAuthorizations == null || deviceAuthorizations.isEmpty()) {
+            LOGGER.error("No owner authorization for deviceIdentification {} with alarm notification",
+                    deviceIdentification);
+            throw new FunctionalException(FunctionalExceptionType.UNAUTHORIZED, ComponentType.OSGP_CORE,
+                    new UnknownEntityException(DeviceAuthorization.class, deviceIdentification));
+        }
+
+        return deviceAuthorizations.get(0).getOrganisation().getOrganisationIdentification();
     }
 }
