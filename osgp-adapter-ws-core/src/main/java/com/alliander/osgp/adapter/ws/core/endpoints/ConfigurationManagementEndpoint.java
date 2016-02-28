7d9d12382b0eab53f0fca1e512f4af2060860587
Kevin Smeets <kevin.smeets@cgi.com>
2016-02-28 16:33:09 +0100

    FLEX-1626 ~ Fixes AuthorizeDeviceFunctions FitNesse tests.

--- osgp-adapter-ws-core/src/main/java/com/alliander/osgp/adapter/ws/core/endpoints/ConfigurationManagementEndpoint.java
+++ osgp-adapter-ws-core/src/main/java/com/alliander/osgp/adapter/ws/core/endpoints/ConfigurationManagementEndpoint.java
@@ -87,10 +87,17 @@ public class ConfigurationManagementEndpoint {
 
             final Configuration configuration = this.configurationManagementMapper.map(request.getConfiguration(),
                     Configuration.class);
-            configuration
-                    .setAutomaticSummerTimingEnabled(request.getConfiguration().isIsAutomaticSummerTimingEnabled());
-            configuration.setDhcpEnabled(request.getConfiguration().isIsDhcpEnabled());
-            configuration.setTestButtonEnabled(request.getConfiguration().isIsTestButtonEnabled());
+            if (request.getConfiguration() != null
+                    && request.getConfiguration().isIsAutomaticSummerTimingEnabled() != null) {
+                configuration.setAutomaticSummerTimingEnabled(request.getConfiguration()
+                        .isIsAutomaticSummerTimingEnabled());
+            }
+            if (request.getConfiguration() != null && request.getConfiguration().isIsDhcpEnabled() != null) {
+                configuration.setDhcpEnabled(request.getConfiguration().isIsDhcpEnabled());
+            }
+            if (request.getConfiguration() != null && request.getConfiguration().isIsTestButtonEnabled() != null) {
+                configuration.setTestButtonEnabled(request.getConfiguration().isIsTestButtonEnabled());
+            }
 
             final String correlationUid = this.configurationManagementService.enqueueSetConfigurationRequest(
                     organisationIdentification, request.getDeviceIdentification(), configuration, scheduleTime);
