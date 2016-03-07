d95d9725b899c08b60f66282ccdff919ce6e4572
Eduard Drenth <eduarddrenth@gmail.com>
2016-03-07 15:47:15 +0100

    junit for byte[] to cosemdate conversion

--- osgp-adapter-ws-smartmetering/src/test/java/com/alliander/osgp/adapter/ws/smartmetering/application/mapping/SpecialDayConversionTest.java
+++ osgp-adapter-ws-smartmetering/src/test/java/com/alliander/osgp/adapter/ws/smartmetering/application/mapping/SpecialDayConversionTest.java
@@ -11,15 +11,31 @@ public class SpecialDayConversionTest {
     @Test
     public void testWildCards() {
         final SpecialDay source = new SpecialDay();
-        source.setSpecialDayDate(new byte[] { 20, 15, 6, (byte) 0xFF, (byte) 0xFF });
+        source.setSpecialDayDate(new byte[] { (byte) 0x07, (byte) 0xDF, 6, (byte) 0xFF, (byte) 0xFF });
         final SpecialDayConverter converter = new SpecialDayConverter();
-        final com.alliander.osgp.domain.core.valueobjects.smartmetering.SpecialDay destination = converter.convert(
-                source, converter.getBType());
-        final CosemDate date = destination.getSpecialDayDate();
-        Assert.assertTrue(date.getYear() == 2015);
-        Assert.assertTrue(date.getMonth() == 6);
-        Assert.assertTrue(date.getDayOfMonth() == 0xFF);
-        Assert.assertTrue(date.getDayOfWeek() == 0xFF);
+        com.alliander.osgp.domain.core.valueobjects.smartmetering.SpecialDay destination = converter.convert(source,
+                converter.getBType());
+        CosemDate date = destination.getSpecialDayDate();
+        Assert.assertEquals(2015, date.getYear());
+        Assert.assertEquals(6, date.getMonth());
+        Assert.assertEquals(0xFF, date.getDayOfMonth());
+        Assert.assertEquals(0xFF, date.getDayOfWeek());
+
+        source.setSpecialDayDate(new byte[] { (byte) 0xFF, (byte) 0xFF, (byte) 0xFE, (byte) 0xFD, (byte) 0xFF });
+        destination = converter.convert(source, converter.getBType());
+        date = destination.getSpecialDayDate();
+        Assert.assertEquals(0xFFFF, date.getYear());
+        Assert.assertEquals(0xFE, date.getMonth());
+        Assert.assertEquals(0xFD, date.getDayOfMonth());
+        Assert.assertEquals(0xFF, date.getDayOfWeek());
+
+        source.setSpecialDayDate(new byte[] { (byte) 0xFF, (byte) 0xFF, (byte) 0xFE, 21, 3 });
+        destination = converter.convert(source, converter.getBType());
+        date = destination.getSpecialDayDate();
+        Assert.assertEquals(0xFFFF, date.getYear());
+        Assert.assertEquals(0xFE, date.getMonth());
+        Assert.assertEquals(21, date.getDayOfMonth());
+        Assert.assertEquals(3, date.getDayOfWeek());
     }
 
 }
