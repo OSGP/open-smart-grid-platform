fb2368861aa97addb26bdcfb92bec22ffdcef030
Eduard Drenth <eduarddrenth@gmail.com>
2016-03-13 08:51:10 +0100

    no more scaler in platform layer, scale value directly in protocol layer

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/DlmsMeterValue.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/DlmsMeterValue.java
@@ -1,27 +1,30 @@
 package com.alliander.osgp.dto.valueobjects.smartmetering;
 
+import java.math.BigDecimal;
+
+/**
+ * A metervalue with scaler applied together with its unit on the meter
+ * 
+ * @author dev
+ *
+ */
 public class DlmsMeterValue {
 
-    public DlmsMeterValue(final Long value, final ScalerUnit scalerUnit) {
+    public DlmsMeterValue(final BigDecimal value, final DlmsUnit dlmsUnit) {
         super();
         this.value = value;
-        this.scalerUnit = scalerUnit;
+        this.dlmsUnit = dlmsUnit;
     }
 
-    private final Long value;
-    private final ScalerUnit scalerUnit;
+    private final BigDecimal value;
+    private final DlmsUnit dlmsUnit;
 
-    public Long getValue() {
+    public BigDecimal getValue() {
         return this.value;
     }
 
-    public ScalerUnit getScalerUnit() {
-        return this.scalerUnit;
-    }
-
-    @Override
-    public String toString() {
-        return "DlmsMeterValue [value=" + this.value + ", scalerUnit=" + this.scalerUnit + "]";
+    public DlmsUnit getDlmsUnit() {
+        return this.dlmsUnit;
     }
 
 }
