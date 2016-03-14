a6a034c46584af520fa103c7e3a7de9346ff3518
Rudi de Vries <rudidevries@gmail.com>
2016-03-14 09:10:46 +0100

    Fixes license headers.

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/mapping/CosemDateTimeConverter.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/mapping/CosemDateTimeConverter.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.osgp.adapter.protocol.dlms.application.mapping;
 
 import java.util.List;
@@ -13,7 +20,7 @@ import com.alliander.osgp.dto.valueobjects.smartmetering.CosemDateTime;
 import com.alliander.osgp.dto.valueobjects.smartmetering.CosemTime;
 
 public class CosemDateTimeConverter extends
-BidirectionalConverter<CosemDateTime, org.openmuc.jdlms.datatypes.CosemDateTime> {
+        BidirectionalConverter<CosemDateTime, org.openmuc.jdlms.datatypes.CosemDateTime> {
 
     @Override
     public org.openmuc.jdlms.datatypes.CosemDateTime convertTo(final CosemDateTime source,
