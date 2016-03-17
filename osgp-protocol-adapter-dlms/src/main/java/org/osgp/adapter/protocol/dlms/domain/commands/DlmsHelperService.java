835d3faeefecfe4f0d08c7a1d272b820e002ee68
Eduard Drenth <eduarddrenth@gmail.com>
2016-03-17 14:45:51 +0100

    fix wrong feedback with one GetResult that contains a fault from the meter

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/DlmsHelperService.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/DlmsHelperService.java
@@ -86,10 +86,24 @@ public class DlmsHelperService {
         return getResults;
     }
 
+    /**
+     * Check if the number of result matches the number of expected results,
+     * when there is only one result the {@link AccessResultCode} of that result
+     * is checked.
+     * 
+     * @param getResultList
+     * @param expectedResults
+     * @param description
+     * @throws ProtocolAdapterException
+     */
     public void checkResultList(final List<GetResult> getResultList, final int expectedResults, final String description)
             throws ProtocolAdapterException {
         if (getResultList.isEmpty()) {
             throw new ProtocolAdapterException("No GetResult received: " + description);
+        } else if (getResultList.size() == 1) {
+            if (AccessResultCode.SUCCESS != getResultList.get(0).resultCode()) {
+                throw new ProtocolAdapterException(getResultList.get(0).resultCode().name());
+            }
         }
 
         if (getResultList.size() != expectedResults) {
@@ -544,7 +558,7 @@ public class DlmsHelperService {
         final String rawValueClass = this.getRawValueClassForDebugInfo(dataObject);
 
         return "DataObject: Choice=" + choiceText + ", ResultData is" + dataType + ", value=[" + rawValueClass + "]: "
-                + objectText;
+        + objectText;
     }
 
     private String getObjectTextForDebugInfo(final DataObject dataObject) {
@@ -661,8 +675,8 @@ public class DlmsHelperService {
         final StringBuilder sb = new StringBuilder();
 
         sb.append("logical name: ").append(logicalNameValue[0] & 0xFF).append('-').append(logicalNameValue[1] & 0xFF)
-        .append(':').append(logicalNameValue[2] & 0xFF).append('.').append(logicalNameValue[3] & 0xFF)
-        .append('.').append(logicalNameValue[4] & 0xFF).append('.').append(logicalNameValue[5] & 0xFF);
+                .append(':').append(logicalNameValue[2] & 0xFF).append('.').append(logicalNameValue[3] & 0xFF)
+                .append('.').append(logicalNameValue[4] & 0xFF).append('.').append(logicalNameValue[5] & 0xFF);
 
         return sb.toString();
     }
@@ -688,10 +702,10 @@ public class DlmsHelperService {
         final int clockStatus = bb.get();
 
         sb.append("year=").append(year).append(", month=").append(monthOfYear).append(", day=").append(dayOfMonth)
-        .append(", weekday=").append(dayOfWeek).append(", hour=").append(hourOfDay).append(", minute=")
-        .append(minuteOfHour).append(", second=").append(secondOfMinute).append(", hundredths=")
-        .append(hundredthsOfSecond).append(", deviation=").append(deviation).append(", clockstatus=")
-        .append(clockStatus);
+                .append(", weekday=").append(dayOfWeek).append(", hour=").append(hourOfDay).append(", minute=")
+                .append(minuteOfHour).append(", second=").append(secondOfMinute).append(", hundredths=")
+                .append(hundredthsOfSecond).append(", deviation=").append(deviation).append(", clockstatus=")
+                .append(clockStatus);
 
         return sb.toString();
     }
@@ -702,7 +716,7 @@ public class DlmsHelperService {
 
         final StringBuilder sb = new StringBuilder();
         sb.append("number of bytes=").append(bitStringValue.length).append(", value=").append(bigValue)
-                .append(", bits=").append(stringValue);
+        .append(", bits=").append(stringValue);
 
         return sb.toString();
     }
