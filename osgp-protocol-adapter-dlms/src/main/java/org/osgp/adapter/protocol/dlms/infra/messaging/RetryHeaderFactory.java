a43557acdd020643db8a025c066015ec0bdea860
Rudi de Vries <rudidevries@gmail.com>
2016-03-29 12:41:38 +0200

    Adds configuration for retry header.

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/infra/messaging/RetryHeaderFactory.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/infra/messaging/RetryHeaderFactory.java
@@ -1,9 +1,17 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.osgp.adapter.protocol.dlms.infra.messaging;
 
 import java.util.Calendar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.alliander.osgp.shared.infra.jms.RetryHeader;
@@ -13,13 +21,17 @@ public class RetryHeaderFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RetryHeaderFactory.class);
 
-    private int retryDelay = 30000;
+    @Value("${core.retry.dlms.delay}")
+    private int retryDelay;
 
-    private int maximumRetries = 5;
+    @Value("${core.retry.dlms.retries}")
+    private int maximumRetries;
 
-    private int backoffMultiplier = 2;
+    @Value("${core.retry.dlms.backoff.multiplier}")
+    private int backoffMultiplier;
 
-    private boolean exponentialBackoff = true;
+    @Value("${core.retry.dlms.backoff.exponential}")
+    private boolean exponentialBackoff;
 
     public RetryHeader createRetryHeader(final int retry) {
         final Calendar retryTime = Calendar.getInstance();
