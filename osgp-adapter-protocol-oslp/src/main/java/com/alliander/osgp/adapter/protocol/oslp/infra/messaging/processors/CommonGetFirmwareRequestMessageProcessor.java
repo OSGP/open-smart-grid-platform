f85adbf2285aeed14ece570db5705409b89942f8
Rudi de Vries <rudidevries@gmail.com>
2016-03-30 15:04:41 +0200

    Adds a first implementation of firmware version list.

--- osgp-adapter-protocol-oslp/src/main/java/com/alliander/osgp/adapter/protocol/oslp/infra/messaging/processors/CommonGetFirmwareRequestMessageProcessor.java
+++ osgp-adapter-protocol-oslp/src/main/java/com/alliander/osgp/adapter/protocol/oslp/infra/messaging/processors/CommonGetFirmwareRequestMessageProcessor.java
@@ -8,6 +8,9 @@
 package com.alliander.osgp.adapter.protocol.oslp.infra.messaging.processors;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
@@ -23,6 +26,8 @@ import com.alliander.osgp.adapter.protocol.oslp.device.responses.GetFirmwareVers
 import com.alliander.osgp.adapter.protocol.oslp.infra.messaging.DeviceRequestMessageProcessor;
 import com.alliander.osgp.adapter.protocol.oslp.infra.messaging.DeviceRequestMessageType;
 import com.alliander.osgp.adapter.protocol.oslp.infra.messaging.OslpEnvelopeProcessor;
+import com.alliander.osgp.dto.valueobjects.FirmwareTypeDto;
+import com.alliander.osgp.dto.valueobjects.FirmwareVersionDto;
 import com.alliander.osgp.oslp.OslpEnvelope;
 import com.alliander.osgp.oslp.SignedOslpEnvelopeDto;
 import com.alliander.osgp.oslp.UnsignedOslpEnvelopeDto;
@@ -39,7 +44,7 @@ import com.alliander.osgp.shared.infra.jms.ResponseMessageSender;
  */
 @Component("oslpCommonGetFirmwareRequestMessageProcessor")
 public class CommonGetFirmwareRequestMessageProcessor extends DeviceRequestMessageProcessor implements
-OslpEnvelopeProcessor {
+        OslpEnvelopeProcessor {
     /**
      * Logger for this class
      */
@@ -142,12 +147,15 @@ OslpEnvelopeProcessor {
             final String messageType, final int retryCount) {
 
         ResponseMessageResultType result = ResponseMessageResultType.OK;
-        String firmwareVersion = "";
+
         OsgpException osgpException = null;
 
+        final List<FirmwareVersionDto> firmwareVersions = new ArrayList<>();
         try {
             final GetFirmwareVersionDeviceResponse response = (GetFirmwareVersionDeviceResponse) deviceResponse;
-            firmwareVersion = response.getFirmwareVersion();
+            final String firmwareVersion = response.getFirmwareVersion();
+
+            firmwareVersions.add(new FirmwareVersionDto(FirmwareTypeDto.ACTIVE_FIRMWARE, firmwareVersion));
         } catch (final Exception e) {
             LOGGER.error("Device Response Exception", e);
             result = ResponseMessageResultType.NOT_OK;
@@ -157,7 +165,8 @@ OslpEnvelopeProcessor {
 
         final ProtocolResponseMessage responseMessage = new ProtocolResponseMessage(domain, domainVersion, messageType,
                 deviceResponse.getCorrelationUid(), deviceResponse.getOrganisationIdentification(),
-                deviceResponse.getDeviceIdentification(), result, osgpException, firmwareVersion, retryCount);
+                deviceResponse.getDeviceIdentification(), result, osgpException, (Serializable) firmwareVersions,
+                retryCount);
 
         responseMessageSender.send(responseMessage);
     }
