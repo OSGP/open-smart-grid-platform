89f27f24793e756ab04498e673df556e7cd083e0
Hannek Toebast <hannek24@hotmail.com>
2016-04-05 10:51:32 +0200

    Alarm register needs to extend ActionValueObjectResponseDto

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/AlarmRegisterDto.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/AlarmRegisterDto.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class AlarmRegisterDto implements Serializable {
+public class AlarmRegisterDto extends ActionValueObjectResponseDto implements Serializable {
 
     private static final long serialVersionUID = 2319359505656305783L;
 
