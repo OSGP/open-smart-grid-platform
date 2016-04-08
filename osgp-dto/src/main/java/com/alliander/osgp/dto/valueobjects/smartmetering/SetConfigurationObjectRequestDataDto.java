d40ff2d2f2dd636417c29b425cda2d47ffdd33f7
Hannek Toebast <hannek24@hotmail.com>
2016-04-08 12:16:14 +0200

    Setting configuration object request - SLIM-484

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetConfigurationObjectRequestDataDto.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetConfigurationObjectRequestDataDto.java
@@ -11,7 +11,7 @@ package com.alliander.osgp.dto.valueobjects.smartmetering;
 
 import java.io.Serializable;
 
-public class SetConfigurationObjectRequestDataDto implements Serializable {
+public class SetConfigurationObjectRequestDataDto implements Serializable, ActionValueObjectDto {
 
     private static final long serialVersionUID = -381163520662276868L;
 
--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetConfigurationObjectRequestDto.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/SetConfigurationObjectRequestDto.java
@@ -11,7 +11,7 @@ package com.alliander.osgp.dto.valueobjects.smartmetering;
 
 import java.io.Serializable;
 
-public class SetConfigurationObjectRequestDto implements Serializable, ActionValueObjectDto {
+public class SetConfigurationObjectRequestDto implements Serializable {
 
     private static final long serialVersionUID = -8295596279285780413L;
 
