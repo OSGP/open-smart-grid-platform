4add4d714f28a7a190a2b54d87ccc6a677b8257a
Rudi de Vries <rudidevries@gmail.com>
2016-04-20 12:55:47 +0200

    Implements a crude retrieval of AssociationLnObjects. SLIM-517

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/GetAssociationLnObjectsCommandExecutor.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/GetAssociationLnObjectsCommandExecutor.java
@@ -2,6 +2,8 @@ package org.osgp.adapter.protocol.dlms.domain.commands;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
@@ -18,8 +20,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.alliander.osgp.dto.valueobjects.smartmetering.AccessRightDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.AccessSelectorListDto;
 import com.alliander.osgp.dto.valueobjects.smartmetering.AssociationLnListElementDto;
 import com.alliander.osgp.dto.valueobjects.smartmetering.AssociationLnListTypeDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.AttributeAccessDescriptorDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.AttributeAccessItemDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.AttributeAccessModeTypeDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.MethodAccessDescriptorDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.MethodAccessItemDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.MethodAccessModeTypeDto;
 
 @Component
 public class GetAssociationLnObjectsCommandExecutor implements CommandExecutor<Void, AssociationLnListTypeDto> {
@@ -33,6 +43,7 @@ public class GetAssociationLnObjectsCommandExecutor implements CommandExecutor<V
     private static final int CLASS_ID_INDEX = 0;
     private static final int VERSION_INDEX = 1;
     private static final int OBIS_CODE_INDEX = 2;
+    private static final int ACCESS_RIGHTS_INDEX = 3;
 
     @Autowired
     private DlmsHelperService dlmsHelperService;
@@ -78,11 +89,56 @@ public class GetAssociationLnObjectsCommandExecutor implements CommandExecutor<V
                     this.dlmsHelperService.readLong(obisCodeMetaDataList.get(CLASS_ID_INDEX), "classId"), new Integer(
                             (short) obisCodeMetaDataList.get(VERSION_INDEX).value()),
                             this.dlmsHelperService.readLogicalName(obisCodeMetaDataList.get(OBIS_CODE_INDEX),
-                            "AssociationLN Element"));
+                            "AssociationLN Element"), this.readAccessRights(obisCodeMetaDataList
+                                            .get(ACCESS_RIGHTS_INDEX)));
 
             elements.add(element);
         }
 
         return new AssociationLnListTypeDto(elements);
+    }
+
+    private AccessRightDto readAccessRights(final DataObject dataObject) throws ProtocolAdapterException {
+        if (!dataObject.isComplex()) {
+            return null;
+        }
+
+        final List<DataObject> accessRights = (List<DataObject>) dataObject.value();
+
+        final List<DataObject> attributeAccessDescriptor = (List<DataObject>) accessRights.get(0).value();
+        final List<AttributeAccessItemDto> attributeAccessItems = new ArrayList<>();
+        for (final DataObject attributeAccessItemRaw : attributeAccessDescriptor) {
+            final List<DataObject> attributeAccessItem = (List<DataObject>) attributeAccessItemRaw.value();
+
+            AccessSelectorListDto asl = null;
+            if (attributeAccessItem.get(2).isNull()) {
+                asl = new AccessSelectorListDto(Collections.EMPTY_LIST);
+            } else {
+                // TODO conversion.
+                asl = new AccessSelectorListDto(Arrays.asList(1, 2, 3));
+            }
+
+            attributeAccessItems.add(new AttributeAccessItemDto(this.dlmsHelperService.readLong(
+                    attributeAccessItem.get(0), "").intValue(),
+                    AttributeAccessModeTypeDto.values()[this.dlmsHelperService.readLong(attributeAccessItem.get(1), "")
+                                                        .intValue()], asl));
+        }
+
+        final AttributeAccessDescriptorDto d = new AttributeAccessDescriptorDto(attributeAccessItems);
+
+        final List<DataObject> methodAccessDescriptor = (List<DataObject>) accessRights.get(1).value();
+        final List<MethodAccessItemDto> methodAccessItems = new ArrayList<>();
+        for (final DataObject methodAccessItemRaw : methodAccessDescriptor) {
+            final List<DataObject> methodAccessItem = (List<DataObject>) methodAccessItemRaw.value();
+            methodAccessItems.add(new MethodAccessItemDto(this.dlmsHelperService.readLong(methodAccessItem.get(0), "")
+                    .intValue(), MethodAccessModeTypeDto.values()[this.dlmsHelperService.readLong(
+                            methodAccessItem.get(1), "").intValue()]));
+        }
+
+        final MethodAccessDescriptorDto f = new MethodAccessDescriptorDto(methodAccessItems);
+
+        final AccessRightDto g = new AccessRightDto(d, f);
+
+        return g;
     }
 }
