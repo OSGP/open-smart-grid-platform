ebeee896296728df17601ffcb36b3d8ac217b469
Hannek Toebast <hannek24@hotmail.com>
2016-04-21 17:06:55 +0200

    Changing ActionDto to ActionRequestDto and creating the ActionDto to contain both a request and response - SLIM-515

--- osgp-adapter-domain-smartmetering/src/main/java/com/alliander/osgp/adapter/domain/smartmetering/application/services/ActionMapperService.java
+++ osgp-adapter-domain-smartmetering/src/main/java/com/alliander/osgp/adapter/domain/smartmetering/application/services/ActionMapperService.java
@@ -52,6 +52,7 @@ import com.alliander.osgp.domain.core.valueobjects.smartmetering.SetPushSetupSms
 import com.alliander.osgp.domain.core.valueobjects.smartmetering.SpecialDaysRequestData;
 import com.alliander.osgp.domain.core.valueobjects.smartmetering.SynchronizeTimeRequestData;
 import com.alliander.osgp.dto.valueobjects.smartmetering.ActionDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.ActionRequestDto;
 import com.alliander.osgp.dto.valueobjects.smartmetering.ActivityCalendarDataDto;
 import com.alliander.osgp.dto.valueobjects.smartmetering.ActualMeterReadsDataDto;
 import com.alliander.osgp.dto.valueobjects.smartmetering.AdministrativeStatusTypeDataDto;
@@ -103,7 +104,7 @@ public class ActionMapperService {
     private SetEncryptionKeyExchangeOnGMeterDataConverter setEncryptionKeyExchangeOnGMeterDataConverter;
 
     private static Map<Class<? extends ActionRequest>, ConfigurableMapper> CLASS_TO_MAPPER_MAP = new HashMap<>();
-    private static Map<Class<? extends ActionRequest>, CustomValueToDtoConverter<? extends ActionRequest, ? extends ActionDto>> CUSTOM_CONVERTER_FOR_CLASS = new HashMap<>();
+    private static Map<Class<? extends ActionRequest>, CustomValueToDtoConverter<? extends ActionRequest, ? extends ActionRequestDto>> CUSTOM_CONVERTER_FOR_CLASS = new HashMap<>();
 
     /**
      * Specifies which mapper to use for the core class received.
@@ -140,7 +141,7 @@ public class ActionMapperService {
     /**
      * Specifies to which DTO object the core object needs to be mapped.
      */
-    private static Map<Class<? extends ActionRequest>, Class<? extends ActionDto>> CLASS_MAP = new HashMap<>();
+    private static Map<Class<? extends ActionRequest>, Class<? extends ActionRequestDto>> CLASS_MAP = new HashMap<>();
     static {
         CLASS_MAP.put(PeriodicMeterReadsRequestData.class, PeriodicMeterReadsRequestDataDto.class);
         CLASS_MAP.put(ActualMeterReadsRequestData.class, ActualMeterReadsDataDto.class);
@@ -170,17 +171,17 @@ public class ActionMapperService {
 
             @SuppressWarnings("unchecked")
             // suppress else the compiler will complain
-            final CustomValueToDtoConverter<ActionRequest, ActionDto> customValueToDtoConverter = (CustomValueToDtoConverter<ActionRequest, ActionDto>) CUSTOM_CONVERTER_FOR_CLASS
-            .get(action.getClass());
+            final CustomValueToDtoConverter<ActionRequest, ActionRequestDto> customValueToDtoConverter = (CustomValueToDtoConverter<ActionRequest, ActionRequestDto>) CUSTOM_CONVERTER_FOR_CLASS
+                    .get(action.getClass());
 
             if (customValueToDtoConverter != null) {
-                actionValueObjectDtoList.add(customValueToDtoConverter.convert(action, smartMeter));
+                actionValueObjectDtoList.add(new ActionDto(customValueToDtoConverter.convert(action, smartMeter)));
             } else {
 
                 final ConfigurableMapper mapper = CLASS_TO_MAPPER_MAP.get(action.getClass());
-                final Class<? extends ActionDto> clazz = CLASS_MAP.get(action.getClass());
+                final Class<? extends ActionRequestDto> clazz = CLASS_MAP.get(action.getClass());
                 if (mapper != null) {
-                    actionValueObjectDtoList.add(this.performDefaultMapping(action, mapper, clazz));
+                    actionValueObjectDtoList.add(new ActionDto(this.performDefaultMapping(action, mapper, clazz)));
                 } else {
                     throw new FunctionalException(FunctionalExceptionType.VALIDATION_ERROR,
                             ComponentType.DOMAIN_SMART_METERING, new AssertionError("No mapper defined for class: "
@@ -191,9 +192,9 @@ public class ActionMapperService {
         return new BundleMessageDataContainerDto(actionValueObjectDtoList);
     }
 
-    private ActionDto performDefaultMapping(final ActionRequest action, final ConfigurableMapper mapper,
-            final Class<? extends ActionDto> clazz) throws FunctionalException {
-        final ActionDto actionValueObjectDto = mapper.map(action, clazz);
+    private ActionRequestDto performDefaultMapping(final ActionRequest action, final ConfigurableMapper mapper,
+            final Class<? extends ActionRequestDto> clazz) throws FunctionalException {
+        final ActionRequestDto actionValueObjectDto = mapper.map(action, clazz);
 
         if (actionValueObjectDto == null) {
             throw new FunctionalException(FunctionalExceptionType.UNSUPPORTED_DEVICE_ACTION,
