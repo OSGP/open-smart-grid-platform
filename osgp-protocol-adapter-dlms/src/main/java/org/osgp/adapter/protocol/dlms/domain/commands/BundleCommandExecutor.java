503c8cc94caae59bff4961539d58314becbe62bf
Rudi de Vries <rudidevries@gmail.com>
2016-04-25 16:09:31 +0200

    Fixes license headers. SLIM-526

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/BundleCommandExecutor.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/BundleCommandExecutor.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.osgp.adapter.protocol.dlms.domain.commands;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +15,7 @@ import com.alliander.osgp.dto.valueobjects.smartmetering.ActionDto;
 import com.alliander.osgp.dto.valueobjects.smartmetering.ActionResponseDto;
 
 public abstract class BundleCommandExecutor<T extends ActionDto, R extends ActionResponseDto> implements
-CommandExecutor<T, R> {
+        CommandExecutor<T, R> {
 
     @Autowired
     private CommandExecutorMap bundleCommandExecutorMap;
