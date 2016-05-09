0c5847b77d535f6ddb8855bf1521584ffc88397a
Robin Bakkerus <robin.bakkerus@cgi.com>
2016-05-09 11:56:09 +0200

    SLIM-533, rename valueobjects to get rid of Dto

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/BundleMessagesResponseDto.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/BundleMessagesResponseDto.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BundleMessagesResponseListDto implements Serializable {
+public class BundleMessagesResponseDto implements Serializable {
 
     /**
      * Serial Version UID.
@@ -20,7 +20,7 @@ public class BundleMessagesResponseListDto implements Serializable {
 
     private List<ActionResponseDto> actionValueObjectResponseDtoList;
 
-    public BundleMessagesResponseListDto(
+    public BundleMessagesResponseDto(
             final List<ActionResponseDto> actionValueObjectResponseDtoList) {
         this.actionValueObjectResponseDtoList = actionValueObjectResponseDtoList;
     }
