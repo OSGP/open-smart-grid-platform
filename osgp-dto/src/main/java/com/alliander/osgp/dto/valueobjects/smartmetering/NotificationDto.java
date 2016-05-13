051b7662444d635bdcd7bd8d90ddc157cfe01c09
Rudi de Vries <rudidevries@gmail.com>
2016-05-13 15:18:55 +0200

    Fixes naming issue in AssociationLnListType that caused problems with Orika. Adds DTO's for Notification object, so webservice code can be cleanly separated from application code when using Notifications. SLIM-505

---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/NotificationDto.java
@@ -0,0 +1,44 @@
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+import java.io.Serializable;
+
+public class NotificationDto implements Serializable {
+
+    private static final long serialVersionUID = 4424128385545688694L;
+
+    private String message;
+    private String result;
+    private String deviceIdentification;
+    private String correlationUid;
+
+    protected NotificationTypeDto notificationType;
+
+    public NotificationDto(final String message, final String result, final String deviceIdentification,
+            final String correlationUid, final NotificationTypeDto notificationType) {
+        this.message = message;
+        this.result = result;
+        this.deviceIdentification = deviceIdentification;
+        this.correlationUid = correlationUid;
+        this.notificationType = notificationType;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+
+    public String getDeviceIdentification() {
+        return this.deviceIdentification;
+    }
+
+    public String getCorrelationUid() {
+        return this.correlationUid;
+    }
+
+    public NotificationTypeDto getNotificationType() {
+        return this.notificationType;
+    }
+}
