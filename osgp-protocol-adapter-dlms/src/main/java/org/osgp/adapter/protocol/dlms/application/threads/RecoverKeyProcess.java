f2a3955c01adeb88dbc0781ec704a7895057269d
Robin Bakkerus <robin.bakkerus@cgi.com>
2016-05-30 13:52:18 +0200

    SLIM-543, SLIM-599 fixed authentication problem also in RecoverKeyProcess

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/threads/RecoverKeyProcess.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/threads/RecoverKeyProcess.java
@@ -14,6 +14,7 @@ import org.bouncycastle.util.encoders.Hex;
 import org.openmuc.jdlms.Authentication;
 import org.openmuc.jdlms.DlmsConnection;
 import org.openmuc.jdlms.TcpConnectionBuilder;
+import org.openmuc.jdlms.Authentication.CryptographicAlgorithm;
 import org.osgp.adapter.protocol.dlms.application.services.DomainHelperService;
 import org.osgp.adapter.protocol.dlms.domain.entities.DlmsDevice;
 import org.osgp.adapter.protocol.dlms.domain.entities.SecurityKey;
@@ -137,12 +138,14 @@ public class RecoverKeyProcess implements Runnable {
                 .getKey());
         final byte[] encryptionKey = Hex.decode(this.getSecurityKey(SecurityKeyType.E_METER_ENCRYPTION).getKey());
 
-        Authentication auth = Authentication.newGmacAuthentication(authenticationKey, encryptionKey);
+        Authentication auth = Authentication.newGmacAuthentication(authenticationKey, encryptionKey, CryptographicAlgorithm.AES_GMC_128);
         
         final TcpConnectionBuilder tcpConnectionBuilder = 
                 new TcpConnectionBuilder(InetAddress.getByName(this.device.getIpAddress()))
-                .setAuthentication(auth)
-                .setLogicalDeviceId(this.logicalDeviceAddress);
+                    .setAuthentication(auth)
+                    .setResponseTimeout(this.responseTimeout)
+                    .setLogicalDeviceId(this.logicalDeviceAddress)
+                    .setClientId(clientAccessPoint);
 
         final Integer challengeLength = this.device.getChallengeLength();
         if (challengeLength != null) {
