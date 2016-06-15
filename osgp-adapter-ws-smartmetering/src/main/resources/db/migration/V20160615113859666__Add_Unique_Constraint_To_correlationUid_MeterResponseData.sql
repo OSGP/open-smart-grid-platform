8741db9ed32d75e01fd0bd646a6b46720621fd3b
Hannek Toebast <hannek24@hotmail.com>
2016-06-15 13:24:04 +0200

    Fix for index creation - SLIM-601

--- osgp-adapter-ws-smartmetering/src/main/resources/db/migration/V20160615113859666__Add_Unique_Constraint_To_correlationUid_MeterResponseData.sql
+++ osgp-adapter-ws-smartmetering/src/main/resources/db/migration/V20160615113859666__Add_Unique_Constraint_To_correlationUid_MeterResponseData.sql
@@ -1,3 +1,16 @@
-DROP INDEX meter_response_data_correlation_uid_idx;
+do
+	$$
+	declare l_count integer;
+	begin
+		select count(*) into l_count
+		from pg_indexes
+		where schemaname = 'public'
+		and tablename = 'meter_response_data'
+		and indexname = 'meter_response_data_correlation_uid_idx';
+	
+		if l_count = 0 then 
+			execute 'CREATE UNIQUE INDEX ON meter_response_data (correlation_uid)';
+		end if;
 
-CREATE UNIQUE INDEX ON meter_response_data (correlation_uid)
+end;
+$$
