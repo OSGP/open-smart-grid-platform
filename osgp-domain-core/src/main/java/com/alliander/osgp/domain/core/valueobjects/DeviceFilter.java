0f4f1e87c78e0faeb8a39911ef2280cd5a255172
Joep-Simons <joep.simons@cgi.com>
2016-07-05 15:59:35 +0200

    Added default constructor text.

--- osgp-domain-core/src/main/java/com/alliander/osgp/domain/core/valueobjects/DeviceFilter.java
+++ osgp-domain-core/src/main/java/com/alliander/osgp/domain/core/valueobjects/DeviceFilter.java
@@ -29,7 +29,7 @@ public class DeviceFilter {
     private String model;
 
     public DeviceFilter() {
-
+        // Default constructor.
     }
 
     public DeviceFilter(final String organisationIdentification, final String deviceIdentification,
