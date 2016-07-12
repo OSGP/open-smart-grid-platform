6d684229744409777c158440485f08347331c252
thijlboonen <thijl.boonen@cgi.com>
2016-07-12 14:45:13 +0200

    Adds qualifier to firmwareDirectory

--- osgp-adapter-ws-core/src/main/java/com/alliander/osgp/adapter/ws/core/application/services/FirmwareManagementService.java
+++ osgp-adapter-ws-core/src/main/java/com/alliander/osgp/adapter/ws/core/application/services/FirmwareManagementService.java
@@ -25,6 +25,7 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -91,6 +92,7 @@ public class FirmwareManagementService {
     private WritableDeviceRepository deviceRepository;
 
     @Resource
+    @Qualifier("wsCoreFirmwareManagementFirmwareDirectory")
     private String firmwareDirectory;
 
     @Autowired
