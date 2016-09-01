3c7275c889d47ce8f5ad8dd6c5632d7bea342417
Hannek Toebast <hannek24@hotmail.com>
2016-09-01 13:00:11 +0200

    Had to be included in previous commit 'all scenarios working' - SLIM-637

--- integration-tests-cucumber/src/test/java/com/alliander/osgp/platform/cucumber/smartmeteringinstallation/CoupleMbusDeviceWithDevice.java
+++ integration-tests-cucumber/src/test/java/com/alliander/osgp/platform/cucumber/smartmeteringinstallation/CoupleMbusDeviceWithDevice.java
@@ -10,6 +10,8 @@ package com.alliander.osgp.platform.cucumber.smartmeteringinstallation;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Assert;
 import org.slf4j.Logger;
@@ -18,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alliander.osgp.platform.cucumber.SmartMetering;
 import com.alliander.osgp.platform.cucumber.hooks.CoupleDeviceHooks;
+import com.alliander.osgp.platform.cucumber.hooks.DeviceHooks;
 import com.alliander.osgp.platform.cucumber.smartmeteringmonitoring.ActualMeterReadsGas;
 import com.alliander.osgp.platform.cucumber.support.DeviceId;
 import com.alliander.osgp.platform.cucumber.support.OrganisationId;
@@ -31,6 +34,7 @@ import cucumber.api.java.en.When;
 
 public class CoupleMbusDeviceWithDevice extends SmartMetering {
     private static final String PATH_RESULT = "/Envelope/Body/CoupleMbusDeviceResponse/Result/text()";
+    private static final String PATH_DESCRIPTION = "/Envelope/Body/CoupleMbusDeviceResponse/Description/text()";
 
     private static final String XPATH_MATCHER_RESULT = "OK";
     private static final String XPATH_MATCHER_NOT_OK_RESULT = "NOT OK";
@@ -50,6 +54,9 @@ public class CoupleMbusDeviceWithDevice extends SmartMetering {
     private CoupleDeviceHooks coupleDeviceHooks;
 
     @Autowired
+    private DeviceHooks deviceHooks;
+
+    @Autowired
     private ServiceEndpoint serviceEndpoint;
 
     @Autowired
@@ -58,6 +65,7 @@ public class CoupleMbusDeviceWithDevice extends SmartMetering {
     @Given("^an uncoupled gas device with DeviceID \"([^\"]*)\"$")
     public void anUncoupledGasDeviceWithDeviceID(String gasDevice) {
         this.deviceId.setDeviceIdG(gasDevice);
+        this.deviceHooks.activateDevice(gasDevice);
         this.coupleDeviceHooks.deCoupleDevices(this.deviceId.getDeviceIdE(), gasDevice);
     }
 
@@ -67,15 +75,35 @@ public class CoupleMbusDeviceWithDevice extends SmartMetering {
         this.coupleDeviceHooks.clearChannelForSmartMeterDevice(this.deviceId.getDeviceIdG());
     }
 
-    @When("^the Link G-meter request on an inactive device is received$")
-    public void theLinkGMeterRequestOnAnInactiveDeviceIsReceived() throws Throwable {
+    @When("^the Link G-meter request on an unknown \"([^\"]*)\" device is received")
+    public void theLinkGMeterRequestOnAnUnknownDeviceIsReceived(String unknownDevice) throws Throwable {
         PROPERTIES_MAP.put(DEVICE_IDENTIFICATION_G_LABEL, this.deviceId.getDeviceIdG());
         PROPERTIES_MAP.put(DEVICE_IDENTIFICATION_E_LABEL, this.deviceId.getDeviceIdE());
         PROPERTIES_MAP.put(CHANNEL_LABEL, String.valueOf(this.deviceId.getGasDeviceChannel()));
         PROPERTIES_MAP.put(ORGANISATION_IDENTIFICATION_LABEL, this.organisationId.getOrganisationId());
         PROPERTIES_MAP.put(ENDPOINT_LABEL, this.serviceEndpoint.getServiceEndpoint());
 
-        this.requestRunner(TestStepStatus.FAILED, PROPERTIES_MAP, TEST_CASE_NAME_REQUEST, TEST_CASE_XML, TEST_SUITE_XML);
+        this.requestRunner(TestStepStatus.OK, PROPERTIES_MAP, TEST_CASE_NAME_REQUEST, TEST_CASE_XML, TEST_SUITE_XML);
+
+        final Pattern messagePattern = Pattern.compile("Device with id \"" + unknownDevice + "\" could not be found");
+        final Matcher messageMatcher = messagePattern.matcher(this.response);
+        Assert.assertTrue(messageMatcher.find());
+
+    }
+
+    @When("^the Link G-meter request on inactive device \"([^\"]*)\" is received$")
+    public void theLinkGMeterRequestOnAnInactiveDeviceIsReceived(String inactiveDevice) throws Throwable {
+        PROPERTIES_MAP.put(DEVICE_IDENTIFICATION_G_LABEL, this.deviceId.getDeviceIdG());
+        PROPERTIES_MAP.put(DEVICE_IDENTIFICATION_E_LABEL, this.deviceId.getDeviceIdE());
+        PROPERTIES_MAP.put(CHANNEL_LABEL, String.valueOf(this.deviceId.getGasDeviceChannel()));
+        PROPERTIES_MAP.put(ORGANISATION_IDENTIFICATION_LABEL, this.organisationId.getOrganisationId());
+        PROPERTIES_MAP.put(ENDPOINT_LABEL, this.serviceEndpoint.getServiceEndpoint());
+
+        this.requestRunner(TestStepStatus.OK, PROPERTIES_MAP, TEST_CASE_NAME_REQUEST, TEST_CASE_XML, TEST_SUITE_XML);
+
+        final Pattern messagePattern = Pattern.compile("Device " + inactiveDevice + " is not active in the platform");
+        final Matcher messageMatcher = messagePattern.matcher(this.response);
+        Assert.assertTrue(messageMatcher.find());
 
     }
 
@@ -108,12 +136,23 @@ public class CoupleMbusDeviceWithDevice extends SmartMetering {
         Assert.assertTrue(this.coupleDeviceHooks.areDevicesCoupled(deviceId, mbusDeviceId, mbusChannel));
     }
 
-    @Then("^the response \"([^\"]*)\" should be given$")
-    public void theResponseShouldBeGiven(String status) throws Throwable {
+    @Then("^the response 'SmartMeter with id \"([^\"]*)\" could not be found' should be given$")
+    public void theResponseSmartMeterWithIdCouldNotBeFoundShouldBeGiven(String unknownDeviceName) throws Throwable {
 
         PROPERTIES_MAP.put(CORRELATION_UID_LABEL, this.correlationUid);
         this.responseRunner(PROPERTIES_MAP, TEST_CASE_NAME_RESPONSE, LOGGER);
-        Assert.assertTrue(this.runXpathResult.assertXpath(this.response, PATH_RESULT, status));
+        Assert.assertTrue(this.runXpathResult.assertXpath(this.response, PATH_DESCRIPTION, "SmartMeter with id \""
+                + unknownDeviceName + "\" could not be found"));
+        Assert.assertTrue(this.runXpathResult.assertXpath(this.response, PATH_RESULT, XPATH_MATCHER_NOT_OK_RESULT));
+    }
+
+    @Then("^the not active response \"([^\"]*)\" should be given$")
+    public void theNotActiveResponseShouldBeGiven(String message) throws Throwable {
+
+        PROPERTIES_MAP.put(CORRELATION_UID_LABEL, this.correlationUid);
+        this.responseRunner(PROPERTIES_MAP, TEST_CASE_NAME_RESPONSE, LOGGER);
+        Assert.assertTrue(this.runXpathResult.assertXpath(this.response, PATH_DESCRIPTION, message));
+        Assert.assertTrue(this.runXpathResult.assertXpath(this.response, PATH_RESULT, XPATH_MATCHER_NOT_OK_RESULT));
     }
 
     @When("^mbus device \"([^\"]*)\" is not coupled with device \"([^\"]*)\"$")
@@ -128,7 +167,7 @@ public class CoupleMbusDeviceWithDevice extends SmartMetering {
 
     @When("^device with DeviceID \"([^\"]*)\" is inactive$")
     public void mbusDeviceWithDeviceIDIsInactive(String mbusDeviceId) throws Throwable {
-        this.coupleDeviceHooks.inactivateDevice(mbusDeviceId);
+        this.deviceHooks.inactivateDevice(mbusDeviceId);
     }
 
     @And("^a coupled gas device \"([^\"]*)\" on MBUS channel (\\d+)$")
@@ -151,6 +190,25 @@ public class CoupleMbusDeviceWithDevice extends SmartMetering {
     @Given("^an unknown gas device with DeviceID \"([^\"]*)\"$")
     public void anUnknownGasDeviceWithDeviceID(String gasDevice) throws Throwable {
         this.deviceId.setDeviceIdG(gasDevice);
+    }
+
+    @Given("^an unknown device with DeviceID \"([^\"]*)\"$")
+    public void anUnkownDeviceWithDeviceID(String deviceIdE) throws Throwable {
+        this.deviceId.setDeviceIdE(deviceIdE);
+    }
+
+    @Given("^an inactive gas device with DeviceID \"([^\"]*)\"$")
+    public void anInactiveGasDeviceWithDeviceID(String deviceIdG) throws Throwable {
+        this.deviceHooks.inactivateDevice(deviceIdG);
+        this.deviceId.setDeviceIdG(deviceIdG);
+    }
+
+    @Then("^the response \"([^\"]*)\" should be given$")
+    public void theResponseShouldBeGiven(String status) throws Throwable {
+
+        PROPERTIES_MAP.put(CORRELATION_UID_LABEL, this.correlationUid);
+        this.responseRunner(PROPERTIES_MAP, TEST_CASE_NAME_RESPONSE, LOGGER);
+        Assert.assertTrue(this.runXpathResult.assertXpath(this.response, PATH_RESULT, status));
     }
 
 }
