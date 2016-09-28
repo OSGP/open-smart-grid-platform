d65367ac007da18faacd0b986f50f62571bf7577
Rudi de Vries <rudidevries@gmail.com>
2016-09-28 08:36:48 +0000

    Adds support for UpdateFirmware in bundle. SLIM-268

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/UpdateFirmwareCommandExecutor.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/UpdateFirmwareCommandExecutor.java
@@ -6,8 +6,16 @@ import org.osgp.adapter.protocol.dlms.domain.entities.DlmsDevice;
 import org.osgp.adapter.protocol.dlms.exceptions.ProtocolAdapterException;
 import org.springframework.stereotype.Component;
 
+import com.alliander.osgp.dto.valueobjects.smartmetering.ActionRequestDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.ActionResponseDto;
+import com.alliander.osgp.dto.valueobjects.smartmetering.UpdateFirmwareRequestDto;
+
 @Component
 public class UpdateFirmwareCommandExecutor extends AbstractCommandExecutor<String, AccessResultCode> {
+
+    public UpdateFirmwareCommandExecutor() {
+        super(UpdateFirmwareRequestDto.class);
+    }
 
     @Override
     public AccessResultCode execute(final DlmsConnection conn, final DlmsDevice device, final String firmwareIdentifier)
@@ -16,4 +24,16 @@ public class UpdateFirmwareCommandExecutor extends AbstractCommandExecutor<Strin
         return AccessResultCode.OBJECT_UNDEFINED;
     }
 
+    @Override
+    public String fromBundleRequestInput(final ActionRequestDto bundleInput) throws ProtocolAdapterException {
+        // TODO Auto-generated method stub
+        return ((UpdateFirmwareRequestDto) bundleInput).getFirmwareIdentifier();
+    }
+
+    @Override
+    public ActionResponseDto asBundleResponse(final AccessResultCode executionResult) throws ProtocolAdapterException {
+        this.checkAccessResultCode(executionResult);
+
+        return new ActionResponseDto("Update firmware was successful");
+    }
 }
