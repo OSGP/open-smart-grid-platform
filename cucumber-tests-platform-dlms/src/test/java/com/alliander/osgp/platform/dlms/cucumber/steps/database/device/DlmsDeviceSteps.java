04a5f79d481c8ee3d5071a2987928e14a3af0f48
Martijn Sips <martijn.sips@cgi.com>
2016-10-07 10:02:54 +0200

    Added smartmeter step

--- cucumber-tests-platform-dlms/src/test/java/com/alliander/osgp/platform/dlms/cucumber/steps/database/device/DlmsDeviceSteps.java
+++ cucumber-tests-platform-dlms/src/test/java/com/alliander/osgp/platform/dlms/cucumber/steps/database/device/DlmsDeviceSteps.java
@@ -3,6 +3,7 @@
  */
 package com.alliander.osgp.platform.dlms.cucumber.steps.database.device;
 
+import static com.alliander.osgp.platform.cucumber.core.Helpers.getFloat;
 import static com.alliander.osgp.platform.cucumber.core.Helpers.getString;
 import static com.alliander.osgp.platform.cucumber.core.Helpers.getBoolean;
 
@@ -14,8 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
 import com.alliander.osgp.domain.core.entities.Device;
+import com.alliander.osgp.domain.core.entities.SmartMeter;
 import com.alliander.osgp.domain.core.repositories.DeviceAuthorizationRepository;
 import com.alliander.osgp.domain.core.repositories.DeviceRepository;
+import com.alliander.osgp.domain.core.repositories.SmartMeterRepository;
+import com.alliander.osgp.platform.cucumber.steps.Defaults;
 import com.alliander.osgp.platform.cucumber.steps.database.DeviceSteps;
 
 import cucumber.api.java.en.And;
@@ -28,6 +32,15 @@ public class DlmsDeviceSteps {
 
     private static final String DEFAULT_COMMUNICATION_METHOD = "GPRS";
 	private static final Boolean DEFAULT_IP_ADDRESS_IS_STATIC = true;
+	private static final String DEFAULT_ALIAS = "Test";
+	private static final String DEFAULT_CONTAINER_CITY = "Esloo";
+	private static final String DEFAULT_CONTAINER_POSTAL_CODE = "6171 AE";
+	private static final String DEFAULT_CONTAINER_STREET = "Mauritsweg";
+	private static final String DEFAULT_CONTAINER_NUMBER = "109";
+	private static final String DEFAULT_CONTAINER_MUNICIPALITY = "Stein";
+	private static final Float DEFAULT_GPSLATITUDE = 5.0F;
+	private static final Float DEFAULT_GPSLONGITUDE = 5.0F;
+	private static final Boolean DEFAULT_ACTIVE = true;
 
 	@Autowired
     private DeviceRepository deviceRepository;
@@ -37,6 +50,9 @@ public class DlmsDeviceSteps {
     
     @Autowired
     private DeviceAuthorizationRepository deviceAuthorizationRepository;
+    
+    @Autowired
+    private SmartMeterRepository smartMeterRepository;
 
     @Autowired
     private DeviceSteps deviceSteps;
@@ -45,8 +61,8 @@ public class DlmsDeviceSteps {
     public void a_dlms_device(final Map<String, String> settings) throws Throwable {
 
         // First create the device itself
-        this.deviceSteps.aDevice(settings);
-
+        this.deviceSteps.aSmartMeter(settings);
+        
         // Now create the DLMS device in the DLMS database
         final String deviceIdentification = getString(settings, "DeviceIdentification",
                 DeviceSteps.DEFAULT_DEVICE_IDENTIFICATION);
@@ -60,10 +76,12 @@ public class DlmsDeviceSteps {
         this.dlmsDeviceRepository.save(dlmsDevice);
         
         // Now add additional data in the core device.
-        Device device = deviceRepository.findByDeviceIdentification(deviceIdentification);
+        //Device device = deviceRepository.findByDeviceIdentification(deviceIdentification);
         // TODO: Set the correct gateway
         //device.updateGatewayDevice(gatewayDevice);
-        deviceRepository.save(device);
+        //deviceRepository.save(device);
+        
+        
     }
 
     /**
--- cucumber-tests-platform/src/test/java/com/alliander/osgp/platform/cucumber/steps/database/DeviceSteps.java
+++ cucumber-tests-platform/src/test/java/com/alliander/osgp/platform/cucumber/steps/database/DeviceSteps.java
@@ -26,11 +26,13 @@ import com.alliander.osgp.domain.core.entities.Device;
 import com.alliander.osgp.domain.core.entities.DeviceAuthorization;
 import com.alliander.osgp.domain.core.entities.DeviceModel;
 import com.alliander.osgp.domain.core.entities.Organisation;
+import com.alliander.osgp.domain.core.entities.SmartMeter;
 import com.alliander.osgp.domain.core.repositories.DeviceAuthorizationRepository;
 import com.alliander.osgp.domain.core.repositories.DeviceModelRepository;
 import com.alliander.osgp.domain.core.repositories.DeviceRepository;
 import com.alliander.osgp.domain.core.repositories.OrganisationRepository;
 import com.alliander.osgp.domain.core.repositories.ProtocolInfoRepository;
+import com.alliander.osgp.domain.core.repositories.SmartMeterRepository;
 import com.alliander.osgp.domain.core.valueobjects.DeviceFunctionGroup;
 import com.alliander.osgp.platform.cucumber.core.ScenarioContext;
 import com.alliander.osgp.platform.cucumber.steps.Defaults;
@@ -72,6 +74,9 @@ public class DeviceSteps {
 
     @Autowired
     private ProtocolInfoRepository protocolInfoRepository;
+    
+    @Autowired 
+    private SmartMeterRepository smartMeterRepository;
 
     /**
      * Generic method which adds a device using the settings.
@@ -87,8 +92,33 @@ public class DeviceSteps {
         final String deviceIdentification = settings.get("DeviceIdentification");
         Device device = new Device(deviceIdentification);
 
+        updateDevice(device, settings);
+    }
+    
+    @Given("^a smart meter$")
+    public void aSmartMeter(final Map<String, String> settings) {
+    	SmartMeter smartMeter = new SmartMeter(
+        		getString(settings, "DeviceIdentification", Defaults.DEFAULT_DEVICE_IDENTIFICATION),
+        		getString(settings, "Alias", DEFAULT_ALIAS),
+        		getString(settings, "ContainerCity", DEFAULT_CONTAINER_CITY),
+        		getString(settings, "ContainerPostalCode", DEFAULT_CONTAINER_POSTALCODE),
+        		getString(settings, "ContainerStreet", DEFAULT_CONTAINER_STREET),
+        		getString(settings, "ContainerNumber", DEFAULT_CONTAINER_NUMBER),
+        		getString(settings, "ContainerMunicipality", DEFAULT_CONTAINER_MUNICIPALITY),
+        		getFloat(settings, "GPSLatitude", DEFAULT_LATITUDE),
+        		getFloat(settings, "GPSLongitude", DEFAULT_LONGITUDE)
+        		);
+    	
+    	smartMeterRepository.save(smartMeter);
+    	
+    	Device device = deviceRepository.findByDeviceIdentification(getString(settings, "DeviceIdentification", Defaults.DEFAULT_DEVICE_IDENTIFICATION));	
+    	updateDevice(device, settings);
+    	
+    }
+    
+    private void updateDevice(Device device, final Map<String, String> settings) {
+
         // Now set the optional stuff
-        device.setId(getLong(settings, "DeviceId", this.DEFAULT_DEVICE_ID));
         device.setActivated(getBoolean(settings, "IsActivated", this.DEFAULT_IS_ACTIVATED));
         device.setTechnicalInstallationDate(getDate(settings, "TechnicalInstallationDate").toDate());
 
