96da5330cde1db3519e41f68bfbf754c8f6ad6b1
Martijn Sips <martijn.sips@cgi.com>
2016-11-01 13:21:36 +0100

    Updated initializer

--- osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/application/config/OsgpProtocolAdapterIec61850Initializer.java
+++ osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/application/config/OsgpProtocolAdapterIec61850Initializer.java
@@ -7,56 +7,28 @@
  */
 package com.alliander.osgp.adapter.protocol.iec61850.application.config;
 
-import java.io.FileNotFoundException;
-import java.util.TimeZone;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NameNotFoundException;
-import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
-import ch.qos.logback.core.joran.spi.JoranException;
-import ch.qos.logback.ext.spring.LogbackConfigurer;
+import com.alliander.osgp.shared.application.config.AbstractApplicationInitializer;
 
 /**
  * Web application Java configuration class.
  */
-public class OsgpProtocolAdapterIec61850Initializer implements WebApplicationInitializer {
+public class OsgpProtocolAdapterIec61850Initializer extends AbstractApplicationInitializer implements WebApplicationInitializer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationContext.class);
-
+    public OsgpProtocolAdapterIec61850Initializer() {
+        super(ApplicationContext.class, "java:comp/env/osgp/AdapterProtocolIec61850/log-config");
+    }
+    
+    /**
+     * 
+     */
     @Override
     public void onStartup(final ServletContext servletContext) throws ServletException {
-        // Force the timezone of application to UTC (required for
-        // Hibernate/JDBC)
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-
-        Context initialContext;
-        try {
-            initialContext = new InitialContext();
-            final String logLocation = (String) initialContext
-                    .lookup("java:comp/env/osgp/AdapterProtocolIec61850/log-config");
-            LogbackConfigurer.initLogging(logLocation);
-        } catch (final NameNotFoundException | FileNotFoundException | JoranException e) {
-            // Do nothing, if the file referred in context.xml is not found,
-            // the default logback.xml will be used.
-            LOGGER.info("Caught an exception [" + e.getMessage() + "]");
-            LOGGER.info("Using classpath logback.xml");
-        } catch (final NamingException e) {
-            throw new ServletException("naming exception", e);
-        }
-
-        final AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(ApplicationContext.class);
-
-        servletContext.addListener(new ContextLoaderListener(rootContext));
+        startUp(servletContext);
     }
 }
+
