e84690c57b98cc6fe71ba4a2427408dbccdd1fcc
Rudi de Vries <rudidevries@gmail.com>
2016-12-01 08:46:37 +0100

    SLIM-779 Adds missing license header.

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/factories/DlmsConnector.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/factories/DlmsConnector.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2016 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.osgp.adapter.protocol.dlms.domain.factories;
 
 import java.io.IOException;
@@ -40,7 +47,7 @@ public class DlmsConnector {
         TcpConnectionBuilder tcpConnectionBuilder;
         try {
             tcpConnectionBuilder = new TcpConnectionBuilder(InetAddress.getByName(device.getIpAddress()))
-                    .setResponseTimeout(this.responseTimeout).setLogicalDeviceId(this.logicalDeviceAddress);
+            .setResponseTimeout(this.responseTimeout).setLogicalDeviceId(this.logicalDeviceAddress);
         } catch (final UnknownHostException e) {
             throw new ConnectionException(e);
         }
