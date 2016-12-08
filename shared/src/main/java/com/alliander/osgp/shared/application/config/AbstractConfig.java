5d31cc1b5fd586acf190167c3d3de093c8daf9b9
Hans Rooden <hans.rooden@cgi.com>
2016-12-08 13:32:12 +0000

    OP-471 Fixes Jasper interface config by re-ordering property sources

--- shared/src/main/java/com/alliander/osgp/shared/application/config/AbstractConfig.java
+++ shared/src/main/java/com/alliander/osgp/shared/application/config/AbstractConfig.java
@@ -7,23 +7,100 @@
  */
 package com.alliander.osgp.shared.application.config;
 
-import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 
 /**
  * Base class for Configuration classes.
  */
 public abstract class AbstractConfig {
 
-    @Resource
+    /**
+     * Qualifier to detect a class path type resource (i.e. 
+     */
+    private static final String CLASS_PATH_QUALIFIER = "class path resource [";
+    
+    /**
+     * Qualifier to detect a file type resource (i.e. /etc/osp/xxx.properties)
+     */
+    private static final String RESOURCE_PATH_QUALIFIER = "URL [file:";
+    
+    /**
+     * Qualifier to detect a specific global.properties file resource (i.e. /etc/osp/global.properties)
+     */
+    private static final String RESOURCE_GLOBAL_PATH_QUALIFIER = "global.properties";
+    
+    /**
+     * Standard spring environment (autowired with setter) 
+     */
     protected Environment environment;
   
-    // To resolve ${} in @Value
+    /**
+     * Default implementation to resolve ${} values in annotations.  
+     * @return static PropertySourcesPlaceholderConfigurer
+     */
     @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
-        return new PropertySourcesPlaceholderConfigurer();
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        PropertySourcesPlaceholderConfigurer ppc = new PropertySourcesPlaceholderConfigurer();
+        ppc.setIgnoreUnresolvablePlaceholders(true);
+        return ppc; 
+    }
+    
+    /**
+     * Special setter for Spring environment, which reorders the property sources in defined order (high to lowest priority):
+     * - environment config
+     * - local config files
+     * - global config files
+     * - classpath config files
+     * @param configurableEnvironment Spring environment
+     */
+    @Autowired
+    public void setConfigurableEnvironment(final ConfigurableEnvironment configurableEnvironment) {
+        this.environment = configurableEnvironment;
+        reorderEnvironment(configurableEnvironment);
+    }
+
+    private static void reorderEnvironment(final ConfigurableEnvironment configurableEnvironment) {
+        List<PropertySource<?>> env = new ArrayList<>();  
+        List<PropertySource<?>> file = new ArrayList<>();  
+        List<PropertySource<?>> global = new ArrayList<>();
+        List<PropertySource<?>> classpath = new ArrayList<>();
+        
+        MutablePropertySources sources = configurableEnvironment.getPropertySources();
+
+        // Divide property sources in groups 
+        for (PropertySource<?> source : sources) {
+            if (source.getName().contains(RESOURCE_GLOBAL_PATH_QUALIFIER)) {
+                global.add(source);
+            } else if (source.getName().startsWith(RESOURCE_PATH_QUALIFIER)) {
+                file.add(source);
+            } else if (source.getName().startsWith(CLASS_PATH_QUALIFIER)) {
+                classpath.add(source);
+            } else {
+                env.add(source);
+            }
+        }
+
+        // Re-add all property sources in correct priority order
+        addSources(sources, env);
+        addSources(sources, file);
+        addSources(sources, global);
+        addSources(sources, classpath);
+    }
+    
+    private static void addSources(MutablePropertySources sources, List<PropertySource<?>> sourcesToAdd) {
+        // Remove and add each source
+        for (PropertySource<?> source : sourcesToAdd) {
+            sources.remove(source.getName());
+            sources.addLast(source);
+        }
     }
 }
