3e8bd310a8bd1b62ddad23110ca114d847211c94
Martijn Sips <martijn.sips@cgi.com>
2017-01-04 15:07:24 +0000

    FLEX-2603 ~ Fixed IEC tests

--- cucumber-tests-platform/src/test/java/com/alliander/osgp/platform/cucumber/support/ws/microgrids/AdHocManagementClient.java
+++ cucumber-tests-platform/src/test/java/com/alliander/osgp/platform/cucumber/support/ws/microgrids/AdHocManagementClient.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
+import com.alliander.osgp.adapter.ws.microgrids.domain.repositories.RtuResponseDataRepository;
 import com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.GetDataAsyncRequest;
 import com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.GetDataAsyncResponse;
 import com.alliander.osgp.adapter.ws.schema.microgrids.adhocmanagement.GetDataRequest;
@@ -29,6 +30,9 @@ public class AdHocManagementClient extends BaseClient {
     @Autowired
     private WebServiceTemplateFactory webServiceTemplateFactoryMicrogridsAdHocManagement;
 
+    @Autowired
+    private RtuResponseDataRepository rtuResponseDataRepository;
+    
     @Value("${iec61850.rtu.response.wait.check.interval:1000}")
     private int waitCheckIntervalMillis;
     @Value("${iec61850.rtu.response.wait.fail.duration:15000}")
@@ -44,8 +48,25 @@ public class AdHocManagementClient extends BaseClient {
     public GetDataResponse getData(final GetDataAsyncRequest request)
             throws WebServiceSecurityException, GeneralSecurityException, IOException {
 
+        final String correlationUid = request.getAsyncRequest().getCorrelationUid();
+        this.waitForRtuResponseData(correlationUid);
+
         final WebServiceTemplate webServiceTemplate = this.webServiceTemplateFactoryMicrogridsAdHocManagement
                 .getTemplate(this.getOrganizationIdentification(), this.getUserName());
         return (GetDataResponse) webServiceTemplate.marshalSendAndReceive(request);
+    }
+    
+    private void waitForRtuResponseData(final String correlationUid) {
+        try {
+            for (int timeSpentWaiting = 0; timeSpentWaiting < this.waitFailMillis; timeSpentWaiting += this.waitCheckIntervalMillis) {
+                Thread.sleep(this.waitCheckIntervalMillis);
+                if (this.rtuResponseDataRepository.findSingleResultByCorrelationUid(correlationUid) != null) {
+                    return;
+                }
+            }
+            throw new AssertionError("RtuResponseData not available within " + this.waitFailMillis + " milliseconds.");
+        } catch (final InterruptedException e) {
+            throw new AssertionError("Waiting for RtuResponseData was interrupted.", e);
+        }
     }
 }
