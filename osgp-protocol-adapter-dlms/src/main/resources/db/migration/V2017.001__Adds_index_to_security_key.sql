c1f8413e25566aa0cf1b136656b774f1eed52010
Hannek Toebast <hannek24@hotmail.com>
2017-01-10 09:33:05 +0100

    Add index - SLIM-814

--- osgp-protocol-adapter-dlms/src/main/resources/db/migration/V2017.001__Adds_index_to_security_key.sql
+++ osgp-protocol-adapter-dlms/src/main/resources/db/migration/V2017.001__Adds_index_to_security_key.sql
@@ -5,10 +5,10 @@ IF NOT EXISTS (
     SELECT 1
     FROM   pg_catalog.pg_class c
     JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-    WHERE  c.relname = 'dlms_device_id_idx'
+    WHERE  c.relname = 'security_key_dlms_device_id_idx'
     AND    n.nspname = current_schema
     ) THEN
-    CREATE INDEX dlms_device_id_idx ON security_key (dlms_device_id);
+    CREATE INDEX security_key_dlms_device_id_idx ON security_key (dlms_device_id);
 END IF;
 
 END;
