7b0e60054555e32e6922d4022ab0cbb66c06d372
Rianne Fijn <rianne.fijn@cgi.com>
2017-01-18 15:36:02 +0100

    Adds wsdl's/xsd's for core, OC-257.

---
+++ osgp-ws-core/src/main/java/com/alliander/osgp/ws/core/config/CoreWebServiceConfig.java
@@ -0,0 +1,91 @@
+package com.alliander.osgp.ws.core.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
+import org.springframework.ws.wsdl.WsdlDefinition;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+
+/**
+ * Copyright 2017 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+@Configuration
+public class CoreWebServiceConfig {
+
+    @Bean
+    public PayloadValidatingInterceptor payloadValidatingInterceptor() {
+        final PayloadValidatingInterceptor payloadValidatingInterceptor = new PayloadValidatingInterceptor();
+        final Resource[] resources = new Resource[] { new ClassPathResource("schemas/common.xsd"),
+                new ClassPathResource("schemas/adhocmanagement.xsd"),
+                new ClassPathResource("schemas/configurationmanagement.xsd"),
+                new ClassPathResource("schemas/deviceinstallation.xsd"),
+                new ClassPathResource("schemas/devicemanagement.xsd"),
+                new ClassPathResource("schemas/firmwaremanagement.xsd") };
+        payloadValidatingInterceptor.setSchemas(resources);
+        return payloadValidatingInterceptor;
+    }
+
+    @Bean(name = "common")
+    public SimpleXsdSchema commonXsd() {
+        return new SimpleXsdSchema(new ClassPathResource("schemas/common.xsd"));
+    }
+
+    @Bean(name = "AdHocManagement")
+    public WsdlDefinition AdHocManagementWsdl() {
+        return new SimpleWsdl11Definition(new ClassPathResource("AdhocManagement.wsdl"));
+    }
+
+    @Bean(name = "adhocmanagement")
+    public SimpleXsdSchema adhocManagementXsd() {
+        return new SimpleXsdSchema(new ClassPathResource("schemas/adhocmanagement"));
+    }
+
+    @Bean(name = "ConfigurationManagement")
+    public WsdlDefinition configurationManagementWsdl() {
+        return new SimpleWsdl11Definition(new ClassPathResource("ConfigurationManagement.wsdl"));
+    }
+
+    @Bean(name = "configurationmanagement")
+    public SimpleXsdSchema configurationManagementXsd() {
+        return new SimpleXsdSchema(new ClassPathResource("schemas/configurationmanagement"));
+    }
+
+    @Bean(name = "DeviceInstallation")
+    public WsdlDefinition deviceInstallationWsdl() {
+        return new SimpleWsdl11Definition(new ClassPathResource("DeviceInstallation.wsdl"));
+    }
+
+    @Bean(name = "deviceinstallation")
+    public SimpleXsdSchema deviceInstallationXsd() {
+        return new SimpleXsdSchema(new ClassPathResource("schemas/deviceinstallation"));
+    }
+
+    @Bean(name = "DeviceManagement")
+    public WsdlDefinition deviceManagementWsdl() {
+        return new SimpleWsdl11Definition(new ClassPathResource("DeviceManagement.wsdl"));
+    }
+
+    @Bean(name = "devicemanagement")
+    public SimpleXsdSchema deviceManagementXsd() {
+        return new SimpleXsdSchema(new ClassPathResource("schemas/devicemanagement"));
+    }
+
+    @Bean(name = "FirmwareManagement")
+    public WsdlDefinition firmwareManagementWsdl() {
+        return new SimpleWsdl11Definition(new ClassPathResource("FirmwareManagement.wsdl"));
+    }
+
+    @Bean(name = "firmwaremanagement")
+    public SimpleXsdSchema firmwareManagementXsd() {
+        return new SimpleXsdSchema(new ClassPathResource("schemas/firmwaremanagement"));
+    }
+}
