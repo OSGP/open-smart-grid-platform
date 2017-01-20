18adf8770c0b872858a43e1a36ab4096a52ec040
Rianne Fijn <rianne.fijn@cgi.com>
2017-01-20 14:22:42 +0100

    Moves wsdl/xsd's for microgrids to Shared, OC-257

--- osgp-adapter-ws-microgrids/src/main/java/com/alliander/osgp/adapter/ws/microgrids/application/config/ApplicationContext.java
+++ osgp-adapter-ws-microgrids/src/main/java/com/alliander/osgp/adapter/ws/microgrids/application/config/ApplicationContext.java
@@ -10,6 +10,7 @@ package com.alliander.osgp.adapter.ws.microgrids.application.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -17,6 +18,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 import com.alliander.osgp.shared.application.config.AbstractConfig;
+import com.alliander.osgp.ws.microgrids.config.MicroGridsWebServiceConfig;
 
 /**
  * An application context Java configuration class. The usage of Java
@@ -26,6 +28,7 @@ import com.alliander.osgp.shared.application.config.AbstractConfig;
 @ComponentScan(basePackages = { "com.alliander.osgp.domain.microgrids", "com.alliander.osgp.adapter.ws.microgrids",
         "com.alliander.osgp.domain.logging", "com.alliander.osgp.domain.core.services" })
 @ImportResource("classpath:applicationContext.xml")
+@Import({ MicroGridsWebServiceConfig.class })
 @PropertySources({ @PropertySource("classpath:osgp-adapter-ws-microgrids.properties"),
         @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true),
         @PropertySource(value = "file:${osgp/AdapterWsMicrogrids/config}", ignoreResourceNotFound = true), })
