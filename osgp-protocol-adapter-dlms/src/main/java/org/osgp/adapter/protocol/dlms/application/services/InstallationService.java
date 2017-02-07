0551e3503eef5499c16682026ee506768c9ebc8e
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2017-02-07 19:00:27 +0100

    SLIM-850 Updates installation service to re-encrypt device keys
    Device keys from an add device request are decrypted using PKI
    encryption with the RsaEncryptionService before they are stored
    with the old symmetrical AES encryption by the EncryptionService.

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/services/InstallationService.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/application/services/InstallationService.java
@@ -9,11 +9,15 @@ package org.osgp.adapter.protocol.dlms.application.services;
 
 import org.osgp.adapter.protocol.dlms.application.mapping.InstallationMapper;
 import org.osgp.adapter.protocol.dlms.domain.entities.DlmsDevice;
+import org.osgp.adapter.protocol.dlms.domain.entities.SecurityKeyType;
 import org.osgp.adapter.protocol.dlms.domain.repositories.DlmsDeviceRepository;
+import org.osgp.adapter.protocol.dlms.exceptions.ProtocolAdapterException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alliander.osgp.dto.valueobjects.smartmetering.SmartMeteringDeviceDto;
+import com.alliander.osgp.shared.security.EncryptionService;
+import com.alliander.osgp.shared.security.RsaEncryptionService;
 
 @Service(value = "dlmsInstallationService")
 public class InstallationService {
@@ -24,10 +28,62 @@ public class InstallationService {
     @Autowired
     private InstallationMapper installationMapper;
 
+    @Autowired
+    private EncryptionService encryptionService;
+
+    @Autowired
+    private RsaEncryptionService rsaEncryptionService;
+
     // === ADD METER ===
-    public void addMeter(final SmartMeteringDeviceDto smartMeteringDevice) {
+    public void addMeter(final SmartMeteringDeviceDto smartMeteringDevice) throws ProtocolAdapterException {
+        this.reEncryptKeys(smartMeteringDevice);
         final DlmsDevice dlmsDevice = this.installationMapper.map(smartMeteringDevice, DlmsDevice.class);
         this.dlmsDeviceRepository.save(dlmsDevice);
     }
 
+    private void reEncryptKeys(final SmartMeteringDeviceDto smartMeteringDevice) throws ProtocolAdapterException {
+        this.reEncryptMasterKey(smartMeteringDevice);
+        this.reEncryptAuthenticationKey(smartMeteringDevice);
+        this.reEncryptEncryptionKey(smartMeteringDevice);
+    }
+
+    private void reEncryptMasterKey(final SmartMeteringDeviceDto smartMeteringDevice) throws ProtocolAdapterException {
+        final byte[] reEncryptedMasterKey = this.reEncryptKey(smartMeteringDevice.getMasterKey(),
+                SecurityKeyType.E_METER_MASTER);
+        smartMeteringDevice.setMasterKey(reEncryptedMasterKey);
+    }
+
+    private void reEncryptAuthenticationKey(final SmartMeteringDeviceDto smartMeteringDevice)
+            throws ProtocolAdapterException {
+        final byte[] reEncryptedAuthenticationKey = this.reEncryptKey(smartMeteringDevice.getAuthenticationKey(),
+                SecurityKeyType.E_METER_AUTHENTICATION);
+        smartMeteringDevice.setAuthenticationKey(reEncryptedAuthenticationKey);
+    }
+
+    private void reEncryptEncryptionKey(final SmartMeteringDeviceDto smartMeteringDevice)
+            throws ProtocolAdapterException {
+        final byte[] reEncryptedEncryptionKey = this.reEncryptKey(smartMeteringDevice.getGlobalEncryptionUnicastKey(),
+                SecurityKeyType.E_METER_ENCRYPTION);
+        smartMeteringDevice.setGlobalEncryptionUnicastKey(reEncryptedEncryptionKey);
+    }
+
+    private byte[] reEncryptKey(final byte[] asymmetricEncryptedKey, final SecurityKeyType keyType)
+            throws ProtocolAdapterException {
+
+        if (asymmetricEncryptedKey == null) {
+            return null;
+        }
+
+        try {
+            /*
+             * Replace the asymmetric encryption for which the public key is
+             * shared with web service callers by a faster symmetric encryption
+             * for use inside the protocol adapter only.
+             */
+            final byte[] decryptedKeyBytes = this.rsaEncryptionService.decrypt(asymmetricEncryptedKey);
+            return this.encryptionService.encrypt(decryptedKeyBytes);
+        } catch (final Exception e) {
+            throw new ProtocolAdapterException("Error processing " + keyType + " key", e);
+        }
+    }
 }
