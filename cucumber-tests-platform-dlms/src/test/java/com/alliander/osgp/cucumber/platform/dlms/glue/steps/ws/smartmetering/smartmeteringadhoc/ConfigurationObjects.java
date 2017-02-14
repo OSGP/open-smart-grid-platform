d1060f99f13deae9b9fa4020801efd6c52370ab2
hannek24 <hannek24@hotmail.com>
2017-02-14 11:59:33 +0000

    bringing down 'get all attr.' timeout value from 30 to 5 min.

--- cucumber-tests-platform-dlms/src/test/java/com/alliander/osgp/cucumber/platform/dlms/glue/steps/ws/smartmetering/smartmeteringadhoc/ConfigurationObjects.java
+++ cucumber-tests-platform-dlms/src/test/java/com/alliander/osgp/cucumber/platform/dlms/glue/steps/ws/smartmetering/smartmeteringadhoc/ConfigurationObjects.java
@@ -44,9 +44,10 @@ public class ConfigurationObjects extends SmartMeteringStepsBase {
     public void allTheConfigurationItemsShouldBeReturned(final Map<String, String> settings) throws Throwable {
         PROPERTIES_MAP.put(Keys.KEY_DEVICE_IDENTIFICATION,
                 getString(settings, Keys.KEY_DEVICE_IDENTIFICATION, Defaults.DEFAULT_DEVICE_IDENTIFICATION));
-        PROPERTIES_MAP
-                .put(Keys.KEY_CORRELATION_UID, ScenarioContext.Current().get(Keys.KEY_CORRELATION_UID).toString());
-        PROPERTIES_MAP.put(MAX_TIME, "1800000");
+        PROPERTIES_MAP.put(Keys.KEY_CORRELATION_UID,
+                ScenarioContext.Current().get(Keys.KEY_CORRELATION_UID).toString());
+        PROPERTIES_MAP.put(MAX_TIME, "300000"); // 5 min = 5 x 60 x 1000 mil.
+                                                // sec.
 
         this.requestRunner(TestStepStatus.OK, PROPERTIES_MAP, TEST_CASE_NAME_RESPONSE_REQUEST, TEST_CASE_XML,
                 TEST_SUITE_XML);
