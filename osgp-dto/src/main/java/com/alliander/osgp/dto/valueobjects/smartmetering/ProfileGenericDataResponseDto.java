f149eff486c63b398926338af3f100fca5184a70
Robin Bakkerus <robin.bakkerus@cgi.com>
2017-02-17 07:11:22 +0100

    SLIM-854, updated review comments, round 4

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ProfileGenericDataResponseDto.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ProfileGenericDataResponseDto.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Smart Society Services B.V.
+ * Copyright 2017 Smart Society Services B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
  *
@@ -7,16 +7,18 @@
  */
 package com.alliander.osgp.dto.valueobjects.smartmetering;
 
+import java.util.List;
+
 public class ProfileGenericDataResponseDto extends ActionResponseDto {
 
     private static final long serialVersionUID = -156966569210717654L;
 
     private final ObisCodeValuesDto logicalName;
-    private final CaptureObjectsDto captureObjects;
-    private final ProfileEntriesDto profileEntries;
+    private List<CaptureObjectDto> captureObjects;
+    private List<ProfileEntryDto> profileEntries;
 
-    public ProfileGenericDataResponseDto(ObisCodeValuesDto logicalName, CaptureObjectsDto captureObjects,
-            ProfileEntriesDto profileEntries) {
+    public ProfileGenericDataResponseDto(ObisCodeValuesDto logicalName, List<CaptureObjectDto> captureObjects,
+            List<ProfileEntryDto> profileEntries) {
         super();
         this.logicalName = logicalName;
         this.captureObjects = captureObjects;
@@ -27,11 +29,11 @@ public class ProfileGenericDataResponseDto extends ActionResponseDto {
         return this.logicalName;
     }
 
-    public CaptureObjectsDto getCaptureObjects() {
+    public List<CaptureObjectDto> getCaptureObjects() {
         return this.captureObjects;
     }
 
-    public ProfileEntriesDto getProfileEntries() {
+    public List<ProfileEntryDto> getProfileEntries() {
         return this.profileEntries;
     }
 }
