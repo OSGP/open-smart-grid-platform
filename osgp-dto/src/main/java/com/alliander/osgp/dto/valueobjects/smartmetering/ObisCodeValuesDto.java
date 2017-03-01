724398a6fd5c29fcb61b7d0195ab5d6e1c39f51e
Rudi de Vries <rudidevries@gmail.com>
2017-03-01 11:05:27 +0000

    Adds equals, hashCode and toString to ObisCodeValuesDto. SLIM-780

--- osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ObisCodeValuesDto.java
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/ObisCodeValuesDto.java
@@ -13,12 +13,12 @@ public class ObisCodeValuesDto implements Serializable {
 
     private static final long serialVersionUID = 6725616781596815362L;
 
-    private byte a;
-    private byte b;
-    private byte c;
-    private byte d;
-    private byte e;
-    private byte f;
+    private final byte a;
+    private final byte b;
+    private final byte c;
+    private final byte d;
+    private final byte e;
+    private final byte f;
 
     public ObisCodeValuesDto(final byte a, final byte b, final byte c, final byte d, final byte e, final byte f) {
         super();
@@ -52,5 +52,47 @@ public class ObisCodeValuesDto implements Serializable {
 
     public byte getF() {
         return this.f;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.a;
+        result = prime * result + this.b;
+        result = prime * result + this.c;
+        result = prime * result + this.d;
+        result = prime * result + this.e;
+        result = prime * result + this.f;
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+        final ObisCodeValuesDto other = (ObisCodeValuesDto) obj;
+        if (this.a != other.a)
+            return false;
+        if (this.b != other.b)
+            return false;
+        if (this.c != other.c)
+            return false;
+        if (this.d != other.d)
+            return false;
+        if (this.e != other.e)
+            return false;
+        if (this.f != other.f)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d.%d.%d.%d.%d.%d", this.a, this.b, this.c, this.d, this.e, this.f);
     }
 }
