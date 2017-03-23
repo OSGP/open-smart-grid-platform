1dfa9eafd471dcb025dc514379fa5c2f3782d218
Robin Bakkerus <robin.bakkerus@cgi.com>
2017-03-23 09:27:39 +0000

    SLIM-889, merged with development

--- cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/hooks/OslpMockServerHooks.java
+++ cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/hooks/OslpMockServerHooks.java
@@ -1,8 +1,8 @@
 /**
  * Copyright 2016 Smart Society Services B.V.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License.  
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 package com.alliander.osgp.cucumber.platform.glue.hooks;
@@ -18,21 +18,21 @@ import cucumber.api.java.Before;
 public class OslpMockServerHooks extends GlueBase {
 
     @Autowired
-    private MockOslpServer mockServer;
+    private MockOslpServer oslpMockServer;
 
     @Before("@OslpMockServer")
     public void startOslpMockServer() throws Throwable {
-        if (this.mockServer != null) {
-            this.mockServer.resetServer();
+        if (this.oslpMockServer != null) {
+            this.oslpMockServer.resetServer();
 
-            this.mockServer.stop();
+            this.oslpMockServer.stop();
         }
 
-        this.mockServer.start();
+        this.oslpMockServer.start();
     }
 
     @After("@OslpMockServer")
     public void stopOslpMockServer() {
-        this.mockServer.stop();
+        this.oslpMockServer.stop();
     }
 }
