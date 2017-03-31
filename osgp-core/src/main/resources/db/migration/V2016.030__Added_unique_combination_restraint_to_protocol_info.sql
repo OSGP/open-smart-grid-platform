82c418346f68302648870c56a0abe0a01a4383df
Carlo Geenen <carlo.geenen@cgi.com>
2017-03-31 07:56:05 +0000

    Made flyway scripts idempotent to prevent flyway checksum exceptions.

--- osgp-core/src/main/resources/db/migration/V2016.030__Added_unique_combination_restraint_to_protocol_info.sql
+++ osgp-core/src/main/resources/db/migration/V2016.030__Added_unique_combination_restraint_to_protocol_info.sql
@@ -1,1 +1,17 @@
-ALTER TABLE protocol_info ADD CONSTRAINT unique_protocol_version UNIQUE (protocol, protocol_version);
+DO $$
+BEGIN
+
+IF NOT EXISTS (SELECT constraint_name FROM information_schema.constraint_column_usage 
+	WHERE table_name = 'protocol_info' AND constraint_name = 'unique_protocol_version')
+
+THEN
+
+	ALTER TABLE protocol_info ADD CONSTRAINT unique_protocol_version UNIQUE (protocol, protocol_version);
+
+END IF;
+
+END;
+$$
+
+
+
