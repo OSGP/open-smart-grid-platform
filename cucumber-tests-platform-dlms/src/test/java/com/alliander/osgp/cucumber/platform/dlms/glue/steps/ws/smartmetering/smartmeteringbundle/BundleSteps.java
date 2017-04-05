bdf6b6e08443a5170669230a8f8d78fa42e32f29
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2017-04-05 17:06:01 +0200

    SLIM-898 ~ Adds tests for bundled profile generic

--- cucumber-tests-platform-dlms/src/test/java/com/alliander/osgp/cucumber/platform/dlms/glue/steps/ws/smartmetering/smartmeteringbundle/BundleSteps.java
+++ cucumber-tests-platform-dlms/src/test/java/com/alliander/osgp/cucumber/platform/dlms/glue/steps/ws/smartmetering/smartmeteringbundle/BundleSteps.java
@@ -1,5 +1,9 @@
 package com.alliander.osgp.cucumber.platform.dlms.glue.steps.ws.smartmetering.smartmeteringbundle;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +12,19 @@ import com.alliander.osgp.adapter.ws.schema.smartmetering.bundle.Actions;
 import com.alliander.osgp.adapter.ws.schema.smartmetering.bundle.BundleAsyncRequest;
 import com.alliander.osgp.adapter.ws.schema.smartmetering.bundle.BundleAsyncResponse;
 import com.alliander.osgp.adapter.ws.schema.smartmetering.bundle.BundleRequest;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.bundle.BundleResponse;
 import com.alliander.osgp.adapter.ws.schema.smartmetering.bundle.GetProfileGenericDataRequest;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.common.CaptureObject;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.common.ProfileEntry;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.monitoring.ProfileGenericData;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.monitoring.ProfileGenericDataResponseData;
+import com.alliander.osgp.cucumber.platform.core.Helpers;
 import com.alliander.osgp.cucumber.platform.core.ScenarioContext;
 import com.alliander.osgp.cucumber.platform.dlms.Keys;
 import com.alliander.osgp.cucumber.platform.dlms.builders.BundleRequestBuilder;
 import com.alliander.osgp.cucumber.platform.dlms.builders.GetProfileGenericDataRequestBuilder;
 import com.alliander.osgp.cucumber.platform.dlms.support.ws.smartmetering.SmartMeteringBundleManagementClient;
+import com.alliander.osgp.cucumber.platform.helpers.SettingsHelper;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -40,14 +51,64 @@ public class BundleSteps {
     }
 
     @Then("the profile generic data should be part of the bundle response")
-    public void thenTheProfileGenericDataShouldBePartOfTheBundleResponse(final Map<String, String> settings) {
-
-        final String correlationUid = (String) ScenarioContext.Current().get(Keys.CORRELATION_UID);
-        final String deviceIdentification = (String) ScenarioContext.Current().get(Keys.DEVICE_IDENTIFICATION);
+    public void thenTheProfileGenericDataShouldBePartOfTheBundleResponse(final Map<String, String> settings)
+            throws Throwable {
 
         final BundleAsyncRequest request = new BundleAsyncRequest();
-        request.setCorrelationUid(correlationUid);
-        request.setDeviceIdentification(deviceIdentification);
+        request.setCorrelationUid((String) ScenarioContext.Current().get(Keys.CORRELATION_UID));
+        request.setDeviceIdentification((String) ScenarioContext.Current().get(Keys.DEVICE_IDENTIFICATION));
 
+        final BundleResponse bundleResponse = this.client.getBundleResponse(request);
+        final ProfileGenericDataResponseData profileGenericDataResponseData = (ProfileGenericDataResponseData) bundleResponse
+                .getAllResponses().getResponseList().get(0);
+        final ProfileGenericData profileGenericData = profileGenericDataResponseData.getProfileGenericData();
+
+        this.assertEqualCaptureObjects(profileGenericData.getCaptureObjectList().getCaptureObjects(), settings);
+        this.assertEqualProfileEntries(profileGenericData.getProfileEntryList().getProfileEntries(), settings);
+    }
+
+    private void assertEqualCaptureObjects(final List<CaptureObject> actualCaptureObjects,
+            final Map<String, String> expectedValues) throws AssertionError {
+
+        final int expectedNumberOfCaptureObjects = Helpers.getInteger(expectedValues, "NumberOfCaptureObjects", 0);
+
+        assertEquals("Number of capture objects", expectedNumberOfCaptureObjects, actualCaptureObjects.size());
+
+        for (int i = 0; i < expectedNumberOfCaptureObjects; i++) {
+            final CaptureObject actualCaptureObject = actualCaptureObjects.get(i);
+            this.assertEqualCaptureObject(actualCaptureObject, expectedValues, i + 1);
+        }
+    }
+
+    private void assertEqualCaptureObject(final CaptureObject actualCaptureObject,
+            final Map<String, String> expectedValues, final int index) throws AssertionError {
+        final Long expectedClassId = SettingsHelper.getLongValue(expectedValues, "CaptureObject_ClassId", index);
+        assertEquals("ClassId of CaptureObject " + index, expectedClassId,
+                Long.valueOf(actualCaptureObject.getClassId()));
+
+        final String expectedLogicalName = SettingsHelper.getStringValue(expectedValues, "CaptureObject_LogicalName",
+                index);
+        assertEquals("LogicalName of CaptureObject " + index, expectedLogicalName,
+                actualCaptureObject.getLogicalName());
+
+        final BigInteger expectedAttributeIndex = SettingsHelper.getBigIntegerValue(expectedValues,
+                "CaptureObject_AttributeIndex", index);
+        assertEquals("AttributeIndex of CaptureObject " + index, expectedAttributeIndex,
+                actualCaptureObject.getAttributeIndex());
+
+        final Long expectedDataIndex = SettingsHelper.getLongValue(expectedValues, "CaptureObject_DataIndex", index);
+        assertEquals("DataIndex of CaptureObject " + index, expectedDataIndex,
+                Long.valueOf(actualCaptureObject.getDataIndex()));
+
+        final String expectedUnit = SettingsHelper.getStringValue(expectedValues, "CaptureObject_Unit", index);
+        assertEquals("Unit of CaptureObject " + index, expectedUnit, actualCaptureObject.getUnit().value());
+    }
+
+    private void assertEqualProfileEntries(final List<ProfileEntry> actualProfileEntries,
+            final Map<String, String> expectedValues) {
+        final int expectedNumberOfProfileEntries = SettingsHelper.getIntegerValue(expectedValues,
+                "NumberOfProfileEntries", 0);
+
+        assertEquals("Number of profile entries", expectedNumberOfProfileEntries, actualProfileEntries.size());
     }
 }
