6dbbe5d57216720b3fe5c76dd5c0a836ec3a0209
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2017-04-19 17:32:30 +0200

    ZS-212 ~ Removes commented code

--- cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/steps/mocks/RtuSimulatorSteps.java
+++ cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/steps/mocks/RtuSimulatorSteps.java
@@ -42,31 +42,6 @@ public class RtuSimulatorSteps extends GlueBase {
         }
     }
 
-    /**
-     * If this method is user, then (in order to correctly start a different
-     * RtuSimulator), these setting must be supplied in the feature file:
-     * ServerName, IcdFilename and Port In addition, the Port should be unique,
-     * and the RTU device should unique as well because the platform caches the
-     * server based on the RTU device name!
-     *
-     * @param settings
-     * @throws Throwable
-     */
-    // @Given("^an rtu simulator started with$")
-    // public void anRtuSimulatorStartedWithSettings(final Map<String, String>
-    // settings) throws Throwable {
-    // this.validateRtuSimulatorSettings(settings);
-    // final String givenServerName = getString(settings,
-    // Keys.KEY_IEC61850_SERVERNAME);
-    // final String givenIcdFilename = getString(settings,
-    // Keys.KEY_IEC61850_ICD_FILENAME);
-    // final String givenPort = getString(settings, Keys.KEY_IEC61850_PORT);
-    //
-    // this.mockServer = new Iec61850MockServer(givenServerName,
-    // givenIcdFilename, Integer.valueOf(givenPort));
-    // this.mockServer.restart(settings);
-    // }
-
     @Then("^the rtu simulator should contain$")
     public void theRtuSimulatorShouldContain(final List<List<String>> mockValues) throws Throwable {
         for (final List<String> mockValue : mockValues) {
@@ -81,18 +56,4 @@ public class RtuSimulatorSteps extends GlueBase {
         }
     }
 
-    // private void validateRtuSimulatorSettings(final Map<String, String>
-    // settings) {
-    // final String givenIcdFilename = getString(settings,
-    // Keys.KEY_IEC61850_ICD_FILENAME);
-    // final String givenServerName = getString(settings,
-    // Keys.KEY_IEC61850_SERVERNAME);
-    // final String givenPort = getString(settings, Keys.KEY_IEC61850_PORT);
-    // Assert.assertNotNull("For another RtuSimuler the ServerName must be
-    // given", givenServerName);
-    // Assert.assertNotNull("For another RtuSimuler the IcdFilename must be
-    // given", givenIcdFilename);
-    // Assert.assertNotNull("For another RtuSimuler the Port must be given",
-    // givenPort);
-    // }
 }
