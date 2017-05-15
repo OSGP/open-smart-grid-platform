4dfb5b5d44e33163bdf900305150074ff4895e5e
Rianne Fijn <rianne.fijn@cgi.com>
2017-05-15 15:23:03 +0200

    SLIM-961, add mbus device attributes to database.

---
+++ osgp-core/src/main/resources/db/migration/V20170515144737123__Add_Attributes_For_MBus_Devices.sql
@@ -0,0 +1,34 @@
+DO
+$$
+BEGIN
+
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+ 	WHERE table_schema=current_schema 
+ 	AND table_name = 'smart_meter' 
+ 	AND column_name='mbus_identification_number') THEN
+	ALTER TABLE ONLY "smart_meter" ADD COLUMN "mbus_identification_number" VARCHAR(8);
+END IF;
+
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+ 	WHERE table_schema=current_schema 
+ 	AND table_name = 'smart_meter' 
+ 	AND column_name='mbus_manufacturer_identification') THEN
+	ALTER TABLE ONLY "smart_meter" ADD COLUMN "mbus_manufacturer_identification" VARCHAR(3);
+END IF;
+
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+ 	WHERE table_schema=current_schema 
+ 	AND table_name = 'smart_meter' 
+ 	AND column_name='mbus_version') THEN
+	ALTER TABLE ONLY "smart_meter" ADD COLUMN "mbus_version" VARCHAR(3);
+END IF;
+
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns 
+ 	WHERE table_schema=current_schema 
+ 	AND table_name = 'smart_meter' 
+ 	AND column_name='mbus_device_type_identification') THEN
+	ALTER TABLE ONLY "smart_meter" ADD COLUMN "mbus_device_type_identification" VARCHAR(2);
+END IF;
+
+END;
+$$
