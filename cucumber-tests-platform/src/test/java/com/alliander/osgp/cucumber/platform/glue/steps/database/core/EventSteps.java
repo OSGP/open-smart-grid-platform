d7ca6404699b55ce53e05aa78a5e588d6d64eff6
Hans Rooden <villgust@gmail.com>
2017-05-22 22:00:17 +0200

    FLEX-3267 Fixes for event wait condition (#243)
    * FLEX-3267 ~ Waits for relay status updates
    
    * FLEX-3267 Fixes wait loop for events

--- cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/steps/database/core/EventSteps.java
+++ cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/steps/database/core/EventSteps.java
@@ -27,8 +27,11 @@ import com.alliander.osgp.cucumber.platform.PlatformDefaults;
 import com.alliander.osgp.cucumber.platform.PlatformKeys;
 import com.alliander.osgp.domain.core.entities.Device;
 import com.alliander.osgp.domain.core.entities.Event;
+import com.alliander.osgp.domain.core.entities.RelayStatus;
+import com.alliander.osgp.domain.core.entities.Ssld;
 import com.alliander.osgp.domain.core.repositories.DeviceRepository;
 import com.alliander.osgp.domain.core.repositories.EventRepository;
+import com.alliander.osgp.domain.core.repositories.SsldRepository;
 import com.alliander.osgp.domain.core.valueobjects.EventType;
 
 import cucumber.api.java.en.Given;
@@ -40,6 +43,9 @@ public class EventSteps extends GlueBase {
     private DeviceRepository deviceRepository;
 
     @Autowired
+    private SsldRepository ssldRepository;
+
+    @Autowired
     private EventRepository eventRepository;
 
     @Given("^an event$")
@@ -47,9 +53,10 @@ public class EventSteps extends GlueBase {
         final Device device = this.deviceRepository
                 .findByDeviceIdentification(getString(data, PlatformKeys.KEY_DEVICE_IDENTIFICATION));
 
-        Event event = new Event(device, getDateTime2(getString(data, PlatformKeys.DATE), DateTime.now()).toDate(),
+        final Event event = new Event(device, getDateTime2(getString(data, PlatformKeys.DATE), DateTime.now()).toDate(),
                 getEnum(data, PlatformKeys.EVENT_TYPE, EventType.class, EventType.DIAG_EVENTS_GENERAL),
-                getString(data, PlatformKeys.KEY_DESCRIPTION, ""), getInteger(data, PlatformKeys.KEY_INDEX, PlatformDefaults.DEFAULT_INDEX));
+                getString(data, PlatformKeys.KEY_DESCRIPTION, ""),
+                getInteger(data, PlatformKeys.KEY_INDEX, PlatformDefaults.DEFAULT_INDEX));
 
         this.eventRepository.save(event);
     }
@@ -58,22 +65,26 @@ public class EventSteps extends GlueBase {
     public void theEventIsStored(final Map<String, String> expectedEntity) throws Throwable {
 
         // Convert comma separated events into a mutable list (for comparison)
-        final List<String> expectedEvents = new ArrayList<>(Arrays.asList((expectedEntity.containsKey(PlatformKeys.KEY_EVENTS))
-                ? getString(expectedEntity, PlatformKeys.KEY_EVENTS).split(PlatformKeys.SEPARATOR_COMMA)
-                : (expectedEntity.containsKey(PlatformKeys.KEY_EVENT))
-                        ? getString(expectedEntity, PlatformKeys.KEY_EVENT).split(PlatformKeys.SEPARATOR_COMMA) : new String[0]));
+        final List<String> expectedEvents = new ArrayList<>(
+                Arrays.asList((expectedEntity.containsKey(PlatformKeys.KEY_EVENTS))
+                        ? getString(expectedEntity, PlatformKeys.KEY_EVENTS).split(PlatformKeys.SEPARATOR_COMMA)
+                        : (expectedEntity.containsKey(PlatformKeys.KEY_EVENT))
+                                ? getString(expectedEntity, PlatformKeys.KEY_EVENT).split(PlatformKeys.SEPARATOR_COMMA)
+                                : new String[0]));
 
         // Convert comma separated indexes into a mutable list (for comparison)
-        final List<String> expectedIndexes = new ArrayList<>(Arrays.asList((expectedEntity.containsKey(PlatformKeys.KEY_INDEXES))
-                ? getString(expectedEntity, PlatformKeys.KEY_INDEXES).split(PlatformKeys.SEPARATOR_COMMA)
-                : (expectedEntity.containsKey(PlatformKeys.KEY_INDEX)
-                        && !expectedEntity.get(PlatformKeys.KEY_INDEX).equals("EMPTY"))
-                                ? getString(expectedEntity, PlatformKeys.KEY_INDEX).split(PlatformKeys.SEPARATOR_COMMA)
-                                : new String[] {"0"}));
+        final List<String> expectedIndexes = new ArrayList<>(
+                Arrays.asList((expectedEntity.containsKey(PlatformKeys.KEY_INDEXES))
+                        ? getString(expectedEntity, PlatformKeys.KEY_INDEXES).split(PlatformKeys.SEPARATOR_COMMA)
+                        : (expectedEntity.containsKey(PlatformKeys.KEY_INDEX)
+                                && !expectedEntity.get(PlatformKeys.KEY_INDEX).equals("EMPTY"))
+                                        ? getString(expectedEntity, PlatformKeys.KEY_INDEX)
+                                                .split(PlatformKeys.SEPARATOR_COMMA)
+                                        : new String[] { "0" }));
 
-        Assert.assertEquals("Number of events and indexes must be equal in scenario input", 
-                expectedEvents.size(), expectedIndexes.size());
-        
+        Assert.assertEquals("Number of events and indexes must be equal in scenario input", expectedEvents.size(),
+                expectedIndexes.size());
+
         // Wait for the correct events to be available
         Wait.until(() -> {
             final Device device = this.deviceRepository
@@ -82,31 +93,53 @@ public class EventSteps extends GlueBase {
             // Read the actual events received and check the desired size
             final List<Event> actualEvents = this.eventRepository.findByDevice(device);
 
+            // Assume default 1 expected event
+            int expectedNumberOfEvents = 1;
             if (expectedEntity.containsKey(PlatformKeys.NUMBER_OF_EVENTS)) {
-                Assert.assertEquals((int) getInteger(expectedEntity, PlatformKeys.NUMBER_OF_EVENTS), actualEvents.size());
+                expectedNumberOfEvents = (int) getInteger(expectedEntity, PlatformKeys.NUMBER_OF_EVENTS);
             }
 
+            Assert.assertEquals(expectedNumberOfEvents, actualEvents.size());
+            
             // Validate all expected events have been received
-            for (Event actualEvent : actualEvents) {
+            for (final Event actualEvent : actualEvents) {
                 int foundEventIndex = -1;
-                
-                Integer actualIndex = Character.getNumericValue(actualEvent.getIndex());
-                
-                // Try to find each event and corresponding index in the expected events list
+
+                final Integer actualIndex = Character.getNumericValue(actualEvent.getIndex());
+
+                // Try to find each event and corresponding index in the
+                // expected events list
                 for (int i = 0; i < expectedEvents.size(); i++) {
-                    if (EventType.valueOf(expectedEvents.get(i).trim()) == actualEvent.getEventType() && 
-                            Integer.parseInt(expectedIndexes.get(i)) == actualIndex) {
+                    if (EventType.valueOf(expectedEvents.get(i).trim()) == actualEvent.getEventType()
+                            && Integer.parseInt(expectedIndexes.get(i)) == actualIndex) {
                         foundEventIndex = i;
                         break;
                     }
                 }
 
-                Assert.assertTrue("Unable to find event [" + actualEvent.getEventType() + "] with index [" + actualIndex + "]", foundEventIndex != -1);
+                Assert.assertTrue(
+                        "Unable to find event [" + actualEvent.getEventType() + "] with index [" + actualIndex + "]",
+                        foundEventIndex != -1);
 
-                // Correct combination of event and index are found, remove them from the expected results lists
+                // Correct combination of event and index are found, remove them
+                // from the expected results lists
                 expectedEvents.remove(foundEventIndex);
                 expectedIndexes.remove(foundEventIndex);
             }
         });
+
+        // Wait until the relay statuses have been updated in the DB
+        final int numStatuses = Integer.parseInt(getString(expectedEntity, PlatformKeys.NUMBER_OF_STATUSES));
+        if (numStatuses > 0) {
+            Wait.until(() -> {
+                final Ssld ssld = this.ssldRepository
+                        .findByDeviceIdentification(getString(expectedEntity, PlatformKeys.KEY_DEVICE_IDENTIFICATION));
+                final List<RelayStatus> relayStatuses = ssld.getRelayStatusses();
+
+                Assert.assertTrue("Number of relay_status records = " + relayStatuses.size()
+                        + ", wait until this equals " + numStatuses, relayStatuses.size() == numStatuses);
+            });
+        }
     }
+
 }
