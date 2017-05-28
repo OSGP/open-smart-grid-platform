e06bf6e157ccf6196565329509ca16d9ed164ca5
kevinsmeets <kevin.smeets@cgi.com>
2017-05-28 14:25:16 +0000

    OP-598 ~ Removes FitNesse and test project folders.

--- fitnesse/run-fitnesse.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if [%MAVEN_REPO%] == [] echo "ERROR: \%MAVEN_REPO\% is not set! FitNesse will not be able to run your tests!"
-
-java -DMAVEN_REPO=%MAVEN_REPO% -jar fitnesse-standalone.jar -p 8181 -e 0 -o
