2f2ff022cbaf1ed8f8a9b99f96347a37af01f582
Robin Bakkerus <robin.bakkerus@cgi.com>
2017-05-30 07:20:30 +0000

    SLIM-975, updated review comments

--- cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringinstallation/DeviceSimulatorSteps.java
+++ cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringinstallation/DeviceSimulatorSteps.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Smart Society Services B.V.
+ * Copyright 2017 Smart Society Services B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
  *
@@ -43,8 +43,8 @@ public class DeviceSimulatorSteps extends AbstractSmartMeteringSteps {
         this.dynamicPropertiesHost = dynamicPropertiesHost;
         this.dynamicPropertiesPort = dynamicPropertiesPort;
 
-        final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         try {
+            final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
             final String request = String.format(CLEANUP_PROPS_REQUEST, this.getUrl());
             final HttpGet httpGetRequest = new HttpGet(request);
             httpClient.execute(httpGetRequest);
