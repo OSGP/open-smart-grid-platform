7cba9824407fc04da84477733ba85d01d63032e9
Robin Bakkerus <robin.bakkerus@cgi.com>
2017-05-30 10:45:40 +0000

    SLIM-975, improved matching algoritm

--- osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/utils/FindMatchingChannelHelper.java
+++ osgp-protocol-adapter-dlms/src/main/java/org/osgp/adapter/protocol/dlms/domain/commands/utils/FindMatchingChannelHelper.java
@@ -21,10 +21,13 @@ public class FindMatchingChannelHelper {
     }
 
     /**
-     * because it is not clear yet, how to match the 4 properties with 4
-     * corresponding attributes from the e-meter currently the match is only on
-     * the e-meter attribute 5 (which does not have a corresponding dbs
-     * property)
+     * Here we check if the values from the database that came from the shipment
+     * file, match with the corresponding return values from the e-meter. The
+     * primary-address is not stored in the database, and the rule is, that
+     * value (that corresponds with attr-id:5) is > 0. For the other fields, the
+     * rule is that return values from the e-meter may be empty. In that case
+     * they match, otherwise this value should be compared with the dbs value,
+     * where some specific converting rules may apply.
      *
      * @param channel
      * @param channelValues
@@ -34,10 +37,90 @@ public class FindMatchingChannelHelper {
     public static boolean mbusChannelMatches(final ChannelElementValues channelValues,
             final MbusChannelElementsDto requestData) {
 
-        final long hexValue = Long.parseLong(requestData.getMbusIdentificationNumber(), 16);
-        final int mbusDeviceValue = channelValues.getIdentificationNumber();
-
-        return channelValues.getPrimaryAddress() > 0 && mbusDeviceValue == hexValue;
+        if (channelValues.getPrimaryAddress() == 0) {
+            return false;
+        } else if (!matchIdentificationNumber(channelValues, requestData)) {
+            return false;
+        } else if (!matchManufacturerId(channelValues, requestData)) {
+            return false;
+        } else if (!matchDeviceType(channelValues, requestData)) {
+            return false;
+        } else if (!matchVersion(channelValues, requestData)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
+    private static boolean matchIdentificationNumber(final ChannelElementValues channelValues,
+            final MbusChannelElementsDto requestData) {
+        if (channelValues.getIdentificationNumber() > 0) {
+            final String mbusId = requestData.getMbusIdentificationNumber();
+            final long hexValue = mbusId == null ? -1 : Long.parseLong(mbusId, 16);
+            if (channelValues.getIdentificationNumber() != hexValue) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean matchManufacturerId(final ChannelElementValues channelValues,
+            final MbusChannelElementsDto requestData) {
+        if (channelValues.getManufacturerIdentification() > 0) {
+            final int manufacturerId = calcManufacturerId(requestData.getMbusManufacturerIdentification());
+            if (channelValues.getManufacturerIdentification() != manufacturerId) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean matchDeviceType(final ChannelElementValues channelValues,
+            final MbusChannelElementsDto requestData) {
+        if (channelValues.getDeviceTypeIdentification() > 0) {
+            final String dbsDeviceType = requestData.getMbusDeviceTypeIdentification();
+            final int deviceType = (dbsDeviceType == null) ? -1 : Integer.parseInt(dbsDeviceType);
+            if (channelValues.getDeviceTypeIdentification() != deviceType) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean matchVersion(final ChannelElementValues channelValues,
+            final MbusChannelElementsDto requestData) {
+        if (channelValues.getVersion() > 0) {
+            final String dbsVersion = requestData.getMbusVersion();
+            final int version = (dbsVersion == null) ? -1 : Integer.parseInt(dbsVersion);
+            if (channelValues.getVersion() != version) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * This return the integer value from the given
+     * mbusManufacturerIdentification String value, that is stored in the dbs as
+     * part of the shipment value according to EN 62056-21. with the following
+     * formula: Man. ID = [ASCII(1st letter)– 64] * 32 * 32 +[ASCII(2nd letter)
+     * – 64] * 32 +[ASCII(3rd letter) – 64] We expect a string of three chars,
+     * if not 0 will be returned.
+     *
+     * @param mbusManufacturerIdentification
+     * @return
+     */
+    private static int calcManufacturerId(final String mbusManufacturerIdentification) {
+        int result = 0;
+        if (mbusManufacturerIdentification.length() == 3) {
+            final char[] chars = mbusManufacturerIdentification.toCharArray();
+            result += (chars[0] - 64) * 32 * 32;
+            result += (chars[1] - 64) * 32;
+            result += (chars[2] - 64);
+        }
+        return result;
+    }
 }
--- osgp-protocol-adapter-dlms/src/test/java/org/osgp/adapter/protocol/dlms/domain/commands/utils/FindMatchingChannelHelperTest.java
+++ osgp-protocol-adapter-dlms/src/test/java/org/osgp/adapter/protocol/dlms/domain/commands/utils/FindMatchingChannelHelperTest.java
@@ -9,19 +9,85 @@ import com.alliander.osgp.dto.valueobjects.smartmetering.MbusChannelElementsDto;
 public class FindMatchingChannelHelperTest {
 
     /**
-     * obviously, once the rules become clear how to match, this junit test
-     * should enhanced accordingly
+     * happy flow all dbs fields are filled and e-meter returns values for all
+     * attr-ids
      */
     @Test
-    public void test() {
+    public void testAllFilled() {
         final Integer channel = 1;
-        final short primaryAddress = (short) 1;
+        final short primaryAddress = 1;
         final int identificationNumber = 302343985;
+        final int manufacturerIdentification = 12514;
+        final short version = 66;
+        final short deviceTypeIdentification = 3;
+
         final ChannelElementValues channelValues = new ChannelElementValues(channel, primaryAddress,
-                identificationNumber, 1, (short) 1, (short) 1);
-        final MbusChannelElementsDto requestData = new MbusChannelElementsDto("TESTE1024", "12056731", "", "", "");
+                identificationNumber, manufacturerIdentification, version, deviceTypeIdentification);
+        final MbusChannelElementsDto requestData = new MbusChannelElementsDto("TST", "12056731", "LGB", "066", "3");
+
         final boolean matches = FindMatchingChannelHelper.mbusChannelMatches(channelValues, requestData);
         Assert.assertTrue("mbus channel should match", matches);
     }
 
+    /**
+     * dbs fields are filled and e-meter returns values all 0 values
+     */
+    @Test
+    public void testDbsFilledAllAttrIdsEmpty() {
+        final Integer channel = 0;
+        final short primaryAddress = 0;
+        final int identificationNumber = 0;
+        final int manufacturerIdentification = 0;
+        final short version = 0;
+        final short deviceTypeIdentification = 0;
+
+        final ChannelElementValues channelValues = new ChannelElementValues(channel, primaryAddress,
+                identificationNumber, manufacturerIdentification, version, deviceTypeIdentification);
+        final MbusChannelElementsDto requestData = new MbusChannelElementsDto("TST", "12056731", "LGB", "066", "3");
+
+        final boolean matches = FindMatchingChannelHelper.mbusChannelMatches(channelValues, requestData);
+        Assert.assertFalse("mbus channel does not match", matches);
+    }
+
+    /**
+     * dbs fields are filled and e-meter returns values all 0 values, except for
+     * primary-address
+     */
+    @Test
+    public void testDbsFilledSomeAttrIdsEmpty() {
+        final Integer channel = 0;
+        final short primaryAddress = 1;
+        final int identificationNumber = 0;
+        final int manufacturerIdentification = 0;
+        final short version = 0;
+        final short deviceTypeIdentification = 0;
+
+        final ChannelElementValues channelValues = new ChannelElementValues(channel, primaryAddress,
+                identificationNumber, manufacturerIdentification, version, deviceTypeIdentification);
+        final MbusChannelElementsDto requestData = new MbusChannelElementsDto("TST", "12056731", "LGB", "066", "3");
+
+        final boolean matches = FindMatchingChannelHelper.mbusChannelMatches(channelValues, requestData);
+        Assert.assertTrue("mbus channel should match", matches);
+    }
+
+    /**
+     * dbs fields are filled and e-meter returns values all 0 values, except for
+     * primary-address
+     */
+    @Test
+    public void testDbsNullAllAttrFilled() {
+        final Integer channel = 1;
+        final short primaryAddress = 1;
+        final int identificationNumber = 302343985;
+        final int manufacturerIdentification = 12514;
+        final short version = 66;
+        final short deviceTypeIdentification = 3;
+
+        final ChannelElementValues channelValues = new ChannelElementValues(channel, primaryAddress,
+                identificationNumber, manufacturerIdentification, version, deviceTypeIdentification);
+        final MbusChannelElementsDto requestData = new MbusChannelElementsDto(null, null, null, null, null);
+
+        final boolean matches = FindMatchingChannelHelper.mbusChannelMatches(channelValues, requestData);
+        Assert.assertFalse("mbus channel should not match", matches);
+    }
 }
