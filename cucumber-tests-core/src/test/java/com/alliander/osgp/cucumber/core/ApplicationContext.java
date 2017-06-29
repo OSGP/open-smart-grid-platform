aa63dae557c5d97edee078a811bddb1c68fdfb40
Elkan Roelen <ElkanRoelen@users.noreply.github.com>
2017-06-29 21:35:27 +0200

    Allow DA Cucumber scripts by adding org.osgpfoundation.osgp
    To allow DA Platform cucumber testing, we need to add org.osgpfoundation.osgp.cucumber packages tot the component scan.

--- cucumber-tests-core/src/test/java/com/alliander/osgp/cucumber/core/ApplicationContext.java
+++ cucumber-tests-core/src/test/java/com/alliander/osgp/cucumber/core/ApplicationContext.java
@@ -2,7 +2,7 @@ package com.alliander.osgp.cucumber.core;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = { "com.alliander.osgp.cucumber" })
+@ComponentScan(basePackages = { "com.alliander.osgp.cucumber", "org.osgpfoundation.osgp.cucumber" })
 public class ApplicationContext {
 
 }
