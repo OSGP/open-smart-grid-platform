8cdfd831a0568a338a482aa1b0c27540d5b0524c
martijnsips <martijn.sips@cgi.com>
2017-08-28 10:04:36 +0200

    OP-656 ~ Reverted back to normal mvn install shell command

--- jenkins/pipeline/OSGP Integration-Tests pr.groovy
+++ jenkins/pipeline/OSGP Integration-Tests pr.groovy
@@ -33,20 +33,20 @@ pipeline {
 
         stage ('Build') {
             steps {
-                withMaven(
-			// Maven installation declared in the Jenkins "Global Tool Configuration"
-			maven: 'M3',
-			// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
-			// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-			mavenSettingsConfig: 'my-maven-settings',
-			mavenLocalRepo: '.repository') {
-
-			// Run the maven build
-			sh "mvn clean install -DskipTestJarWithDependenciesAssembly=false"
-
-		} // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
                 // TODO: use withMaven
-                //sh "/usr/local/apache-maven/bin/mvn clean install -DskipTestJarWithDependenciesAssembly=false"
+                //withMaven(
+                      // Maven installation declared in the Jenkins "Global Tool Configuration"
+                //	maven: 'M3',
+                      // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+                      // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+                //	mavenSettingsConfig: 'my-maven-settings',
+                //	mavenLocalRepo: '.repository') {
+
+                      // Run the maven build
+                //	sh "mvn clean install -DskipTestJarWithDependenciesAssembly=false"
+                //} // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
+                
+                sh "/usr/local/apache-maven/bin/mvn clean install -DskipTestJarWithDependenciesAssembly=false"
             }
         }
 
