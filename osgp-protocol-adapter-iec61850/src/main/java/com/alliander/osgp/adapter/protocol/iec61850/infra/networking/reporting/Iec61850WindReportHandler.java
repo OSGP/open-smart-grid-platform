4e7902bfe94d832b826acb1fe6a7bd1a5cb98759
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2017-09-06 12:08:20 +0200

    ZS-253 ~ Fixes report handling for wind device

--- osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/reporting/Iec61850WindReportHandler.java
+++ osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/reporting/Iec61850WindReportHandler.java
@@ -10,6 +10,8 @@ package com.alliander.osgp.adapter.protocol.iec61850.infra.networking.reporting;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.openmuc.openiec61850.ModelNode;
 import org.slf4j.Logger;
@@ -26,8 +28,15 @@ public class Iec61850WindReportHandler implements Iec61850ReportHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(Iec61850WindReportHandler.class);
 
     private static final String SYSTEM_TYPE = "WIND";
-
+    private static final List<String> NODES_USING_ID_LIST = new ArrayList<>();
     private static final List<String> COMPOSITE_NODES = Arrays.asList(new String[] { "W" });
+
+    private static final Pattern NODE_PATTERN = Pattern
+            .compile("\\A(.*)WIND([1-9]\\d*+)/(LLN0|DRCC|DGEN|MMXU|GGIO)([1-9]\\d*+)?\\.(.*)\\Z");
+
+    static {
+        intializeNodesUsingIdList();
+    }
 
     private int systemId;
 
@@ -49,25 +58,24 @@ public class Iec61850WindReportHandler implements Iec61850ReportHandler {
 
         final List<MeasurementDto> measurements = new ArrayList<>();
 
-        if (this.isCompositeNode(member)) {
+        if (this.isCompositeNode(member.getFcmodelNode())) {
             for (final ModelNode child : member.getFcmodelNode().getChildren()) {
-                final String compositeNodeName = this.getCompositeNodeName(child);
 
                 final RtuReadCommand<MeasurementDto> command = Iec61850WindCommandFactory.getInstance()
-                        .getCommand(compositeNodeName);
+                        .getCommand(this.getChildCommandName(child));
 
                 if (command == null) {
-                    LOGGER.warn("No command found for node {}", compositeNodeName);
+                    LOGGER.warn("No command found for node {}", child.getReference().getName());
                 } else {
                     measurements.add(command.translate(member.getChild(child.getName())));
                 }
             }
         } else {
             final RtuReadCommand<MeasurementDto> command = Iec61850WindCommandFactory.getInstance()
-                    .getCommand(member.getFcmodelNode().getName());
+                    .getCommand(this.getCommandName(member));
 
             if (command == null) {
-                LOGGER.warn("No command found for node {}", member.getFcmodelNode().getName());
+                LOGGER.warn("No command found for node {}", member.getFcmodelNode().getReference().getName());
             } else {
                 measurements.add(command.translate(member));
             }
@@ -75,12 +83,49 @@ public class Iec61850WindReportHandler implements Iec61850ReportHandler {
         return measurements;
     }
 
-    private boolean isCompositeNode(final ReadOnlyNodeContainer member) {
-        return COMPOSITE_NODES.contains(member.getFcmodelNode().getName());
+    private static void intializeNodesUsingIdList() {
+        NODES_USING_ID_LIST.add("W.phsA");
+        NODES_USING_ID_LIST.add("W.phsB");
+        NODES_USING_ID_LIST.add("W.phsC");
     }
 
-    private String getCompositeNodeName(final ModelNode childNode) {
-        return childNode.getParent().getName() + "." + childNode.getName();
+    private static boolean useId(final String nodeName) {
+        return NODES_USING_ID_LIST.contains(nodeName);
+    }
+
+    private boolean isCompositeNode(final ModelNode node) {
+        return COMPOSITE_NODES.contains(node.getName());
+    }
+
+    private String getCommandName(final ReadOnlyNodeContainer member) {
+
+        final String nodeName = member.getFcmodelNode().getName();
+        if (useId(nodeName)) {
+            final String refName = member.getFcmodelNode().getReference().toString();
+            return nodeName + this.getIndex(refName);
+        } else {
+            return nodeName;
+        }
+    }
+
+    private String getChildCommandName(final ModelNode child) {
+
+        final String nodeName = child.getParent().getName() + "." + child.getName();
+        if (useId(nodeName)) {
+            final String refName = child.getReference().toString();
+            return nodeName + this.getIndex(refName);
+        } else {
+            return nodeName;
+        }
+    }
+
+    private String getIndex(final String reference) {
+        String index = "";
+        final Matcher reportMatcher = NODE_PATTERN.matcher(reference);
+        if (reportMatcher.matches()) {
+            index = reportMatcher.group(4);
+        }
+        return index;
     }
 
 }
