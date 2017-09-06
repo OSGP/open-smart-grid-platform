411aeb1533dc0736de9541ef48890549df20d502
MartijnSipsCGI <martijn.sips@cgi.com>
2017-09-06 07:57:41 +0200

    Adds collect script

---
+++ collectArtifactsFromBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Collecting all artifacts ..."
+rm -rf target/artifacts
+mkdir -p target/artifacts
+find . -name *.war -exec cp -f {} target/artifacts \;
+
+echo "retrieve additional artifacts ..."
+VERSION=`grep "<version>" pom.xml | sed "s#<[/]\?version>##g;s# ##g"`
+echo "  [$VERSION]"
+ARTIFACTORY_URL=https://artifactory.smartsocietyservices.com
+CURL_URL=${ARTIFACTORY}/artifactory/osgp-snapshots/com/alliander/osgp/config/${VERSION}/config-${VERSION}.zip
+CURL_TARGET_FILE=target/artifacts/config-${VERSION}.zip
+echo "  [curl -XGET \"${CURL_URL}\" -o ${CURL_TARGET_FILE}]"
+curl -XGET "${CURL_URL}" -o ${CURL_TARGET_FILE}
+
+echo "Done."
