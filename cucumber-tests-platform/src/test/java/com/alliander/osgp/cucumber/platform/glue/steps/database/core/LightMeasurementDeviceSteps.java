6e04e454842d8b001f368a9f5fe7ad1212206311
Ruud Lemmers <ruud.lemmers@cgi.com>
2017-09-06 10:14:36 +0200

    Flex 3434 find light measurement devices db (#317)
    * FLEX-3434 ~ Cleans light measurement devices
    
    * FLEX-3434 ~ Merges development and adds transactional annotation
    
    * FLEX-3434 ~ Updates submodules
    
    * FLEX-3434 ~ Updates submodules
    
    * FLEX-3434 ~ Enables creation of a single LMD

--- cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/steps/database/core/LightMeasurementDeviceSteps.java
+++ cucumber-tests-platform/src/test/java/com/alliander/osgp/cucumber/platform/glue/steps/database/core/LightMeasurementDeviceSteps.java
@@ -8,11 +8,9 @@
 package com.alliander.osgp.cucumber.platform.glue.steps.database.core;
 
 import java.net.InetAddress;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.List;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -42,27 +40,23 @@ public class LightMeasurementDeviceSteps extends BaseDeviceSteps {
      * for the default organization.
      */
     public void createLightMeasurementDevices() {
-        List<LightMeasurementDevice> devices = new ArrayList<>();
         final LightMeasurementDevice lmd01 = this.createLightMeasurementDevice("LMD-01", "N-01", "#c9eec9", (short) 1);
         // Set the last communication time to 2017-08-01 at 13:00 UTC
         final Date lastCommunicationTimeLmd01 = new GregorianCalendar(2017, Calendar.AUGUST, 1, 13, 0).getTime();
 
         lmd01.setLastCommunicationTime(lastCommunicationTimeLmd01);
-        devices.add(lmd01);
-        devices.add(this.createLightMeasurementDevice("LMD-02", "E-01", "#eec9c9", (short) 2));
-        devices.add(this.createLightMeasurementDevice("LMD-03", "S-01", "#c9c9ee", (short) 3));
-        devices.add(this.createLightMeasurementDevice("LMD-04", "W-01", "#eeeec9", (short) 4));
+        this.lightMeasurementDeviceRepository.save(lmd01);
 
-        devices = this.lightMeasurementDeviceRepository.save(devices);
-
-        for (final LightMeasurementDevice device : devices) {
-            this.setDefaultDeviceAuthorizationForDevice(device);
-        }
+        this.createLightMeasurementDevice("LMD-02", "E-01", "#eec9c9", (short) 2);
+        this.createLightMeasurementDevice("LMD-03", "S-01", "#c9c9ee", (short) 3);
+        this.createLightMeasurementDevice("LMD-04", "W-01", "#eeeec9", (short) 4);
     }
 
     /**
-     * Create a single light measurement device.
+     * Create a single light measurement device, including rights for the
+     * default organization.
      */
+    @Transactional("txMgrCore")
     public LightMeasurementDevice createLightMeasurementDevice(final String deviceIdentification, final String code,
             final String color, final short digitalInput) {
         final String deviceType = "LMD";
@@ -82,7 +76,10 @@ public class LightMeasurementDeviceSteps extends BaseDeviceSteps {
         lightMeasurementDevice.setLastCommunicationTime(technicalInstallationDate);
         lightMeasurementDevice.setDigitalInput(digitalInput);
 
+        // Setting the default authorization both creates the device and adds
+        // the device authorization.
+        this.setDefaultDeviceAuthorizationForDevice(lightMeasurementDevice);
+
         return lightMeasurementDevice;
     }
-
 }
