bc793e66193441e6c3b11f7305d6e6e8429ab529
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2017-09-11 10:27:42 +0200

    ZS-232 ~ Adds cucumber test for enable reporting on connect

--- cucumber-tests-platform-microgrids/src/test/java/com/alliander/osgp/cucumber/platform/microgrids/glue/steps/database/adapterprotocolie61850/Iec61850DeviceSteps.java
+++ cucumber-tests-platform-microgrids/src/test/java/com/alliander/osgp/cucumber/platform/microgrids/glue/steps/database/adapterprotocolie61850/Iec61850DeviceSteps.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.alliander.osgp.adapter.protocol.iec61850.domain.entities.Iec61850Device;
 import com.alliander.osgp.adapter.protocol.iec61850.domain.repositories.Iec61850DeviceRepository;
 import com.alliander.osgp.cucumber.core.GlueBase;
+import com.alliander.osgp.cucumber.core.Helpers;
 import com.alliander.osgp.cucumber.core.ScenarioContext;
 import com.alliander.osgp.cucumber.platform.helpers.SettingsHelper;
 import com.alliander.osgp.cucumber.platform.microgrids.PlatformMicrogridsDefaults;
@@ -66,9 +67,11 @@ public class Iec61850DeviceSteps extends GlueBase {
     @Given("^an rtu iec61850 device$")
     public void anRtuIec61850Device(final Map<String, String> settings) throws Throwable {
 
-        ScenarioContext.current().put(PlatformMicrogridsKeys.KEY_DEVICE_IDENTIFICATION, PlatformMicrogridsDefaults.DEFAULT_DEVICE_IDENTIFICATION);
+        ScenarioContext.current().put(PlatformMicrogridsKeys.KEY_DEVICE_IDENTIFICATION,
+                PlatformMicrogridsDefaults.DEFAULT_DEVICE_IDENTIFICATION);
         final Map<String, String> rtuSettings = SettingsHelper.addAsDefaults(settings, RTU_DEFAULT_SETTINGS);
-        rtuSettings.put(PlatformMicrogridsKeys.KEY_NETWORKADDRESS, this.iec61850MockServerConfig.iec61850MockNetworkAddress());
+        rtuSettings.put(PlatformMicrogridsKeys.KEY_NETWORKADDRESS,
+                this.iec61850MockServerConfig.iec61850MockNetworkAddress());
 
         this.rtuDeviceSteps.anRtuDevice(rtuSettings);
 
@@ -86,11 +89,14 @@ public class Iec61850DeviceSteps extends GlueBase {
          * the device. ICD filename, port and servername may be null
          */
         final Iec61850Device iec61850Device = new Iec61850Device(
-                getString(settings, PlatformMicrogridsKeys.KEY_DEVICE_IDENTIFICATION, PlatformMicrogridsDefaults.DEFAULT_DEVICE_IDENTIFICATION));
+                getString(settings, PlatformMicrogridsKeys.KEY_DEVICE_IDENTIFICATION,
+                        PlatformMicrogridsDefaults.DEFAULT_DEVICE_IDENTIFICATION));
 
         iec61850Device.setIcdFilename(getString(settings, PlatformMicrogridsKeys.KEY_IEC61850_ICD_FILENAME));
         iec61850Device.setPort(getInteger(settings, PlatformMicrogridsKeys.KEY_IEC61850_PORT));
         iec61850Device.setServerName(getString(settings, PlatformMicrogridsKeys.KEY_IEC61850_SERVERNAME));
+        iec61850Device.setEnableAllReportsOnConnect(Helpers.getBoolean(settings,
+                PlatformMicrogridsKeys.ENABLE_ALL_REPORTS, PlatformMicrogridsDefaults.ENABLE_ALL_REPORTS));
 
         this.iec61850DeviceRespository.save(iec61850Device);
     }
