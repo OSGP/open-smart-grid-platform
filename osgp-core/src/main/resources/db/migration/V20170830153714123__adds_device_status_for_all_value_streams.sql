4b0128d34491c7d4b84385edd30342fe2d94b0d1
Rianne Fijn <rianne.fijn@cgi.com>
2017-09-11 15:00:59 +0200

    OC-359 updated version flyway script.

--- osgp-core/src/main/resources/db/migration/V20170830153714123__adds_device_status_for_all_value_streams.sql
+++ osgp-core/src/main/resources/db/migration/V20170830153714123__adds_device_status_for_all_value_streams.sql
@@ -5,12 +5,66 @@ BEGIN
   IF NOT EXISTS(SELECT 1 FROM information_schema.columns WHERE table_schema = current_schema AND table_name = 'device' AND column_name = 'device_lifecycle_status')
   THEN
   	ALTER TABLE device ADD COLUMN device_lifecycle_status VARCHAR(255);
+  	
+  	-- Migrating publiclighting devices to appropriate device lifecycle status --
+  	-- A public lighting device without a device_type is not yet registered with the platform, and therefor gets the status NEW_IN_INVENTORY --
+  	UPDATE device SET device_lifecycle_status = 'NEW_IN_INVENTORY' 
+  	WHERE (device.device_type = 'SSLD' OR device.device_type = 'LMD' OR device.device_type = 'PSLD' OR device.device_type IS NULL)
+  	AND device.is_active = false 
+  	AND device.is_activated = false 
+  	AND device.technical_installation_date IS NULL;
+
+  	-- Once a device is registered, it's device_type can not be null anymore --
+  	UPDATE device SET device_lifecycle_status = 'REGISTERED' 
+   	WHERE (device.device_type = 'SSLD' OR device.device_type = 'LMD' OR device.device_type = 'PSLD')
+  	AND device.is_active = true 
+  	AND device.is_activated = true 
+  	AND device.technical_installation_date IS NULL;
+  	
+  	UPDATE device SET device_lifecycle_status = 'IN_USE'
+  	WHERE (device.device_type = 'SSLD' OR device.device_type = 'LMD' OR device.device_type = 'PSLD')
+  	AND device.is_active = true 
+  	AND device.is_activated = true 
+  	AND device.technical_installation_date IS NOT NULL;
+  	
+  	UPDATE device SET device_lifecycle_status = 'DESTROYED' 
+  	WHERE (device.device_type = 'SSLD' OR device.device_type = 'LMD' OR device.device_type = 'PSLD')
+  	AND device.is_active = false 
+  	AND device.is_activated = false 
+  	AND device.technical_installation_date IS NOT NULL;
+  
+  	-- Migrating rtu devices to appropriate device lifecycle status--
+  	UPDATE device SET device_lifecycle_status = 'IN_USE' 
+  	WHERE device.device_type = 'RTU' 
+  	AND device.is_active = true 
+  	AND device.is_activated = true; 
+  	
+  	UPDATE device SET device_lifecycle_status = 'READY_FOR_USE' 
+  	WHERE device.device_type = 'RTU' 
+  	AND device.is_active = false 
+  	AND device.is_activated = false;
+  	
+  	-- Migrating smartmetering devices to appropriate device lifecycle status--
+  	UPDATE device SET device_lifecycle_status = 'DESTROYED' 
+  	WHERE (device.device_type = 'SMART_METER_E' OR device.device_type = 'SMART_METER_G')
+  	AND device.is_active = false 
+  	AND device.is_activated = false;
+  	
+  	UPDATE device SET device_lifecycle_status = 'IN_USE' 
+  	WHERE (device.device_type = 'SMART_METER_E' OR device.device_type = 'SMART_METER_G')
+  	AND device.is_active = true 
+  	AND device.is_activated = true;
+  	
+  	-- Get rid of the is_active column, this is replaced by device_lifecycle_status 'IN_USE'--
   	ALTER TABLE device DROP COLUMN is_active;
+  	
+  	-- Make device_lifecycle_status non-nullable --
+  	ALTER TABLE device ALTER COLUMN device_lifecycle_status SET NOT NULL;
+  	
   END IF;
   
   IF NOT EXISTS(SELECT 1 FROM device_function_mapping WHERE function = 'SET_DEVICE_LIFECYCLE_STATUS')
-  THEN
-  INSERT INTO device_function_mapping values ('OWNER', 'SET_DEVICE_LIFECYCLE_STATUS');
+  THEN INSERT INTO device_function_mapping values ('OWNER', 'SET_DEVICE_LIFECYCLE_STATUS');
   END IF;
   
 END;
