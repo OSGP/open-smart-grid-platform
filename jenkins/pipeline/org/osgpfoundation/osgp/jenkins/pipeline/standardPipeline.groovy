f1e1fe594eab6daea5656d160dd175ae82a29443
Martijn Sips <martijn.sips@cgi.com>
2017-09-19 10:04:22 +0200

    Op 644 deploy pr artifacts (#323)
    * OP-644 ~ Updated webapps to deploy pr build artifacts
    
    * Adds deployment_type
    
    * Corrected snapshots to snapshot
    
    * Updated submodules and groovy scripts
    
    * OP-644 ~ Merged submodules
    
    * Updated submodules
    
    * OP-644 ~ Corrected what to download
    
    * OP-644 ~ Corrected what to download
    
    * Corrected what to download
    
    * Cleans the workspace if the build was successfull
    
    * Disables archiveing the artifacts from withMaven
    
    * Disables archiveing the artifacts from withMaven
    
    * Disables archiveing the artifacts from withMaven
    
    * Disables archiveing the artifacts from withMaven
    
    * Updated submodules
    
    * Adds download of the da artifacts
    
    * Adds some comment
    
    * Adds download of IEC61850 artifact
    
    * Added statement to stop tomcat and httpd
    
    * Fixed quotes
    
    * Fixed quotes
    
    * OP-644 ~ Updated certifacte
    
    * updated identifyfile
    
    * Added tomcat_restart = false
    
    * OP-644 ~ Fixed typo
    
    * OP-644 ~ Removed obsolete data
    
    * Adds ec2_instance_type parameter
    
    * OP-644 ~ Added some comments
    
    * OP-644 ~ Removed whitespace, and updated scripts

--- jenkins/pipeline/org/osgpfoundation/osgp/jenkins/pipeline/standardPipeline.groovy
+++ jenkins/pipeline/org/osgpfoundation/osgp/jenkins/pipeline/standardPipeline.groovy
@@ -17,7 +17,12 @@ def call(body) {
 
     pipeline {
         agent any
-        
+
+        environment {
+            // Default the pom version
+            POMVERSION="latest"
+        }
+
         options {
             ansiColor('xterm')
             timestamps()
@@ -52,9 +57,48 @@ def call(body) {
                 steps {
                     withMaven(
                             maven: 'Apache Maven 3.5.0',
-                            mavenLocalRepo: '.repository') {
+                            mavenLocalRepo: '.repository',
+                            options: [
+                                    artifactsPublisher(disabled: true),
+                            ]) {
                         sh "mvn clean install ${config.mavenParameters}"
                     }
+
+                    // Collect all build wars and copy them to target/artifacts
+                    sh "rm -rf target/artifacts && mkdir -p target/artifacts && find . -name *.war -exec cp -f {} target/artifacts \\;"
+
+                    // Clone the release repository in order to deploy
+                    sh "git clone git@github.com:SmartSocietyServices/release.git"
+
+                    script {
+                        // Determine the actual pom version from the pom.
+                        POMVERSION = sh ( script: "grep \"<version>\" pom.xml | sed \"s#<[/]\\?version>##g;s# ##g\" | grep SNAPSHOT", returnStdout: true).trim()
+                    }
+                    echo "Using version ${POMVERSION} (from pom) to collect artifacts which are needed to deploy a new environment but weren't build in this job."
+
+                    // Download missing artifacts from artifactory for the same version
+                    // - The following artifacts are not in this repository
+                    sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ configuration_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
+                    sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ dlms_simulator_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
+                    // - The following artifacts are not specified in the root pom.xml, thus they should be retrieved from the artifactory.
+                    sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ distribution_automation_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
+                    sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ iec61850_simulator_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
+
+                    // Now create a new single instance (not stream specific) and put all the artifacts in /data/software/artifacts
+                    sh "cd release && plays/deploy-files-to-system.yml -e osgp_version=${POMVERSION} -e deployment_name=${servername} -e directory_to_deploy=../../target/artifacts -e tomcat_restart=false -e ec2_instance_type=m4.large"
+                }
+            }
+
+            stage ('Deploy local artifacts') {
+                steps {
+                    // Deploy stream specific system using the local artifacts from /data/software/artifacts on the system
+                    build job: 'Deploy an AWS System', parameters: [
+                            string(name: 'SERVERNAME', value: servername),
+                            string(name: 'PLAYBOOK', value: playbook),
+                            booleanParam(name: 'INSTALL_FROM_LOCAL_DIR', value: true),
+                            string(name: 'ARTIFACT_DIRECTORY', value: "/data/software/artifacts"),
+                            string(name: 'OSGP_VERSION', value: POMVERSION),
+                            booleanParam(name: 'ARTIFACT_DIRECTORY_REMOTE_SRC', value: true)]
                 }
             }
         }
