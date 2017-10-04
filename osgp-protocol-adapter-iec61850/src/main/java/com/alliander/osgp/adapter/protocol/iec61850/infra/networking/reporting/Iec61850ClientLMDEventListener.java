992e4cd20210b8b73b6e7e7df28902bc75a7013b
Kevin Smeets <kevin.smeets@cgi.com>
2017-10-04 14:39:32 +0200

    FLEX-3547 ~ Updates check on LMD state of light sensor. (#133)

--- osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/reporting/Iec61850ClientLMDEventListener.java
+++ osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/reporting/Iec61850ClientLMDEventListener.java
@@ -111,7 +111,7 @@ public class Iec61850ClientLMDEventListener extends Iec61850ClientBaseEventListe
         return String.format("reportId: %s, timeOfEntry: %s, sqNum: %s%s%s", report.getRptId(),
                 timeOfEntry == null ? "-" : timeOfEntry, report.getSqNum(), report.getSubSqNum() == null ? ""
                         : " subSqNum: " + report.getSubSqNum(),
-                        report.isMoreSegmentsFollow() ? " (more segments follow for this sqNum)" : "");
+                report.isMoreSegmentsFollow() ? " (more segments follow for this sqNum)" : "");
     }
 
     private EventNotificationDto getEventNotificationForReportedData(final FcModelNode evnRpn,
@@ -119,12 +119,17 @@ public class Iec61850ClientLMDEventListener extends Iec61850ClientBaseEventListe
             final Integer index) {
         EventTypeDto eventType;
         final boolean lightSensorValue = this.determineLightSensorValue(evnRpn, reportDescription);
+        //@formatter:off
+        /*
+         * 0 -> false -> NIGHT_DAY --> LIGHT_SENSOR_REPORTS_LIGHT
+         * 1 -> true  -> DAY_NIGHT --> LIGHT_SENSOR_REPORTS_DARK
+         */
+        //@formatter:on
         if (lightSensorValue) {
-            eventType = EventTypeDto.LIGHT_SENSOR_REPORTS_LIGHT;
-        } else {
             eventType = EventTypeDto.LIGHT_SENSOR_REPORTS_DARK;
+        } else {
+            eventType = EventTypeDto.LIGHT_SENSOR_REPORTS_LIGHT;
         }
-
         return new EventNotificationDto(deviceIdentification, timeOfEntry, eventType, reportDescription, index);
     }
 
@@ -152,30 +157,30 @@ public class Iec61850ClientLMDEventListener extends Iec61850ClientBaseEventListe
         sb.append("\t           BufOvfl:\t").append(report.isBufOvfl()).append(System.lineSeparator());
         sb.append("\t           EntryId:\t").append(report.getEntryId()).append(System.lineSeparator());
         sb.append("\tInclusionBitString:\t").append(Arrays.toString(report.getInclusionBitString()))
-        .append(System.lineSeparator());
+                .append(System.lineSeparator());
         sb.append("\tMoreSegmentsFollow:\t").append(report.isMoreSegmentsFollow()).append(System.lineSeparator());
         sb.append("\t             SqNum:\t").append(report.getSqNum()).append(System.lineSeparator());
         sb.append("\t          SubSqNum:\t").append(report.getSubSqNum()).append(System.lineSeparator());
         sb.append("\t       TimeOfEntry:\t").append(report.getTimeOfEntry()).append(System.lineSeparator());
         if (report.getTimeOfEntry() != null) {
             sb.append("\t                   \t(")
-            .append(new DateTime(report.getTimeOfEntry().getTimestampValue() + IEC61850_ENTRY_TIME_OFFSET))
-            .append(')').append(System.lineSeparator());
+                    .append(new DateTime(report.getTimeOfEntry().getTimestampValue() + IEC61850_ENTRY_TIME_OFFSET))
+                    .append(')').append(System.lineSeparator());
         }
         final List<BdaReasonForInclusion> reasonCodes = report.getReasonCodes();
         if (reasonCodes != null && !reasonCodes.isEmpty()) {
             sb.append("\t       ReasonCodes:").append(System.lineSeparator());
             for (final BdaReasonForInclusion reasonCode : reasonCodes) {
                 sb.append("\t                   \t")
-                .append(reasonCode.getReference() == null ? HexConverter.toHexString(reasonCode.getValue())
-                        : reasonCode).append("\t(")
+                        .append(reasonCode.getReference() == null ? HexConverter.toHexString(reasonCode.getValue())
+                                : reasonCode).append("\t(")
                         .append(new Iec61850BdaReasonForInclusionHelper(reasonCode).getInfo()).append(')')
                         .append(System.lineSeparator());
             }
         }
         sb.append("\t           optFlds:").append(report.getOptFlds()).append("\t(")
-        .append(new Iec61850BdaOptFldsHelper(report.getOptFlds()).getInfo()).append(')')
-        .append(System.lineSeparator());
+                .append(new Iec61850BdaOptFldsHelper(report.getOptFlds()).getInfo()).append(')')
+                .append(System.lineSeparator());
         final DataSet dataSet = report.getDataSet();
         if (dataSet == null) {
             sb.append("\t           DataSet:\tnull").append(System.lineSeparator());
@@ -218,7 +223,7 @@ public class Iec61850ClientLMDEventListener extends Iec61850ClientBaseEventListe
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see
      * org.openmuc.openiec61850.ClientEventListener#associationClosed(java.io
      * .IOException)
