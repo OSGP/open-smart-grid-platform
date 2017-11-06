279d0c6ba5ef9b28fbb7b5028e54f3558683aca7
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2017-11-06 17:16:53 +0100

    SLIM-1382 ~ Adds DTOs for Get M-Bus encryption key status request

---
+++ osgp-dto/src/main/java/com/alliander/osgp/dto/valueobjects/smartmetering/EncryptionKeyStatusTypeDto.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2017 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.alliander.osgp.dto.valueobjects.smartmetering;
+
+public enum EncryptionKeyStatusTypeDto {
+
+    NO_ENCRYPTION_KEY(0),
+    ENCRYPTION_KEY_SET(1),
+    ENCRYPTION_KEY_TRANSFERED(2),
+    ENCRYPTION_KEY_SET_AND_TRANSFERRED(3),
+    ENCRYPTION_KEY_IN_USE(4);
+
+    private int value;
+
+    private EncryptionKeyStatusTypeDto(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
