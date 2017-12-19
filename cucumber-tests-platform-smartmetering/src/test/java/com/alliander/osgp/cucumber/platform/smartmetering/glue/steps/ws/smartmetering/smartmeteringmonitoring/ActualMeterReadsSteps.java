9e8153fbe208005c373c58d9436c8f0bb6a04521
bjornamkreutz <bjorn.amkreutz@cgi.com>
2017-12-19 14:11:56 +0000

    SLIM-1421 Renames classes for in line with other step classes

--- cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringmonitoring/ActualMeterReadsSteps.java
+++ cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringmonitoring/ActualMeterReadsSteps.java
@@ -28,7 +28,7 @@ import com.alliander.osgp.cucumber.platform.smartmetering.support.ws.smartmeteri
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class ActualMeterReads {
+public class ActualMeterReadsSteps {
 
     @Autowired
     private SmartMeteringMonitoringRequestClient<ActualMeterReadsAsyncResponse, ActualMeterReadsRequest> requestClient;
