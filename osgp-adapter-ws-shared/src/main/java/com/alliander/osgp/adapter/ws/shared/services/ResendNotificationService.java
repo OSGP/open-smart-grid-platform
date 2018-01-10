01d37feccb0ddcd2c2c8f274e071346d7c3e56cc
carlogeenen <carlo.geenen@cgi.com>
2018-01-10 13:47:53 +0000

    Made number_of_notifications_send NOT NULL in database, adjusted ResendNotificationService so it will only resend the notification if needed with respect to the properties.

--- osgp-adapter-ws-shared/src/main/java/com/alliander/osgp/adapter/ws/shared/services/ResendNotificationService.java
+++ osgp-adapter-ws-shared/src/main/java/com/alliander/osgp/adapter/ws/shared/services/ResendNotificationService.java
@@ -1,14 +1,14 @@
 package com.alliander.osgp.adapter.ws.shared.services;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import javax.annotation.Resource;
-
+import org.apache.commons.lang3.EnumUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.support.CronSequenceGenerator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,34 +21,58 @@ import com.alliander.osgp.adapter.ws.schema.smartmetering.notification.Notificat
 public class ResendNotificationService {
 
 	@Autowired
-    private int resendNotificationMaximum;
-    
+	private String resendNotificationCronExpression;
+
+	@Autowired
+	private int resendNotificationMultiplier;
+
+	@Autowired
+	private int resendNotificationMaximum;
+
 	@Autowired
 	private NotificationService notificationService;
 
 	@Autowired
 	private ResponseDataRepository responseDataRepository;
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ResendNotificationService.class);
+
 	public void execute() {
 		List<ResponseData> notificationsToResend = new ArrayList<ResponseData>();
 		notificationsToResend = this.responseDataRepository
 				.findByNumberOfNotificationsSendLessThan(this.resendNotificationMaximum);
-
-		// check modified by time with respect to number_of_notifications_send here?
-
 		for (ResponseData responseData : notificationsToResend) {
-			NotificationType notificationType = NotificationType.valueOf(responseData.getMessageType());
-			this.notificationService.sendNotification(responseData.getOrganisationIdentification(),
-					responseData.getDeviceIdentification(), responseData.getResultType().name(),
-					responseData.getCorrelationUid(), getNotificationMessage(responseData.getMessageType()),
-					notificationType);
-			//if getNumberOfNotificationsSend != null, do following, or just fill in 1.
-			responseData.setNumberOfNotificationsSend(responseData.getNumberOfNotificationsSend() + 1);
-			// add +1 to number_of_notifications_send, update modification
-			// time(automatically).
-			this.responseDataRepository.save(responseData);
-		}
+			if (EnumUtils.isValidEnum(NotificationType.class, responseData.getMessageType())) {
+				int multiplier = this.resendNotificationMultiplier;
+				if (responseData.getNumberOfNotificationsSend() == 0) {
+					multiplier = 1;
+				} else {
+					multiplier = responseData.getNumberOfNotificationsSend() * multiplier;
+				}
 
+				Date modificationDate = responseData.getModificationTime();
+				Date currentDate = new Date();
+				long previousModificationTime = currentDate.getTime() - modificationDate.getTime();
+
+				final CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator(
+						this.resendNotificationCronExpression);
+				final Date nextFirstExecutionDate = cronSequenceGenerator.next(currentDate);
+				final Date nextSecondExecutionDate = cronSequenceGenerator.next(nextFirstExecutionDate);
+				final long cronSequenceInterval = nextSecondExecutionDate.getTime() - nextFirstExecutionDate.getTime();
+
+				if ((cronSequenceInterval * multiplier) < previousModificationTime) {
+					LOGGER.info("Found response data for resending notification");
+					NotificationType notificationType = NotificationType.valueOf(responseData.getMessageType());
+					this.notificationService.sendNotification(responseData.getOrganisationIdentification(),
+							responseData.getDeviceIdentification(), responseData.getResultType().name(),
+							responseData.getCorrelationUid(), getNotificationMessage(responseData.getMessageType()),
+							notificationType);
+					LOGGER.info("Notification has been resend");
+					responseData.setNumberOfNotificationsSend(responseData.getNumberOfNotificationsSend() + 1);
+					this.responseDataRepository.save(responseData);
+				}
+			}
+		}
 	}
 
 	private String getNotificationMessage(String responseData) {
--- osgp-adapter-ws-smartmetering/src/main/resources/db/migration/V20180108101800777__Adds_number_of_notifications.sql
+++ osgp-adapter-ws-smartmetering/src/main/resources/db/migration/V20180108101800777__Adds_number_of_notifications.sql
@@ -9,7 +9,7 @@ IF NOT EXISTS (
     AND    table_name  = 'response_data'
     AND	   column_name = 'number_of_notifications_send') THEN
 
-    ALTER TABLE response_data ADD COLUMN number_of_notifications_send smallint DEFAULT(0);
+    ALTER TABLE response_data ADD COLUMN number_of_notifications_send smallint NOT NULL DEFAULT(0);
 
 END IF;
 
