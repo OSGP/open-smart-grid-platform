70b06a933eb70bd29c5b331239b444dca71f8ce9
carlogeenen <carlo.geenen@cgi.com>
2018-01-15 13:15:03 +0000

    Undo pom.xml changes. Added headers in files. Updated restored ResendNotificationConfig.java.

--- osgp-adapter-ws-smartmetering/src/main/java/com/alliander/osgp/adapter/ws/smartmetering/application/config/ResendNotificationConfig.java
+++ osgp-adapter-ws-smartmetering/src/main/java/com/alliander/osgp/adapter/ws/smartmetering/application/config/ResendNotificationConfig.java
@@ -15,51 +15,72 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.alliander.osgp.adapter.ws.shared.services.ResponseDataCleanupJob;
+import com.alliander.osgp.adapter.ws.shared.services.ResendNotificationJob;
 import com.alliander.osgp.shared.application.config.AbstractSchedulingConfig;
 
 @EnableScheduling
 @Configuration
 @PropertySource("classpath:osgp-adapter-ws-smartmetering.properties")
 @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true)
-@PropertySource(value = "file:${osgp/AdapterWsSmartMetering/config}", ignoreResourceNotFound = true)
-public class NotificationConfig extends AbstractSchedulingConfig {
+@PropertySource(value = "file:${osgp/smartmetering/config}", ignoreResourceNotFound = true)
+public class ResendNotificationConfig extends AbstractSchedulingConfig{
 
-    private static final String KEY_RESEND_NOTIFICATION_CRON_EXPRESSION = "smartmetering.scheduling.job.resend.notification.cron.expression";
-    private static final String KEY_RESEMD_NOTIFICATION_THREAD_COUNT = "smartmetering.scheduling.job.resend.notification.thread.count";
-    private static final String KEY_RESEMD_NOTIFICATION_MULTIPLIER = "smartmetering.scheduling.job.resend.notification.multiplier";
-    
-    @Value("${db.driver}")
-    private String databaseDriver;
+	    private static final String KEY_RESEND_NOTIFICATION_CRON_EXPRESSION = "smartmetering.scheduling.job.resend.notification.cron.expression";
+	    private static final String KEY_RESEND_NOTIFICATION_THREAD_COUNT = "smartmetering.scheduling.job.resend.notification.thread.count";
 
-    @Value("${db.password}")
-    private String databasePassword;
+	    @Value("${db.driver}")
+	    private String databaseDriver;
 
-    @Value("${db.protocol}")
-    private String databaseProtocol;
+	    @Value("${db.password}")
+	    private String databasePassword;
 
-    @Value("${db.host}")
-    private String databaseHost;
+	    @Value("${db.protocol}")
+	    private String databaseProtocol;
 
-    @Value("${db.port}")
-    private String databasePort;
+	    @Value("${db.host}")
+	    private String databaseHost;
 
-    @Value("${db.name}")
-    private String databaseName;
+	    @Value("${db.port}")
+	    private String databasePort;
 
-    @Value("${db.username}")
-    private String databaseUsername;
-    
-    
-    
-    @Bean(destroyMethod = "shutdown")
-    public Scheduler resendNotificationScheduler() throws SchedulerException {
-        return this.constructScheduler(ResponseDataCleanupJob.class, KEY_RESEMD_NOTIFICATION_THREAD_COUNT,
-        		KEY_RESEND_NOTIFICATION_CRON_EXPRESSION, this.getDatabaseUrl(), this.databaseUsername, this.databasePassword,
-                this.databaseDriver);
-    }
-    
-    private String getDatabaseUrl() {
-        return this.databaseProtocol + this.databaseHost + ":" + this.databasePort + "/" + this.databaseName;
-    }
+	    @Value("${db.name}")
+	    private String databaseName;
+
+	    @Value("${db.username}")
+	    private String databaseUsername;
+	    
+	    @Value("${smartmetering.scheduling.job.resend.notification.cron.expression}")
+	    private String resendNotificationCronExpression;
+	  
+	    @Bean
+	    public String resendNotificationCronExpression() {
+	        return this.resendNotificationCronExpression;
+	    }
+	    
+	    @Value("${smartmetering.scheduling.job.resend.notification.maximum}")
+	    private int resendNotificationMaximum;
+	  
+	    @Bean
+	    public int resendNotificationMaximum() {
+	        return this.resendNotificationMaximum;
+	    }    
+	    
+	    @Value("${smartmetering.scheduling.job.resend.notification.multiplier}")
+	    private int resendNotificationMultiplier;
+	  
+	    @Bean
+	    public int resendNotificationMultiplier() {
+	        return this.resendNotificationMultiplier;
+	    }
+	    
+	    @Bean(destroyMethod = "shutdown")
+	    public Scheduler resendNotificationScheduler() throws SchedulerException {
+	        return this.constructScheduler(ResendNotificationJob.class, KEY_RESEND_NOTIFICATION_THREAD_COUNT,
+	        		KEY_RESEND_NOTIFICATION_CRON_EXPRESSION, this.getDatabaseUrl(), this.databaseUsername, this.databasePassword,
+	                this.databaseDriver);
+	    }
+	    
+	    private String getDatabaseUrl() {
+	        return this.databaseProtocol + this.databaseHost + ":" + this.databasePort + "/" + this.databaseName;
+	    }
 }
