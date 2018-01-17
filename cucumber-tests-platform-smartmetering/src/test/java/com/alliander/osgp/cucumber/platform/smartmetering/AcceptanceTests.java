99aa6a8ac1c8ed2522c873ee863ea35f92e2501f
carlogeenen <carlo.geenen@cgi.com>
2018-01-17 12:13:46 +0000

    Undo changes.

--- cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/AcceptanceTests.java
+++ cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/AcceptanceTests.java
@@ -15,12 +15,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "classpath:features/functional-exceptions", "classpath:features/osgp-adapter-ws-core",
-		"classpath:features/osgp-adapter-ws-smartmetering" }, tags = { "~@Skip", "~@NightlyBuildOnly" }, glue = {
-				"classpath:com.alliander.osgp.cucumber.platform.glue",
-				"classpath:com.alliander.osgp.cucumber.platform.common.glue",
-				"classpath:com.alliander.osgp.cucumber.platform.smartmetering.glue" }, plugin = { "pretty",
-						"html:target/output/Cucumber-report", "html:target/output/Cucumber-html-report.html",
-						"json:target/output/cucumber.json" }, snippets = SnippetType.CAMELCASE, dryRun = false)
+        "classpath:features/osgp-adapter-ws-smartmetering" }, tags = { "~@Skip", "~@NightlyBuildOnly" }, glue = {
+                "classpath:com.alliander.osgp.cucumber.platform.glue",
+                "classpath:com.alliander.osgp.cucumber.platform.common.glue",
+                "classpath:com.alliander.osgp.cucumber.platform.smartmetering.glue" }, plugin = { "pretty",
+                        "html:target/output/Cucumber-report", "html:target/output/Cucumber-html-report.html",
+                        "json:target/output/cucumber.json" }, snippets = SnippetType.CAMELCASE, dryRun = false)
 public class AcceptanceTests {
 
 }
