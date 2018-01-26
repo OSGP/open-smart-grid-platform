ab957bd8759939ca3a45f1bfab2de1c1b7792570
bjornamkreutz <bjorn.amkreutz@cgi.com>
2018-01-26 12:54:59 +0000

    ZS-302 Corrects typo

--- osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/reporting/Iec61850ClientLMDEventListener.java
+++ osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/reporting/Iec61850ClientLMDEventListener.java
@@ -190,15 +190,6 @@ public class Iec61850ClientLMDEventListener extends Iec61850ClientBaseEventListe
         } else {
             sb.append("\t           DataSet:\t").append(report.getDataSetRef()).append(System.lineSeparator());
             this.handleDataSetMembers(dataSetMembers, sb);
-            if (!dataSetMembers.isEmpty()) {
-                sb.append("\t   DataSet members:\t").append(dataSetMembers.size()).append(System.lineSeparator());
-                for (final FcModelNode member : dataSetMembers) {
-                    sb.append("\t            member:\t").append(member).append(System.lineSeparator());
-                    if (member.getReference().toString().contains("CSLC.EvnRpn")) {
-                        sb.append(this.evnRpnInfo("\t                   \t\t", member));
-                    }
-                }
-            }
         }
         this.logger.info(sb.append(System.lineSeparator()).toString());
     }
