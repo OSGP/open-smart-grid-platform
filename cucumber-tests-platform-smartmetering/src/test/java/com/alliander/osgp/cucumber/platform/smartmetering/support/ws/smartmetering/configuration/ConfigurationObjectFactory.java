616305dbd79d2ccf7d011e06654a563d73566e9c
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2018-02-20 14:21:29 +0000

    SLIM-1626 Adds ConfigurationObject factory method

--- cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/support/ws/smartmetering/configuration/ConfigurationObjectFactory.java
+++ cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/support/ws/smartmetering/configuration/ConfigurationObjectFactory.java
@@ -8,8 +8,12 @@
 package com.alliander.osgp.cucumber.platform.smartmetering.support.ws.smartmetering.configuration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import org.springframework.util.CollectionUtils;
 
 import com.alliander.osgp.adapter.ws.schema.smartmetering.configuration.ConfigurationFlag;
 import com.alliander.osgp.adapter.ws.schema.smartmetering.configuration.ConfigurationFlagType;
@@ -24,6 +28,36 @@ public class ConfigurationObjectFactory {
 
     private ConfigurationObjectFactory() {
         // Private constructor for utility class
+    }
+
+    public static ConfigurationObject forGprsOperationModeAndFlags(final GprsOperationModeType gprsOperationMode,
+            final Set<ConfigurationFlagType> enableFlags, final Set<ConfigurationFlagType> disableFlags) {
+
+        final ConfigurationObject configurationObject = new ConfigurationObject();
+        configurationObject.setGprsOperationMode(gprsOperationMode);
+        final List<ConfigurationFlag> configurationFlagList = new ArrayList<>();
+        configurationFlagList.addAll(getFlags(enableFlags, true));
+        configurationFlagList.addAll(getFlags(disableFlags, false));
+        if (!configurationFlagList.isEmpty()) {
+            final ConfigurationFlags configurationFlags = new ConfigurationFlags();
+            configurationFlags.getConfigurationFlag().addAll(configurationFlagList);
+            configurationObject.setConfigurationFlags(configurationFlags);
+        }
+        return configurationObject;
+    }
+
+    private static List<ConfigurationFlag> getFlags(final Set<ConfigurationFlagType> flagTypes, final boolean enabled) {
+        if (CollectionUtils.isEmpty(flagTypes)) {
+            return Collections.emptyList();
+        }
+        final List<ConfigurationFlag> configurationFlags = new ArrayList<>();
+        for (final ConfigurationFlagType flagType : flagTypes) {
+            final ConfigurationFlag configurationFlag = new ConfigurationFlag();
+            configurationFlag.setConfigurationFlagType(flagType);
+            configurationFlag.setEnabled(enabled);
+            configurationFlags.add(configurationFlag);
+        }
+        return configurationFlags;
     }
 
     public static ConfigurationObject fromParameterMap(final Map<String, String> requestParameters) {
