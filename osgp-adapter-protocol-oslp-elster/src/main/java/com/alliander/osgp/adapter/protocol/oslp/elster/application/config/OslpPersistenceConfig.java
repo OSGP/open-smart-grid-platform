a76a5b4d0a25527c4af0b5dd108cac07e9506f20
Robin den Adel <robin.den.adel@cgi.com>
2018-03-05 15:55:32 +0100

    OC-317: Uses the abstract persistence config class from Shared.

--- osgp-adapter-protocol-oslp-elster/src/main/java/com/alliander/osgp/adapter/protocol/oslp/elster/application/config/OslpPersistenceConfig.java
+++ osgp-adapter-protocol-oslp-elster/src/main/java/com/alliander/osgp/adapter/protocol/oslp/elster/application/config/OslpPersistenceConfig.java
@@ -7,67 +7,56 @@
  */
 package com.alliander.osgp.adapter.protocol.oslp.elster.application.config;
 
-import java.util.Properties;
-
 import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
 
-import org.hibernate.ejb.HibernatePersistence;
 import org.jboss.netty.logging.InternalLoggerFactory;
 import org.jboss.netty.logging.Slf4JLoggerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alliander.osgp.adapter.protocol.oslp.elster.domain.repositories.OslpDeviceRepository;
-import com.alliander.osgp.adapter.protocol.oslp.elster.exceptions.ProtocolAdapterException;
-import com.alliander.osgp.shared.application.config.AbstractConfig;
+import com.alliander.osgp.shared.application.config.AbstractPersistenceConfig;
 import com.alliander.osgp.shared.infra.db.DefaultConnectionPoolFactory;
 import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * An application context Java configuration class.
  */
-@EnableJpaRepositories(entityManagerFactoryRef = "oslpEntityManagerFactory", basePackageClasses = { OslpDeviceRepository.class })
+@EnableJpaRepositories(entityManagerFactoryRef = "oslpEntityManagerFactory", basePackageClasses = {
+        OslpDeviceRepository.class })
 @Configuration
 @EnableTransactionManagement()
-@PropertySources({ @PropertySource("classpath:osgp-adapter-protocol-oslp-elster.properties"),
-    @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true),
-    @PropertySource(value = "file:${osgp/AdapterProtocolOslpElster/config}", ignoreResourceNotFound = true), })
-public class OslpPersistenceConfig extends AbstractConfig {
+@PropertySource("classpath:osgp-adapter-protocol-oslp-elster.properties")
+@PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true)
+@PropertySource(value = "file:${osgp/AdapterProtocolOslpElster/config}", ignoreResourceNotFound = true)
+public class OslpPersistenceConfig extends AbstractPersistenceConfig {
 
-    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username.oslp";
-    private static final String PROPERTY_NAME_DATABASE_PW = "db.password.oslp";
+    @Value("${db.username.oslp}")
+    private String username;
 
-    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
-    private static final String PROPERTY_NAME_DATABASE_PROTOCOL = "db.protocol";
+    @Value("${db.password.oslp}")
+    private String password;
 
-    private static final String PROPERTY_NAME_DATABASE_HOST = "db.host.oslp";
-    private static final String PROPERTY_NAME_DATABASE_PORT = "db.port.oslp";
-    private static final String PROPERTY_NAME_DATABASE_NAME = "db.name.oslp";
+    @Value("${db.host.oslp}")
+    private String databaseHost;
 
-    private static final String PROPERTY_NAME_DATABASE_MIN_POOL_SIZE = "db.min_pool_size";
-    private static final String PROPERTY_NAME_DATABASE_MAX_POOL_SIZE = "db.max_pool_size";
-    private static final String PROPERTY_NAME_DATABASE_AUTO_COMMIT = "db.auto_commit";
-    private static final String PROPERTY_NAME_DATABASE_IDLE_TIMEOUT = "db.idle_timeout";
+    @Value("${db.port.oslp}")
+    private int databasePort;
 
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
-    private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+    @Value("${db.name.oslp}")
+    private String databaseName;
 
-    private static final String PROPERTY_NAME_OSLP_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan.oslp";
+    @Value("${entitymanager.packages.to.scan.oslp}")
+    private String entitymanagerPackagesToScan;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OslpPersistenceConfig.class);
-
-    private HikariDataSource dataSource;
+    private HikariDataSource dataSourceOslp;
 
     public OslpPersistenceConfig() {
         InternalLoggerFactory.setDefaultFactory(new Slf4JLoggerFactory());
@@ -78,98 +67,35 @@ public class OslpPersistenceConfig extends AbstractConfig {
      *
      * @return DataSource
      */
-    public DataSource getOslpDataSource() {
-        if (this.dataSource == null) {
-            final String username = this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME);
-            final String password = this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_PW);
-
-            final String driverClassName = this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER);
-            final String databaseProtocol = this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_PROTOCOL);
-
-            final String databaseHost = this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_HOST);
-            final int databasePort = Integer
-                    .parseInt(this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_PORT));
-            final String databaseName = this.environment.getRequiredProperty(PROPERTY_NAME_DATABASE_NAME);
-
-            final int minPoolSize = Integer.parseInt(this.environment
-                    .getRequiredProperty(PROPERTY_NAME_DATABASE_MIN_POOL_SIZE));
-            final int maxPoolSize = Integer.parseInt(this.environment
-                    .getRequiredProperty(PROPERTY_NAME_DATABASE_MAX_POOL_SIZE));
-            final boolean isAutoCommit = Boolean.parseBoolean(this.environment
-                    .getRequiredProperty(PROPERTY_NAME_DATABASE_AUTO_COMMIT));
-            final int idleTimeout = Integer.parseInt(this.environment
-                    .getRequiredProperty(PROPERTY_NAME_DATABASE_IDLE_TIMEOUT));
-
-            final DefaultConnectionPoolFactory.Builder builder = new DefaultConnectionPoolFactory.Builder()
-            .withUsername(username).withPassword(password).withDriverClassName(driverClassName)
-            .withProtocol(databaseProtocol).withDatabaseHost(databaseHost).withDatabasePort(databasePort)
-            .withDatabaseName(databaseName).withMinPoolSize(minPoolSize).withMaxPoolSize(maxPoolSize)
-            .withAutoCommit(isAutoCommit).withIdleTimeout(idleTimeout);
+    public DataSource getDataSourceOslp() {
+        if (this.dataSourceOslp == null) {
+            final DefaultConnectionPoolFactory.Builder builder = super.builder().withUsername(this.username)
+                    .withPassword(this.password).withDatabaseHost(this.databaseHost).withDatabasePort(this.databasePort)
+                    .withDatabaseName(this.databaseName);
             final DefaultConnectionPoolFactory factory = builder.build();
-            this.dataSource = factory.getDefaultConnectionPool();
+            this.dataSourceOslp = factory.getDefaultConnectionPool();
         }
-        return this.dataSource;
+        return this.dataSourceOslp;
     }
 
-    /**
-     * Method for creating the Transaction Manager.
-     *
-     * @return JpaTransactionManager
-     * @throws ClassNotFoundException
-     *             when class not found
-     */
+    @Override
     @Bean
-    public JpaTransactionManager transactionManager() throws ProtocolAdapterException {
-        final JpaTransactionManager transactionManager = new JpaTransactionManager();
-
-        try {
-            transactionManager.setEntityManagerFactory(this.oslpEntityManagerFactory().getObject());
-            transactionManager.setTransactionSynchronization(JpaTransactionManager.SYNCHRONIZATION_ALWAYS);
-        } catch (final ClassNotFoundException e) {
-            final String msg = "Error in creating transaction manager bean";
-            LOGGER.error(msg, e);
-            throw new ProtocolAdapterException(msg, e);
-        }
-
-        return transactionManager;
+    public JpaTransactionManager transactionManager() {
+        return super.transactionManager();
     }
 
-    /**
-     * Method for creating the Entity Manager Factory Bean.
-     *
-     * @return LocalContainerEntityManagerFactoryBean
-     * @throws ClassNotFoundException
-     *             when class not found
-     */
-    @Bean
-    public LocalContainerEntityManagerFactoryBean oslpEntityManagerFactory() throws ClassNotFoundException {
-        final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-
-        entityManagerFactoryBean.setPersistenceUnitName("OSGP_PROTOCOL_ADAPTER_OSLP_SETTINGS");
-        entityManagerFactoryBean.setDataSource(this.getOslpDataSource());
-        entityManagerFactoryBean.setPackagesToScan(this.environment
-                .getRequiredProperty(PROPERTY_NAME_OSLP_ENTITYMANAGER_PACKAGES_TO_SCAN));
-        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
-
-        final Properties jpaProperties = new Properties();
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_DIALECT,
-                this.environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL,
-                this.environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY,
-                this.environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL,
-                this.environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-
-        entityManagerFactoryBean.setJpaProperties(jpaProperties);
-
-        return entityManagerFactoryBean;
+    @Override
+    @Bean(name = "oslpEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        return super.entityManagerFactory("OSGP_PROTOCOL_ADAPTER_OSLP_SETTINGS", this.getDataSourceOslp(),
+                this.entitymanagerPackagesToScan);
     }
 
+    @Override
     @PreDestroy
     public void destroyDataSource() {
-        if (this.dataSource != null) {
-            this.dataSource.close();
+        if (this.dataSourceOslp != null) {
+            this.dataSourceOslp.close();
         }
     }
 
