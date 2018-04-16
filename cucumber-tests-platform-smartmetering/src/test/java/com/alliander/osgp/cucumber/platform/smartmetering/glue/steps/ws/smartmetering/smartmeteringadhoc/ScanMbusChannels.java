92d5bac60ef005f6395910d0cd4b62c36224757b
carlogeenen <carlo.geenen@cgi.com>
2018-04-16 14:06:21 +0200

    Created initial test scenario implementation.

---
+++ cucumber-tests-platform-smartmetering/src/test/java/com/alliander/osgp/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringadhoc/ScanMbusChannels.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2018 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.alliander.osgp.cucumber.platform.smartmetering.glue.steps.ws.smartmetering.smartmeteringadhoc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alliander.osgp.adapter.ws.schema.smartmetering.adhoc.ScanMbusChannelsAsyncRequest;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.adhoc.ScanMbusChannelsAsyncResponse;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.adhoc.ScanMbusChannelsRequest;
+import com.alliander.osgp.adapter.ws.schema.smartmetering.adhoc.ScanMbusChannelsResponse;
+import com.alliander.osgp.cucumber.core.ScenarioContext;
+import com.alliander.osgp.cucumber.platform.PlatformKeys;
+import com.alliander.osgp.cucumber.platform.smartmetering.PlatformSmartmeteringKeys;
+import com.alliander.osgp.cucumber.platform.smartmetering.support.ws.smartmetering.adhoc.ScanMbusChannelsRequestFactory;
+import com.alliander.osgp.cucumber.platform.smartmetering.support.ws.smartmetering.adhoc.SmartMeteringAdHocRequestClient;
+import com.alliander.osgp.cucumber.platform.smartmetering.support.ws.smartmetering.adhoc.SmartMeteringAdHocResponseClient;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ScanMbusChannels {
+
+    @Autowired
+    private SmartMeteringAdHocRequestClient<ScanMbusChannelsAsyncResponse, ScanMbusChannelsRequest> requestClient;
+
+    @Autowired
+    private SmartMeteringAdHocResponseClient<ScanMbusChannelsResponse, ScanMbusChannelsAsyncRequest> responseClient;
+
+    @When("^the scan M-Bus channels request is received$")
+    public void theScanMBusChannelsRequestIsReceived(final Map<String, String> settings) throws Throwable {
+
+        final ScanMbusChannelsRequest request = ScanMbusChannelsRequestFactory.fromParameterMap(settings);
+        final ScanMbusChannelsAsyncResponse asyncResponse = this.requestClient.doRequest(request);
+
+        assertNotNull("AsyncResponse should not be null", asyncResponse);
+        ScenarioContext.current().put(PlatformKeys.KEY_CORRELATION_UID, asyncResponse.getCorrelationUid());
+    }
+
+    @Then("^the found M-bus devices are in the response$")
+    public void theFoundMBusDevicesAreInTheResponse(final Map<String, String> settings) throws Throwable {
+        final ScanMbusChannelsAsyncRequest asyncRequest = ScanMbusChannelsRequestFactory.fromScenarioContext();
+        final ScanMbusChannelsResponse response = this.responseClient.getResponse(asyncRequest);
+
+        // TODO: Assert
+        assertEquals("Result is not as expected.", settings.get(PlatformSmartmeteringKeys.RESULT),
+                response.getResult().name());
+        final String actual = response.getResultString();
+        assertTrue("Result contains no data.", StringUtils.isNotBlank(actual));
+        final String expected = settings.get(PlatformSmartmeteringKeys.RESPONSE_PART);
+        assertTrue("Result data is not as expected; expected '" + expected + "' to be part of '" + actual + "'",
+                actual.contains(expected));
+    }
+
+}
---
+++ cucumber-tests-platform-smartmetering/src/test/resources/features/osgp-adapter-ws-smartmetering/ScanMbusChannels.feature
@@ -0,0 +1,27 @@
+@SmartMetering @Platform
+Feature: SmartMetering Scan M-Bus Channels
+  As a grid operator
+  I want to be able to scan the M-Bus channels 
+  So I can use the outcome in my installation flow
+@test
+  Scenario: scan the four m-bus channels of an dlms gateway device
+    Given a dlms device
+      | DeviceIdentification | TEST1024000000001 |
+      | DeviceType           | SMART_METER_E     |
+    And a dlms device
+      | DeviceIdentification           | TESTG102400000001 |
+      | DeviceType                     | SMART_METER_G     |
+      | GatewayDeviceIdentification    | TEST1024000000001 |
+      | Channel                        |                 1 |
+      | MbusIdentificationNumber       |          12056731 |
+      | MbusManufacturerIdentification | LGB               |
+      | MbusVersion                    |                66 |
+      | MbusDeviceTypeIdentification   |                 3 |
+    When the scan M-Bus channels request is received
+      | DeviceIdentification | TEST1024000000001 |
+    Then the found M-bus devices are in the response
+      | DeviceIdentification             | TEST1024000000001 |
+      | Channel1MbusIdentificationNumber |          12056731 |
+      | Channel2MbusIdentificationNumber |                   |
+      | Channel3MbusIdentificationNumber |                   |
+      | Channel4MbusIdentificationNumber |                   |
