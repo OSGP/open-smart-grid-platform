c40fbcbc39804330e06728f6068b62fd20c2464d
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2018-08-03 17:07:01 +0200

    Fixes tests

--- cucumber-tests-core/src/test/java/org/opensmartgridplatform/cucumber/core/ApplicationContext.java
+++ cucumber-tests-core/src/test/java/org/opensmartgridplatform/cucumber/core/ApplicationContext.java
@@ -2,7 +2,7 @@ package org.opensmartgridplatform.cucumber.core;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"org.opensmartgridplatform.cucumber"})
+@ComponentScan(basePackages = { "org.opensmartgridplatform.cucumber", "com.alliander.osgp.cucumber" })
 public class ApplicationContext {
 
 }
