6e5d224b8dfcecb0d82bf07c654527f7a01bba68
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2018-10-29 11:55:21 +0100

    OC-478 ~ Adds sonar preview analysis to pipeline

--- jenkins/pipeline/osgp-pr-build.groovy
+++ jenkins/pipeline/osgp-pr-build.groovy
@@ -79,6 +79,16 @@ pipeline {
             }
         }
 
+        stage ('Sonar Analysis') {
+            steps {
+                withSonarQubeEnv('SonarQube local') {
+                    sh "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar -Dmaven.test.failure.ignore=true -Dclirr=true " +
+                          "-Dsonar.github.repository=OSGP/open-smart-grid-platform -Dsonar.analysis.mode=preview " +
+                          "-Dsonar.issuesReport.console.enable=true -Dsonar.forceUpdate=true -Dsonar.github.pullRequest=$ghprbPullId " 
+                }
+            }
+        }
+
         stage ('Deploy local artifacts') {
             steps {
                 // Deploy stream specific system using the local artifacts from /data/software/artifacts on the system
