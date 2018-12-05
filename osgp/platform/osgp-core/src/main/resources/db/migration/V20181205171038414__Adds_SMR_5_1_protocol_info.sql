adb6dc20a20feadcdc0b6a8f7f1739ad0703f2e6
Rob van Maris <commit@robvanmaris.nl>
2018-12-05 17:34:05 +0100

    SLIM-1853: Add SMR 5.1 to protocol_info table in the osgp_core database.

---
+++ osgp/platform/osgp-core/src/main/resources/db/migration/V20181205171038414__Adds_SMR_5_1_protocol_info.sql
@@ -0,0 +1,39 @@
+DO
+$$
+BEGIN
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM   protocol_info
+    WHERE  protocol = 'SMR'
+    AND    protocol_version  = '5.1') THEN
+
+INSERT INTO protocol_info(
+  id,
+  creation_time,
+	modification_time,
+	version,
+	protocol,
+	protocol_version,
+	outgoing_protocol_requests_queue,
+	incoming_protocol_responses_queue,
+	incoming_protocol_requests_queue,
+	outgoing_protocol_responses_queue,
+	parallel_requests_allowed)
+VALUES (
+  nextval('protocol_info_id_seq'),
+	'2018-12-10 00:00:00',
+	'2018-12-10 00:00:00',
+	0,
+	'SMR',
+	'5.1',
+	'protocol-dlms.1_0.osgp-core.1_0.requests',
+	'osgp-core.1_0.protocol-dlms.1_0.responses',
+	'osgp-core.1_0.protocol-dlms.1_0.requests',
+	'protocol-dlms.1_0.osgp-core.1_0.responses',
+	true);
+
+END IF;
+
+END;
+$$
