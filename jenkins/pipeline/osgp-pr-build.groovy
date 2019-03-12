d4ae08979e454cb97bdd3d6d878b2ce0f2eed908
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2019-03-12 11:42:35 +0100

    Adds copy step to pr build script

--- jenkins/pipeline/osgp-pr-build.groovy
+++ jenkins/pipeline/osgp-pr-build.groovy
@@ -81,7 +81,9 @@ pipeline {
                 // Download missing artifacts from artifactory for the same version
                 // - The following artifacts are not in this repository
                 sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ configuration_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
-                sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ dlms_simulator_artifacts_pr }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
+                sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ dlms_simulator_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
+                // Make sure a standalone version of the dlms device simulator is present
+                sh "cp -p target/artifacts/dlms-device-simulator-${POMVERSION}.jar target/artifacts/dlms-device-simulator-${POMVERSION}-standalone.jar"
                 // - The following artifacts are not specified in the root pom.xml, thus they should be retrieved from the artifactory.
                 sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ distribution_automation_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
                 sh "cd release && plays/download-artifacts.yml -e artifactstodownload='{{ iec61850_simulator_artifacts }}' -e deployment_type=snapshot -e osgp_version=${POMVERSION} -e tmp_artifacts_directory=../../target/artifacts"
