bf07cb5f379a2c393824f6eb7cb91d6d428809ba
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2019-03-12 12:59:48 +0100

    OC-504 ~ Resets release branch to master

--- jenkins/pipeline/osgp-pr-build.groovy
+++ jenkins/pipeline/osgp-pr-build.groovy
@@ -5,7 +5,7 @@ def servername = stream + '-at-pr-' + env.BUILD_NUMBER
 def playbook = stream + '-at.yml'
 
 // Choose the branch to use for SmartSocietyServices/release repository. Default value is 'master'.
-def branchReleaseRepo = 'fix-pr-build'
+def branchReleaseRepo = 'master'
 
 pipeline {
     agent {
