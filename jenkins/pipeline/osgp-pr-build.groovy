76a806e03530a39990f4da5bea7e4373b1fd6a84
Rob van Maris <commit@robvanmaris.nl>
2019-03-12 12:59:20 +0100

    SLIM-1873: Try to fix pr build.

--- jenkins/pipeline/osgp-pr-build.groovy
+++ jenkins/pipeline/osgp-pr-build.groovy
@@ -5,7 +5,7 @@ def servername = stream + '-at-pr-' + env.BUILD_NUMBER
 def playbook = stream + '-at.yml'
 
 // Choose the branch to use for SmartSocietyServices/release repository. Default value is 'master'.
-def branchReleaseRepo = 'master'
+def branchReleaseRepo = 'fix-pr-build'
 
 pipeline {
     agent {
