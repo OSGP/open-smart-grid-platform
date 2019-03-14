d381edd4eaf32750c505825088d0b504d23c981a
Ruud Lemmers <ruud.lemmers@cgi.com>
2019-03-14 15:50:43 +0000

    OC-504 ~ Removes commented line

--- osgp/protocol-adapter-iec60870/osgp-protocol-simulator-iec60870/src/test/java/org/opensmartgridplatform/simulator/protocol/iec60870/server/Iec60870ASduHandlerRegistryTests.java
+++ osgp/protocol-adapter-iec60870/osgp-protocol-simulator-iec60870/src/test/java/org/opensmartgridplatform/simulator/protocol/iec60870/server/Iec60870ASduHandlerRegistryTests.java
@@ -17,7 +17,6 @@ import org.opensmartgridplatform.simulator.protocol.iec60870.server.handlers.Iec
 import org.opensmartgridplatform.simulator.protocol.iec60870.server.handlers.Iec60870SingleCommandASduHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
