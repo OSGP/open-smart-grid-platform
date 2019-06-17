5d8cf05626da6ed9d48d80eb0d80a5611b340278
stefanermens <stefan.ermens@alliander.com>
2019-06-17 16:28:49 +0200

    SLIM-1988 Add test for DlmsPushNotificationDecoder

---
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/networking/DlmsPushNotificationDecoderTest.java
@@ -0,0 +1,136 @@
+package org.opensmartgridplatform.adapter.protocol.dlms.infra.networking;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmTypeDto.COMMUNICATION_ERROR_M_BUS_CHANNEL_4;
+import static org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmTypeDto.FRAUD_ATTEMPT_M_BUS_CHANNEL_1;
+import static org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmTypeDto.REPLACE_BATTERY;
+
+import java.util.Set;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opensmartgridplatform.adapter.protocol.dlms.infra.networking.DlmsPushNotificationDecoder.DecodingState;
+import org.opensmartgridplatform.dlms.DlmsPushNotification;
+import org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmTypeDto;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DlmsPushNotificationDecoderTest {
+
+    private static final int EQUIPMENT_IDENTIFIER_LENGTH = 17;
+    private static final int COMMA_LENGTH = 1;
+
+    private static final String PUSH_SCHEDULER_TRIGGER = "Push scheduler";
+    private static final String PUSH_ALARM_TRIGGER = "Push alarm monitor";
+
+    private static final String IDENTIFIER = "EXXXX123456789012";
+    private static final byte COMMA = 0x2C;
+
+    private DlmsPushNotificationDecoder decoder;
+
+    @Mock
+    ChannelHandlerContext ctx;
+
+    @Mock
+    Channel channel;
+
+    @Test
+    public void decodeDsmr4AlarmsWithLogicalName() throws UnknownDecodingStateException,
+            UnrecognizedMessageDataException {
+
+        // SETUP
+
+        decoder = new DlmsPushNotificationDecoder();
+
+        ChannelBuffer buffer = mock(ChannelBuffer.class);
+        DecodingState state = DecodingState.EQUIPMENT_IDENTIFIER;
+
+        // Set logical name of Push schedule object. (Note: for this test any logical name should work)
+        final byte[] logicalName = new byte[] { 0x00, 0x00, 0x0F, 0x00, 0x04, (byte)0xFF };
+
+        byte[] bytes = setupDsmr4Buffer(buffer, IDENTIFIER, logicalName);
+
+        // CALL
+
+        Object pushNotificationObject = decoder.decode(ctx, channel, buffer, state);
+
+        // VERIFY
+
+        assertThat(pushNotificationObject instanceof DlmsPushNotification).isTrue();
+        DlmsPushNotification dlmsPushNotification = (DlmsPushNotification) pushNotificationObject;
+        assertThat(dlmsPushNotification.getEquipmentIdentifier()).isEqualTo(IDENTIFIER);
+        assertThat(dlmsPushNotification.getTriggerType()).isEqualTo(PUSH_SCHEDULER_TRIGGER);
+        assertThat(dlmsPushNotification.getAlarms().isEmpty()).isTrue();
+        assertThat(dlmsPushNotification.toByteArray()).isEqualTo(bytes);
+    }
+
+    @Test
+    public void decodeDsmr4AlarmsWithAlarmRegister() throws UnknownDecodingStateException,
+            UnrecognizedMessageDataException {
+
+        // SETUP
+
+        decoder = new DlmsPushNotificationDecoder();
+
+        ChannelBuffer buffer = mock(ChannelBuffer.class);
+        DecodingState state = DecodingState.EQUIPMENT_IDENTIFIER;
+
+        // Create alarm register with 3 alarms: replace battery and 2 mbus alarms
+        final byte[] alarmRegister = new byte[] { 0x00, 0x18, 0x00, 0x02 };
+
+        byte[] bytes = setupDsmr4Buffer(buffer, IDENTIFIER, alarmRegister);
+
+        // CALL
+
+        Object pushNotificationObject = decoder.decode(ctx, channel, buffer, state);
+
+        // VERIFY
+
+        assertThat(pushNotificationObject instanceof DlmsPushNotification).isTrue();
+        DlmsPushNotification dlmsPushNotification = (DlmsPushNotification) pushNotificationObject;
+        assertThat(dlmsPushNotification.getEquipmentIdentifier()).isEqualTo(IDENTIFIER);
+        assertThat(dlmsPushNotification.getTriggerType()).isEqualTo(PUSH_ALARM_TRIGGER);
+        assertThat(dlmsPushNotification.toByteArray()).isEqualTo(bytes);
+
+        Set<AlarmTypeDto> alarms = dlmsPushNotification.getAlarms();
+        assertThat(alarms.size()).isEqualTo(3);
+        assertThat(alarms.contains(REPLACE_BATTERY)).isTrue();
+        assertThat(alarms.contains(COMMUNICATION_ERROR_M_BUS_CHANNEL_4)).isTrue();
+        assertThat(alarms.contains(FRAUD_ATTEMPT_M_BUS_CHANNEL_1)).isTrue();
+    }
+
+    private byte[] setupDsmr4Buffer(ChannelBuffer buffer, String identifier, byte[] data) {
+
+        final byte[] bytes = new byte[EQUIPMENT_IDENTIFIER_LENGTH + COMMA_LENGTH + data.length] ;
+
+        System.arraycopy(identifier.getBytes(), 0, bytes, 0, EQUIPMENT_IDENTIFIER_LENGTH);
+        System.arraycopy(new byte[] { COMMA }, 0, bytes, EQUIPMENT_IDENTIFIER_LENGTH, COMMA_LENGTH);
+        System.arraycopy(data, 0, bytes, EQUIPMENT_IDENTIFIER_LENGTH + COMMA_LENGTH, data.length);
+
+        doAnswer(invocation-> {
+            byte[] outputValue = (byte[])invocation.getArguments()[0];
+            System.arraycopy(bytes, 0, outputValue, 0, EQUIPMENT_IDENTIFIER_LENGTH + 1);
+            return null;
+        }).when(buffer).readBytes(any(byte[].class), eq(0), eq(EQUIPMENT_IDENTIFIER_LENGTH + 1));
+
+        doAnswer(invocation-> {
+            byte[] outputValue = (byte[])invocation.getArguments()[0];
+            System.arraycopy(bytes, EQUIPMENT_IDENTIFIER_LENGTH + 1, outputValue, 0, data.length);
+            return null;
+        }).when(buffer).readBytes(any(byte[].class), eq(0), eq(data.length));
+
+        when(buffer.writerIndex()).thenReturn(bytes.length);
+        when(buffer.readerIndex()).thenReturn(EQUIPMENT_IDENTIFIER_LENGTH + 1);
+
+        return bytes;
+    }
+}
