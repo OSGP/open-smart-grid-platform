a41362212b19ecee113abb5764397775dff0de4d
Kevin Smeets <kevin.smeets@cgi.com>
2019-06-26 16:11:33 +0000

    FLEX-4911 ~ Copies from FLEX-4908 feature branch.

--- integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/glue/steps/database/logging/DeviceLogItemSteps.java
+++ integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/glue/steps/database/logging/DeviceLogItemSteps.java
@@ -10,21 +10,55 @@ package org.opensmartgridplatform.cucumber.platform.glue.steps.database.logging;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.opensmartgridplatform.cucumber.core.ReadSettingsHelper.getString;
 
 import java.util.List;
+import java.util.Map;
 
-import cucumber.api.java.en.Then;
 import org.opensmartgridplatform.cucumber.core.GlueBase;
+import org.opensmartgridplatform.cucumber.platform.PlatformDefaults;
+import org.opensmartgridplatform.cucumber.platform.PlatformKeys;
 import org.opensmartgridplatform.logging.domain.entities.DeviceLogItem;
 import org.opensmartgridplatform.logging.domain.repositories.DeviceLogItemPagingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
 public class DeviceLogItemSteps extends GlueBase {
 
     @Autowired
     private DeviceLogItemPagingRepository deviceLogItemRepository;
+
+    @Given("^I have a device log item$")
+    public void iHaveADeviceLogItem(final Map<String, String> settings) {
+
+        final String deviceIdentification = getString(settings, PlatformKeys.KEY_DEVICE_IDENTIFICATION);
+        final String deviceUid = getString(settings, PlatformKeys.KEY_DEVICE_UID, PlatformDefaults.DEVICE_UID);
+        final String decodedMessage = "O S L P";
+        final String encodedMessage = "0x4F 0x53 0x4C 0x50";
+        final boolean incoming = true;
+        final String organisationIdentification = getString(settings, PlatformKeys.KEY_ORGANIZATION,
+                PlatformDefaults.DEFAULT_ORGANIZATION_IDENTIFICATION);
+        final Integer payloadMessageSerializedSize = 4;
+        final boolean valid = true;
+
+        final DeviceLogItem deviceLogItem = new DeviceLogItem.Builder().withDeviceIdentification(deviceIdentification)
+                .withDeviceUid(deviceUid).withDecodedMessage(decodedMessage).withEncodedMessage(encodedMessage)
+                .withIncoming(incoming).withOrganisationIdentification(organisationIdentification)
+                .withPayloadMessageSerializedSize(payloadMessageSerializedSize).withValid(valid).build();
+
+        this.deviceLogItemRepository.save(deviceLogItem);
+    }
+
+    @Given("^I have (\\d+) device log items$")
+    public void iHaveDeviceLogItems(final int number, final Map<String, String> settings) {
+        for (int i = 0; i < number; i++) {
+            this.iHaveADeviceLogItem(settings);
+        }
+    }
 
     @Then("^the get administrative status communication for device \"([^\"]*)\" should be in the device_log_item "
             + "table$")
@@ -32,8 +66,7 @@ public class DeviceLogItemSteps extends GlueBase {
             final String deviceIdentification) {
 
         final Pageable pageable = new PageRequest(0, Integer.MAX_VALUE);
-        final List<org.opensmartgridplatform.logging.domain.entities.DeviceLogItem> deviceLogItems =
-                this.deviceLogItemRepository
+        final List<org.opensmartgridplatform.logging.domain.entities.DeviceLogItem> deviceLogItems = this.deviceLogItemRepository
                 .findByDeviceIdentification(deviceIdentification, pageable).getContent();
         assertTrue("number of device log items for " + deviceIdentification,
                 this.countGetAdministrativeStatusLogItems(deviceLogItems) > 0);
@@ -45,8 +78,7 @@ public class DeviceLogItemSteps extends GlueBase {
             final String deviceIdentification) {
 
         final Pageable pageable = new PageRequest(0, Integer.MAX_VALUE);
-        final List<org.opensmartgridplatform.logging.domain.entities.DeviceLogItem> deviceLogItems =
-                this.deviceLogItemRepository
+        final List<org.opensmartgridplatform.logging.domain.entities.DeviceLogItem> deviceLogItems = this.deviceLogItemRepository
                 .findByDeviceIdentification(deviceIdentification, pageable).getContent();
         assertEquals("number of device log items for " + deviceIdentification, 0,
                 this.countGetAdministrativeStatusLogItems(deviceLogItems));
@@ -58,7 +90,7 @@ public class DeviceLogItemSteps extends GlueBase {
     }
 
     private boolean isGetAdministrativeStatusLogItem(final DeviceLogItem deviceLogItem) {
-        return deviceLogItem.getEncodedMessage() != null && deviceLogItem.getDecodedMessage()
-                .contains("GET_ADMINISTRATIVE_STATUS");
+        return deviceLogItem.getEncodedMessage() != null
+                && deviceLogItem.getDecodedMessage().contains("GET_ADMINISTRATIVE_STATUS");
     }
 }
