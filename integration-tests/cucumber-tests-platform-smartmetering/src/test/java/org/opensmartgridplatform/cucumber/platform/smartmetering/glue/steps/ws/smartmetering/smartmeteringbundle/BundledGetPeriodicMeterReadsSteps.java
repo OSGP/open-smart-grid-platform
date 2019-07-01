288423f404ee796b304db243e03c739ba9483b4f
Coen Damen <coen.damen@alliander.com>
2019-07-01 08:20:03 +0200

    SLIM-1993 added logging to failing cucumber test

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringbundle/BundledGetPeriodicMeterReadsSteps.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringbundle/BundledGetPeriodicMeterReadsSteps.java
@@ -1,25 +1,28 @@
 /**
  * Copyright 2017 Smart Society Services B.V.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 package org.opensmartgridplatform.cucumber.platform.smartmetering.glue.steps.ws.smartmetering.smartmeteringbundle;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.Map;
-
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import org.opensmartgridplatform.adapter.ws.schema.smartmetering.bundle.GetPeriodicMeterReadsRequest;
 import org.opensmartgridplatform.adapter.ws.schema.smartmetering.bundle.PeriodicMeterReadsResponse;
 import org.opensmartgridplatform.adapter.ws.schema.smartmetering.common.Response;
 import org.opensmartgridplatform.cucumber.platform.smartmetering.support.ws.smartmetering.bundle.GetPeriodicMeterReadsRequestBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
 
 public class BundledGetPeriodicMeterReadsSteps extends BaseBundleSteps {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BundledGetPeriodicMeterReadsSteps.class);
 
     @Given("^the bundle request contains a get periodic meter reads action$")
     public void theBundleRequestContainsAGetPeriodicMeterReadsAction() throws Throwable {
@@ -43,6 +46,9 @@ public class BundledGetPeriodicMeterReadsSteps extends BaseBundleSteps {
     public void theBundleResponseShouldContainAGetPeriodicMeterReadsResponse() throws Throwable {
 
         final Response response = this.getNextBundleResponse();
+
+
+        LOGGER.info("Got PeriodicMeterReadResponse. exception == {}, result == {}", response.getException(), response.getResultString());
 
         assertTrue("Not a valid response", response instanceof PeriodicMeterReadsResponse);
     }
