38e2962288233ea43dec066095a20d753ed6fda0
Kevin Smeets <kevin.smeets@cgi.com>
2019-07-12 10:26:15 +0000

    Creates all-in-one jar of osgp-mode-cli project.
    This project has been packaged using maven-shade-plugin. The dependencies of the project are included in the jar file. Build it using 'mvn package'. A shell script has been included to start the command line application. The ICD server model file for Kaifa SSLDs has been included as well.

---
+++ osgp/protocol-adapter-iec61850/osgp-mode-cli/connect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "X$1" == "X" ]; then
+  echo "Provide IP address or hostname of the KAIFA SSLD."
+  echo "Example: $0 127.0.0.1"
+  exit 1
+fi
+
+HOST=$1
+PORT=102
+SERVER_NAME=SWDeviceGenericIO
+ICD_FILE=SWDevice-010805.icd
+
+echo "Attempting to connect to host: $HOST port: $PORT, using server name: $SERVER_NAME and ICD file: $ICD_FILE"
+
+java -jar target/osgp-mode-cli-iec61850-0.0.1-SNAPSHOT.jar -h $HOST -p $PORT -s $SERVER_NAME -m $ICD_FILE
+
+exit 0
