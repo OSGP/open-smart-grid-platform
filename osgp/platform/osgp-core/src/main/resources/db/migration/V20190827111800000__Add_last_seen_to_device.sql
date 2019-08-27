dbd5e33110881cfd2fe349f6af2bf250287c08a4
Coen Damen <coen.damen@alliander.com>
2019-08-27 13:55:44 +0200

    SLIM-1964 finalize migration scripts

--- osgp/platform/osgp-core/src/main/resources/db/migration/V20190827111800000__Add_last_seen_to_device.sql
+++ osgp/platform/osgp-core/src/main/resources/db/migration/V20190827111800000__Add_last_seen_to_device.sql
@@ -4,7 +4,7 @@ BEGIN
 
 IF NOT EXISTS (SELECT 1 FROM information_schema.columns
  	WHERE table_schema=current_schema AND table_name = 'device' AND column_name = 'last_seen') THEN
-	 	ALTER TABLE dlms_device ADD COLUMN last_seen  timestamp without time zone ;
+	 	ALTER TABLE device ADD COLUMN last_seen  timestamp without time zone ;
 END IF;
 
 END;
