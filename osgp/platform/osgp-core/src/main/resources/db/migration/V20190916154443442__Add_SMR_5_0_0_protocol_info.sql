10413432ce8822f183e3cae9bc985ec32a70edb1
Adriaan Koster <info@adriaankoster.nl>
2019-09-20 11:32:13 +0200

    SLIM-2059 Bugfixes (#112)

---
+++ osgp/platform/osgp-core/src/main/resources/db/migration/V20190916154443442__Add_SMR_5_0_0_protocol_info.sql
@@ -0,0 +1,37 @@
+DO
+$$
+BEGIN
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM   protocol_info
+    WHERE  protocol = 'SMR'
+    AND    protocol_version  = '5.0.0') THEN
+
+INSERT INTO protocol_info(
+    creation_time,
+    modification_time,
+    version,
+    protocol,
+    protocol_version,
+    outgoing_protocol_requests_queue,
+    incoming_protocol_responses_queue,
+    incoming_protocol_requests_queue,
+    outgoing_protocol_responses_queue,
+    parallel_requests_allowed)
+VALUES (
+    '2019-09-16 00:00:00',
+    '2019-09-16 00:00:00',
+    0,
+    'SMR',
+    '5.0.0',
+    'protocol-dlms.1_0.osgp-core.1_0.requests',
+    'osgp-core.1_0.protocol-dlms.1_0.responses',
+    'osgp-core.1_0.protocol-dlms.1_0.requests',
+    'protocol-dlms.1_0.osgp-core.1_0.responses',
+    true);
+
+END IF;
+
+END;
+$$
