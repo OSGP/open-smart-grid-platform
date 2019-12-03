c5536a936f71edf972ee3bd9be6cf547587085a2
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2019-12-03 17:37:26 +0100

    OC-522 ~ Updates OSGP Logging configuration

--- osgp/platform/osgp-logging/src/main/java/org/opensmartgridplatform/logging/application/config/ApplicationContext.java
+++ osgp/platform/osgp-logging/src/main/java/org/opensmartgridplatform/logging/application/config/ApplicationContext.java
@@ -7,77 +7,35 @@
  */
 package org.opensmartgridplatform.logging.application.config;
 
-import javax.annotation.Resource;
-
-import org.apache.activemq.RedeliveryPolicy;
-import org.opensmartgridplatform.logging.domain.entities.WebServiceMonitorLogItem;
-import org.opensmartgridplatform.logging.domain.repositories.WebServiceMonitorLogRepository;
-import org.opensmartgridplatform.logging.infra.jms.LoggingMessageListener;
-import org.opensmartgridplatform.shared.application.config.AbstractMessagingConfig;
-import org.opensmartgridplatform.shared.application.config.jms.JmsConfiguration;
-import org.opensmartgridplatform.shared.application.config.jms.JmsConfigurationFactory;
+import org.opensmartgridplatform.shared.application.config.AbstractConfig;
+import org.opensmartgridplatform.shared.application.config.messaging.DefaultJmsConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * An application context Java configuration class.
  */
 @Configuration
-@ComponentScan(basePackageClasses = { WebServiceMonitorLogItem.class, LoggingMessageListener.class })
-@EnableJpaRepositories(basePackageClasses = { WebServiceMonitorLogRepository.class })
-@EnableTransactionManagement
+@ComponentScan(basePackages = { "org.opensmartgridplatform.logging" })
 @PropertySource("classpath:osgp-logging.properties")
 @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true)
 @PropertySource(value = "file:${osgp/Logging/config}", ignoreResourceNotFound = true)
-@Import({ LoggingConfig.class, ProtocolMessagingConfig.class })
-public class ApplicationContext extends AbstractMessagingConfig {
+public class ApplicationContext extends AbstractConfig {
 
-    private static final String PROPERTY_NAME_JMS_DEFAULT_INITIAL_REDELIVERY_DELAY = "jms.default.initial.redelivery.delay";
-    private static final String PROPERTY_NAME_JMS_DEFAULT_MAXIMUM_REDELIVERIES = "jms.default.maximum.redeliveries";
-    private static final String PROPERTY_NAME_JMS_DEFAULT_MAXIMUM_REDELIVERY_DELAY = "jms.default.maximum.redelivery.delay";
-    private static final String PROPERTY_NAME_JMS_DEFAULT_REDELIVERY_DELAY = "jms.default.redelivery.delay";
+    @Value(value = "${max.retry.count:3}")
+    private int maxRetryCount;
 
-    @Resource
-    private Environment environment;
-
-    // === JMS SETTINGS ===
-
-    @Override
     @Bean
-    public RedeliveryPolicy defaultRedeliveryPolicy() {
-        final RedeliveryPolicy redeliveryPolicy = new RedeliveryPolicy();
-        redeliveryPolicy.setInitialRedeliveryDelay(Long
-                .parseLong(this.environment.getRequiredProperty(PROPERTY_NAME_JMS_DEFAULT_INITIAL_REDELIVERY_DELAY)));
-        redeliveryPolicy.setMaximumRedeliveries(
-                Integer.parseInt(this.environment.getRequiredProperty(PROPERTY_NAME_JMS_DEFAULT_MAXIMUM_REDELIVERIES)));
-        redeliveryPolicy.setMaximumRedeliveryDelay(Long
-                .parseLong(this.environment.getRequiredProperty(PROPERTY_NAME_JMS_DEFAULT_MAXIMUM_REDELIVERY_DELAY)));
-        redeliveryPolicy.setRedeliveryDelay(
-                Long.parseLong(this.environment.getRequiredProperty(PROPERTY_NAME_JMS_DEFAULT_REDELIVERY_DELAY)));
-
-        return redeliveryPolicy;
+    public DefaultJmsConfiguration defaultJmsConfiguration() {
+        return new DefaultJmsConfiguration();
     }
 
+    // The Max count to retry a failed response
     @Bean
-    public JmsConfiguration loggingRequestsJmsConfiguration(final JmsConfigurationFactory jmsConfigurationFactory) {
-        return jmsConfigurationFactory.initializeReceiveConfiguration("jms.logging", this.loggingMessageListener());
-    }
-
-    @Bean
-    public DefaultMessageListenerContainer loggingMessageListenerContainer(
-            final JmsConfiguration loggingRequestsJmsConfiguration) {
-        return loggingRequestsJmsConfiguration.getMessageListenerContainer();
-    }
-
-    @Bean
-    public LoggingMessageListener loggingMessageListener() {
-        return new LoggingMessageListener();
+    public int getMaxRetryCount() {
+        return this.maxRetryCount;
     }
 }
