8bdaa759604f70beb6358c9a59e65bd4f6fc73b7
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2019-12-06 16:56:31 +0100

    OC-522 ~ Fixes osgp logging

--- osgp/platform/osgp-logging/src/main/java/org/opensmartgridplatform/logging/application/config/ApplicationContext.java
+++ osgp/platform/osgp-logging/src/main/java/org/opensmartgridplatform/logging/application/config/ApplicationContext.java
@@ -7,22 +7,28 @@
  */
 package org.opensmartgridplatform.logging.application.config;
 
+import org.opensmartgridplatform.logging.application.config.messaging.InboundLoggingRequestsMessagingConfig;
+import org.opensmartgridplatform.logging.application.config.messaging.InboundProtocolLogItemRequestsMessagingConfig;
+import org.opensmartgridplatform.logging.infra.jms.LoggingMessageListener;
 import org.opensmartgridplatform.shared.application.config.AbstractConfig;
 import org.opensmartgridplatform.shared.application.config.messaging.DefaultJmsConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 /**
  * An application context Java configuration class.
  */
 @Configuration
-@ComponentScan(basePackages = { "org.opensmartgridplatform.logging" })
+@ComponentScan(basePackageClasses = { PersistenceConfig.class, LoggingMessageListener.class,
+        InboundLoggingRequestsMessagingConfig.class })
 @PropertySource("classpath:osgp-logging.properties")
 @PropertySource(value = "file:${osgp/Global/config}", ignoreResourceNotFound = true)
 @PropertySource(value = "file:${osgp/Logging/config}", ignoreResourceNotFound = true)
+@Import(value = { InboundLoggingRequestsMessagingConfig.class, InboundProtocolLogItemRequestsMessagingConfig.class })
 public class ApplicationContext extends AbstractConfig {
 
     @Value(value = "${max.retry.count:3}")
