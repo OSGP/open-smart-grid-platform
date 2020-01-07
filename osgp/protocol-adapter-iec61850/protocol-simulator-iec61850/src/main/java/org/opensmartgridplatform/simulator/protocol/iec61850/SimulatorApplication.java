21f0f642f18a169a47ebb35e314d0e38c0105506
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2020-01-07 16:25:02 +0100

    FLEX-5156 ~ Updates spring boot version in IEC61850 simulator

--- osgp/protocol-adapter-iec61850/protocol-simulator-iec61850/src/main/java/org/opensmartgridplatform/simulator/protocol/iec61850/SimulatorApplication.java
+++ osgp/protocol-adapter-iec61850/protocol-simulator-iec61850/src/main/java/org/opensmartgridplatform/simulator/protocol/iec61850/SimulatorApplication.java
@@ -12,7 +12,7 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
