5d9400b4d73983210f7b3ee3e9aad08a522c0aa3
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2020-02-14 16:30:45 +0100

    OC-533 Makes build fail on error running tests (#161)
    * OC-533 Makes build fail on error running tests
    
    When an error is introduced in the Spring or glue context of Cucumber
    tests an error is thrown preventing further tests to be executed.
    This happens because the Cucumber feature runner makes the JUnit runner
    abort the tests throwing a StoppedByUserException.
    In such cases the Jenkins console log contains the text "End. Retval =
    1", but does not contain any information on failed scenarios.
    The LogParserPublisher Jenkins plugin which is activated in order to
    check the log for issues with the build has configuration in a file
    named console-test-rules marking this situation as an error.
    By adding parameter failBuildOnError with value true, the build will be
    failed when the LogParserPublisher determines an error has occurred.
    
    * OC-533 ~ Adds test result failure details logging
    
    Because there are some builds where the Cucumber test results are all
    successful, yet the all-over result has failed because a failure is
    present in the JUnit run results, detailed failure logging is added to
    investigate what can be the cause of this.
    
    * OC-533 ~ Modifies test result when all failures are in the mechanism
    
    Failures in the test mechanism are related to errors during the test run
    that could not be related to test cases. Modify the test result to avoid
    failing the build in case all failures are test mechanism related and
    some tests have succeeded.
    
    Log test mechanism related failures with their exception traces so they
    can be examined from the log to see if the test setup can be changed to
    prevent them from occurring.

--- integration-tests/cucumber-tests-execution/src/main/java/org/opensmartgridplatform/cucumber/execution/AppBase.java
+++ integration-tests/cucumber-tests-execution/src/main/java/org/opensmartgridplatform/cucumber/execution/AppBase.java
@@ -9,7 +9,9 @@ package org.opensmartgridplatform.cucumber.execution;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.List;
 import java.util.TimeZone;
+import java.util.stream.Collectors;
 
 import org.apache.tools.ant.taskdefs.optional.junit.XMLJUnitResultFormatter;
 import org.joda.time.DateTimeZone;
@@ -17,6 +19,7 @@ import org.junit.runner.Computer;
 import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
@@ -27,6 +30,11 @@ import junit.runner.Version;
 
 public abstract class AppBase {
 
+    private static final int SUCCESS = 0;
+    private static final int FAILURE = 1;
+    private static final int INCORRECT_USAGE = -1;
+    private static final int ISSUES_IN_TEST_MECHANISM = -2;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AppBase.class);
 
     private static final String END_RETVAL = "End. Retval = {}";
@@ -34,7 +42,9 @@ public abstract class AppBase {
     @Option(name = "-report", metaVar = "DIR", usage = "Directory to produce test reports")
     private File reportDir;
 
-    @Option(name = "-skip-xml-report", metaVar = "DIR", usage = "Suppress the JUnit XML report generation (for more logging)")
+    @Option(name = "-skip-xml-report",
+            metaVar = "DIR",
+            usage = "Suppress the JUnit XML report generation (for more logging)")
     private boolean skipXmlReport;
 
     /**
@@ -59,7 +69,7 @@ public abstract class AppBase {
             LOGGER.error("Incorrect usage", e);
             LOGGER.error("java -jar <...>.jar [opts] ...");
             p.printUsage(System.err);
-            return -1;
+            return INCORRECT_USAGE;
         } catch (final Exception e) {
             LOGGER.error("Caught Exception", e);
             final int retval = 1;
@@ -92,6 +102,61 @@ public abstract class AppBase {
         }
 
         final Result result = junit.run(jUnitCommandLineParseResult.createRequest(new Computer()));
-        return result.wasSuccessful() ? 0 : 1;
+
+        final List<Failure> failuresLinkedToTestMechanism = result.getFailures()
+                .stream()
+                .filter(failure -> Description.TEST_MECHANISM.equals(failure.getDescription()))
+                .collect(Collectors.toList());
+
+        final int testMechanismRelatedFailureCount = failuresLinkedToTestMechanism.size();
+        if (testMechanismRelatedFailureCount > 0) {
+            LOGGER.warn("The test run has lead to {} failure(s) related to the test mechanism",
+                    testMechanismRelatedFailureCount);
+            failuresLinkedToTestMechanism.forEach(this::logFailureDetails);
+        }
+
+        final int failureCount = result.getFailureCount();
+        if (result.getRunCount() == 0 && failureCount > 0) {
+            LOGGER.error(
+                    "Test result has {} failures, while no tests were executed. "
+                            + "Fail the build since this probably indicates issues in setting up the test context.",
+                    failureCount);
+            return FAILURE;
+        }
+
+        final int failuresLinkedToTests = failureCount - testMechanismRelatedFailureCount;
+
+        if (failureCount > 0 && failuresLinkedToTests == 0) {
+            LOGGER.error(
+                    "Test result has {} failures, while none of these were directly related to tests that were executed. "
+                            + "Returning exceptional value {} instead of {} to indicate issues without failing the build.",
+                    failureCount, ISSUES_IN_TEST_MECHANISM, SUCCESS);
+            return ISSUES_IN_TEST_MECHANISM;
+        }
+
+        return failuresLinkedToTests == 0 ? SUCCESS : FAILURE;
+    }
+
+    private void logFailureDetails(final Failure failure) {
+        final Description description = failure.getDescription();
+        final Throwable thrownException = failure.getException();
+        final String failureDescription = this.failureDescription(description);
+        LOGGER.warn("Failure: {}", failureDescription, thrownException);
+    }
+
+    private String failureDescription(final Description description) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(description.getDisplayName()).append(System.lineSeparator());
+        sb.append("\tclassName: ").append(description.getClassName()).append(System.lineSeparator());
+        sb.append("\tmethodName: ").append(description.getMethodName()).append(System.lineSeparator());
+        sb.append("\ttestClass: ")
+                .append(description.getTestClass() == null ? "" : description.getTestClass().getName())
+                .append(System.lineSeparator());
+        sb.append("\ttype: ")
+                .append(description.isTest() ? "test" : "suite with " + description.getChildren().size() + " children");
+        if (description.equals(Description.TEST_MECHANISM)) {
+            sb.append(" (resulted from a step in the test-running mechanism that went wrong)");
+        }
+        return sb.toString();
     }
 }
