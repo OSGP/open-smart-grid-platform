7274da211c857acda66672572acf1deedf13b21c
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2020-03-11 11:58:02 +0000

    OC-526 ~ Adds Jenkins pipeline for dependency reports
    Adds a Jenkins pipeline to run the maven site command and publish HTML
    reports that are configured for dependency checks and updates.
    
    Configures rules for the maven versions plugin to prevent alpha, beta,
    milestone and release candidate versions to be reported as newer
    versions that are available for dependencies or plugins.
    
    Configures the maven site plugin to the latest version currently
    available.

---
+++ jenkins/pipeline/osgp-dependency-check-build.groovy
@@ -0,0 +1,57 @@
+// Pipeline script for the OSGP Dependency Check Build job in Jenkins
+
+pipeline {
+    agent any
+
+    options {
+        ansiColor('xterm')
+        timestamps()
+        timeout(240)
+        // Only keep the 10 most recent builds
+        buildDiscarder(logRotator(numToKeepStr:'10'))
+    }
+
+    stages {
+
+        stage('Maven Build') {
+            steps {
+                withMaven(
+                        maven: 'Apache Maven 3.6.2',
+                        mavenLocalRepo: '.repository',
+                        publisherStrategy: 'EXPLICIT',
+                        options: [
+                                sitePublisher(disabled: false)
+                        ]) {
+                    sh "mvn -V -B clean site -Dmaven.site.distributionManagement.site.url="
+                }
+                publishHTML(target: [
+                        allowMissing: false,
+                        alwaysLinkToLastBuild: true,
+                        keepAll: false,
+                        reportDir: 'super/target/site',
+                        reportFiles: '*.html',
+                        reportName: 'Maven Site Reports'
+                    ])
+            }
+        }
+
+    }
+
+    post {
+        always {
+            echo "End of pipeline"
+        }
+        failure {
+            emailext (
+                subject: '${DEFAULT_SUBJECT}',
+                body: '${DEFAULT_CONTENT}',
+                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+                to: '${DEFAULT_RECIPIENTS}',
+                from: '${DEFAULT_REPLYTO}')
+        }
+        cleanup {
+            // Delete workspace folder.
+            cleanWs()
+        }
+    }
+}
