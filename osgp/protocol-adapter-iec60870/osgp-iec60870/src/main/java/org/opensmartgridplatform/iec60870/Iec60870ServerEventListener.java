339eff838a7c6a5537efa6c2f9e96da3e4814a0d
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2020-04-22 14:50:28 +0200

    FLEX-5297 ~ Sends end of initialization ASDU on first connection
    Updates the Iec60870ServerEventListener, so it can be created in a way
    that makes sure it sends an end of initialization ASDU when the first
    connection to the server is created.
    The current implementation assumes a couple of things, that may need to
    be modified with later insights (for instance the information object
    address to be used is set to 0, and the IeCauseOfInitialization is
    created with values 0 and false).
    
    The Iec60870SimulatorConfig is extended with a boolean value
    (sendEndOfInitialization) indicating whether the end of initialization
    ASDU is sent when a first connection is established or not. This value
    is used when creating an Iec60870Server as option with the server event
    listener.
    
    A client ASDU handler (EndOfInitializationAsduHandler) is added, mainly
    to prevent errors about unknown ASDU types in the logging. For now its
    implementation only has some informational log output.

--- osgp/protocol-adapter-iec60870/osgp-iec60870/src/main/java/org/opensmartgridplatform/iec60870/Iec60870ServerEventListener.java
+++ osgp/protocol-adapter-iec60870/osgp-iec60870/src/main/java/org/opensmartgridplatform/iec60870/Iec60870ServerEventListener.java
@@ -8,9 +8,15 @@
 package org.opensmartgridplatform.iec60870;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.openmuc.j60870.ASdu;
+import org.openmuc.j60870.ASduType;
+import org.openmuc.j60870.CauseOfTransmission;
 import org.openmuc.j60870.Connection;
 import org.openmuc.j60870.ServerEventListener;
+import org.openmuc.j60870.ie.IeCauseOfInitialization;
+import org.openmuc.j60870.ie.InformationObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,15 +30,28 @@ public class Iec60870ServerEventListener implements ServerEventListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Iec60870ServerEventListener.class);
 
+    private static final ASdu END_OF_INITIALIZATION = new ASdu(ASduType.M_EI_NA_1, false,
+            CauseOfTransmission.INITIALIZED, false, false, 0, 0,
+            new InformationObject(0, new IeCauseOfInitialization(0, false)));
+
     private final Iec60870ConnectionRegistry iec60870ConnectionRegistry;
     private final Iec60870AsduHandlerRegistry iec60870AsduHandlerRegistry;
     private final int connectionTimeout;
+    private final boolean sendEndOfInitialization;
+    private AtomicBoolean initializationComplete = new AtomicBoolean(false);
 
     public Iec60870ServerEventListener(final Iec60870ConnectionRegistry iec60870ConnectionRegistry,
             final Iec60870AsduHandlerRegistry iec60870AsduHandlerRegistry, final int connectionTimeout) {
+        this(iec60870ConnectionRegistry, iec60870AsduHandlerRegistry, connectionTimeout, false);
+    }
+
+    public Iec60870ServerEventListener(final Iec60870ConnectionRegistry iec60870ConnectionRegistry,
+            final Iec60870AsduHandlerRegistry iec60870AsduHandlerRegistry, final int connectionTimeout,
+            final boolean sendEndOfInitialization) {
         this.iec60870ConnectionRegistry = iec60870ConnectionRegistry;
         this.iec60870AsduHandlerRegistry = iec60870AsduHandlerRegistry;
         this.connectionTimeout = connectionTimeout;
+        this.sendEndOfInitialization = sendEndOfInitialization;
     }
 
     @Override
@@ -47,9 +66,22 @@ public class Iec60870ServerEventListener implements ServerEventListener {
             LOGGER.error("Exception occurred while connection ({}) was waiting for StartDT.", connection, e);
             return;
         }
-
+        this.sendEndOfInitializationOnFirstConnection(connection);
         this.iec60870ConnectionRegistry.registerConnection(connection);
         LOGGER.info("Connection ({}) listening for incoming commands.", connection);
+    }
+
+    private void sendEndOfInitializationOnFirstConnection(final Connection connection) {
+        if (this.sendEndOfInitialization && this.initializationComplete.compareAndSet(false, true)) {
+            try {
+                LOGGER.info("Sending end of initialization ASDU on first connection");
+                connection.send(END_OF_INITIALIZATION);
+            } catch (final IOException e) {
+                LOGGER.error("Sending end of initialization ASDU on first connection failed", e);
+            }
+        } else {
+            LOGGER.debug("Not sending end of initialization ASDU as initialization was already complete");
+        }
     }
 
     @Override
@@ -61,5 +93,4 @@ public class Iec60870ServerEventListener implements ServerEventListener {
     public void connectionAttemptFailed(final IOException e) {
         LOGGER.warn("Connection attempt failed: {}", e.getMessage());
     }
-
 }
