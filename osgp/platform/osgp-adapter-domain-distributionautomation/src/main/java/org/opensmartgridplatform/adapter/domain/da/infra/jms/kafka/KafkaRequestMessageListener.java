ea38a9642344777a59d377afb5da5d2456ddca76
Robin den Adel <robin.den.adel@cgi.com>
2020-05-08 12:57:04 +0200

    OC-575: Introduces kafka request queue + listener.

---
+++ osgp/platform/osgp-adapter-domain-distributionautomation/src/main/java/org/opensmartgridplatform/adapter/domain/da/infra/jms/kafka/KafkaRequestMessageListener.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2020 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.opensmartgridplatform.adapter.domain.da.infra.jms.kafka;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import org.opensmartgridplatform.shared.infra.jms.MessageProcessor;
+import org.opensmartgridplatform.shared.infra.jms.MessageProcessorMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * Fetches inbound requests from Kafka Adapter from queue.
+ */
+@Component(value = "domainDistributionAutomationInboundKafkaRequestsMessageListener")
+public class KafkaRequestMessageListener implements MessageListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaRequestMessageListener.class);
+
+    @Autowired
+    @Qualifier("domainDistributionAutomationInboundKafkaRequestsMessageProcessorMap")
+    private MessageProcessorMap messageProcessorMap;
+
+    @Override
+    public void onMessage(final Message message) {
+        try {
+            LOGGER.info("Received message of type: {}", message.getJMSType());
+
+            final ObjectMessage objectMessage = (ObjectMessage) message;
+
+            final MessageProcessor processor = this.messageProcessorMap.getMessageProcessor(objectMessage);
+
+            processor.processMessage(objectMessage);
+
+        } catch (final JMSException ex) {
+            LOGGER.error("Exception: {} ", ex.getMessage(), ex);
+        }
+    }
+}
