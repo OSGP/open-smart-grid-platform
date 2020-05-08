3b549207d59eb89901a38522a1b60c54bc295d08
Robin den Adel <robin.den.adel@cgi.com>
2020-05-08 09:19:57 +0200

    OC-575: Handles duplicate code.

--- osgp/platform/osgp-adapter-domain-distributionautomation/src/main/java/org/opensmartgridplatform/adapter/domain/da/infra/jms/ws/KafkaResponseMessageSender.java
+++ osgp/platform/osgp-adapter-domain-distributionautomation/src/main/java/org/opensmartgridplatform/adapter/domain/da/infra/jms/ws/KafkaResponseMessageSender.java
@@ -7,10 +7,6 @@
  */
 package org.opensmartgridplatform.adapter.domain.da.infra.jms.ws;
 
-import javax.jms.ObjectMessage;
-import javax.jms.Session;
-
-import org.opensmartgridplatform.shared.infra.jms.Constants;
 import org.opensmartgridplatform.shared.infra.jms.NotificationResponseMessageSender;
 import org.opensmartgridplatform.shared.infra.jms.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,52 +16,14 @@ import org.springframework.stereotype.Component;
 
 // Send response message to the kafka adapter.
 @Component(value = "domainDistributionAutomationOutboundKafkaResponsesMessageSender")
-public class KafkaResponseMessageSender implements NotificationResponseMessageSender {
+public class KafkaResponseMessageSender extends BaseResponseMessageSender implements NotificationResponseMessageSender {
 
     @Autowired
     @Qualifier("domainDistributionAutomationOutboundKafkaResponsesJmsTemplate")
     private JmsTemplate jmsTemplate;
 
-    /**
-     * Send a response message to the web service adapter using a custom time to
-     * live.
-     *
-     * @param responseMessage
-     *            The response message to send.
-     * @param timeToLive
-     *            The custom time to live value in milliseconds.
-     */
-    public void send(final ResponseMessage responseMessage, final Long timeToLive, final String messageType) {
-
-        // Keep the original time to live from configuration.
-        final Long originalTimeToLive = this.jmsTemplate.getTimeToLive();
-        if (timeToLive != null) {
-            // Set the custom time to live.
-            this.jmsTemplate.setTimeToLive(timeToLive);
-        }
-
-        this.jmsTemplate.send((final Session session) -> {
-            final ObjectMessage objectMessage = session.createObjectMessage(responseMessage);
-            objectMessage.setJMSCorrelationID(responseMessage.getCorrelationUid());
-            objectMessage.setJMSType(messageType);
-            objectMessage.setStringProperty(Constants.ORGANISATION_IDENTIFICATION,
-                    responseMessage.getOrganisationIdentification());
-            objectMessage.setStringProperty(Constants.DEVICE_IDENTIFICATION, responseMessage.getDeviceIdentification());
-            objectMessage.setStringProperty(Constants.RESULT, responseMessage.getResult().toString());
-            if (responseMessage.getOsgpException() != null) {
-                objectMessage.setStringProperty(Constants.DESCRIPTION, responseMessage.getOsgpException().getMessage());
-            }
-            return objectMessage;
-        });
-
-        if (timeToLive != null) {
-            // Restore the time to live from the configuration.
-            this.jmsTemplate.setTimeToLive(originalTimeToLive);
-        }
-    }
-
     @Override
     public void send(final ResponseMessage responseMessage, final String messageType) {
-        this.send(responseMessage, null, messageType);
+        this.send(this.jmsTemplate, responseMessage, null, messageType);
     }
 }
