e4204c55c3f81a81bf061c1d05841b5e93c57743
Erik van Barneveld <erikvb@vbits.nl>
2020-06-08 15:27:25 +0200

    SLIM-2077 first test

--- osgp/platform/osgp-secret-management/src/integration-test/java/org/opensmartgridplatform/secretmgmt/application/endpoints/SecretManagementEndpointIT.java
+++ osgp/platform/osgp-secret-management/src/integration-test/java/org/opensmartgridplatform/secretmgmt/application/endpoints/SecretManagementEndpointIT.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.opensmartgridplatform.schemas.security.secretmanagement._2020._05.Caller;
 import org.opensmartgridplatform.schemas.security.secretmanagement._2020._05.GetSecretsRequest;
 import org.opensmartgridplatform.schemas.security.secretmanagement._2020._05.GetSecretsResponse;
+import org.opensmartgridplatform.schemas.security.secretmanagement._2020._05.SecretType;
+import org.opensmartgridplatform.schemas.security.secretmanagement._2020._05.SecretTypes;
 import org.opensmartgridplatform.secretmgmt.application.services.SecretManagementService;
 import org.opensmartgridplatform.secretmgmt.application.services.encryption.providers.EncryptionProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureDataJpa
@@ -38,8 +43,25 @@ public class SecretManagementEndpointIT {
     }
 
     @Test
-    public void getSecretsBySoapCall() throws Exception {
+    public void getSecretsBySoapCall() {
         GetSecretsRequest request = new GetSecretsRequest();
-        //GetSecretsResponse response = secretManagementEndpoint.getSecretsRequest(request);
+        Caller caller = new Caller();
+        caller.setUserName("soap-api-user");
+        caller.setOrganisationIdentification("soap-user-org");
+        caller.setApplicationName("TEST");
+        request.setCaller(caller);
+
+        SecretTypes secretTypesToGet = new SecretTypes();
+
+        List<SecretType> secretTypeList = secretTypesToGet.getSecretType();
+
+        secretTypeList.add(SecretType.E_METER_AUTHENTICATION_KEY);
+        secretTypeList.add(SecretType.E_METER_ENCRYPTION_KEY_UNICAST);
+
+        request.setSecretTypes(secretTypesToGet);
+        request.setCorrelationUid("1234");
+        request.setDeviceId("E123");
+        GetSecretsResponse response = secretManagementEndpoint.getSecretsRequest(request);
+
     }
 }
