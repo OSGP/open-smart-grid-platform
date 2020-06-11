4a00be4cdd082095a98254d08d79e54bcb9d34c7
Coen Damen <coen.damen@alliander.com>
2020-06-11 10:36:58 +0200

    SLIM-2491 update column script

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/resources/db/migration/V20200611090400000__Alter_invocation_counter_to_bigint.sql
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/resources/db/migration/V20200611090400000__Alter_invocation_counter_to_bigint.sql
@@ -2,10 +2,8 @@ DO
 $$
 BEGIN
 
-ALTER TABLE dlms_device ALTER COLUMN invocation_counter SET DEFAULT 0;
-UPDATE dlms_device SET invocation_counter = 0 WHERE invocation_counter IS NULL;
+ALTER TABLE dlms_device ALTER COLUMN invocation_counter TYPE BIGINT;
 
 END;
 $$
-
 
