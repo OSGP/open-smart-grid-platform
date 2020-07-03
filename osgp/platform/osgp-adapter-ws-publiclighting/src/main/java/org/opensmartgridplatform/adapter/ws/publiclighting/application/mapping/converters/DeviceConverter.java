76bf78dbc81ccdcd1648eaa04c801d4a61b0d586
Robin den Adel <robin.den.adel@cgi.com>
2020-07-03 13:02:59 +0200

    OC-608: Adds a converter for the mapping.

---
+++ osgp/platform/osgp-adapter-ws-publiclighting/src/main/java/org/opensmartgridplatform/adapter/ws/publiclighting/application/mapping/converters/DeviceConverter.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2020 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.opensmartgridplatform.adapter.ws.publiclighting.application.mapping.converters;
+
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.codec.binary.Base64;
+import org.opensmartgridplatform.adapter.ws.schema.publiclighting.adhocmanagement.Device;
+import org.opensmartgridplatform.domain.core.valueobjects.Address;
+import org.opensmartgridplatform.domain.core.valueobjects.GpsCoordinates;
+
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.converter.BidirectionalConverter;
+import ma.glasnost.orika.metadata.Type;
+
+public class DeviceConverter
+        extends BidirectionalConverter<org.opensmartgridplatform.domain.core.entities.Device, Device> {
+
+    @Override
+    public Device convertTo(final org.opensmartgridplatform.domain.core.entities.Device source,
+            final Type<Device> destinationType, final MappingContext mappingContext) {
+        final Device device = new Device();
+        final String deviceIdentification = source.getDeviceIdentification();
+        device.setDeviceUid(Base64.encodeBase64String(deviceIdentification.getBytes(StandardCharsets.US_ASCII)));
+        device.setDeviceIdentification(deviceIdentification);
+        final Address containerAddress = source.getContainerAddress();
+        device.setContainerPostalCode(containerAddress.getPostalCode());
+        device.setContainerCity(containerAddress.getCity());
+        device.setContainerStreet(containerAddress.getStreet());
+        device.setContainerNumber(containerAddress.getNumber().toString());
+        final GpsCoordinates gpsCoordinates = source.getGpsCoordinates();
+        device.setGpsLatitude(gpsCoordinates.getLatitude());
+        device.setGpsLongitude(gpsCoordinates.getLongitude());
+        device.setDeviceType(source.getDeviceType());
+        device.setActivated(source.isActivated());
+
+        return device;
+    }
+
+    @Override
+    public org.opensmartgridplatform.domain.core.entities.Device convertFrom(final Device source,
+            final Type<org.opensmartgridplatform.domain.core.entities.Device> destinationType,
+            final MappingContext mappingContext) {
+
+        final String deviceIdentification = source.getDeviceIdentification();
+        final Address containerAddress = new Address(source.getContainerCity(), source.getContainerPostalCode(),
+                source.getContainerStreet(), Integer.valueOf(source.getContainerNumber()), "", "");
+        final GpsCoordinates gpsCoordinates = new GpsCoordinates(source.getGpsLatitude(), source.getGpsLongitude());
+        final org.opensmartgridplatform.domain.core.entities.Device device = new org.opensmartgridplatform.domain.core.entities.Device(
+                deviceIdentification, "", containerAddress, gpsCoordinates, null);
+        device.updateRegistrationData(null, source.getDeviceType());
+        return device;
+    }
+
+}
---
+++ osgp/platform/osgp-adapter-ws-publiclighting/src/test/java/org/opensmartgridplatform/adapter/ws/publiclighting/application/mapping/converters/DeviceMappingTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2020 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.opensmartgridplatform.adapter.ws.publiclighting.application.mapping.converters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.opensmartgridplatform.adapter.ws.publiclighting.application.mapping.AdHocManagementMapper;
+import org.opensmartgridplatform.adapter.ws.schema.publiclighting.adhocmanagement.Device;
+import org.opensmartgridplatform.domain.core.valueobjects.Address;
+import org.opensmartgridplatform.domain.core.valueobjects.GpsCoordinates;
+
+class DeviceMappingTest {
+
+    private static final String DEVICE_IDENTIFICATION = "TestDevice-1";
+    private static final String DEVICE_TYPE = "SSLD";
+    private static final String CONTAINER_CITY = "some-city";
+    private static final Integer CONTAINER_NUMBER = 42;
+    private static final String CONTAINER_POSTAL_CODE = "1234AB";
+    private static final String CONTAINER_STREET = "some-street";
+    private static final Float GPS_LATITUDE = 51.0f;
+    private static final Float GPS_LONGITUDE = 5.0f;
+    private static final AdHocManagementMapper mapper = new AdHocManagementMapper();
+
+    @Test
+    void testConvertToCore() {
+        final org.opensmartgridplatform.domain.core.entities.Device device = mapper.map(this.adhocManagementDevice(),
+                org.opensmartgridplatform.domain.core.entities.Device.class);
+
+        assertThat(device).usingRecursiveComparison()
+                .ignoringFields("creationTime", "modificationTime")
+                .isEqualTo(this.coreDevice());
+    }
+
+    @Test
+    void testConvertToAdHocMananagement() {
+        final Device device = mapper.map(this.coreDevice(),
+                org.opensmartgridplatform.adapter.ws.schema.publiclighting.adhocmanagement.Device.class);
+
+        assertThat(device).usingRecursiveComparison()
+                .ignoringFields("deviceUid")
+                .isEqualTo(this.adhocManagementDevice());
+    }
+
+    private Device adhocManagementDevice() {
+        final Device device = new Device();
+        device.setDeviceIdentification(DEVICE_IDENTIFICATION);
+        device.setContainerPostalCode(CONTAINER_POSTAL_CODE);
+        device.setContainerCity(CONTAINER_CITY);
+        device.setContainerStreet(CONTAINER_STREET);
+        device.setContainerNumber(CONTAINER_NUMBER.toString());
+        device.setGpsLatitude(GPS_LATITUDE);
+        device.setGpsLongitude(GPS_LONGITUDE);
+        device.setDeviceType(DEVICE_TYPE);
+        device.setActivated(true);
+
+        return device;
+    }
+
+    private org.opensmartgridplatform.domain.core.entities.Device coreDevice() {
+        final Address containerAddress = new Address(CONTAINER_CITY, CONTAINER_POSTAL_CODE, CONTAINER_STREET,
+                CONTAINER_NUMBER, "", "");
+        final GpsCoordinates gpsCoordinates = new GpsCoordinates(GPS_LATITUDE, GPS_LONGITUDE);
+
+        final org.opensmartgridplatform.domain.core.entities.Device device = new org.opensmartgridplatform.domain.core.entities.Device(
+                DEVICE_IDENTIFICATION, "", containerAddress, gpsCoordinates, null);
+        device.updateRegistrationData(null, DEVICE_TYPE);
+        return device;
+    }
+}
