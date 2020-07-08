8d8c1256b3601adea7f2bc2a72c84fe66e0e56bb
Ruud Lemmers <ruud.lemmers@cgi.com>
2020-07-08 23:05:58 +0200

    FLEX-5402 ~ Updates DeviceLogItemSteps

--- integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/glue/steps/database/logging/DeviceLogItemSteps.java
+++ integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/glue/steps/database/logging/DeviceLogItemSteps.java
@@ -11,16 +11,23 @@ package org.opensmartgridplatform.cucumber.platform.glue.steps.database.logging;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.opensmartgridplatform.cucumber.core.ReadSettingsHelper.getString;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.opensmartgridplatform.cucumber.platform.PlatformDefaults;
 import org.opensmartgridplatform.cucumber.platform.PlatformKeys;
 import org.opensmartgridplatform.logging.domain.entities.DeviceLogItem;
 import org.opensmartgridplatform.logging.domain.repositories.DeviceLogItemPagingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.orm.jpa.JpaTransactionManager;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -29,6 +36,10 @@ public class DeviceLogItemSteps {
 
     @Autowired
     private DeviceLogItemPagingRepository deviceLogItemRepository;
+
+    @Autowired
+    @Qualifier("txMgrLogging")
+    JpaTransactionManager txMgrLogging;
 
     @Given("^I have a device log item$")
     public void iHaveADeviceLogItem(final Map<String, String> settings) {
@@ -44,11 +55,39 @@ public class DeviceLogItemSteps {
         final boolean valid = true;
 
         final DeviceLogItem deviceLogItem = new DeviceLogItem.Builder().withDeviceIdentification(deviceIdentification)
-                .withDeviceUid(deviceUid).withDecodedMessage(decodedMessage).withEncodedMessage(encodedMessage)
-                .withIncoming(incoming).withOrganisationIdentification(organisationIdentification)
-                .withPayloadMessageSerializedSize(payloadMessageSerializedSize).withValid(valid).build();
+                .withDeviceUid(deviceUid)
+                .withDecodedMessage(decodedMessage)
+                .withEncodedMessage(encodedMessage)
+                .withIncoming(incoming)
+                .withOrganisationIdentification(organisationIdentification)
+                .withPayloadMessageSerializedSize(payloadMessageSerializedSize)
+                .withValid(valid)
+                .build();
 
-        this.deviceLogItemRepository.save(deviceLogItem);
+        final DeviceLogItem savedDeviceLogItem = this.deviceLogItemRepository.save(deviceLogItem);
+
+        final String modificationTimeString = getString(settings, PlatformKeys.KEY_MODIFICATION_TIME);
+        if (modificationTimeString != null) {
+            final ZonedDateTime modificationTime = ZonedDateTime.parse(modificationTimeString);
+            this.updateModificationTime(savedDeviceLogItem.getId(), modificationTime);
+        }
+    }
+
+    private void updateModificationTime(final Long deviceLogItemId, final ZonedDateTime modificationTime) {
+        if (modificationTime != null) {
+            final EntityManager em = this.txMgrLogging.getEntityManagerFactory().createEntityManager();
+            final EntityTransaction transaction = em.getTransaction();
+
+            transaction.begin();
+            final String sql = "UPDATE device_log_item SET modification_time = :modification_time WHERE id = :id";
+            final Query updateQuery = em.createNativeQuery(sql);
+            updateQuery.setParameter("id", deviceLogItemId);
+            updateQuery.setParameter("modification_time", modificationTime);
+            updateQuery.executeUpdate();
+            transaction.commit();
+
+            em.close();
+        }
     }
 
     @Given("^I have (\\d+) device log items$")
@@ -74,9 +113,11 @@ public class DeviceLogItemSteps {
 
         final Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE);
         final List<org.opensmartgridplatform.logging.domain.entities.DeviceLogItem> deviceLogItems = this.deviceLogItemRepository
-                .findByDeviceIdentification(deviceIdentification, pageable).getContent();
+                .findByDeviceIdentification(deviceIdentification, pageable)
+                .getContent();
         assertThat(this.countGetAdministrativeStatusLogItems(deviceLogItems) > 0)
-                .as("number of device log items for " + deviceIdentification).isTrue();
+                .as("number of device log items for " + deviceIdentification)
+                .isTrue();
     }
 
     @Then("^the get administrative status communication for device \"([^\"]*)\" should not be in the device_log_item table$")
@@ -85,13 +126,16 @@ public class DeviceLogItemSteps {
 
         final Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE);
         final List<org.opensmartgridplatform.logging.domain.entities.DeviceLogItem> deviceLogItems = this.deviceLogItemRepository
-                .findByDeviceIdentification(deviceIdentification, pageable).getContent();
+                .findByDeviceIdentification(deviceIdentification, pageable)
+                .getContent();
         assertThat(this.countGetAdministrativeStatusLogItems(deviceLogItems))
-                .as("number of device log items for " + deviceIdentification).isEqualTo(0);
+                .as("number of device log items for " + deviceIdentification)
+                .isEqualTo(0);
     }
 
     private long countGetAdministrativeStatusLogItems(final List<DeviceLogItem> deviceLogItems) {
-        return deviceLogItems.stream().filter(deviceLogItem -> this.isGetAdministrativeStatusLogItem(deviceLogItem))
+        return deviceLogItems.stream()
+                .filter(deviceLogItem -> this.isGetAdministrativeStatusLogItem(deviceLogItem))
                 .count();
     }
 
