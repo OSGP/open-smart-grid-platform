d93c8976c369d040c9a55e643c4520a6046e468d
Coen Damen <coen.damen@alliander.com>
2020-07-20 13:56:38 +0200

    SLIM-2575 changed enum 5_0 to 5_0_0 and value 5.0 to 5.0.0

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/application/services/ThrottlingService.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/application/services/ThrottlingService.java
@@ -63,11 +63,11 @@ public class ThrottlingService {
 
         this.newConnectionRequest();
 
-        LOGGER.debug("Requesting openConnection. available = {} ", this.openConnectionsSemaphore.availablePermits());
+        LOGGER.info("Requesting openConnection. available = {} ", this.openConnectionsSemaphore.availablePermits());
 
         try {
             this.openConnectionsSemaphore.acquire();
-            LOGGER.debug("openConnection granted. available = {} ", this.openConnectionsSemaphore.availablePermits());
+            LOGGER.info("openConnection granted. available = {} ", this.openConnectionsSemaphore.availablePermits());
         } catch (InterruptedException e) {
             LOGGER.warn("Unable to acquire Open Connection", e);
             Thread.currentThread().interrupt();
@@ -76,7 +76,7 @@ public class ThrottlingService {
 
     public void closeConnection() {
 
-        LOGGER.debug("closeConnection(). available = {} ", this.openConnectionsSemaphore.availablePermits());
+        LOGGER.info("closeConnection(). available = {} ", this.openConnectionsSemaphore.availablePermits());
         this.openConnectionsSemaphore.release();
     }
 
@@ -84,11 +84,11 @@ public class ThrottlingService {
 
         awaitReset();
 
-        LOGGER.debug("newConnectionRequest(). available = {} ", this.newConnectionRequestsSemaphore.availablePermits());
+        LOGGER.info("newConnectionRequest(). available = {} ", this.newConnectionRequestsSemaphore.availablePermits());
 
         try {
             this.newConnectionRequestsSemaphore.acquire();
-            LOGGER.debug("Request newConnection granted. available = {} ",
+            LOGGER.info("Request newConnection granted. available = {} ",
                     this.newConnectionRequestsSemaphore.availablePermits());
         } catch (InterruptedException e) {
             LOGGER.warn("Unable to acquire New Connection Request", e);
@@ -117,9 +117,12 @@ public class ThrottlingService {
 
                 int nrOfPermitsToBeReleased =
                         maxNewConnectionRequests - newConnectionRequestsSemaphore.availablePermits();
+
+                LOGGER.info("releasing {} permits on newConnectionRequestsSemaphore", nrOfPermitsToBeReleased);
+
                 newConnectionRequestsSemaphore.release(nrOfPermitsToBeReleased);
 
-                LOGGER.debug("ThrottlingService - Timer Reset and Unlocking, newConnectionRequests available = {}  ",
+                LOGGER.info("ThrottlingService - Timer Reset and Unlocking, newConnectionRequests available = {}  ",
                         newConnectionRequestsSemaphore.availablePermits());
             } finally {
                 resetTimerLock.unlock();
