aa45fa43c1b762d84f5e27e4d09c580f42f6b953
Kevin Smeets <kevin.smeets@cgi.com>
2020-07-27 14:13:15 +0200

    Disables 'Destroy an AWS System'.

--- jenkins/pipeline/osgp-pr-build.groovy
+++ jenkins/pipeline/osgp-pr-build.groovy
@@ -198,7 +198,7 @@ echo Found cucumber tags: [$EXTRACTED_TAGS]'''
         always {
             echo "End of pipeline"
             // Always destroy the test environment
-            build job: 'Destroy an AWS System', parameters: [string(name: 'SERVERNAME', value: servername), string(name: 'PLAYBOOK', value: playbook)]
+            //build job: 'Destroy an AWS System', parameters: [string(name: 'SERVERNAME', value: servername), string(name: 'PLAYBOOK', value: playbook)]
         }
         success {
             setBuildStatus("Build failed", "SUCCESS")
