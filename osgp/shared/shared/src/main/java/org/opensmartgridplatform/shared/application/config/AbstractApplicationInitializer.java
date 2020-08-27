0c9376e54f61e246e57ad25eb79af048735cea4f
Joost Knapen <joost.knapen@alliander.com>
2020-08-27 10:28:06 +0000

    OC-597: reinitialize logback with the new configuration file

--- osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/AbstractApplicationInitializer.java
+++ osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/AbstractApplicationInitializer.java
@@ -8,6 +8,7 @@
 package org.opensmartgridplatform.shared.application.config;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.TimeZone;
 
 import javax.naming.Context;
@@ -18,17 +19,21 @@ import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.core.joran.spi.JoranException;
 
 /**
  * Web application Java configuration class.
  */
 public abstract class AbstractApplicationInitializer {
 
-    protected Logger logger;
+    protected final Logger logger;
     private final Class<?> contextClass;
     private final String logConfig;
     protected AnnotationConfigWebApplicationContext rootContext;
@@ -44,6 +49,7 @@ public abstract class AbstractApplicationInitializer {
      */
     public AbstractApplicationInitializer(final Class<?> contextClass, final String logConfig) {
         this.contextClass = contextClass;
+        this.logger = LoggerFactory.getLogger(this.contextClass);
         this.logConfig = logConfig;
         this.rootContext = new AnnotationConfigWebApplicationContext();
     }
@@ -77,38 +83,30 @@ public abstract class AbstractApplicationInitializer {
         try {
             initialContext = new InitialContext();
             logLocation = (String) initialContext.lookup(this.logConfig);
-        } catch (final NamingException e) {
-            this.logger = LoggerFactory.getLogger(this.contextClass);
+
+            // Load specific logback configuration, otherwise fallback to
+            // classpath logback.xml
+
+            if (new File(logLocation).exists()) {
+                final LoggerContext loggerContext = (LoggerContext) StaticLoggerBinder.getSingleton()
+                        .getLoggerFactory();
+
+                // in the current version logback automatically configures at
+                // startup the context, so we have to reset it
+                loggerContext.reset();
+
+                // reinitialize the logger context. calling this method allows
+                // configuration through groovy or xml
+                new ContextInitializer(loggerContext).configureByResource(ResourceUtils.getURL(logLocation));
+
+                this.logger.info("Initialized logging using {} ({})", this.logConfig, logLocation);
+
+            } else {
+                this.logger.info("{} not found, falling back to default logback initialization", logLocation);
+            }
+        } catch (final NamingException | FileNotFoundException | JoranException e) {
             this.logger.info("Failed to initialize logging using {}", this.logConfig, e);
             throw new ServletException(e);
         }
-
-        // Load specific logback configuration, otherwise fallback to classpath
-        // logback.xml
-        // The system property should be set BEFORE initialization of the
-        // logging system;
-        // therefore this unusual notation of not setting the logger in fa the
-        // constructor.
-        final boolean logLocationExists = new File(logLocation).exists();
-        if (logLocationExists) {
-            System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, logLocation);
-        }
-
-        this.logger = LoggerFactory.getLogger(this.contextClass);
-
-        if (logLocationExists) {
-            this.logger.info("Initialized logging using {} ({})", this.logConfig, logLocation);
-        } else {
-            this.logger.info("{} not found", logLocation);
-        }
-
-        // Remove the property, to be sure other applications won't be using it.
-        // After some testing, it as found that it might need a Thread.sleep
-        // because it seems like the logging system is not fully initialized
-        // before the first log line has been written.
-        // Although as seen from
-        // http://logback.qos.ch/manual/configuration.html#configFileProperty
-        // the sleep should not be needed.
-        System.clearProperty(ContextInitializer.CONFIG_FILE_PROPERTY);
     }
 }
