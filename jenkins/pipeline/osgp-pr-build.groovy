28f3506d32851fa0b364f00fcaebae4b5dc7e8b1
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2020-09-15 09:20:23 +0200

    Updates cucumuber buildstatus in jenkins pipeline

--- jenkins/pipeline/osgp-pr-build.groovy
+++ jenkins/pipeline/osgp-pr-build.groovy
@@ -185,7 +185,7 @@ echo Found cucumber tags: [$EXTRACTED_TAGS]'''
         stage('Reporting') {
             steps {
                 jacoco execPattern: '**/code-coverage/jacoco-it.exec'
-                cucumber buildStatus: null, fileIncludePattern: '**/cucumber.json', sortingMethod: 'ALPHABETICAL'
+                cucumber buildStatus: 'FAILURE', fileIncludePattern: '**/cucumber.json', sortingMethod: 'ALPHABETICAL'
                 archiveArtifacts '**/target/*.tgz'
 
                 // Check the console log for failed tests
