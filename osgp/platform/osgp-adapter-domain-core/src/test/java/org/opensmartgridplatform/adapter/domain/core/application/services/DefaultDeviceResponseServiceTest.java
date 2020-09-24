d88ecc727682877b9523eeb284d31c3526955c3d
Arjen <arjenmeer@hotmail.nl>
2020-09-24 14:12:19 +0200

    no longer using assertEquals but now using assert that recursive

--- osgp/platform/osgp-adapter-domain-core/src/test/java/org/opensmartgridplatform/adapter/domain/core/application/services/DefaultDeviceResponseServiceTest.java
+++ osgp/platform/osgp-adapter-domain-core/src/test/java/org/opensmartgridplatform/adapter/domain/core/application/services/DefaultDeviceResponseServiceTest.java
@@ -1,5 +1,6 @@
 package org.opensmartgridplatform.adapter.domain.core.application.services;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -14,6 +15,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import org.opensmartgridplatform.shared.exceptionhandling.ComponentType;
@@ -40,6 +42,13 @@ public class DefaultDeviceResponseServiceTest {
     	int messagePriority = 3;
     	ResponseMessageResultType deviceResult = ResponseMessageResultType.NOT_OK;
     	OsgpException exception = new OsgpException(ComponentType.DOMAIN_CORE, "There was an exception");
+
+		ResponseMessage expectedResponseMessage = ResponseMessage.newResponseMessageBuilder()
+				.withIds(ids)
+				.withResult(ResponseMessageResultType.NOT_OK)
+				.withOsgpException(exception)
+				.withMessagePriority(messagePriority)
+				.build();
     	
     	//Act
     	this.defaultDeviceResponseService.handleDefaultDeviceResponse(ids, messageType, messagePriority, deviceResult, exception);
@@ -48,83 +57,85 @@ public class DefaultDeviceResponseServiceTest {
     	verify(webServiceResponseMessageSender).send(argument.capture());
 
     	//Assert
-    	assertEquals("orginazationTestId", argument.getValue().getOrganisationIdentification());
-    	assertEquals("deviceIdTest", argument.getValue().getDeviceIdentification());
-    	assertEquals("correlationUid", argument.getValue().getCorrelationUid());
-    	assertEquals(3, argument.getValue().getMessagePriority());
-    	assertEquals(ResponseMessageResultType.NOT_OK, argument.getValue().getResult());
-    	assertEquals(exception, argument.getValue().getOsgpException());
+		assertThat(argument.getValue()).usingRecursiveComparison().isEqualTo(expectedResponseMessage);
     }
     
     @Test
     public void testDefaultDeviseResponseWithNotOkTypeAndNoException2() {
     	//Arrange
-    	CorrelationIds correlationIds = new CorrelationIds("orginazationTestId", "deviceIdTest", "correlationUid");
+    	CorrelationIds ids = new CorrelationIds("orginazationTestId", "deviceIdTest", "correlationUid");
     	String messageType = "Warning";
     	int messagePriority = 3;
     	ResponseMessageResultType deviceResult = ResponseMessageResultType.NOT_OK;
     	OsgpException exception = null;
     	OsgpException osgpException = new TechnicalException(ComponentType.DOMAIN_CORE, "An unknown error occurred");
+
+		ResponseMessage expectedResponseMessage = ResponseMessage.newResponseMessageBuilder()
+				.withIds(ids)
+				.withResult(ResponseMessageResultType.NOT_OK)
+				.withOsgpException(osgpException)
+				.withMessagePriority(messagePriority)
+				.build();
     	
-    	this.defaultDeviceResponseService.handleDefaultDeviceResponse(correlationIds, messageType, messagePriority, deviceResult, exception);
+    	this.defaultDeviceResponseService.handleDefaultDeviceResponse(ids, messageType, messagePriority, deviceResult, exception);
     	//Act
     	ArgumentCaptor<ResponseMessage> argument = ArgumentCaptor.forClass(ResponseMessage.class);
     	verify(webServiceResponseMessageSender).send(argument.capture());
 
     	//Assert
-    	assertEquals("orginazationTestId", argument.getValue().getOrganisationIdentification());
-    	assertEquals("deviceIdTest", argument.getValue().getDeviceIdentification());
-    	assertEquals("correlationUid", argument.getValue().getCorrelationUid());
-    	assertEquals(3, argument.getValue().getMessagePriority());
-    	assertEquals(ResponseMessageResultType.NOT_OK, argument.getValue().getResult());
-    	assertEquals(osgpException.getComponentType(), argument.getValue().getOsgpException().getComponentType());
+		assertThat(argument.getValue()).usingRecursiveComparison().isEqualTo(expectedResponseMessage);
     }
     
     @Test
     public void testDefaultDeviseResponseWithOkTypeAndException() {
     	//Arrange
-    	CorrelationIds correlationIds = new CorrelationIds("orginazationTestId", "deviceIdTest", "correlationUid");
+    	CorrelationIds ids = new CorrelationIds("orginazationTestId", "deviceIdTest", "correlationUid");
     	String messageType = "Warning";
     	int messagePriority = 3;
     	ResponseMessageResultType deviceResult = ResponseMessageResultType.OK;
     	OsgpException exception = new OsgpException(ComponentType.DOMAIN_CORE, "There was an exception");
-    	
-    	//Act
-    	this.defaultDeviceResponseService.handleDefaultDeviceResponse(correlationIds, messageType, messagePriority, deviceResult, exception);
+
+		ResponseMessage expectedResponseMessage = ResponseMessage.newResponseMessageBuilder()
+				.withIds(ids)
+				.withResult(ResponseMessageResultType.NOT_OK)
+				.withOsgpException(exception)
+				.withMessagePriority(messagePriority)
+				.build();
+
+
+		//Act
+    	this.defaultDeviceResponseService.handleDefaultDeviceResponse(ids, messageType, messagePriority, deviceResult, exception);
     
     	ArgumentCaptor<ResponseMessage> argument = ArgumentCaptor.forClass(ResponseMessage.class);
     	verify(webServiceResponseMessageSender).send(argument.capture());
 
     	//Assert
-    	assertEquals("orginazationTestId", argument.getValue().getOrganisationIdentification());
-    	assertEquals("deviceIdTest", argument.getValue().getDeviceIdentification());
-    	assertEquals("correlationUid", argument.getValue().getCorrelationUid());
-    	assertEquals(3, argument.getValue().getMessagePriority());
-    	assertEquals(ResponseMessageResultType.NOT_OK, argument.getValue().getResult());
-    	assertEquals(exception, argument.getValue().getOsgpException());
+		assertThat(argument.getValue()).usingRecursiveComparison().isEqualTo(expectedResponseMessage);
     }
     
     @Test
     public void testDefaultDeviseResponseWithOkTypeAndNoException() {
     	//Arrange
-    	CorrelationIds correlationIds = new CorrelationIds("orginazationTestId", "deviceIdTest", "correlationUid");
+    	CorrelationIds ids = new CorrelationIds("orginazationTestId", "deviceIdTest", "correlationUid");
     	String messageType = "Warning";
     	int messagePriority = 3;
     	ResponseMessageResultType deviceResult = ResponseMessageResultType.OK;
     	OsgpException exception = null;
-    	
-    	//Act
-    	this.defaultDeviceResponseService.handleDefaultDeviceResponse(correlationIds, messageType, messagePriority, deviceResult, exception);
+
+		ResponseMessage expectedResponseMessage = ResponseMessage.newResponseMessageBuilder()
+				.withIds(ids)
+				.withResult(ResponseMessageResultType.OK)
+				.withOsgpException(exception)
+				.withMessagePriority(messagePriority)
+				.build();
+		
+		//Act
+    	this.defaultDeviceResponseService.handleDefaultDeviceResponse(ids, messageType, messagePriority, deviceResult, exception);
     
     	ArgumentCaptor<ResponseMessage> argument = ArgumentCaptor.forClass(ResponseMessage.class);
     	verify(webServiceResponseMessageSender).send(argument.capture());
 
     	//Assert
-    	assertEquals("orginazationTestId", argument.getValue().getOrganisationIdentification());
-    	assertEquals("deviceIdTest", argument.getValue().getDeviceIdentification());
-    	assertEquals("correlationUid", argument.getValue().getCorrelationUid());
-    	assertEquals(3, argument.getValue().getMessagePriority());
-    	assertEquals(ResponseMessageResultType.OK, argument.getValue().getResult());
-    	assertEquals(exception, argument.getValue().getOsgpException());
+		assertThat(argument.getValue()).usingRecursiveComparison().isEqualTo(expectedResponseMessage);
     }
 }
