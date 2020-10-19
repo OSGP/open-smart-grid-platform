98ba0b2395d33ee42fac88ec89d30f5bd13e3b0f
Jan Dik <jandik@gmail.com>
2020-10-19 11:15:39 +0200

    SLIM-2597 fixed SQL errors (column secret_status NULL)

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/database/device/DlmsDeviceSteps.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/database/device/DlmsDeviceSteps.java
@@ -493,16 +493,16 @@ public class DlmsDeviceSteps {
                     DatabaseConnectionParameters.getUser(), DatabaseConnectionParameters.getPassword());
         }
 
+        //TODO replace SQL with domain-objects/builders
+        Base.exec("INSERT INTO encrypted_secret (device_identification,secret_type,encoded_secret,secret_status,creation_time,encryption_key_reference_id)" +
+                "VALUES ('TEST1024000000001', 'E_METER_AUTHENTICATION_KEY', 'c19fe80a22a0f6c5cdaad0826c4d204f23694ded08d811b66e9b845d9f2157d2', 'ACTIVE', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
+                "('TEST1024000000001', 'E_METER_ENCRYPTION_KEY_UNICAST', '867424ac75b6d53c89276d304608321f0a1f6e401f453f84adf3477c7ee1623c', 'ACTIVE', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
+                "('TEST1024000000001', 'E_METER_MASTER_KEY', '55dc88791e6c8f6aff4c8be7714fb8d2ae3d02693ec474593acd3523ee032638', 'ACTIVE', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'));");
 
-        Base.exec("INSERT INTO encrypted_secret (device_identification,secret_type,encoded_secret,creation_time,encryption_key_reference_id)" +
-                "VALUES ('TEST1024000000001', 'E_METER_AUTHENTICATION_KEY', 'c19fe80a22a0f6c5cdaad0826c4d204f23694ded08d811b66e9b845d9f2157d2', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
-                "('TEST1024000000001', 'E_METER_ENCRYPTION_KEY_UNICAST', '867424ac75b6d53c89276d304608321f0a1f6e401f453f84adf3477c7ee1623c', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
-                "('TEST1024000000001', 'E_METER_MASTER_KEY', '55dc88791e6c8f6aff4c8be7714fb8d2ae3d02693ec474593acd3523ee032638', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'));");
-
-        Base.exec("INSERT INTO encrypted_secret (device_identification,secret_type,encoded_secret,creation_time,encryption_key_reference_id)" +
-                "VALUES ('TEST1027000000001', 'E_METER_AUTHENTICATION_KEY', 'c19fe80a22a0f6c5cdaad0826c4d204f23694ded08d811b66e9b845d9f2157d2', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
-                "('TEST1027000000001', 'E_METER_ENCRYPTION_KEY_UNICAST', '867424ac75b6d53c89276d304608321f0a1f6e401f453f84adf3477c7ee1623c', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
-                "('TEST1027000000001', 'E_METER_MASTER_KEY', '55dc88791e6c8f6aff4c8be7714fb8d2ae3d02693ec474593acd3523ee032638', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'));");
+        Base.exec("INSERT INTO encrypted_secret (device_identification,secret_type,encoded_secret,secret_status,creation_time,encryption_key_reference_id)" +
+                "VALUES ('TEST1027000000001', 'E_METER_AUTHENTICATION_KEY', 'c19fe80a22a0f6c5cdaad0826c4d204f23694ded08d811b66e9b845d9f2157d2', 'ACTIVE', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
+                "('TEST1027000000001', 'E_METER_ENCRYPTION_KEY_UNICAST', '867424ac75b6d53c89276d304608321f0a1f6e401f453f84adf3477c7ee1623c', 'ACTIVE', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'))," +
+                "('TEST1027000000001', 'E_METER_MASTER_KEY', '55dc88791e6c8f6aff4c8be7714fb8d2ae3d02693ec474593acd3523ee032638', 'ACTIVE', NOW(), (SELECT id FROM public.encryption_key_reference where encryption_provider_type = 'JRE'));");
 
         Base.close();
     }
