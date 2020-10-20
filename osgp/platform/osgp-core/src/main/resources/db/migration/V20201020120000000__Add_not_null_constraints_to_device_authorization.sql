ed80245808fe33daa0aaca4688a3a5ff3bcf426b
Gerben Kroes <gerben@kroesctrl.nl>
2020-10-20 14:10:11 +0200

    First delete records were device or organisation is NULL;

--- osgp/platform/osgp-core/src/main/resources/db/migration/V20201020120000000__Add_not_null_constraints_to_device_authorization.sql
+++ osgp/platform/osgp-core/src/main/resources/db/migration/V20201020120000000__Add_not_null_constraints_to_device_authorization.sql
@@ -1,2 +1,4 @@
+DELETE FROM device_authorization WHERE device IS NULL OR organisation IS NULL;
+
 ALTER TABLE device_authorization ALTER COLUMN device SET NOT NULL;
 ALTER TABLE device_authorization ALTER COLUMN organisation SET NOT NULL;
