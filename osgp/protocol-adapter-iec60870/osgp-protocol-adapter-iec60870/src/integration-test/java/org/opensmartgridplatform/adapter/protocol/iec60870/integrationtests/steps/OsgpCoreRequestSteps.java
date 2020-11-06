6f0062ea08f22ef7882b42fdbc2bba205eead4e1
Joost Knapen <joostknapen@e-scope.com>
2020-11-06 12:29:26 +0100

    FLEX-5428: fixed test where list was expected instead of an eventNotificationDTO

--- osgp/protocol-adapter-iec60870/osgp-protocol-adapter-iec60870/src/integration-test/java/org/opensmartgridplatform/adapter/protocol/iec60870/integrationtests/steps/OsgpCoreRequestSteps.java
+++ osgp/protocol-adapter-iec60870/osgp-protocol-adapter-iec60870/src/integration-test/java/org/opensmartgridplatform/adapter/protocol/iec60870/integrationtests/steps/OsgpCoreRequestSteps.java
@@ -15,7 +15,6 @@ import static org.opensmartgridplatform.adapter.protocol.iec60870.testutils.Test
 import static org.opensmartgridplatform.adapter.protocol.iec60870.testutils.TestDefaults.DEFAULT_MESSAGE_TYPE;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -92,11 +91,8 @@ public class OsgpCoreRequestSteps {
 
         assertThat(requestMessage.getDeviceIdentification()).isEqualTo(expectedDeviceIdentification);
         final Serializable request = requestMessage.getRequest();
-        assertThat(request).isInstanceOf(List.class);
-        final List<?> eventNotifications = (List<?>) request;
-        assertThat(eventNotifications).hasSize(1);
-        assertThat(eventNotifications).hasOnlyElementsOfType(EventNotificationDto.class);
-        final EventNotificationDto eventNotification = (EventNotificationDto) eventNotifications.get(0);
-        assertThat(eventNotification.getEventType().name()).isEqualTo(expectedEventType);
+        assertThat(request).isInstanceOf(EventNotificationDto.class);
+        final EventNotificationDto eventNotificationDto = (EventNotificationDto) request;
+        assertThat(eventNotificationDto.getEventType().name()).isEqualTo(expectedEventType);
     }
 }
