b558cfa5038f6196739fa5c96f7518a4f26f81fa
Jan Dik <jandik@gmail.com>
2020-11-12 15:07:29 +0100

    SLIM-2597 fixed compilation failure

---
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/application/services/EncryptionService.java
@@ -0,0 +1,27 @@
+package org.opensmartgridplatform.adapter.protocol.dlms.application.services;
+
+import org.opensmartgridplatform.shared.exceptionhandling.ComponentType;
+import org.opensmartgridplatform.shared.exceptionhandling.FunctionalException;
+import org.opensmartgridplatform.shared.exceptionhandling.FunctionalExceptionType;
+import org.opensmartgridplatform.shared.security.RsaEncryptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EncryptionService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EncryptionService.class);
+
+    @Autowired
+    private RsaEncryptionService rsaEncryptionService;
+
+    public byte[] rsaDecrypt(final byte[] externallyEncryptedKey) throws FunctionalException {
+        try {
+            return this.rsaEncryptionService.decrypt(externallyEncryptedKey);
+        } catch (final Exception e) {
+            LOGGER.error("Unexpected exception during decryption", e);
+            throw new FunctionalException(FunctionalExceptionType.DECRYPTION_EXCEPTION, ComponentType.PROTOCOL_DLMS, e);
+        }
+    }
+}
