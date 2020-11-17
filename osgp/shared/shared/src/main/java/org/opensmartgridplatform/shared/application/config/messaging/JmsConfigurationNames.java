058f066ef2a20b7425ae415c76e962f340dd458a
Robin den Adel <robin.den.adel@cgi.com>
2020-11-17 15:20:02 +0100

    OC-541: Cleans up messaging code. (#508)

--- osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/messaging/JmsConfigurationNames.java
+++ osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/messaging/JmsConfigurationNames.java
@@ -5,7 +5,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
-package org.opensmartgridplatform.shared.application.config.jms;
+package org.opensmartgridplatform.shared.application.config.messaging;
 
 /**
  * Names of JMS configurations. See {@link JmsConfigurationFactory}.
