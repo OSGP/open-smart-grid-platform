c41d0b6ea45619490d1ac22b40a0f9c77297d98a
Thijnvdb <thijnvandenbergh@gmail.com>
2020-11-17 10:42:37 +0100

    implements feedback

--- osgp/platform/osgp-adapter-domain-tariffswitching/src/test/java/org/opensmartgridplatform/adapter/domain/tariffswitching/application/services/ScheduleManagementServiceTest.java
+++ osgp/platform/osgp-adapter-domain-tariffswitching/src/test/java/org/opensmartgridplatform/adapter/domain/tariffswitching/application/services/ScheduleManagementServiceTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2015 Smart Society Services B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.opensmartgridplatform.adapter.domain.tariffswitching.application.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,9 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -63,68 +72,80 @@ public class ScheduleManagementServiceTest{
 	
 	@InjectMocks
 	private ScheduleManagementService scheduleManagementService;
+
+	@Captor
+	final ArgumentCaptor<RequestMessage> requestMessageArgumentCaptor =
+			ArgumentCaptor.forClass(RequestMessage.class);
+	@Captor
+	final ArgumentCaptor<Long> scheduleTimeCaptor = ArgumentCaptor.forClass(long.class);
+	@Captor
+	final ArgumentCaptor<String> ipCaptor = ArgumentCaptor.forClass(String.class);
+	@Captor
+	final ArgumentCaptor<String> messageTypeCaptor = ArgumentCaptor.forClass(String.class);
+	@Captor
+	final ArgumentCaptor<Integer> priorityCapture = ArgumentCaptor.forClass(int.class);
 	
 	private List<ScheduleEntry> scheduleEntries;
 	private List<DeviceOutputSetting> deviceOutputSettings;
 	private List<LightValue> lightValues;
-	private final long scheduleTime = (long) 0.0f;
-	private final String messageType = "testType";
-	private final int priority = 1;
+	private static final long SCHEDULE_TIME = 0L;
+	private static final String MESSAGE_TYPE = "testType";
+	private static final int PRIORITY = 1;
 
-	public void setup() throws FunctionalException {
-		this.scheduleEntries = new ArrayList<>();
-		this.deviceOutputSettings = new ArrayList<>();
-		this.lightValues = new ArrayList<>();
 
-		final DeviceOutputSetting dos = Mockito.mock(DeviceOutputSetting.class);
-		when(dos.getOutputType()).thenReturn(RelayType.TARIFF_REVERSED);
-		
-		final ScheduleEntry entry = Mockito.mock(ScheduleEntry.class);
-		when(entry.getLightValue()).thenReturn(this.lightValues);
-		
-		final LightValue value = Mockito.mock(LightValue.class);
-		
-		//idk how many entries are typically required
-		for(int i =0; i < 3; i++) {
-			this.scheduleEntries.add(entry);
-			this.deviceOutputSettings.add(dos);
-			this.lightValues.add(value);
-		}
-		
-		when(this.device.getId()).thenReturn((long) 0.0f);
-		when(this.ssld.getOutputSettings()).thenReturn(this.deviceOutputSettings);
-		when(this.ssldRepository.findById(any())).thenReturn(Optional.of(this.ssld));
-		when(this.device.getIpAddress()).thenReturn("127.0.0.1");
+	@BeforeEach
+	private void setup() throws FunctionalException {
+		when(this.device.getDeviceType()).thenReturn(Ssld.SSLD_TYPE);
 		when(this.deviceDomainService.searchActiveDevice(any(), any(ComponentType.class))).thenReturn(this.device);
 	}
 	
 	@Test
-	public void testSetTariffSchedule() throws FunctionalException {
-		this.setup();
-		when(this.device.getDeviceType()).thenReturn(Ssld.SSLD_TYPE);
+	void testSetTariffSchedule() throws FunctionalException {
+		this.arrangeTestSetTariffSchedule();
+		this.scheduleManagementService.setTariffSchedule(this.correlationIds, this.scheduleEntries, SCHEDULE_TIME,
+				MESSAGE_TYPE, PRIORITY);
 
-		final ArgumentCaptor<RequestMessage> requestMessageArgumentCaptor =
-				ArgumentCaptor.forClass(RequestMessage.class);
-		final ArgumentCaptor<Long> scheduleTimeCaptor = ArgumentCaptor.forClass(long.class);
-		final ArgumentCaptor<String> ipCaptor = ArgumentCaptor.forClass(String.class);
-		final ArgumentCaptor<String> messageTypeCaptor = ArgumentCaptor.forClass(String.class);
-		final ArgumentCaptor<Integer> priorityCapture = ArgumentCaptor.forClass(int.class);
+		verify(this.osgpCoreRequestMessageSender).send(this.requestMessageArgumentCaptor.capture(),
+				this.messageTypeCaptor.capture(), this.priorityCapture.capture(), this.ipCaptor.capture(),
+				this.scheduleTimeCaptor.capture());
 
-		this.scheduleManagementService.setTariffSchedule(this.correlationIds, this.scheduleEntries, this.scheduleTime,
-				this.messageType, this.priority);
+		assertThat(MESSAGE_TYPE).isEqualTo(this.messageTypeCaptor.getValue());
+		assertThat(SCHEDULE_TIME).isEqualTo(this.scheduleTimeCaptor.getValue());
+		assertThat(PRIORITY).isEqualTo(this.priorityCapture.getValue());
+	}
 
-		verify(this.osgpCoreRequestMessageSender).send(requestMessageArgumentCaptor.capture(), messageTypeCaptor.capture(),priorityCapture.capture(),ipCaptor.capture(), scheduleTimeCaptor.capture());
+	private void arrangeTestSetTariffSchedule() {
+		this.scheduleEntries = new ArrayList<>();
+		this.deviceOutputSettings = new ArrayList<>();
+		this.lightValues = new ArrayList<>();
 
-		assertThat(this.messageType).isEqualTo(messageTypeCaptor.getValue());
-		assertThat(this.scheduleTime).isEqualTo(scheduleTimeCaptor.getValue());
-		assertThat(this.priority).isEqualTo(priorityCapture.getValue());
+		final DeviceOutputSetting deviceOutputSetting = Mockito.mock(DeviceOutputSetting.class);
+		when(deviceOutputSetting.getOutputType()).thenReturn(RelayType.TARIFF_REVERSED);
+
+		final ScheduleEntry entry = Mockito.mock(ScheduleEntry.class);
+		when(entry.getLightValue()).thenReturn(this.lightValues);
+
+		final LightValue value = Mockito.mock(LightValue.class);
+
+		//idk how many entries are typically required
+		for(int i =0; i < 3; i++) {
+			this.scheduleEntries.add(entry);
+			this.deviceOutputSettings.add(deviceOutputSetting);
+			this.lightValues.add(value);
+		}
+
+		when(this.device.getIpAddress()).thenReturn("127.0.0.1");
+		when(this.device.getId()).thenReturn(0L);
+		when(this.ssld.getOutputSettings()).thenReturn(this.deviceOutputSettings);
+		when(this.ssldRepository.findById(any())).thenReturn(Optional.of(this.ssld));
 	}
 	
 	@Test
-	public void testSetTariffScheduleFunctionalExceptionThrown() throws FunctionalException {
+	void testSetTariffScheduleFunctionalExceptionThrown() {
 		//incorrect type should return an exception
+		when(this.device.getDeviceType()).thenReturn(Ssld.PSLD_TYPE);
 		assertThatThrownBy(()-> this.scheduleManagementService.setTariffSchedule(this.correlationIds, this.scheduleEntries,
-				this.scheduleTime, this.messageType, this.priority));
+				SCHEDULE_TIME, MESSAGE_TYPE, PRIORITY)).isInstanceOf(FunctionalException.class);
 		verify(this.osgpCoreRequestMessageSender, never()).send(any(RequestMessage.class), eq("messageType"), eq(1), eq(null));
 	}
 }
