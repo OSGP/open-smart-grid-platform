f2f3130864bb87f32850908d8328cadab0ab36bb
Jaco <jaco.boon@gmail.com>
2020-11-25 15:13:12 +0100

    rolledback to development version

--- runTestsAtRemoteServer.sh
+++ runTestsAtRemoteServer.sh
@@ -70,7 +70,7 @@ CMD="ssh -oStrictHostKeyChecking=no -oTCPKeepAlive=yes -oServerAliveInterval=50 
 ${CMD}
 
 echo '- Create zip file from files from server ...'
-CMD="sudo tar zhcf /tmp/${SERVER}-${PROJECT}.tgz /etc/osgp /etc/httpd/conf.d /usr/share/tomcat/conf /var/log/tomcat /var/log/osgp --warning=no-file-changed --verbose --verbose"
+CMD="sudo tar zhcvf /tmp/${SERVER}-${PROJECT}.tgz /etc/osgp /etc/httpd/conf.d /usr/share/tomcat/conf /var/log/tomcat /var/log/osgp --warning=no-file-changed"
 echo "  [${CMD}]"
 CMD="ssh -oStrictHostKeyChecking=no ${SSH_KEY_FILE} ${USER}@${SERVER} \"\"cd /data/software/${PROJECT} && ${CMD}\"\""
 ${CMD}
