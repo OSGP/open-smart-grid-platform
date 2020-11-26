d7107aa3cc70eea4ec19ea2aac47b9dfa49a1273
Marcel Jansen <34770714+Marcel-Jansen@users.noreply.github.com>
2020-11-26 15:37:21 +0100

    Make failing ssh command more verbose

--- runTestsAtRemoteServer.sh
+++ runTestsAtRemoteServer.sh
@@ -72,7 +72,7 @@ ${CMD}
 echo "- Create zip file from files from server ..."
 CMD="sudo tar zhcf /tmp/${SERVER}-${PROJECT}.tgz /etc/osgp /etc/httpd/conf.d /usr/share/tomcat/conf /var/log/tomcat /var/log/osgp --warning=no-file-changed --verbose --verbose"
 echo "  [${CMD}]"
-CMD="ssh -oStrictHostKeyChecking=no ${SSH_KEY_FILE} ${USER}@${SERVER} \"\"cd /data/software/${PROJECT} && ${CMD}\"\""
+CMD="ssh -v -oStrictHostKeyChecking=no ${SSH_KEY_FILE} ${USER}@${SERVER} \"\"cd /data/software/${PROJECT} && ${CMD}\"\""
 ${CMD}
 
 echo "- Take ownership over /tmp/${SERVER}-${PROJECT}.tgz ..."
