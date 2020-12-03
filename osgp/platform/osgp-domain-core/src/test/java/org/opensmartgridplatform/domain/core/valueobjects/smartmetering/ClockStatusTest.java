63e0d12cc51be47442c0aae373cd885950e7e71c
Michiel <oschmichiel@gmail.com>
2020-12-03 14:01:04 +0100

    Remove public from ClockStatusTest

--- osgp/platform/osgp-domain-core/src/test/java/org/opensmartgridplatform/domain/core/valueobjects/smartmetering/ClockStatusTest.java
+++ osgp/platform/osgp-domain-core/src/test/java/org/opensmartgridplatform/domain/core/valueobjects/smartmetering/ClockStatusTest.java
@@ -13,28 +13,28 @@ import java.util.EnumSet;
 
 import org.junit.jupiter.api.Test;
 
-public class ClockStatusTest {
+class ClockStatusTest {
     private static final ClockStatus CLOCK_STATUS_NOT_SPECIFIED = new ClockStatus(
             (byte) ClockStatus.STATUS_NOT_SPECIFIED);
 
     @Test
-    public void testGetStatusWithEmptyStatusBits() {
+    void testGetStatusWithEmptyStatusBits() {
         final ClockStatus clockStatus = new ClockStatus(EnumSet.noneOf(ClockStatusBit.class));
 
         final int result = clockStatus.getStatus();
 
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isZero();
     }
 
     @Test
-    public void testGetStatusWithStatusNotSpecified() {
+    void testGetStatusWithStatusNotSpecified() {
         final int result = CLOCK_STATUS_NOT_SPECIFIED.getStatus();
 
         assertThat(result).isEqualTo(ClockStatus.STATUS_NOT_SPECIFIED);
     }
 
     @Test
-    public void testHasStatusBitWithStatusNotSpeficied() {
+    void testHasStatusBitWithStatusNotSpeficied() {
         final boolean result = CLOCK_STATUS_NOT_SPECIFIED.hasStatusBits();
 
         assertThat(result).isFalse();
