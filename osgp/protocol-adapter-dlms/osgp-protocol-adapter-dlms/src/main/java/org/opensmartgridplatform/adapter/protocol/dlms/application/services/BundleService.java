cc450dada53f6093f207df50835c3b3a18215444
Jaco <jaco.boon@gmail.com>
2020-12-14 21:26:11 +0100

    fixed fracked up formatting

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/application/services/BundleService.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/application/services/BundleService.java
@@ -1,9 +1,9 @@
 /**
  * Copyright 2016 Smart Society Services B.V.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 package org.opensmartgridplatform.adapter.protocol.dlms.application.services;
@@ -83,8 +83,8 @@ public class BundleService {
                 } catch (final Exception exception) {
                     log.error("Error while executing bundle action for {} with {} [deviceId={}]", actionRequestClass
                             .getName(), executorName, device.getDeviceIdentification(), exception);
-                    final String responseMessage = executor == null ? "Unable to handle request" :
-                            "Error handling request with " + executorName;
+                    final String responseMessage = executor == null ? "Unable to handle request" : "Error handling " +
+                            "request with " + executorName;
                     this.addFaultResponse(actionDto, exception, responseMessage, device);
                 }
             }
