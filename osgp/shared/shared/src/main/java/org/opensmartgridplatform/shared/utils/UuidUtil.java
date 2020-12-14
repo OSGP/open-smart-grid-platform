44d2e9cc8215c0d77294b21280d8caf904cbe86d
Robin den Adel <robin.den.adel@cgi.com>
2020-12-14 10:20:42 +0100

    OC-680: Adds javadoc to the util class.

--- osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/utils/UuidUtil.java
+++ osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/utils/UuidUtil.java
@@ -10,16 +10,28 @@ package org.opensmartgridplatform.shared.utils;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
+/**
+ * Class for util methods dealing with universally unique identifiers (UUIDs).
+ *
+ */
 public class UuidUtil {
 
     private UuidUtil() {
         // hide implicit public constructor
     }
 
+    /**
+     * @return a byte array representing a random UUID
+     */
     public static byte[] getBytesFromRandomUuid() {
         return getBytesFromUUID(UUID.randomUUID());
     }
 
+    /**
+     * @param uuid
+     *            the UUID to generate the byte array with
+     * @return a byte array representing the UUID given as a parameter
+     */
     public static byte[] getBytesFromUUID(final UUID uuid) {
         final ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
         bb.putLong(uuid.getMostSignificantBits());
