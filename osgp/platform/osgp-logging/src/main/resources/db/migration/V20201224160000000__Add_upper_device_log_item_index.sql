db624bdec2df3b170da1f226b2ca0b41d4b0e627
JelleHoffman <jelle.hoffman@hotmail.com>
2021-01-05 09:33:57 +0000

    Removing old index

--- osgp/platform/osgp-logging/src/main/resources/db/migration/V20201224160000000__Add_upper_device_log_item_index.sql
+++ osgp/platform/osgp-logging/src/main/resources/db/migration/V20201224160000000__Add_upper_device_log_item_index.sql
@@ -1,3 +1,4 @@
 DROP INDEX IF EXISTS device_log_item_device_identification_upper;
+DROP INDEX IF EXISTS device_log_item_device_identification_modification_time_idx;
 CREATE INDEX IF NOT EXISTS device_log_item_device_identification_upper_modification_time ON device_log_item(UPPER(device_identification), modification_time);
 ANALYZE device_log_item;
