388edb77c15586d8e70ea7ac060e095194d12ce4
JelleHoffman <jelle.hoffman@hotmail.com>
2021-01-05 09:04:43 +0000

    Adding modification time to index.

--- osgp/platform/osgp-logging/src/main/resources/db/migration/V20201224160000000__Add_upper_device_log_item_index.sql
+++ osgp/platform/osgp-logging/src/main/resources/db/migration/V20201224160000000__Add_upper_device_log_item_index.sql
@@ -1,2 +1,3 @@
-CREATE INDEX IF NOT EXISTS device_log_item_device_identification_upper ON device_log_item(UPPER(device_identification));
+DROP INDEX IF EXISTS device_log_item_device_identification_upper;
+CREATE INDEX IF NOT EXISTS device_log_item_device_identification_upper_modification_time ON device_log_item(UPPER(device_identification), modification_time);
 ANALYZE device_log_item;
