90fbac293f9a5c4d6eea912400bd9d631a01e3d7
Michiel <oschmichiel@gmail.com>
2021-01-14 10:23:42 +0100

    Update PushSetupAlarmDtoConverterTest.java
    One test was fixed

--- osgp/platform/osgp-adapter-domain-smartmetering/src/test/java/org/opensmartgridplatform/adapter/domain/smartmetering/application/mapping/customconverters/PushSetupAlarmDtoConverterTest.java
+++ osgp/platform/osgp-adapter-domain-smartmetering/src/test/java/org/opensmartgridplatform/adapter/domain/smartmetering/application/mapping/customconverters/PushSetupAlarmDtoConverterTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import ma.glasnost.orika.MappingContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -19,10 +20,8 @@ import org.opensmartgridplatform.domain.core.valueobjects.smartmetering.PushSetu
 import org.opensmartgridplatform.domain.core.valueobjects.smartmetering.WindowElement;
 import org.opensmartgridplatform.dto.valueobjects.smartmetering.PushSetupAlarmDto;
 
-import ma.glasnost.orika.MappingContext;
-
 @ExtendWith(MockitoExtension.class)
-public class PushSetupAlarmDtoConverterTest {
+class PushSetupAlarmDtoConverterTest {
     @Mock
     private ConfigurationMapper configurationMapper;
     @Mock
@@ -34,14 +33,14 @@ public class PushSetupAlarmDtoConverterTest {
     private PushSetupAlarmDtoConverter pushSetupAlarmDtoConverter;
 
     @Test
-    public void convertTest() {
+    void convertTest() {
         this.pushSetupAlarmDtoConverter = new PushSetupAlarmDtoConverter(this.configurationMapper);
 
-        final List<WindowElement> testList = new ArrayList<WindowElement>();
+        final List<WindowElement> testList = new ArrayList<>();
         testList.add(new WindowElement(new CosemDateTime(), new CosemDateTime()));
         when(this.pushSetupAlarm.getCommunicationWindow()).thenReturn(testList);
 
-        final List<CosemObjectDefinition> pushObjectList = new ArrayList<CosemObjectDefinition>();
+        final List<CosemObjectDefinition> pushObjectList = new ArrayList<>();
         final CosemObisCode code = new CosemObisCode(1, 2, 3, 4, 5, 6);
         pushObjectList.add(new CosemObjectDefinition(1, code, 2));
         when(this.pushSetupAlarm.getPushObjectList()).thenReturn(pushObjectList);
@@ -54,11 +53,11 @@ public class PushSetupAlarmDtoConverterTest {
     }
 
     @Test
-    public void convertTestWithEmptyLists() {
+    void convertTestWithEmptyLists() {
         this.pushSetupAlarmDtoConverter = new PushSetupAlarmDtoConverter(this.configurationMapper);
 
-        when(this.pushSetupAlarm.getCommunicationWindow()).thenReturn(new ArrayList<>());
-        when(this.pushSetupAlarm.getPushObjectList()).thenReturn(new ArrayList<>());
+        when(this.pushSetupAlarm.getCommunicationWindow()).thenReturn(null);
+        when(this.pushSetupAlarm.getPushObjectList()).thenReturn(null);
 
         final PushSetupAlarmDto result = this.pushSetupAlarmDtoConverter.convert(this.pushSetupAlarm, null,
                 this.mappingContext);
