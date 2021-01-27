f0b40c01e4ff2a6ebbc26327f9d3b11ceb8248ba
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2021-01-27 18:15:51 +0100

    OC-679 ~ Sorts defaults and keys

--- integration-tests/cucumber-tests-platform-distributionautomation/src/test/java/org/opensmartgridplatform/cucumber/platform/distributionautomation/PlatformDistributionAutomationDefaults.java
+++ integration-tests/cucumber-tests-platform-distributionautomation/src/test/java/org/opensmartgridplatform/cucumber/platform/distributionautomation/PlatformDistributionAutomationDefaults.java
@@ -14,11 +14,11 @@ import com.alliander.data.scadameasurementpublishedevent.UnitMultiplier;
 public class PlatformDistributionAutomationDefaults {
 
     public static final Integer COMMON_ADDRESS = 1511;
-    public static final Integer PORT = 2404;
-    public static final String PROFILE = "default_controlled_station";
     public static final String MQTT_HOST = "0.0.0.0";
     public static final Integer MQTT_PORT = 8883;
     public static final String MQTT_QOS = "EXACTLY_ONCE";
+    public static final Integer PORT = 2404;
+    public static final String PROFILE = "default_controlled_station";
     public static final UnitMultiplier UNIT_MULTIPLIER = UnitMultiplier.none;
 
 }
