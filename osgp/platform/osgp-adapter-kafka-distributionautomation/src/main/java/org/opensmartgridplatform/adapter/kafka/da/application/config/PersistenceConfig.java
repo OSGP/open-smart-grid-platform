5ee5bcd1da6c3599d7f4480d73bf7bf26b5fb77f
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2021-01-29 12:00:14 +0100

    OC-679 ~ Replaces constant by configurable @Value field

--- osgp/platform/osgp-adapter-kafka-distributionautomation/src/main/java/org/opensmartgridplatform/adapter/kafka/da/application/config/PersistenceConfig.java
+++ osgp/platform/osgp-adapter-kafka-distributionautomation/src/main/java/org/opensmartgridplatform/adapter/kafka/da/application/config/PersistenceConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.opensmartgridplatform.adapter.kafka.da.domain.repositories.LocationRepository;
 import org.opensmartgridplatform.shared.application.config.AbstractPersistenceConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -16,7 +17,8 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 @EnableJpaRepositories(basePackageClasses = { LocationRepository.class })
 public class PersistenceConfig extends AbstractPersistenceConfig {
 
-    private static final String ENTITY_PACKAGES_TO_SCAN = "org.opensmartgridplatform.adapter.kafka.da.domain.entities";
+    @Value("${entitymanager.packages.to.scan}")
+    private String entityPackagesToScan;
 
     @Bean(destroyMethod = "close")
     public DataSource dataSource() {
@@ -39,7 +41,7 @@ public class PersistenceConfig extends AbstractPersistenceConfig {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         return super.entityManagerFactory("OSGP_ADAPTER_KAFKA_DISTRIBUTIONAUTOMATION", this.dataSource(),
-                ENTITY_PACKAGES_TO_SCAN);
+                this.entityPackagesToScan);
     }
 
 }
