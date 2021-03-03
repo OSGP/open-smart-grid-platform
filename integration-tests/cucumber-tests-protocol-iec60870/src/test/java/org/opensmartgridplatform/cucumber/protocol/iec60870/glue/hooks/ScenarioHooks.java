ffd11163146c6eb516d4814b2ff6be64d24d4337
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2021-03-03 11:03:16 +0100

    FLEX-5434 ~ Adds mock server scenario hooks

--- integration-tests/cucumber-tests-protocol-iec60870/src/test/java/org/opensmartgridplatform/cucumber/protocol/iec60870/glue/hooks/ScenarioHooks.java
+++ integration-tests/cucumber-tests-protocol-iec60870/src/test/java/org/opensmartgridplatform/cucumber/protocol/iec60870/glue/hooks/ScenarioHooks.java
@@ -9,7 +9,13 @@
  */
 package org.opensmartgridplatform.cucumber.protocol.iec60870.glue.hooks;
 
+import org.openmuc.j60870.ASduType;
 import org.opensmartgridplatform.cucumber.protocol.iec60870.database.Iec60870Database;
+import org.opensmartgridplatform.cucumber.protocol.iec60870.mock.Iec60870MockServer;
+import org.opensmartgridplatform.simulator.protocol.iec60870.domain.Iec60870AsduFactory;
+import org.opensmartgridplatform.simulator.protocol.iec60870.domain.profile.DefaultControlledStationAsduFactory;
+import org.opensmartgridplatform.simulator.protocol.iec60870.domain.profile.LightMeasurementDeviceAsduFactory;
+import org.opensmartgridplatform.simulator.protocol.iec60870.server.handlers.Iec60870InterrogationCommandAsduHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.cucumber.java.Before;
@@ -19,9 +25,27 @@ public class ScenarioHooks {
     @Autowired
     private Iec60870Database database;
 
+    @Autowired
+    private Iec60870MockServer mockServer;
+
     @Before(order = 1000)
     public void beforeScenario() {
         this.database.prepareForScenario();
     }
 
+    @Before("@Iec60870MockServerLightMeasurement")
+    public void initMockServerLightMeasurement() {
+        final Iec60870AsduFactory factory = new LightMeasurementDeviceAsduFactory();
+        factory.setIec60870Server(this.mockServer.getRtuSimulator());
+        this.mockServer.addIec60870ASduHandler(ASduType.C_IC_NA_1,
+                new Iec60870InterrogationCommandAsduHandler(factory));
+    }
+
+    @Before("@Iec60870MockServerDefaultControlledStation")
+    public void initMockServerDefaultControlledStation() {
+        final Iec60870AsduFactory factory = new DefaultControlledStationAsduFactory();
+        factory.setIec60870Server(this.mockServer.getRtuSimulator());
+        this.mockServer.addIec60870ASduHandler(ASduType.C_IC_NA_1,
+                new Iec60870InterrogationCommandAsduHandler(factory));
+    }
 }
