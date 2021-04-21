3da98dc17340da6f0ee7b69945a59c6ee7e44f62
Coen Damen <cgmmdamen@gmail.com>
2021-04-21 11:09:27 +0200

    SLIM-1883 adds sql file

---
+++ osgp/platform/osgp-core/src/main/resources/db/migration/V20210324084615051__Add_btsid_and_cellid_to_device.sql
@@ -0,0 +1,23 @@
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+      SELECT 1 FROM information_schema.columns
+      WHERE TABLE_SCHEMA = current_schema AND TABLE_NAME = 'device' AND COLUMN_NAME = 'bts_id')
+    THEN
+        ALTER TABLE device ADD COLUMN bts_id INTEGER;
+    END IF;
+
+    COMMENT ON COLUMN device.bts_id IS 'The ID of the Base Transceiver Station to which this device is linked.';
+
+    IF NOT EXISTS (
+      SELECT 1 FROM information_schema.columns
+      WHERE TABLE_SCHEMA = current_schema AND TABLE_NAME = 'device' AND COLUMN_NAME = 'cell_id')
+    THEN
+        ALTER TABLE device ADD COLUMN cell_id INTEGER;
+    END IF;
+
+    COMMENT ON COLUMN device.cell_id IS 'The ID of the Cell to which this device is linked.';
+
+END;
+$$
