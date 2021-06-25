a240c10a412c49140bfb73feb9c1db7b370a1339
Gerben Kroes <gerben@kroesctrl.nl>
2021-06-25 14:37:38 +0200

    Bundle exception handling

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/messaging/DlmsConnectionMessageProcessor.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/messaging/DlmsConnectionMessageProcessor.java
@@ -175,8 +175,7 @@ public abstract class DlmsConnectionMessageProcessor {
     }
 
     final RetryHeader retryHeader;
-    if (result == ResponseMessageResultType.NOT_OK
-        && !(exception instanceof NonRetryableException)) {
+    if (this.shouldRetry(result, exception, responseObject)) {
       retryHeader = this.retryHeaderFactory.createRetryHeader(messageMetadata.getRetryCount());
     } else {
       retryHeader = this.retryHeaderFactory.createEmtpyRetryHeader();
@@ -196,5 +195,13 @@ public abstract class DlmsConnectionMessageProcessor {
             .build();
 
     responseMessageSender.send(responseMessage);
+  }
+
+  protected boolean shouldRetry(
+      final ResponseMessageResultType result,
+      final Exception exception,
+      final Serializable responseObject) {
+    return result == ResponseMessageResultType.NOT_OK
+        && !(exception instanceof NonRetryableException);
   }
 }
