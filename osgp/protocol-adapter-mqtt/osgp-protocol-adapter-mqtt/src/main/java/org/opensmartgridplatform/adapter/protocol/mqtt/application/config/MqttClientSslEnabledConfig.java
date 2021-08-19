8923b49dce48a885a1c1ab715b3b3ffa9220a9e5
Sander Verbruggen <sander.verbruggen@alliander.com>
2021-08-19 15:22:08 +0200

    FDP-293: Read SSL certificates from (maintained) OS file instead of keystore
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>

--- osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/config/MqttClientSslEnabledConfig.java
+++ osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/config/MqttClientSslEnabledConfig.java
@@ -11,8 +11,6 @@ package org.opensmartgridplatform.adapter.protocol.mqtt.application.config;
 
 import com.hivemq.client.mqtt.MqttClientSslConfig;
 import org.opensmartgridplatform.shared.application.config.mqtt.MqttClientSslConfigFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -22,18 +20,10 @@ import org.springframework.core.io.Resource;
 @Configuration
 @Conditional(MqttClientSslEnabledCondition.class)
 public class MqttClientSslEnabledConfig {
-
-  private static final Logger LOG = LoggerFactory.getLogger(MqttClientSslEnabledConfig.class);
-
   @Bean
   public MqttClientSslConfig mqttClientSslConfig(
-      @Value("${mqtt.client.ssl.truststore.location}") final Resource truststoreLocation,
-      @Value("${mqtt.client.ssl.truststore.password}") final String truststorePassword,
-      @Value("${mqtt.client.ssl.truststore.type}") final String truststoreType) {
+      @Value("${mqtt.client.ssl.certFile.location}") final Resource certFileLocation) {
 
-    LOG.info("MQTT SSL ENABLED.");
-
-    return MqttClientSslConfigFactory.getMqttClientSslConfig(
-        truststoreLocation, truststorePassword, truststoreType);
+    return MqttClientSslConfigFactory.getMqttClientSslConfig(certFileLocation);
   }
 }
--- osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/mqtt/MqttClientSslConfigFactory.java
+++ osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/mqtt/MqttClientSslConfigFactory.java
@@ -15,6 +15,8 @@ import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import javax.net.ssl.TrustManagerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,12 +41,19 @@ public class MqttClientSslConfigFactory {
         .build();
   }
 
+  public static MqttClientSslConfig getMqttClientSslConfig(final Resource certFileLocation) {
+
+    return MqttClientSslConfig.builder()
+        .trustManagerFactory(getTruststoreFactory(certFileLocation))
+        .build();
+  }
+
   private static TrustManagerFactory getTruststoreFactory(
       final Resource trustStoreResource,
       final String trustStorePassword,
       final String trustStoreType) {
 
-    try (InputStream in = trustStoreResource.getInputStream()) {
+    try (final InputStream in = trustStoreResource.getInputStream()) {
       LOGGER.info("Load truststore from path: {}", trustStoreResource.getURI());
 
       final KeyStore trustStore = KeyStore.getInstance(trustStoreType);
@@ -53,6 +62,35 @@ public class MqttClientSslConfigFactory {
       final TrustManagerFactory tmf =
           TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
       tmf.init(trustStore);
+
+      return tmf;
+    } catch (final IOException e) {
+      throw new UncheckedIOException(e);
+    } catch (final GeneralSecurityException e) {
+      throw new SecurityException(e);
+    }
+  }
+
+  /**
+   * Creates a TrustManagerFactory from a .crt file.
+   *
+   * @param certFileLocation Location of the .crt file
+   * @return TrustManagerFactory
+   */
+  private static TrustManagerFactory getTruststoreFactory(final Resource certFileLocation) {
+    try (final InputStream in = certFileLocation.getInputStream()) {
+      LOGGER.info("Load truststore from path: {}", certFileLocation.getURI());
+
+      final CertificateFactory cf = CertificateFactory.getInstance("X.509");
+      final X509Certificate caCert = (X509Certificate) cf.generateCertificate(in);
+
+      final TrustManagerFactory tmf =
+          TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      final KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+      keyStore.load(null); // You don't need the KeyStore instance to come from a file.
+      keyStore.setCertificateEntry("caCert", caCert);
+
+      tmf.init(keyStore);
 
       return tmf;
     } catch (final IOException e) {
