253cdce0bb0d7d1f0ca989578ed5d8099d852049
Jelle Hoffman <jelle.hoffman@hotmail.com>
2021-08-23 16:28:45 +0200

    Feature/FDP 293 Adds connection logging to central MQTT broker if enabled (#701)
    * Updated logging to get some more info during startup if enabled
    
    * Update formatting
    
    * FDP-293: Read SSL certificates from (maintained) OS file instead of keystore
    
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>
    
    * Separated MQTT SSL Enabled conditions
    
    * Removal off simulator references
    
    Co-authored-by: Sander Verbruggen <sander.verbruggen@alliander.com>

---
+++ osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/config/MqttClientSslKeystoreEnabledConfig.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Alliander N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.opensmartgridplatform.adapter.protocol.mqtt.application.config;
+
+import com.hivemq.client.mqtt.MqttClientSslConfig;
+import org.opensmartgridplatform.shared.application.config.mqtt.MqttClientSslConfigFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+@Conditional(MqttClientSslKeystoreEnabledCondition.class)
+public class MqttClientSslKeystoreEnabledConfig {
+  @Bean
+  public MqttClientSslConfig mqttClientSslConfig(
+      @Value("${mqtt.client.ssl.truststore.location}") final Resource truststoreLocation,
+      @Value("${mqtt.client.ssl.truststore.password}") final String truststorePassword,
+      @Value("${mqtt.client.ssl.truststore.type}") final String truststoreType) {
+
+    return MqttClientSslConfigFactory.getMqttClientSslConfig(
+        truststoreLocation, truststorePassword, truststoreType);
+  }
+}
