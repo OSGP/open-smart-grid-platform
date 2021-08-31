33183ab9a69cd884e765f72d0520866983d14d89
Jaco <jaco.boon@gmail.com>
2021-08-31 19:40:49 +0200

    CommandExecutor for ClearMBusStatus
    Signed-off-by: Jaco <jaco.boon@gmail.com>

---
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/alarm/ClearMBusStatusCommandExecutor.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2021 Alliander N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.alarm;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.openmuc.jdlms.AccessResultCode;
+import org.openmuc.jdlms.AttributeAddress;
+import org.openmuc.jdlms.GetResult;
+import org.openmuc.jdlms.MethodParameter;
+import org.openmuc.jdlms.MethodResult;
+import org.openmuc.jdlms.MethodResultCode;
+import org.openmuc.jdlms.ObisCode;
+import org.openmuc.jdlms.SetParameter;
+import org.openmuc.jdlms.datatypes.DataObject;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.AbstractCommandExecutor;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.utils.JdlmsObjectToStringUtil;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.entities.DlmsDevice;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.entities.Protocol;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.factories.DlmsConnectionManager;
+import org.opensmartgridplatform.adapter.protocol.dlms.exceptions.ConnectionException;
+import org.opensmartgridplatform.adapter.protocol.dlms.exceptions.NotSupportedByProtocolException;
+import org.opensmartgridplatform.adapter.protocol.dlms.exceptions.ProtocolAdapterException;
+import org.opensmartgridplatform.dlms.interfaceclass.InterfaceClass;
+import org.opensmartgridplatform.dlms.interfaceclass.attribute.DataAttribute;
+import org.opensmartgridplatform.dlms.interfaceclass.attribute.ExtendedRegisterAttribute;
+import org.opensmartgridplatform.dlms.interfaceclass.method.MBusClientMethod;
+import org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmNotificationsDto;
+import org.opensmartgridplatform.dto.valueobjects.smartmetering.SetAlarmNotificationsRequestDto;
+import org.opensmartgridplatform.shared.infra.jms.MessageMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ClearMBusStatusCommandExecutor
+    extends AbstractCommandExecutor<AlarmNotificationsDto, AccessResultCode> {
+
+  private static final int CLASS_ID_READ_STATUS = InterfaceClass.EXTENDED_REGISTER.id();
+  private static final int ATTR_ID_READ_STATUS = ExtendedRegisterAttribute.VALUE.attributeId();
+
+  private static final AttributeAddress ATTR_READ_STATUS_MBUS_1 =
+      new AttributeAddress(CLASS_ID_READ_STATUS, "0.1.24.2.6.255", ATTR_ID_READ_STATUS);
+  private static final AttributeAddress ATTR_READ_STATUS_MBUS_2 =
+      new AttributeAddress(CLASS_ID_READ_STATUS, "0.2.24.2.6.255", ATTR_ID_READ_STATUS);
+  private static final AttributeAddress ATTR_READ_STATUS_MBUS_3 =
+      new AttributeAddress(CLASS_ID_READ_STATUS, "0.3.24.2.6.255", ATTR_ID_READ_STATUS);
+  private static final AttributeAddress ATTR_READ_STATUS_MBUS_4 =
+      new AttributeAddress(CLASS_ID_READ_STATUS, "0.4.24.2.6.255", ATTR_ID_READ_STATUS);
+
+  private static final int CLASS_ID_CLEAR_STATUS = InterfaceClass.DATA.id();
+  private static final int ATTR_ID_CLEAR_STATUS = DataAttribute.VALUE.attributeId();
+
+  private static final AttributeAddress ATTR_CLEAR_STATUS_MBUS_1 =
+      new AttributeAddress(CLASS_ID_CLEAR_STATUS, "0.1.94.31.10.255", ATTR_ID_CLEAR_STATUS);
+  private static final AttributeAddress ATTR_CLEAR_STATUS_MBUS_2 =
+      new AttributeAddress(CLASS_ID_CLEAR_STATUS, "0.2.94.31.10.255", ATTR_ID_CLEAR_STATUS);
+  private static final AttributeAddress ATTR_CLEAR_STATUS_MBUS_3 =
+      new AttributeAddress(CLASS_ID_CLEAR_STATUS, "0.3.94.31.10.255", ATTR_ID_CLEAR_STATUS);
+  private static final AttributeAddress ATTR_CLEAR_STATUS_MBUS_4 =
+      new AttributeAddress(CLASS_ID_CLEAR_STATUS, "0.4.94.31.10.255", ATTR_ID_CLEAR_STATUS);
+
+  private static final ObisCode OBIS_CODE_CLIENT_SETUP_MBUS_1 = new ObisCode("0.1.24.1.0.255");
+  private static final ObisCode OBIS_CODE_CLIENT_SETUP_MBUS_2 = new ObisCode("0.2.24.1.0.255");
+  private static final ObisCode OBIS_CODE_CLIENT_SETUP_MBUS_3 = new ObisCode("0.3.24.1.0.255");
+  private static final ObisCode OBIS_CODE_CLIENT_SETUP_MBUS_4 = new ObisCode("0.4.24.1.0.255");
+
+  private static final Map<Integer, AttributeAddress> ATTR_READ_STATUS_MAP = new HashMap<>();
+  private static final Map<Integer, AttributeAddress> ATTR_SET_STATUS_MAP = new HashMap<>();
+  private static final Map<Integer, ObisCode> OBIS_CODE_CLIENT_SETUP_MAP = new HashMap<>();
+
+  static {
+    ATTR_READ_STATUS_MAP.put(1, ATTR_READ_STATUS_MBUS_1);
+    ATTR_READ_STATUS_MAP.put(2, ATTR_READ_STATUS_MBUS_2);
+    ATTR_READ_STATUS_MAP.put(3, ATTR_READ_STATUS_MBUS_3);
+    ATTR_READ_STATUS_MAP.put(4, ATTR_READ_STATUS_MBUS_4);
+
+    ATTR_SET_STATUS_MAP.put(1, ATTR_CLEAR_STATUS_MBUS_1);
+    ATTR_SET_STATUS_MAP.put(2, ATTR_CLEAR_STATUS_MBUS_2);
+    ATTR_SET_STATUS_MAP.put(3, ATTR_CLEAR_STATUS_MBUS_3);
+    ATTR_SET_STATUS_MAP.put(4, ATTR_CLEAR_STATUS_MBUS_4);
+
+    OBIS_CODE_CLIENT_SETUP_MAP.put(1, OBIS_CODE_CLIENT_SETUP_MBUS_1);
+    OBIS_CODE_CLIENT_SETUP_MAP.put(2, OBIS_CODE_CLIENT_SETUP_MBUS_2);
+    OBIS_CODE_CLIENT_SETUP_MAP.put(3, OBIS_CODE_CLIENT_SETUP_MBUS_3);
+    OBIS_CODE_CLIENT_SETUP_MAP.put(4, OBIS_CODE_CLIENT_SETUP_MBUS_4);
+  }
+
+  private static final int[] CHANNELS = {1, 2, 3, 4};
+
+  @Autowired
+  public ClearMBusStatusCommandExecutor() {
+    // TODO replace DTO
+    super(SetAlarmNotificationsRequestDto.class);
+  }
+
+  @Override
+  public AccessResultCode execute(
+      final DlmsConnectionManager conn,
+      final DlmsDevice device,
+      final AlarmNotificationsDto alarmNotifications,
+      final MessageMetadata messageMetadata)
+      throws ProtocolAdapterException {
+
+    if (Protocol.forDevice(device) != Protocol.SMR_5_1) {
+      throw new NotSupportedByProtocolException("ClearMBusStatus not supported by protocol.");
+    }
+
+    try {
+      for (final int channel : CHANNELS) {
+        this.clearStatus(conn, channel);
+      }
+    } catch (final IOException e) {
+      throw new ConnectionException(e);
+    }
+
+    return AccessResultCode.SUCCESS;
+  }
+
+  private void clearStatus(final DlmsConnectionManager conn, final int channel)
+      throws IOException, ProtocolAdapterException {
+
+    final long statusMask = this.readStatus(conn, channel);
+
+    final AccessResultCode resultCode = this.clearStatus(statusMask, conn, channel);
+
+    if (resultCode != AccessResultCode.SUCCESS) {
+      throw new ProtocolAdapterException(
+          "Unable to set clear status mask for MBus channel "
+              + channel
+              + ", AccessResultCode="
+              + resultCode
+              + ".");
+    }
+
+    final MethodResult methodResult = this.resetAlarm(conn, channel);
+
+    if (methodResult.getResultCode() != MethodResultCode.SUCCESS) {
+      throw new ProtocolAdapterException(
+          "Call for RESET_ALARM was unsuccessful for MBus channel "
+              + channel
+              + ", MethodResultCode="
+              + methodResult.getResultCode()
+              + ".");
+    }
+  }
+
+  private long readStatus(final DlmsConnectionManager conn, final Integer channel)
+      throws IOException, ProtocolAdapterException {
+    final AttributeAddress attributeAddress = ATTR_READ_STATUS_MAP.get(channel);
+
+    conn.getDlmsMessageListener()
+        .setDescription(
+            "ClearMBusStatus read status"
+                + JdlmsObjectToStringUtil.describeAttributes(attributeAddress));
+
+    log.info(
+        "Reading status for MBus channel {} with attributeAddress: {}.", channel, attributeAddress);
+
+    final GetResult result = conn.getConnection().get(attributeAddress);
+
+    if (result == null) {
+      throw new ProtocolAdapterException(
+          "No GetResult received while reading status for MBus channel " + channel + ".");
+    }
+
+    return this.parseStatusFilter(result.getResultData(), channel);
+  }
+
+  private AccessResultCode clearStatus(
+      final long statusMask, final DlmsConnectionManager conn, final Integer channel)
+      throws IOException {
+    final AttributeAddress attributeAddress = ATTR_SET_STATUS_MAP.get(channel);
+    final SetParameter parameter =
+        new SetParameter(attributeAddress, DataObject.newUInteger32Data(statusMask));
+
+    conn.getDlmsMessageListener()
+        .setDescription(
+            "ClearMBusStatus writing status mask "
+                + JdlmsObjectToStringUtil.describeAttributes(attributeAddress));
+
+    log.info(
+        "Writing clear status mask {} for MBus channel {} with attributeAddress: {}.",
+        statusMask,
+        channel,
+        attributeAddress);
+    return conn.getConnection().set(parameter);
+  }
+
+  private MethodResult resetAlarm(final DlmsConnectionManager conn, final int channel)
+      throws IOException {
+    final MBusClientMethod method = MBusClientMethod.RESET_ALARM;
+    final MethodParameter methodParameter =
+        new MethodParameter(
+            method.getInterfaceClass().id(),
+            OBIS_CODE_CLIENT_SETUP_MAP.get(channel),
+            method.getMethodId());
+
+    conn.getDlmsMessageListener()
+        .setDescription(
+            "ClearMBusStatus for channel"
+                + channel
+                + " - calling client setup: "
+                + JdlmsObjectToStringUtil.describeMethod(methodParameter));
+
+    log.info(
+        "Calling method RESET_ALARM for channel {} with methodParam: {}.",
+        channel,
+        methodParameter);
+    return conn.getConnection().action(methodParameter);
+  }
+
+  private Long parseStatusFilter(final DataObject statusMask, final int channel)
+      throws ProtocolAdapterException {
+
+    if (statusMask == null) {
+      throw new ProtocolAdapterException(
+          "DataObject expected to contain a status mask for MBus channel " + channel + " is null.");
+    }
+
+    if (!statusMask.isNumber()) {
+      throw new ProtocolAdapterException(
+          "DataObject isNumber is expected to be true for status mask of MBus channel "
+              + channel
+              + ".");
+    }
+
+    if (!(statusMask.getValue() instanceof Number)) {
+      throw new ProtocolAdapterException(
+          "Value in DataObject is not a java.lang.Number: "
+              + statusMask.getValue().getClass().getName()
+              + " in MBus channel "
+              + channel
+              + ".");
+    }
+
+    final Number maskValue = statusMask.getValue();
+    return maskValue.longValue();
+  }
+}
---
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/alarm/ClearMBusStatusCommandExecutorTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2021 Alliander N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.alarm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openmuc.jdlms.AccessResultCode;
+import org.openmuc.jdlms.AttributeAddress;
+import org.openmuc.jdlms.DlmsConnection;
+import org.openmuc.jdlms.GetResult;
+import org.openmuc.jdlms.MethodParameter;
+import org.openmuc.jdlms.MethodResult;
+import org.openmuc.jdlms.MethodResultCode;
+import org.openmuc.jdlms.SetParameter;
+import org.openmuc.jdlms.datatypes.DataObject;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.entities.DlmsDevice;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.factories.DlmsConnectionManager;
+import org.opensmartgridplatform.adapter.protocol.dlms.exceptions.NotSupportedByProtocolException;
+import org.opensmartgridplatform.adapter.protocol.dlms.exceptions.ProtocolAdapterException;
+import org.opensmartgridplatform.adapter.protocol.dlms.infra.messaging.DlmsMessageListener;
+import org.opensmartgridplatform.adapter.protocol.dlms.infra.messaging.LoggingDlmsMessageListener;
+import org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmNotificationsDto;
+import org.opensmartgridplatform.shared.infra.jms.MessageMetadata;
+
+@ExtendWith(MockitoExtension.class)
+class ClearMBusStatusCommandExecutorTest {
+
+  private static final String OBIS_CODE_TEMPLATE_READ_STATUS = "0.x.24.2.6.255";
+  private static final int CLASS_ID_READ_STATUS = 4;
+
+  private static final String OBIS_CODE_TEMPLATE_CLEAR_STATUS = "0.x.94.31.10.255";
+  private static final int CLASS_ID_CLEAR_STATUS = 1;
+
+  @Mock private DlmsConnectionManager connectionManager;
+
+  @Mock private DlmsConnection dlmsConnection;
+
+  private DlmsMessageListener dlmsMessageListener;
+
+  @Mock private AlarmNotificationsDto dto;
+
+  @Mock private MessageMetadata messageMetadata;
+
+  @Mock private GetResult getResult;
+
+  @Mock private MethodResult methodResult;
+
+  @Captor private ArgumentCaptor<AttributeAddress> attributeAddressArgumentCaptor;
+
+  @Captor private ArgumentCaptor<SetParameter> setParameterArgumentCaptor;
+
+  @Captor private ArgumentCaptor<MethodParameter> methodParameterArgumentCaptor;
+
+  private ClearMBusStatusCommandExecutor executor;
+
+  @BeforeEach
+  void setup() {
+    this.executor = new ClearMBusStatusCommandExecutor();
+    this.dlmsMessageListener = new LoggingDlmsMessageListener(null, null);
+  }
+
+  @Test
+  void shouldNotExecuteForProtocol_DSMR_4_2_2() {
+    final DlmsDevice dlmsDevice_4_2_2 = new DlmsDevice("DSMR 4.2.2 device");
+    dlmsDevice_4_2_2.setProtocol("DSMR", "4.2.2");
+
+    assertThatExceptionOfType(NotSupportedByProtocolException.class)
+        .isThrownBy(
+            () -> {
+              this.executor.execute(
+                  this.connectionManager, dlmsDevice_4_2_2, this.dto, this.messageMetadata);
+            });
+  }
+
+  @Test
+  void shouldNotExecuteForProtocol_SMR_5_0_0() {
+    final DlmsDevice dlmsDevice_5_0_0 = new DlmsDevice("SMR 5.0.0 device");
+    dlmsDevice_5_0_0.setProtocol("SMR", "5.0.0");
+
+    assertThatExceptionOfType(NotSupportedByProtocolException.class)
+        .isThrownBy(
+            () -> {
+              this.executor.execute(
+                  this.connectionManager, dlmsDevice_5_0_0, this.dto, this.messageMetadata);
+            });
+  }
+
+  @Test
+  void shouldExecuteForProtocol_SMR_5_1() throws ProtocolAdapterException, IOException {
+    final DlmsDevice dlmsDevice_5_1 = new DlmsDevice("SMR 5.1 device");
+    dlmsDevice_5_1.setProtocol("SMR", "5.1");
+
+    when(this.connectionManager.getDlmsMessageListener()).thenReturn(this.dlmsMessageListener);
+    when(this.connectionManager.getConnection()).thenReturn(this.dlmsConnection);
+    when(this.dlmsConnection.get(this.attributeAddressArgumentCaptor.capture()))
+        .thenReturn(this.getResult);
+    when(this.getResult.getResultData()).thenReturn(DataObject.newUInteger32Data(0L));
+    when(this.dlmsConnection.set(this.setParameterArgumentCaptor.capture()))
+        .thenReturn(AccessResultCode.SUCCESS);
+    when(this.methodResult.getResultCode()).thenReturn(MethodResultCode.SUCCESS);
+    when(this.dlmsConnection.action(this.methodParameterArgumentCaptor.capture()))
+        .thenReturn(this.methodResult);
+
+    this.executor.execute(this.connectionManager, dlmsDevice_5_1, this.dto, this.messageMetadata);
+
+    this.assertCurrentStatusAttributeAddresses(this.attributeAddressArgumentCaptor.getAllValues());
+    this.assertClearStatus(this.setParameterArgumentCaptor.getAllValues());
+  }
+
+  private void assertClearStatus(final List<SetParameter> setParameters) {
+    for (int i = 1; i <= 4; i++) {
+      final SetParameter setParameter = setParameters.get(i - 1);
+      assertThat(((Number) setParameter.getData().getValue()).longValue()).isEqualTo(0L);
+
+      final AttributeAddress attributeAddress = setParameter.getAttributeAddress();
+      assertThat(attributeAddress.getInstanceId().asDecimalString())
+          .isEqualTo(OBIS_CODE_TEMPLATE_CLEAR_STATUS.replace('x', (char) (i + '0')));
+      assertThat(attributeAddress.getClassId()).isEqualTo(CLASS_ID_CLEAR_STATUS);
+    }
+  }
+
+  private void assertCurrentStatusAttributeAddresses(final List<AttributeAddress> attributes) {
+    for (int i = 1; i <= 4; i++) {
+      final AttributeAddress attributeAddress = attributes.get(i - 1);
+      assertThat(attributeAddress.getInstanceId().asDecimalString())
+          .isEqualTo(OBIS_CODE_TEMPLATE_READ_STATUS.replace('x', (char) (i + '0')));
+      assertThat(attributeAddress.getClassId()).isEqualTo(CLASS_ID_READ_STATUS);
+    }
+  }
+}
