e238e6371198185acdb2293151d747c7b8088117
JelleHoffman <jelle.hoffman@hotmail.com>
2021-09-16 13:46:48 +0000

    Adding correct amount of test values

--- osgp/platform/osgp-adapter-kafka-distributionautomation/src/test/java/org/opensmartgridplatform/adapter/kafka/da/application/mapping/ScadaMeasurementPublishedEventConverterTest.java
+++ osgp/platform/osgp-adapter-kafka-distributionautomation/src/test/java/org/opensmartgridplatform/adapter/kafka/da/application/mapping/ScadaMeasurementPublishedEventConverterTest.java
@@ -68,7 +68,7 @@ class ScadaMeasurementPublishedEventConverterTest {
   void testConvertScadaMeasurementPublishedEventVersion2() {
     final String data =
         "0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,"
-            + "2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0,4.1,5,6,7,8,9,0,1,2,3,4";
+            + "2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9";
     final int feeder = 8;
     final long utcSeconds = 1598684400;
     final ScadaMeasurementPayload payload =
