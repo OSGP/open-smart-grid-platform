bbd00497d8d3cc3233b9695b8439b7c9ce73b38e
Gerben Kroes <gerben@kroesctrl.nl>
2021-10-11 11:15:01 +0200

    Added a comment
    Signed-off-by: Gerben Kroes <gerben@kroesctrl.nl>

--- osgp/platform/osgp-core/src/main/resources/db/migration/V20211007131424000__deletes_CDMA_specific_protocol_info.sql
+++ osgp/platform/osgp-core/src/main/resources/db/migration/V20211007131424000__deletes_CDMA_specific_protocol_info.sql
@@ -2,6 +2,12 @@ DO
 $$
 begin
 
+/*
+ * There are 3 records created in previous flyway scripts SMR_CDMA (5.1), SMR_CDMA (5.0.0) and DSMR_CDMA (4.2.2)
+ * These records are CDMA specific, and should not be added by the flyway scripts.
+ * In production there could be other specific protocols already created by hand (for example: SMR_GPRS and SMR_LTE_M),
+ * when exist in the database, then we do not want to delete the 3 protocol specific records.
+ */
 if ((select count(*)
      from   protocol_info
      where  protocol like '%SMR_%') = 3) then
