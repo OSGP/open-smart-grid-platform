fdda0653954da9844cdece1b2de03efd0c32a4c5
Harry Middelburg <69151013+harrymiddelburgetall@users.noreply.github.com>
2021-12-08 14:57:43 +0100

    Update osgp-nightly-build.groovy
    Signed-off-by: Harry Middelburg <harry.middelburg@alliander.com>

--- jenkins/pipeline/osgp-nightly-build.groovy
+++ jenkins/pipeline/osgp-nightly-build.groovy
@@ -5,7 +5,7 @@ def servername = stream + '-at-' + env.BUILD_NUMBER
 def playbook = stream + '-at.yml'
 
 // Choose the branch to use for SmartSocietyServices/release repository. Default value is 'master'.
-def branchReleaseRepo = 'master'
+def branchReleaseRepo = 'SMHE-228-Prevent-simultaneous-Replace-Key-operations'
 
 pipeline {
     agent {
