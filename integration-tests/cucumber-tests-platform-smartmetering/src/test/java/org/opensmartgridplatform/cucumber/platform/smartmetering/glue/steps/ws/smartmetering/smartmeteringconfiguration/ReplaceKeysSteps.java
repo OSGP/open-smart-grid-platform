3ecb657f6a7b87ff2c13472f7aef24ae3067aac4
Harry Middelburg <harry.middelburg@alliander.com>
2022-01-06 10:57:39 +0100

    Removed commented lines of code
    Signed-off-by: Harry Middelburg <harry.middelburg@alliander.com>

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringconfiguration/ReplaceKeysSteps.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/ws/smartmetering/smartmeteringconfiguration/ReplaceKeysSteps.java
@@ -61,10 +61,6 @@ public class ReplaceKeysSteps extends AbstractSmartMeteringSteps {
       throws Throwable {
     final List<Map<String, String>> listOfSettingsPerRequest =
         this.createSettingPerRequest(settings);
-    //    if (listOfSettingsPerRequest.size() != 2) {
-    //      throw new IllegalArgumentException("This scenario only expects TWO replace keys
-    // requests");
-    //    }
     final List<String> correlationUIDs = new ArrayList<>();
     for (final Map<String, String> settingsPerRequest : listOfSettingsPerRequest) {
       final ReplaceKeysRequest request =
