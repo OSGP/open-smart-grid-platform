a4b3ab346d14222dc6281e16ce5383c727858db9
Stefan Ermens <stefan.ermens@alliander.com>
2022-03-01 10:19:03 +0100

    SMHE-650 Fix obis code for alarm register 2 (#799)
    Signed-off-by: stefanermens <stefan.ermens@alliander.com>

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/alarm/SetAlarmNotificationsCommandExecutorTest.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/alarm/SetAlarmNotificationsCommandExecutorTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,11 +28,11 @@ import org.openmuc.jdlms.datatypes.DataObject;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.CommandExecutor;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.dlmsobjectconfig.DlmsObjectConfigConfiguration;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.dlmsobjectconfig.DlmsObjectConfigService;
-import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.dlmsobjectconfig.DlmsObjectType;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.stub.DlmsConnectionManagerStub;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.stub.DlmsConnectionStub;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.utils.DlmsHelper;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.entities.DlmsDevice;
+import org.opensmartgridplatform.adapter.protocol.dlms.domain.entities.Protocol;
 import org.opensmartgridplatform.adapter.protocol.dlms.domain.factories.DlmsConnectionManager;
 import org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmNotificationDto;
 import org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmNotificationsDto;
@@ -42,38 +41,25 @@ import org.opensmartgridplatform.shared.exceptionhandling.OsgpException;
 import org.opensmartgridplatform.shared.infra.jms.MessageMetadata;
 
 class SetAlarmNotificationsCommandExecutorTest {
-  private DlmsDevice device;
   private CommandExecutor<AlarmNotificationsDto, AccessResultCode> executor;
   private List<SetParameter> setParametersReceived;
   private DlmsConnectionManager connMgr;
   private MessageMetadata messageMetadata;
+  private DlmsConnectionStub conn;
 
   @BeforeEach
   public void setUp() {
     this.setParametersReceived = new ArrayList<>();
-    this.device = new DlmsDevice("SuperAwesomeHeroicRockstarDevice");
     this.messageMetadata = MessageMetadata.newBuilder().withCorrelationUid("123456").build();
 
     final DlmsObjectConfigConfiguration dlmsObjectConfigConfiguration =
         new DlmsObjectConfigConfiguration();
     final DlmsObjectConfigService dlmsObjectConfigService =
         new DlmsObjectConfigService(
-            new DlmsHelper(), dlmsObjectConfigConfiguration.getDlmsObjectConfigs()) {
-          @Override
-          public Optional<AttributeAddress> findAttributeAddress(
-              final DlmsDevice device, final DlmsObjectType type, final Integer channel) {
-            switch (type) {
-              case ALARM_FILTER_1:
-                return Optional.of(new AttributeAddress(40, "0.0.97.98.10.255", 2));
-              case ALARM_FILTER_2:
-                return Optional.of(new AttributeAddress(40, "0.0.97.98.11.255", 2));
-            }
-            return Optional.empty();
-          }
-        };
+            new DlmsHelper(), dlmsObjectConfigConfiguration.getDlmsObjectConfigs());
     this.executor = new SetAlarmNotificationsCommandExecutor(dlmsObjectConfigService);
 
-    final DlmsConnectionStub conn =
+    this.conn =
         new DlmsConnectionStub() {
           @Override
           public AccessResultCode set(final SetParameter setParameter) {
@@ -82,34 +68,34 @@ class SetAlarmNotificationsCommandExecutorTest {
           }
         };
 
-    // Set the return value to 10 (0b1010):
-    // REPLACE_BATTERY enabled, AUXILIARY_EVENT enabled.
-    conn.addReturnValue(
+    // Set the return value for alarm register 1 to 10 (0b1010):
+    // Enabled events: REPLACE_BATTERY and AUXILIARY_EVENT
+    this.conn.addReturnValue(
         new AttributeAddress(1, "0.0.97.98.10.255", 2), DataObject.newInteger32Data(10));
-    conn.addReturnValue(
-        new AttributeAddress(1, "0.0.97.98.11.255", 2), DataObject.newInteger32Data(0));
 
-    this.connMgr = new DlmsConnectionManagerStub(conn);
+    this.connMgr = new DlmsConnectionManagerStub(this.conn);
   }
 
   @Test
   void testSetSettingThatIsAlreadySet() throws OsgpException {
-    // Setting notifications that are not different from what is on the
-    // meter already,
+    final DlmsDevice device = this.createDevice(Protocol.SMR_5_0_0);
+
+    // Setting notifications that are not different from what is on the meter already,
     // should always be successful.
     final AccessResultCode res =
-        this.execute(new AlarmNotificationDto(AlarmTypeDto.REPLACE_BATTERY, true));
+        this.execute(device, new AlarmNotificationDto(AlarmTypeDto.REPLACE_BATTERY, true));
     assertThat(res).isEqualTo(AccessResultCode.SUCCESS);
-    // Since nothing changed, not a single message should have been sent to
-    // the meter.
+    // Since nothing changed, not a single message should have been sent to the meter.
     assertThat(this.setParametersReceived.size()).isZero();
   }
 
   @Test
   void testSetSettingEnabled() throws OsgpException {
+    final DlmsDevice device = this.createDevice(Protocol.SMR_5_0_0);
+
     // Now we enable something: CLOCK_INVALID to enabled.
     final AccessResultCode res =
-        this.execute(new AlarmNotificationDto(AlarmTypeDto.CLOCK_INVALID, true));
+        this.execute(device, new AlarmNotificationDto(AlarmTypeDto.CLOCK_INVALID, true));
     assertThat(res).isEqualTo(AccessResultCode.SUCCESS);
     assertThat(this.setParametersReceived.size()).isEqualTo(1);
     // Expecting 11 (0b1011).
@@ -118,21 +104,28 @@ class SetAlarmNotificationsCommandExecutorTest {
 
   @ParameterizedTest
   @CsvSource({
-    "1,VOLTAGE_SAG_IN_PHASE_DETECTED_L1",
-    "2,VOLTAGE_SAG_IN_PHASE_DETECTED_L2",
-    "4,VOLTAGE_SAG_IN_PHASE_DETECTED_L3",
-    "8,VOLTAGE_SWELL_IN_PHASE_DETECTED_L1",
-    "16,VOLTAGE_SWELL_IN_PHASE_DETECTED_L2",
-    "32,VOLTAGE_SWELL_IN_PHASE_DETECTED_L3",
-    "3,VOLTAGE_SAG_IN_PHASE_DETECTED_L1;VOLTAGE_SAG_IN_PHASE_DETECTED_L2",
-    "7,VOLTAGE_SAG_IN_PHASE_DETECTED_L1;VOLTAGE_SAG_IN_PHASE_DETECTED_L2;VOLTAGE_SAG_IN_PHASE_DETECTED_L3"
+    "1,VOLTAGE_SAG_IN_PHASE_DETECTED_L1", // 0b00000001
+    "2,VOLTAGE_SAG_IN_PHASE_DETECTED_L2", // 0b00000010
+    "4,VOLTAGE_SAG_IN_PHASE_DETECTED_L3", // 0b00000100
+    "8,VOLTAGE_SWELL_IN_PHASE_DETECTED_L1", // 0b00001000
+    "16,VOLTAGE_SWELL_IN_PHASE_DETECTED_L2", // 0b00010000
+    "32,VOLTAGE_SWELL_IN_PHASE_DETECTED_L3", // 0b00100000
+    "3,VOLTAGE_SAG_IN_PHASE_DETECTED_L1;VOLTAGE_SAG_IN_PHASE_DETECTED_L2", // 0b00000011
+    "7,VOLTAGE_SAG_IN_PHASE_DETECTED_L1;VOLTAGE_SAG_IN_PHASE_DETECTED_L2;VOLTAGE_SAG_IN_PHASE_DETECTED_L3" // 0b00000111
   })
-  void testSetSettingEnabledRegister(final long expectedValue, final String alarmTypesInput)
-      throws OsgpException {
+  void testSetSettingEnabledRegisterAlarmRegister2(
+      final long expectedValue, final String alarmTypesInput) throws OsgpException {
+    final DlmsDevice device = this.createDevice(Protocol.SMR_5_2);
+
+    // Set the return value for alarm register 2 to 0 (no alarms set):
+    this.conn.addReturnValue(
+        new AttributeAddress(1, "0.0.97.98.11.255", 2), DataObject.newInteger32Data(0));
+
     final List<AlarmTypeDto> alarmTypes =
         Arrays.stream(alarmTypesInput.split(";")).map(AlarmTypeDto::valueOf).collect(toList());
     final AccessResultCode res =
         this.execute(
+            device,
             alarmTypes.stream()
                 .map(alarmType -> new AlarmNotificationDto(alarmType, true))
                 .toArray(AlarmNotificationDto[]::new));
@@ -142,12 +135,67 @@ class SetAlarmNotificationsCommandExecutorTest {
         .isEqualTo(expectedValue);
   }
 
-  @Test
-  void testSetSettingEnabledAndDisabled() throws OsgpException {
-    // Now we enable and disable something: CLOCK_INVALID to enabled and
-    // REPLACE_BATTERY to disabled.
+  @ParameterizedTest
+  @CsvSource({
+    "62,VOLTAGE_SAG_IN_PHASE_DETECTED_L1", // 0b00111110
+    "61,VOLTAGE_SAG_IN_PHASE_DETECTED_L2", // 0b00111101
+    "59,VOLTAGE_SAG_IN_PHASE_DETECTED_L3", // 0b00111011
+    "55,VOLTAGE_SWELL_IN_PHASE_DETECTED_L1", // 0b00110111
+    "47,VOLTAGE_SWELL_IN_PHASE_DETECTED_L2", // 0b00101111
+    "31,VOLTAGE_SWELL_IN_PHASE_DETECTED_L3", // 0b00011111
+    "60,VOLTAGE_SAG_IN_PHASE_DETECTED_L1;VOLTAGE_SAG_IN_PHASE_DETECTED_L2", // 0b00111100
+    "56,VOLTAGE_SAG_IN_PHASE_DETECTED_L1;VOLTAGE_SAG_IN_PHASE_DETECTED_L2;VOLTAGE_SAG_IN_PHASE_DETECTED_L3" // 0b00111000
+  })
+  void testSetSettingDisabledRegisterAlarmRegister2(
+      final long expectedValue, final String alarmTypesInput) throws OsgpException {
+    final DlmsDevice device = this.createDevice(Protocol.SMR_5_2);
+
+    // Set the return value for alarm register 2 to 3F (all alarms set):
+    this.conn.addReturnValue(
+        new AttributeAddress(1, "0.0.97.98.11.255", 2), DataObject.newInteger32Data(0x3F));
+
+    final List<AlarmTypeDto> alarmTypes =
+        Arrays.stream(alarmTypesInput.split(";")).map(AlarmTypeDto::valueOf).collect(toList());
     final AccessResultCode res =
         this.execute(
+            device,
+            alarmTypes.stream()
+                .map(alarmType -> new AlarmNotificationDto(alarmType, false))
+                .toArray(AlarmNotificationDto[]::new));
+    assertThat(res).isEqualTo(AccessResultCode.SUCCESS);
+    assertThat(this.setParametersReceived.size()).isEqualTo(1);
+    assertThat((long) this.setParametersReceived.get(0).getData().getValue())
+        .isEqualTo(expectedValue);
+  }
+
+  @Test
+  void testSetSettingThatIsAlreadySetInAlarmRegister2() throws OsgpException {
+    final DlmsDevice device = this.createDevice(Protocol.SMR_5_2);
+
+    // Set the return value for alarm register 2 to 3F (all alarms set):
+    this.conn.addReturnValue(
+        new AttributeAddress(1, "0.0.97.98.11.255", 2), DataObject.newInteger32Data(0x3F));
+
+    // Setting notifications that are not different from what is on the meter already,
+    // should always be successful.
+    final AccessResultCode res =
+        this.execute(
+            device,
+            new AlarmNotificationDto(AlarmTypeDto.VOLTAGE_SWELL_IN_PHASE_DETECTED_L3, true));
+    assertThat(res).isEqualTo(AccessResultCode.SUCCESS);
+    // Since nothing changed, not a single message should have been sent to the meter.
+    assertThat(this.setParametersReceived.size()).isZero();
+  }
+
+  @Test
+  void testSetSettingEnabledAndDisabled() throws OsgpException {
+    final DlmsDevice device = this.createDevice(Protocol.SMR_5_0_0);
+
+    // Both enable and disable in one call:
+    // CLOCK_INVALID to enabled and REPLACE_BATTERY to disabled.
+    final AccessResultCode res =
+        this.execute(
+            device,
             new AlarmNotificationDto(AlarmTypeDto.CLOCK_INVALID, true),
             new AlarmNotificationDto(AlarmTypeDto.REPLACE_BATTERY, false));
     assertThat(res).isEqualTo(AccessResultCode.SUCCESS);
@@ -156,13 +204,20 @@ class SetAlarmNotificationsCommandExecutorTest {
     assertThat((long) this.setParametersReceived.get(0).getData().getValue()).isEqualTo(9);
   }
 
-  private AccessResultCode execute(final AlarmNotificationDto... alarmNotificationDtos)
+  private AccessResultCode execute(
+      final DlmsDevice device, final AlarmNotificationDto... alarmNotificationDtos)
       throws OsgpException {
     final Set<AlarmNotificationDto> alarmNotificationDtoSet =
         new HashSet<>(Arrays.asList(alarmNotificationDtos));
     final AlarmNotificationsDto alarmNotificationsDto =
         new AlarmNotificationsDto(alarmNotificationDtoSet);
-    return this.executor.execute(
-        this.connMgr, this.device, alarmNotificationsDto, this.messageMetadata);
+    return this.executor.execute(this.connMgr, device, alarmNotificationsDto, this.messageMetadata);
+  }
+
+  private DlmsDevice createDevice(final Protocol protocol) {
+    final DlmsDevice device = new DlmsDevice();
+    device.setProtocol(protocol);
+
+    return device;
   }
 }
