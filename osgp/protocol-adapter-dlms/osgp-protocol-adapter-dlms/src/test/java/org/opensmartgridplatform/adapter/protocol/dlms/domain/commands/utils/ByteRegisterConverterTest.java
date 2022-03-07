d6b9f6ce348e1ebf65bcf0e00cfced23badcaf09
stefanermens <stefan.ermens@alliander.com>
2022-03-07 10:29:58 +0100

    Use parameterized test for ByteRegisterConverter
    Signed-off-by: stefanermens <stefan.ermens@alliander.com>

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/utils/ByteRegisterConverterTest.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/utils/ByteRegisterConverterTest.java
@@ -11,69 +11,79 @@ package org.opensmartgridplatform.adapter.protocol.dlms.domain.commands.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class ByteRegisterConverterTest {
 
-  private final Map<TestType, Integer> registerBitIndexPerType = this.createMap();
-
   private final ByteRegisterConverter<TestType> converter =
-      new ByteRegisterConverter<>(this.registerBitIndexPerType, 8);
+      new ByteRegisterConverter<>(this.createMap(), 16);
 
   private enum TestType {
-    TYPE_0,
-    TYPE_1,
-    TYPE_2,
-    TYPE_3,
-    TYPE_4,
-    TYPE_5,
-    TYPE_6,
-    TYPE_7
+    TYPE_0(0),
+    TYPE_1(1),
+    TYPE_2(2),
+    TYPE_3(3),
+    TYPE_4(4),
+    TYPE_5(5),
+    TYPE_6(6),
+    TYPE_7(7),
+    TYPE_8(8),
+    TYPE_9(9),
+    TYPE_10(10),
+    TYPE_11(11),
+    TYPE_12(12),
+    TYPE_13(13),
+    TYPE_14(14),
+    TYPE_15(15);
+
+    final Integer index;
+
+    TestType(final Integer index) {
+      this.index = index;
+    }
+
+    long getValue() {
+      return (long) Math.pow(2, this.index);
+    }
   }
 
   private Map<TestType, Integer> createMap() {
-    final Map<TestType, Integer> registerBitIndexPerType = new HashMap<>();
+    return Arrays.stream(TestType.values())
+        .collect(Collectors.toMap(item -> item, item -> item.index));
+  }
 
-    registerBitIndexPerType.put(TestType.TYPE_0, 0);
-    registerBitIndexPerType.put(TestType.TYPE_1, 1);
-    registerBitIndexPerType.put(TestType.TYPE_2, 2);
-    registerBitIndexPerType.put(TestType.TYPE_3, 3);
-    registerBitIndexPerType.put(TestType.TYPE_4, 4);
-    registerBitIndexPerType.put(TestType.TYPE_5, 5);
-    registerBitIndexPerType.put(TestType.TYPE_6, 6);
-    registerBitIndexPerType.put(TestType.TYPE_7, 7);
+  @ParameterizedTest
+  @EnumSource(TestType.class)
+  void testToBitPosition(final TestType type) {
+    assertThat(this.converter.toBitPosition(type)).isEqualTo(type.index);
+  }
 
-    return registerBitIndexPerType;
+  @ParameterizedTest
+  @EnumSource(TestType.class)
+  void testToTypesWithSingleBitSet(final TestType type) {
+    assertThat(this.converter.toTypes(type.getValue())).containsExactly(type);
   }
 
   @Test
-  void testToBitPosition() {
-    assertThat(this.converter.toBitPosition(TestType.TYPE_0)).isZero();
-    assertThat(this.converter.toBitPosition(TestType.TYPE_1)).isEqualTo(1);
-    assertThat(this.converter.toBitPosition(TestType.TYPE_2)).isEqualTo(2);
-    assertThat(this.converter.toBitPosition(TestType.TYPE_3)).isEqualTo(3);
-    assertThat(this.converter.toBitPosition(TestType.TYPE_4)).isEqualTo(4);
-    assertThat(this.converter.toBitPosition(TestType.TYPE_5)).isEqualTo(5);
-    assertThat(this.converter.toBitPosition(TestType.TYPE_6)).isEqualTo(6);
-    assertThat(this.converter.toBitPosition(TestType.TYPE_7)).isEqualTo(7);
+  void testToTypesWithNoBitsSet() {
+    assertThat(this.converter.toTypes(0x0000L)).isEmpty();
   }
 
   @Test
-  void testToTypes() {
-    assertThat(this.converter.toTypes(0x00L)).isEmpty();
-    assertThat(this.converter.toTypes(0x01L)).containsExactlyInAnyOrder(TestType.TYPE_0);
-    assertThat(this.converter.toTypes(0x02L)).containsExactlyInAnyOrder(TestType.TYPE_1);
-    assertThat(this.converter.toTypes(0x03L))
+  void testToTypesWithMultipleBitsSet() {
+    assertThat(this.converter.toTypes(0x0003L))
         .containsExactlyInAnyOrder(TestType.TYPE_0, TestType.TYPE_1);
-    assertThat(this.converter.toTypes(0x80L)).containsExactlyInAnyOrder(TestType.TYPE_7);
-    assertThat(this.converter.toTypes(0xC0L))
-        .containsExactlyInAnyOrder(TestType.TYPE_7, TestType.TYPE_6);
-    assertThat(this.converter.toTypes(0xFFL))
+    assertThat(this.converter.toTypes(0x00C0L))
+        .containsExactlyInAnyOrder(TestType.TYPE_6, TestType.TYPE_7);
+    assertThat(this.converter.toTypes(0x01FFL))
         .containsExactlyInAnyOrder(
             TestType.TYPE_0,
             TestType.TYPE_1,
@@ -82,19 +92,36 @@ class ByteRegisterConverterTest {
             TestType.TYPE_4,
             TestType.TYPE_5,
             TestType.TYPE_6,
-            TestType.TYPE_7);
+            TestType.TYPE_7,
+            TestType.TYPE_8);
+    assertThat(this.converter.toTypes(0x0300L))
+        .containsExactlyInAnyOrder(TestType.TYPE_8, TestType.TYPE_9);
+    assertThat(this.converter.toTypes(0xC000L))
+        .containsExactlyInAnyOrder(TestType.TYPE_14, TestType.TYPE_15);
   }
 
   @Test
-  void testToLongValue() {
-    assertThat(this.converter.toLongValue(Collections.emptySet())).isEqualTo(0x00L);
-    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_0))).isEqualTo(0x01L);
-    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_1))).isEqualTo(0x02L);
+  void testToTypesWithUnknownBitSet() {
+    assertThat(this.converter.toTypes(0x10000L)).containsNull();
+  }
+
+  @ParameterizedTest
+  @EnumSource(TestType.class)
+  void testToLongValueWithSingleAlarmSet(final TestType type) {
+    assertThat(this.converter.toLongValue(this.createSet(type))).isEqualTo(type.getValue());
+  }
+
+  @Test
+  void testToLongValueWithNoAlarmsSet() {
+    assertThat(this.converter.toLongValue(Collections.emptySet())).isEqualTo(0x0000L);
+  }
+
+  @Test
+  void testToLongValueWithMultipleAlarmsSet() {
     assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_0, TestType.TYPE_1)))
-        .isEqualTo(0x03L);
-    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_7))).isEqualTo(0x80L);
-    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_7, TestType.TYPE_6)))
-        .isEqualTo(0xC0L);
+        .isEqualTo(0x0003L);
+    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_6, TestType.TYPE_7)))
+        .isEqualTo(0x00C0L);
     assertThat(
             this.converter.toLongValue(
                 this.createSet(
@@ -105,8 +132,13 @@ class ByteRegisterConverterTest {
                     TestType.TYPE_4,
                     TestType.TYPE_5,
                     TestType.TYPE_6,
-                    TestType.TYPE_7)))
-        .isEqualTo(0xFFL);
+                    TestType.TYPE_7,
+                    TestType.TYPE_8)))
+        .isEqualTo(0x01FFL);
+    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_8, TestType.TYPE_9)))
+        .isEqualTo(0x0300L);
+    assertThat(this.converter.toLongValue(this.createSet(TestType.TYPE_14, TestType.TYPE_15)))
+        .isEqualTo(0xC000L);
   }
 
   private Set<TestType> createSet(final TestType... types) {
