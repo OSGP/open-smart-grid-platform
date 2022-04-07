99470357b4d71b0119cde864f4f66e4d0e94fc7f
Jelle Hoffman <jelle.hoffman@hotmail.com>
2022-04-07 16:20:01 +0200

    Feature/FDP-688: Adding Gauge
    Signed-off-by: Jelle Hoffman <jelle.hoffman@hotmail.com>

--- osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/metrics/MqqtMetricsService.java
+++ osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/metrics/MqqtMetricsService.java
@@ -19,20 +19,54 @@ import org.springframework.stereotype.Component;
 public class MqqtMetricsService {
   private final MeterRegistry registry;
   private final Counter receivedMessagesCounter;
-  private final Gauge connectedGauge;
+  private final Gauge disconnectedGauge;
+  private Object[] disconnectedList;
+  private static final Object[] CONNECTED = {};
+  private static final Object[] RECONNECTING = {1};
+  private static final Object[] DISCONNECTED = {0, 1};
 
   public MqqtMetricsService(final MeterRegistry registry) {
     this.registry = registry;
+    this.disconnectedList = DISCONNECTED;
     this.receivedMessagesCounter =
         this.createCounter("flexovl.nrg.pull.succeeded", "Successful NRG pull jobs");
-    this.connectedGauge = this.createGauge("flexovl.nrg.pull.succeeded", "Successful NRG pull jobs");
+    this.disconnectedGauge =
+        this.createGauge(
+            "flexovl.nrg.pull.succeeded", "Successful NRG pull jobs", this.disconnectedList, "3");
+  }
+
+  public void receivedMessage() {
+    this.receivedMessagesCounter.increment();
+  }
+
+  public void disconnected() {
+    if (!this.disconnectedList.equals(DISCONNECTED)) {
+      this.disconnectedList = DISCONNECTED;
+    }
+  }
+
+  public void reconnecting() {
+    if (!this.disconnectedList.equals(RECONNECTING)) {
+      this.disconnectedList = RECONNECTING;
+    }
+  }
+
+  public void connected() {
+    if (!this.disconnectedList.equals(CONNECTED)) {
+      this.disconnectedList = CONNECTED;
+      this.disconnectedGauge
+    }
+  }
+
+  private Gauge createGauge(
+      final String name, final String description, final Object[] list, final String baseUnit) {
+    return Gauge.builder(name, () -> list.length)
+        .description(description)
+        .baseUnit(baseUnit)
+        .register(this.registry);
   }
 
   private Counter createCounter(final String name, final String description) {
     return Counter.builder(name).description(description).register(this.registry);
-  }
-
-  private Gauge createGauge(final String name, final String description){
-    return Gauge.builder(
   }
 }
