cfa81d4640a844bcf3337d5ed03823228088c83d
Sander Verbruggen <sander.verbruggen@alliander.com>
2022-04-19 16:04:54 +0200

    FDP-621: Standard naming service for metrics
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>

---
+++ osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/metrics/MetricsNameService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Alliander N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.opensmartgridplatform.shared.metrics;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/** Service for naming metrics. Assures a given prefix is included in the name. */
+@Component
+public class MetricsNameService {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MetricsNameService.class);
+
+  @Value("${metrics.prometheus.applicationname:}")
+  private String applicationName;
+
+  /**
+   * Creates a proper metric name given the requested metricName.
+   *
+   * <p>The naming convention says a metric name should include the application name. Generic
+   * components can't have the application name hardcoded, in which case this class will prepend it
+   * to the requested name. Configure the application using the property <code>
+   * metrics.prometheus.applicationname</code>
+   *
+   * @param metricName requested name for the metric
+   * @return the final metric name, guaranteed to start with the application name if configured.
+   *     Returned as is otherwise.
+   */
+  public String createName(final String metricName) {
+    if (StringUtils.isNotBlank(this.applicationName)
+        && !StringUtils.startsWithIgnoreCase(metricName, this.applicationName)) {
+      LOGGER.debug("Prepending {} to the metric name {}", this.applicationName, metricName);
+      return this.applicationName + "." + metricName;
+    }
+    return metricName;
+  }
+}
