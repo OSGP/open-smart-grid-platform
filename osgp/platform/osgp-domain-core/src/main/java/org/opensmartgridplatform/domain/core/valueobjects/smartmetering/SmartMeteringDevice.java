e850d98d0d69f92e2fa5cc290b5d638f2d88d04e
Jaco Boon <jaco.boon@gmail.com>
2022-05-30 15:37:53 +0200

    Support for protocolVariant in shipmentfile
    Signed-off-by: Jaco Boon <jaco.boon@gmail.com>

--- osgp/platform/osgp-domain-core/src/main/java/org/opensmartgridplatform/domain/core/valueobjects/smartmetering/SmartMeteringDevice.java
+++ osgp/platform/osgp-domain-core/src/main/java/org/opensmartgridplatform/domain/core/valueobjects/smartmetering/SmartMeteringDevice.java
@@ -17,8 +17,6 @@ public class SmartMeteringDevice implements Serializable {
 
   private static final long serialVersionUID = -6133164707489276802L;
 
-  private static final String CDMA = "CDMA";
-
   private String deviceIdentification;
 
   private String deviceType;
@@ -58,11 +56,4 @@ public class SmartMeteringDevice implements Serializable {
   private Short mbusDeviceTypeIdentification;
 
   private byte[] mbusDefaultKey;
-
-  public String getProtocolInfoLookupName() {
-    if (CDMA.equals(this.communicationMethod)) {
-      return String.format("%s_%s", this.protocolName, CDMA);
-    }
-    return this.protocolName;
-  }
 }
