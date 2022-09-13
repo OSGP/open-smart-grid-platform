c4c24ccc8d96f3c4f639867ce797eba4329293b0
Andre Bolder <andre.bolder@alliander.com>
2022-09-13 09:45:16 +0200

    SMHE-729: Merge development in this branch
    Signed-off-by: Andre Bolder <andre.bolder@alliander.com>

--- osgp/shared/osgp-dto/src/main/java/org/opensmartgridplatform/dto/valueobjects/smartmetering/TestAlarmTypeDto.java
+++ osgp/shared/osgp-dto/src/main/java/org/opensmartgridplatform/dto/valueobjects/smartmetering/TestAlarmTypeDto.java
@@ -12,5 +12,5 @@ package org.opensmartgridplatform.dto.valueobjects.smartmetering;
  */
 public enum TestAlarmTypeDto {
   PARTIAL_POWER_OUTAGE, // will deliver PHASE_OUTAGE_TEST_INDICATION
-  LAST_GASP // will deliver LAST_GASP_TEST_INDICATION
+  LAST_GASP // will deliver LAST_GASP_TEST
 }
