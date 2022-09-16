e142ee77b309f5063b3a9aaad59e3912df8a9e03
Andre Bolder <andre.bolder@alliander.com>
2022-09-16 08:07:08 +0200

    SMHE-729: cucumber test additions for test alarms
    Signed-off-by: Andre Bolder <andre.bolder@alliander.com>

--- integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/helpers/DateConverter.java
+++ integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/helpers/DateConverter.java
@@ -8,6 +8,8 @@
  */
 package org.opensmartgridplatform.cucumber.platform.helpers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -15,6 +17,7 @@ import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 public class DateConverter {
+  private static final String DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";
 
   private DateConverter() {}
 
@@ -25,6 +28,16 @@ public class DateConverter {
       return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
     } catch (final DatatypeConfigurationException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  public static XMLGregorianCalendar parseDate(final String dateString) {
+    final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+    try {
+      return createXMLGregorianCalendar(sdf.parse(dateString));
+    } catch (final ParseException e) {
+      throw new IllegalArgumentException(
+          String.format("Date not correctly formatted %s as %s: ", dateString, DATE_FORMAT), e);
     }
   }
 }
