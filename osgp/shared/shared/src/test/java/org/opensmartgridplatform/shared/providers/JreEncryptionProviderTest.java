95d5a4f294b6e8d9c176ddb8e0d29d8a08aacf93
Gerben Kroes <gerben@kroesctrl.nl>
2022-09-27 14:06:30 +0200

    Fix JreEncryptionProvider in JDDK 17, it could not run twice
    Signed-off-by: Gerben Kroes <gerben@kroesctrl.nl>

---
+++ osgp/shared/shared/src/test/java/org/opensmartgridplatform/shared/providers/JreEncryptionProviderTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Alliander N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.opensmartgridplatform.shared.providers;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.junit.jupiter.api.Test;
+import org.opensmartgridplatform.shared.security.EncryptedSecret;
+import org.opensmartgridplatform.shared.security.EncryptionProviderType;
+import org.opensmartgridplatform.shared.security.providers.JreEncryptionProvider;
+import org.springframework.util.ResourceUtils;
+
+class JreEncryptionProviderTest {
+
+  @Test
+  void testDecryptTwice() throws FileNotFoundException, DecoderException {
+    final String encryptedKey =
+        "bf1a7553451978483b6bcaf8d6d3135d6d7879ef8e353149aff4e791321df5b5f572c7bcc8937e02048a1f50483344ea";
+
+    final File file = ResourceUtils.getFile("classpath:keys/secret.aes");
+    final JreEncryptionProvider jreEncryptionProvider = new JreEncryptionProvider(file);
+
+    final String decrypted1 = this.decrypt(jreEncryptionProvider, encryptedKey);
+    final String decrypted2 = this.decrypt(jreEncryptionProvider, encryptedKey);
+    assertThat(decrypted1).isEqualTo(decrypted2);
+  }
+
+  @Test
+  void testEncryptDecrypt() throws FileNotFoundException, DecoderException {
+    final String encryptedKey =
+        "bf1a7553451978483b6bcaf8d6d3135d6d7879ef8e353149aff4e791321df5b5f572c7bcc8937e02048a1f50483344ea";
+
+    final File file = ResourceUtils.getFile("classpath:keys/secret.aes");
+    final JreEncryptionProvider jreEncryptionProvider = new JreEncryptionProvider(file);
+
+    final String decrypted1 = this.decrypt(jreEncryptionProvider, encryptedKey);
+    final String encryptKey2 = this.encrypt(jreEncryptionProvider, decrypted1);
+    final String decrypted2 = this.decrypt(jreEncryptionProvider, encryptKey2);
+    assertThat(decrypted1).isEqualTo(decrypted2);
+  }
+
+  private String decrypt(
+      final JreEncryptionProvider jreEncryptionProvider, final String encryptedKey)
+      throws DecoderException {
+    final EncryptedSecret encryptedSecret =
+        new EncryptedSecret(EncryptionProviderType.JRE, Hex.decodeHex(encryptedKey));
+    final byte[] decryptSecret = jreEncryptionProvider.decrypt(encryptedSecret, "1");
+    return Hex.encodeHexString(decryptSecret);
+  }
+
+  private String encrypt(
+      final JreEncryptionProvider jreEncryptionProvider, final String decryptedKey)
+      throws DecoderException {
+    final EncryptedSecret encryptedSecret =
+        jreEncryptionProvider.encrypt(Hex.decodeHex(decryptedKey), "1");
+    return Hex.encodeHexString(encryptedSecret.getSecret());
+  }
+}
