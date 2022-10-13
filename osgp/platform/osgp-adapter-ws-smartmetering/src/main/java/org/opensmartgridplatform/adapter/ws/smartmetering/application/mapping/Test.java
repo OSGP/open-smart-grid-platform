fb5f0460a57594d4e973fc6bd890568defd997c8
Harry Middelburg <harry.middelburg@alliander.com>
2022-10-13 21:44:18 +0200

    Sonar issue: eliminated regex
    Signed-off-by: Harry Middelburg <harry.middelburg@alliander.com>

---
+++ osgp/platform/osgp-adapter-ws-smartmetering/src/main/java/org/opensmartgridplatform/adapter/ws/smartmetering/application/mapping/Test.java
@@ -0,0 +1,10 @@
+package org.opensmartgridplatform.adapter.ws.smartmetering.application.mapping;
+
+public class Test {
+
+  public static void main(final String[] args) {
+    // TODO Auto-generated method stub
+    final String destination = "\\abc:\\123";
+    System.out.println(destination.matches("\\S++:\\d++"));
+  }
+}
