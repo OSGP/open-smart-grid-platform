ecc4977cb411989633ab4ea42c6b4868958b4427
stefanermens <stefan.ermens@alliander.com>
2022-11-14 15:56:52 +0100

    SMHE-984: Keep format of dataType in json the same as in DLMS documentation
    Signed-off-by: stefanermens <stefan.ermens@alliander.com>

--- osgp/protocol-adapter-dlms/osgp-dlms/src/main/java/org/opensmartgridplatform/dlms/objectconfig/DlmsDataType.java
+++ osgp/protocol-adapter-dlms/osgp-dlms/src/main/java/org/opensmartgridplatform/dlms/objectconfig/DlmsDataType.java
@@ -11,44 +11,74 @@
 
 package org.opensmartgridplatform.dlms.objectconfig;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum DlmsDataType {
   // Common DLMS data types, defined in Green Book
+  @JsonProperty("null_data")
   NULL_DATA,
+  @JsonProperty("array")
   ARRAY,
+  @JsonProperty("structure")
   STRUCTURE,
+  @JsonProperty("boolean")
   BOOLEAN,
+  @JsonProperty("bit-string")
   BIT_STRING,
+  @JsonProperty("double-long")
   DOUBLE_LONG,
+  @JsonProperty("double-long-unsigned")
   DOUBLE_LONG_UNSIGNED,
+  @JsonProperty("octet-string")
   OCTET_STRING,
+  @JsonProperty("visible-string")
   VISIBLE_STRING,
+  @JsonProperty("utf8-string")
   UTF8_STRING,
+  @JsonProperty("bcd")
   BCD,
+  @JsonProperty("integer")
   INTEGER,
+  @JsonProperty("long")
   LONG,
+  @JsonProperty("long-unsigned")
   LONG_UNSIGNED,
+  @JsonProperty("compact-array")
   COMPACT_ARRAY,
+  @JsonProperty("long64")
   LONG64,
+  @JsonProperty("long64-unsigned")
   LONG64_UNSIGNED,
+  @JsonProperty("enum")
   ENUM,
+  @JsonProperty("float32")
   FLOAT32,
+  @JsonProperty("float64")
   FLOAT64,
+  @JsonProperty("date-time")
   DATE_TIME,
+  @JsonProperty("date")
   DATE,
+  @JsonProperty("time")
   TIME,
+  @JsonProperty("dont-care")
   DONT_CARE,
 
   // Data types for specific classes
 
   // Class-id 3: Register
+  @JsonProperty("scal_unit_type")
   SCAL_UNIT_TYPE,
 
   // Class-id 7: Profile
+  @JsonProperty("object_definition")
   OBJECT_DEFINITION,
 
   // Class-id 8: Clock
+  @JsonProperty("clock_status")
   CLOCK_STATUS,
 
   // Class-id 47: GSM diagnostic
+  @JsonProperty("cell_info_type")
   CELL_INFO_TYPE
 }
