d0af03c868e8df078a5dc9b39d05bf7b1555460a
Andre Bolder <andre.bolder@alliander.com>
2022-12-09 16:41:41 +0100

    SMHE-1193: add GMeter with shipmentfile works
    Signed-off-by: Andre Bolder <andre.bolder@alliander.com>

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/resources/db/migration/V20221209092300000__communication_method_nullable_for_gmeters.sql
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/resources/db/migration/V20221209092300000__communication_method_nullable_for_gmeters.sql
@@ -1,6 +1,6 @@
 DO
 $$
     BEGIN
-        alter table device_model alter column communication_method drop not null;
+        alter table dlms_device alter column communication_method drop not null;
     END;
 $$
