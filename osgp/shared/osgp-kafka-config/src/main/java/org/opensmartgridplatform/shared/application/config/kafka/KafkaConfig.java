0735e98d9021ff8178410beba98414c1347c42cb
Jasper Kamerling <jasper.kamerling@alliander.com>
2023-01-02 13:54:06 +0100

    FDP-1023: implement new Kafka property configuration
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- osgp/shared/osgp-kafka-config/src/main/java/org/opensmartgridplatform/shared/application/config/kafka/KafkaConfig.java
+++ osgp/shared/osgp-kafka-config/src/main/java/org/opensmartgridplatform/shared/application/config/kafka/KafkaConfig.java
@@ -8,8 +8,11 @@
  */
 package org.opensmartgridplatform.shared.application.config.kafka;
 
-import java.util.HashMap;
+import static java.util.stream.Collectors.*;
+
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.common.config.ConfigDef;
 import org.springframework.core.env.Environment;
 
 public abstract class KafkaConfig {
@@ -20,22 +23,34 @@ public abstract class KafkaConfig {
     this.environment = environment;
   }
 
-  protected Map<String, Object> createCommonProperties(final String propertiesPrefix) {
-    final Map<String, Object> properties = new HashMap<>();
-    KafkaProperties.commonProperties()
-        .forEach((k, v) -> this.addIfExist(properties, k, propertiesPrefix, v));
-    return properties;
+  protected Map<String, Object> configDefToProperties(
+      final ConfigDef configDef, final String propertiesPrefix) {
+    return configDef.configKeys().entrySet().stream()
+        .filter(entry -> exists(entry.getKey(), propertiesPrefix))
+        .collect(
+            toMap(
+                Map.Entry::getKey,
+                entry -> getValue(entry.getKey(), entry.getValue().type(), propertiesPrefix)));
   }
 
-  protected <T> void addIfExist(
-      final Map<String, Object> properties,
-      final String kafkaProperty,
-      final String prefix,
-      final Class<T> targetType) {
-    final String fullPropertyName = prefix + "." + kafkaProperty;
-    final T value = this.environment.getProperty(fullPropertyName, targetType);
-    if (value != null) {
-      properties.put(kafkaProperty, value);
+  protected boolean exists(final String configKey, final String prefix) {
+    return StringUtils.isNotEmpty(this.environment.getProperty(prefix + "." + configKey))
+        || StringUtils.isNotEmpty(this.environment.getProperty(configKey));
+  }
+
+  protected Object getValue(
+      final String configName, final ConfigDef.Type configType, final String prefix) {
+
+    final String prefixedValue = this.environment.getProperty(prefix + "." + configName);
+    if (StringUtils.isNotEmpty(prefixedValue)) {
+      return ConfigDef.parseType(configName, prefixedValue, configType);
     }
+
+    final String value = this.environment.getProperty(configName);
+    if (StringUtils.isNotEmpty(value)) {
+      return ConfigDef.parseType(configName, value, configType);
+    }
+
+    return null;
   }
 }
--- osgp/shared/osgp-kafka-config/src/main/java/org/opensmartgridplatform/shared/application/config/kafka/oauth/KafkaOAuthConfig.java
+++ osgp/shared/osgp-kafka-config/src/main/java/org/opensmartgridplatform/shared/application/config/kafka/oauth/KafkaOAuthConfig.java
@@ -4,7 +4,15 @@
 
 package org.opensmartgridplatform.shared.application.config.kafka.oauth;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.ConfigDef;
+
 public class KafkaOAuthConfig {
+
+  private static final ConfigDef PRODUCER_CONFIG;
+
+  private static final ConfigDef CONSUMER_CONFIG;
 
   public static final String KAFKA_OAUTH_CLIENT_ID_CONFIG = "oauth.client.id";
   public static final String KAFKA_OAUTH_CLIENT_ID_DOC = "Client id of the azure ad OAuth client";
@@ -15,4 +23,36 @@ public class KafkaOAuthConfig {
   public static final String KAFKA_OAUTH_TOKEN_FILE_CONFIG = "oauth.token.file";
   public static final String KAFKA_OAUTH_TOKEN_FILE_DOC =
       "Path of the file containing the token needed for retrieving the OAuth JWT token";
+
+  static {
+    PRODUCER_CONFIG = addKafkaConfig(ProducerConfig.configDef());
+    CONSUMER_CONFIG = addKafkaConfig(ConsumerConfig.configDef());
+  }
+
+  private static ConfigDef addKafkaConfig(ConfigDef configDef) {
+    return configDef
+        .define(
+            KAFKA_OAUTH_CLIENT_ID_CONFIG,
+            ConfigDef.Type.STRING,
+            ConfigDef.Importance.MEDIUM,
+            KAFKA_OAUTH_CLIENT_ID_DOC)
+        .define(
+            KAFKA_OAUTH_SCOPE_CONFIG,
+            ConfigDef.Type.STRING,
+            ConfigDef.Importance.MEDIUM,
+            KAFKA_OAUTH_SCOPE_DOC)
+        .define(
+            KAFKA_OAUTH_TOKEN_FILE_CONFIG,
+            ConfigDef.Type.STRING,
+            ConfigDef.Importance.MEDIUM,
+            KAFKA_OAUTH_TOKEN_FILE_DOC);
+  }
+
+  public static ConfigDef producerConfigDef() {
+    return new ConfigDef(PRODUCER_CONFIG);
+  }
+
+  public static ConfigDef consumerConfigDef() {
+    return new ConfigDef(CONSUMER_CONFIG);
+  }
 }
