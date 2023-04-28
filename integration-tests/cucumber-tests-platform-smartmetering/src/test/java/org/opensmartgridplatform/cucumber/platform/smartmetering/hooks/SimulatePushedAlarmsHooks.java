05e91f476fe43bffb0ead47077efe71ae98e67f9
Harry Middelburg <harry.middelburg@alliander.com>
2023-04-28 16:52:49 +0200

    Added cucumber test
    Signed-off-by: Harry Middelburg <harry.middelburg@alliander.com>

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/hooks/SimulatePushedAlarmsHooks.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/hooks/SimulatePushedAlarmsHooks.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 /** DLMS specific. */
@@ -32,5 +33,48 @@ public class SimulatePushedAlarmsHooks {
       socket.shutdownOutput();
       socket.shutdownInput();
     }
+  }
+
+  public static void simulateForwardedMx382Alarm(
+      final String equipmentIdentifier, final String host, final int port) throws IOException {
+
+    try (final Socket socket = new Socket(host, port)) {
+      final OutputStream outputStream = socket.getOutputStream();
+      outputStream.write(createMx382message(equipmentIdentifier));
+      socket.shutdownOutput();
+      socket.shutdownInput();
+    }
+  }
+
+  private static byte[] createMx382message(final String equipmentIdentifier) {
+    final byte[] apdu = createMx382Apdu(equipmentIdentifier);
+    final byte[] header = createMx382WpduHeader(apdu.length);
+    final byte[] wnm = new byte[header.length + apdu.length];
+    System.arraycopy(header, 0, wnm, 0, header.length);
+    System.arraycopy(apdu, 0, wnm, header.length, apdu.length);
+    return wnm;
+  }
+
+  private static byte[] createMx382Apdu(final String equipmentIdentifier) {
+    final ByteBuffer buf = ByteBuffer.allocate(64);
+    buf.put((byte) 0xC2);
+    buf.put((byte) 0x00);
+
+    final byte[] classId = new byte[] {0x00, 0x01};
+    buf.put(classId);
+    final byte[] obiscode =
+        new byte[] {(byte) 0x80, (byte) 0x80, (byte) 0x80, (byte) 0x80, (byte) 0x80, (byte) 0x00};
+    buf.put(obiscode);
+    final byte attribute = 0x02;
+    buf.put((byte) attribute);
+    buf.put(equipmentIdentifier.getBytes());
+    buf.flip();
+    final byte[] bytes = new byte[buf.limit()];
+    buf.get(bytes);
+    return bytes;
+  }
+
+  private static byte[] createMx382WpduHeader(final int apduLength) {
+    return new byte[] {0, 1, 0, 103, 0, 102, 0, (byte) apduLength};
   }
 }
--- integration-tests/cucumber-tests-platform-smartmetering/src/test/resources/features/osgp-adapter-ws-smartmetering/ReceiveAlarmNotifications.feature
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/resources/features/osgp-adapter-ws-smartmetering/ReceiveAlarmNotifications.feature
@@ -8,17 +8,28 @@ Feature: SmartMetering - Receive Alarm Notifications
     Given a dlms device
       | DeviceIdentification | TEST1024000000001 |
       | DeviceType           | SMART_METER_E     |
+    And a dlms device
+      | DeviceIdentification | TEST102600000001  |
+      | DeviceType           | SMART_METER_E     |
 
   Scenario: Handle a received alarm notification from a known device
     When an alarm notification is received from a known device
       | DeviceIdentification | TEST1024000000001 |
-    Then the alarm should be pushed to the osgp_logging database device_log_item table
+    Then 2 alarm should be pushed to the osgp_logging database device_log_item table
       | DeviceIdentification | TEST1024000000001 |
     And a push notification alarm should be received
+    
+  Scenario: Handle a received forwarded Mx382 alarm notification from a known device
+    When a forwarded mx382 alarm notification is received from a known device
+      | DeviceIdentification | TEST102600000001 |
+    Then 1 alarm should be pushed to the osgp_logging database device_log_item table
+      | DeviceIdentification | TEST102600000001 |
+    And a push notification alarm should be received
+
   @NightlyBuildOnly
   Scenario: Handle a received alarm notification from an unknown device
     When an alarm notification is received from an unknown device
       | DeviceIdentification | UNKNOWN0000000001 |
-    Then the alarm should be pushed to the osgp_logging database device_log_item table
+    Then 2 alarm should be pushed to the osgp_logging database device_log_item table
       | DeviceIdentification | UNKNOWN0000000001 |
     And a push notification alarm should be received
