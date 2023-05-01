5bb987a7127d1f24b43769cd3615571d7d019fc1
Gerben Kroes <gerben@kroesctrl.nl>
2023-05-01 11:03:42 +0200

    Reset db state for throttling (#1014)
    Signed-off-by: Gerben Kroes <gerben@kroesctrl.nl>

--- osgp/platform/osgp-throttling-service/src/main/java/org/opensmartgridplatform/throttling/config/SchedulingConfig.java
+++ osgp/platform/osgp-throttling-service/src/main/java/org/opensmartgridplatform/throttling/config/SchedulingConfig.java
@@ -13,14 +13,18 @@ package org.opensmartgridplatform.throttling.config;
 import javax.annotation.PostConstruct;
 import org.opensmartgridplatform.shared.application.scheduling.OsgpScheduler;
 import org.opensmartgridplatform.throttling.cleanup.PermitCleanUpJob;
+import org.opensmartgridplatform.throttling.cleanup.ReinitializeStateJob;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SchedulingConfig {
-  @Value("${scheduling.task.cleanup.permits.cron.expression:0 0 3 * * ?}")
+  @Value("${scheduling.task.cleanup.permits.cron.expression:0 0/30 * * * ?}")
   private String permitsCronExpression;
+
+  @Value("${scheduling.task.reinitialize.state.cron.expression:30 0/30 * * * ?}")
+  private String resetDbStateCronExpression;
 
   private final OsgpScheduler osgpScheduler;
 
@@ -31,5 +35,7 @@ public class SchedulingConfig {
   @PostConstruct
   public void initialize() throws SchedulerException {
     this.osgpScheduler.createAndScheduleJob(PermitCleanUpJob.class, this.permitsCronExpression);
+    this.osgpScheduler.createAndScheduleJob(
+        ReinitializeStateJob.class, this.resetDbStateCronExpression);
   }
 }
