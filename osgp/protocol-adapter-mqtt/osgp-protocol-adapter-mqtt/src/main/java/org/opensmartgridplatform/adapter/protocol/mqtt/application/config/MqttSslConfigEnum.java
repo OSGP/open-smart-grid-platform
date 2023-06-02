8cff3c9a649ceee20d8cd05dec6fb60e5efa5af8
unknown <lucianofavoroso@gmail.com>
2023-06-02 16:03:46 +0200

    changed license information for java files

--- osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/config/MqttSslConfigEnum.java
+++ osgp/protocol-adapter-mqtt/osgp-protocol-adapter-mqtt/src/main/java/org/opensmartgridplatform/adapter/protocol/mqtt/application/config/MqttSslConfigEnum.java
@@ -1,3 +1,5 @@
+//  SPDX-FileCopyrightText: Copyright Contributors to the GXF project
+//  SPDX-License-Identifier: Apache-2.0
 package org.opensmartgridplatform.adapter.protocol.mqtt.application.config;
 
 public enum MqttSslConfigEnum {
