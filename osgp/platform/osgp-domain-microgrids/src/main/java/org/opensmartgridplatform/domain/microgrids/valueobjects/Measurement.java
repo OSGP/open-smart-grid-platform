440ca9c6e2beb32de3bff4d9b2b3d9d4a7227d64
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-08-24 12:09:11 +0200

    FDP-1558: Refactored Measurement.java
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- osgp/platform/osgp-domain-microgrids/src/main/java/org/opensmartgridplatform/domain/microgrids/valueobjects/Measurement.java
+++ osgp/platform/osgp-domain-microgrids/src/main/java/org/opensmartgridplatform/domain/microgrids/valueobjects/Measurement.java
@@ -7,21 +7,21 @@ package org.opensmartgridplatform.domain.microgrids.valueobjects;
 import java.io.Serializable;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import org.joda.time.DateTime;
+import java.time.ZonedDateTime;
 
 public class Measurement extends NodeIdentifier implements Serializable {
   /** */
   private static final long serialVersionUID = 3315719218127525093L;
 
   private final int qualifier;
-  private final DateTime time;
+  private final ZonedDateTime time;
   private final double value;
 
   public Measurement(
       final int id,
       final String node,
       final int qualifier,
-      final DateTime time,
+      final ZonedDateTime time,
       final double value) {
     super(id, node);
     this.qualifier = qualifier;
@@ -33,7 +33,7 @@ public class Measurement extends NodeIdentifier implements Serializable {
     return this.qualifier;
   }
 
-  public DateTime getTime() {
+  public ZonedDateTime getTime() {
     return this.time;
   }
 
