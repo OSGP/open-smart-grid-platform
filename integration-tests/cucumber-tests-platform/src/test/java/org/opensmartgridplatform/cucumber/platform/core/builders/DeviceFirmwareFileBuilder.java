ef37ed13104fbb9ea8a75ddc18bd619d90f87249
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-09-01 13:01:25 +0200

    FDP-1158: refactored DeviceFirmwareFileBuilder
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/core/builders/DeviceFirmwareFileBuilder.java
+++ integration-tests/cucumber-tests-platform/src/test/java/org/opensmartgridplatform/cucumber/platform/core/builders/DeviceFirmwareFileBuilder.java
@@ -4,23 +4,24 @@
 
 package org.opensmartgridplatform.cucumber.platform.core.builders;
 
-import java.util.Date;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import org.opensmartgridplatform.cucumber.platform.PlatformKeys;
-import org.opensmartgridplatform.cucumber.platform.helpers.UtcDateHelper;
-import org.opensmartgridplatform.cucumber.platform.inputparsers.DateInputParser;
 import org.opensmartgridplatform.domain.core.entities.Device;
 import org.opensmartgridplatform.domain.core.entities.DeviceFirmwareFile;
 import org.opensmartgridplatform.domain.core.entities.FirmwareFile;
+import org.opensmartgridplatform.shared.utils.JavaTimeHelpers;
 
 public class DeviceFirmwareFileBuilder implements CucumberBuilder<DeviceFirmwareFile> {
 
-  private Date installationDate = UtcDateHelper.getUtcDate();
+  private Instant installationDate = ZonedDateTime.now(ZoneId.of("UTC")).toInstant();
   private String installedBy = "test-org";
   private FirmwareFile firmwareFile;
   private Device device;
 
-  public DeviceFirmwareFileBuilder withInstallationDate(final Date installationDate) {
+  public DeviceFirmwareFileBuilder withInstallationDate(final Instant installationDate) {
     this.installationDate = installationDate;
     return this;
   }
@@ -50,7 +51,9 @@ public class DeviceFirmwareFileBuilder implements CucumberBuilder<DeviceFirmware
   public DeviceFirmwareFileBuilder withSettings(final Map<String, String> inputSettings) {
     if (inputSettings.containsKey(PlatformKeys.DEVICEFIRMWARE_INSTALLATIONDATE)) {
       this.withInstallationDate(
-          DateInputParser.parse(inputSettings.get(PlatformKeys.DEVICEFIRMWARE_INSTALLATIONDATE)));
+          JavaTimeHelpers.parseToZonedDateTime(
+                  inputSettings.get(PlatformKeys.DEVICEFIRMWARE_INSTALLATIONDATE))
+              .toInstant());
     }
 
     if (inputSettings.containsKey(PlatformKeys.DEVICEFIRMWARE_INSTALLED_BY)) {
