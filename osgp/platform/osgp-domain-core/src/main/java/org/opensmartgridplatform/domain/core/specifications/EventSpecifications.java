d9aa003a3c229cdd6885a6036b072a612b1a736f
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-09-19 10:27:30 +0200

    FDP-1158: fix tests
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- osgp/platform/osgp-domain-core/src/main/java/org/opensmartgridplatform/domain/core/specifications/EventSpecifications.java
+++ osgp/platform/osgp-domain-core/src/main/java/org/opensmartgridplatform/domain/core/specifications/EventSpecifications.java
@@ -4,7 +4,7 @@
 
 package org.opensmartgridplatform.domain.core.specifications;
 
-import java.util.Date;
+import java.time.Instant;
 import java.util.List;
 import org.opensmartgridplatform.domain.core.entities.Event;
 import org.opensmartgridplatform.domain.core.entities.Organisation;
@@ -12,9 +12,9 @@ import org.opensmartgridplatform.domain.core.valueobjects.EventType;
 import org.springframework.data.jpa.domain.Specification;
 
 public interface EventSpecifications {
-  Specification<Event> isCreatedAfter(final Date dateFrom);
+  Specification<Event> isCreatedAfter(final Instant dateFrom);
 
-  Specification<Event> isCreatedBefore(final Date dateUntil);
+  Specification<Event> isCreatedBefore(final Instant dateUntil);
 
   Specification<Event> isFromDevice(final String deviceIdentification);
 
