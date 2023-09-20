8f617d138f47f38a99d107d470bdb4f940503271
Gerben Kroes <gerben@kroesctrl.nl>
2023-09-20 16:46:14 +0200

    Merge pull request #1075 from OSGP/feature/SMHE-1730_poll_alarm
    Feature/smhe 1730 poll alarm

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/networking/PushedMessageProcessorTest.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/networking/PushedMessageProcessorTest.java
@@ -21,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensmartgridplatform.adapter.protocol.dlms.infra.messaging.DlmsLogItemRequestMessage;
 import org.opensmartgridplatform.adapter.protocol.dlms.infra.messaging.DlmsLogItemRequestMessageSender;
 import org.opensmartgridplatform.adapter.protocol.dlms.infra.messaging.requests.to.core.OsgpRequestMessageSender;
+import org.opensmartgridplatform.adapter.protocol.jasper.service.DeviceSessionService;
 import org.opensmartgridplatform.dlms.DlmsPushNotification;
 import org.opensmartgridplatform.dto.valueobjects.smartmetering.AlarmTypeDto;
 import org.opensmartgridplatform.shared.infra.jms.MessageMetadata;
@@ -40,6 +41,7 @@ class PushedMessageProcessorTest {
 
   @Mock private OsgpRequestMessageSender osgpRequestMessageSender;
   @Mock private DlmsLogItemRequestMessageSender dlmsLogItemRequestMessageSender;
+  @Mock private DeviceSessionService deviceSessionService;
   @InjectMocks private PushedMessageProcessor processor;
 
   @Test
@@ -48,10 +50,11 @@ class PushedMessageProcessorTest {
     final DlmsPushNotification message =
         this.newDlmsPushNotification(bytes, IDENTIFIER, PUSH_CSD_TRIGGER, Collections.emptySet());
 
-    this.processor.process(message, IDENTIFIER, CORRELATION_UID, IP_ADDRESS);
+    this.processor.process(message, CORRELATION_UID, IDENTIFIER, IP_ADDRESS);
 
     verifyNoInteractions(this.osgpRequestMessageSender);
     verifyNoInteractions(this.dlmsLogItemRequestMessageSender);
+    verifyNoInteractions(this.deviceSessionService);
   }
 
   @Test
@@ -60,7 +63,7 @@ class PushedMessageProcessorTest {
     final DlmsPushNotification message =
         this.newDlmsPushNotification(bytes, IDENTIFIER, PUSH_ALARM_TRIGGER, Collections.emptySet());
 
-    this.processor.process(message, IDENTIFIER, CORRELATION_UID, IP_ADDRESS);
+    this.processor.process(message, CORRELATION_UID, IDENTIFIER, IP_ADDRESS);
 
     verify(this.osgpRequestMessageSender)
         .send(
@@ -68,6 +71,7 @@ class PushedMessageProcessorTest {
             eq(MessageType.PUSH_NOTIFICATION_ALARM.name()),
             any(MessageMetadata.class));
     verify(this.dlmsLogItemRequestMessageSender).send(any(DlmsLogItemRequestMessage.class));
+    verifyNoInteractions(this.deviceSessionService);
   }
 
   @Test
@@ -76,11 +80,12 @@ class PushedMessageProcessorTest {
     final DlmsPushNotification message =
         this.newDlmsPushNotification(bytes, IDENTIFIER, PUSH_SMS_TRIGGER, Collections.emptySet());
 
-    this.processor.process(message, IDENTIFIER, CORRELATION_UID, IP_ADDRESS);
+    this.processor.process(message, CORRELATION_UID, IDENTIFIER, IP_ADDRESS);
 
     verify(this.osgpRequestMessageSender)
         .send(any(RequestMessage.class), eq(MessageType.PUSH_NOTIFICATION_SMS.name()), isNull());
     verify(this.dlmsLogItemRequestMessageSender).send(any(DlmsLogItemRequestMessage.class));
+    verify(this.deviceSessionService).notifyIpAddress(IDENTIFIER, IP_ADDRESS);
   }
 
   private DlmsPushNotification newDlmsPushNotification(
