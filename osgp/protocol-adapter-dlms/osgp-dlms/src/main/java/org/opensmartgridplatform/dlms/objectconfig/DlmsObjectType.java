4583fdd5a12316535238a2bd92b9e6bf697ce510
Natasja <natasja.nortier@alliander.com>
2023-10-02 09:24:01 +0200

    SMHE-1740 object config lijst per sprint - sprint1 update unit tests
    Signed-off-by: Natasja <natasja.nortier@alliander.com>

--- osgp/protocol-adapter-dlms/osgp-dlms/src/main/java/org/opensmartgridplatform/dlms/objectconfig/DlmsObjectType.java
+++ osgp/protocol-adapter-dlms/osgp-dlms/src/main/java/org/opensmartgridplatform/dlms/objectconfig/DlmsObjectType.java
@@ -60,7 +60,9 @@ public enum DlmsObjectType {
   ALARM_REGISTER_2,
   ALARM_REGISTER_3,
   READ_MBUS_STATUS,
-  CLEAR_MBUS_STATUS;
+  CLEAR_MBUS_STATUS,
+  PHASE_OUTAGE_TEST,
+  LAST_GASP_TEST;
 
   public String value() {
     return this.name();
