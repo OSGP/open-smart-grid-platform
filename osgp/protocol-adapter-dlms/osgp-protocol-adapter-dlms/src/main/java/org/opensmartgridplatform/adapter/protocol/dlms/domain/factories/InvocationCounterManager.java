2ce51947099404049244f2186a9cfc70c4ee1e92
Gerben Kroes <gerben@kroesctrl.nl>
2023-10-19 11:17:20 +0200

    Reset ip-address after retrieval of invocation counter from device
    Signed-off-by: Gerben Kroes <gerben@kroesctrl.nl>

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/factories/InvocationCounterManager.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/factories/InvocationCounterManager.java
@@ -59,16 +59,16 @@ public class InvocationCounterManager {
    */
   public void initializeInvocationCounter(
       final MessageMetadata messageMetadata, final DlmsDevice device) throws OsgpException {
+    this.initializeWithInvocationCounterStoredOnDevice(messageMetadata, device, null);
 
     /*
-     * When the invocation counter is out of sync, the device closes the session.
+     * When the invocation counter has been initialized, the device closes the session.
      * By setting the ip-address to null, the application will be forced to get a new ip-address.
+     * The meter will start a new session.
      * This is done by the DlmsConnectionFactory in the method:
      * this.domainHelperService.setIpAddressFromMessageMetadataOrSessionProvider
      */
     device.setIpAddress(null);
-
-    this.initializeWithInvocationCounterStoredOnDevice(messageMetadata, device, null);
   }
 
   /**
--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/factories/InvocationCounterManagerTest.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/test/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/factories/InvocationCounterManagerTest.java
@@ -11,6 +11,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -24,6 +25,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.openmuc.jdlms.AttributeAddress;
@@ -75,15 +77,17 @@ class InvocationCounterManagerTest {
   }
 
   @Test
-  void initializeInvocationCounterForDeviceTaskExecuted() throws OsgpException {
-    this.device.setIpAddress(IP_ADDRESS);
+  void resetIpAddressAfterInitialize() throws OsgpException {
+    this.device = mock(DlmsDevice.class);
 
     this.manager.initializeInvocationCounter(this.messageMetadata, this.device);
 
-    assertThat(this.device.getIpAddress()).isNull();
-    verify(this.connectionFactory, times(1))
+    final InOrder inOrder = inOrder(this.connectionFactory, this.device);
+    inOrder
+        .verify(this.connectionFactory, times(1))
         .createAndHandlePublicClientConnection(
             any(MessageMetadata.class), eq(this.device), isNull(), isNull(), any());
+    inOrder.verify(this.device).setIpAddress(null);
   }
 
   @Test
