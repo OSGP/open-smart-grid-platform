58576f74170391dc73a474fadef0998d34a599db
Andre Bolder <andre.bolder@alliander.com>
2023-11-01 12:57:33 +0100

    SMHE-1613: review comments addressed and changed naming of mbus devices and gateway devices within the DeviceModelCodeUtil
    Signed-off-by: Andre Bolder <andre.bolder@alliander.com>

--- osgp/platform/osgp-adapter-domain-smartmetering/src/main/java/org/opensmartgridplatform/adapter/domain/smartmetering/application/services/util/DeviceModelCodeUtil.java
+++ osgp/platform/osgp-adapter-domain-smartmetering/src/main/java/org/opensmartgridplatform/adapter/domain/smartmetering/application/services/util/DeviceModelCodeUtil.java
@@ -12,21 +12,21 @@ public class DeviceModelCodeUtil {
   }
 
   public static String createDeviceModelCodes(
-      final SmartMeter smartMeter, final List<SmartMeter> smartMeters) {
+      final SmartMeter gatewayDevice, final List<SmartMeter> mbusDevices) {
 
     final String emetercode =
-        smartMeter.getDeviceModel() != null ? smartMeter.getDeviceModel().getModelCode() : "";
-    final String mbus1code = getAndMapMbusChannelSmartMeter(smartMeters, 1);
-    final String mbus2code = getAndMapMbusChannelSmartMeter(smartMeters, 2);
-    final String mbus3code = getAndMapMbusChannelSmartMeter(smartMeters, 3);
-    final String mbus4code = getAndMapMbusChannelSmartMeter(smartMeters, 4);
+        gatewayDevice.getDeviceModel() != null ? gatewayDevice.getDeviceModel().getModelCode() : "";
+    final String mbus1code = getAndMapMbusChannelSmartMeter(mbusDevices, 1);
+    final String mbus2code = getAndMapMbusChannelSmartMeter(mbusDevices, 2);
+    final String mbus3code = getAndMapMbusChannelSmartMeter(mbusDevices, 3);
+    final String mbus4code = getAndMapMbusChannelSmartMeter(mbusDevices, 4);
 
     return String.format("%s,%s,%s,%s,%s", emetercode, mbus1code, mbus2code, mbus3code, mbus4code);
   }
 
   private static String getAndMapMbusChannelSmartMeter(
-      final List<SmartMeter> smartMeters, final int channel) {
-    return smartMeters.stream()
+      final List<SmartMeter> mbusDevices, final int channel) {
+    return mbusDevices.stream()
         .filter(m -> m.getChannel() != null && m.getChannel() == channel)
         .map(m -> m.getDeviceModel() != null ? m.getDeviceModel().getModelCode() : "")
         .map(s -> s.replace(",", "")) // no comma's signs
