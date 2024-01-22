670616495586f5ffbd68106081a165a358755e4f
Gerben Kroes <gerben@kroesctrl.nl>
2024-01-22 10:04:47 +0100

    Change loglevel of message that gas model cannot be found in configuration
    Signed-off-by: Gerben Kroes <gerben@kroesctrl.nl>

--- osgp/protocol-adapter-dlms/osgp-dlms/src/main/java/org/opensmartgridplatform/dlms/objectconfig/configlookup/ConfigLookupGroup.java
+++ osgp/protocol-adapter-dlms/osgp-dlms/src/main/java/org/opensmartgridplatform/dlms/objectconfig/configlookup/ConfigLookupGroup.java
@@ -7,23 +7,30 @@ package org.opensmartgridplatform.dlms.objectconfig.configlookup;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @NoArgsConstructor
+@Slf4j
 public class ConfigLookupGroup {
   protected String name;
   protected String defaulttype;
   protected List<ConfigLookupType> configlookuptypes;
 
   public String getMatchingType(final String deviceModel) {
-    String matchingType = this.defaulttype;
-
+    String lastMatchingType = null;
     for (final ConfigLookupType configLookupType : this.configlookuptypes) {
       if (configLookupType.matches(deviceModel)) {
-        matchingType = configLookupType.type;
+        lastMatchingType = configLookupType.type;
       }
     }
+    if (lastMatchingType != null) {
+      return lastMatchingType;
+    }
 
-    return matchingType;
+    log.warn(
+        "Could not determine matching type for device model: {}. This model should be added to the configuration, to prevent inefficient communication with meter",
+        deviceModel);
+    return this.defaulttype;
   }
 }
