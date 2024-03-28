50dcd916a01d6499b007252f790fd60666b29d5f
Jaco Boon <jaco.boon@gmail.com>
2024-03-28 20:10:11 +0100

    update calculation, update scheduled task in core, test
    Signed-off-by: Jaco Boon <jaco.boon@gmail.com>

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/messaging/MessagePriorityHandler.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/infra/messaging/MessagePriorityHandler.java
@@ -1,18 +1,20 @@
 package org.opensmartgridplatform.adapter.protocol.dlms.infra.messaging;
 
+import org.opensmartgridplatform.shared.infra.jms.MessageMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MessagePriorityCalculator {
+public class MessagePriorityHandler {
 
   private final boolean decreaseEnabled;
   private final int decreaseInterval;
   private final int decreaseValue;
   private final int minimumPriority;
 
-  public MessagePriorityCalculator(
-      @Value("${schedule.retry.dlms.decrease_priority.enabled:false}") final boolean decreaseEnabled,
+  public MessagePriorityHandler(
+      @Value("${schedule.retry.dlms.decrease_priority.enabled:false}")
+          final boolean decreaseEnabled,
       @Value("${schedule.retry.dlms.decrease_priority.interval:1}") final int decreaseInterval,
       @Value("${schedule.retry.dlms.decrease_priority.value:1}") final int decreaseValue,
       @Value("${schedule.retry.dlms.decrease_priority.min_priority:0}") final int minimumPriority) {
@@ -23,10 +25,16 @@ public class MessagePriorityCalculator {
     this.minimumPriority = minimumPriority;
   }
 
-  public int decreasePriority(final int priority, final int retryCount) {
-    if (this.decreaseEnabled && retryCount % this.decreaseInterval == 0) {
+  public int recalculatePriority(final MessageMetadata messageMetadata) {
+    final int retryCount = messageMetadata.getRetryCount();
+    final int priority = messageMetadata.getMessagePriority();
+
+    if (this.decreaseEnabled
+        && (retryCount > 0 || this.decreaseInterval == 1)
+        && retryCount % this.decreaseInterval == 0) {
       return Math.max(priority - this.decreaseValue, this.minimumPriority);
     }
+
     return priority;
   }
 }
