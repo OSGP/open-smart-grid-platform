163ced4149286710fd76710e6a1f15d0f334bf3e
Harry Middelburg <harry.middelburg@alliander.com>
2024-06-07 17:00:10 +0200

    feature/SMHE-2018-invalid-IdentificationNumber
    Signed-off-by: Harry Middelburg <harry.middelburg@alliander.com>

--- osgp/shared/osgp-dto/src/main/java/org/opensmartgridplatform/dto/valueobjects/smartmetering/DecoupleMbusDeviceResponseDto.java
+++ osgp/shared/osgp-dto/src/main/java/org/opensmartgridplatform/dto/valueobjects/smartmetering/DecoupleMbusDeviceResponseDto.java
@@ -4,18 +4,22 @@
 
 package org.opensmartgridplatform.dto.valueobjects.smartmetering;
 
+import java.io.Serial;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class DecoupleMbusDeviceResponseDto extends ActionResponseDto {
 
-  private static final long serialVersionUID = -4454979905929290745L;
+  @Serial private static final long serialVersionUID = -4454979905929290745L;
 
-  private String mbusDeviceIdentification;
+  private static final String SUCCESSFUL_RESULT = "Decouple Mbus Device was successful";
+
+  @Setter private String mbusDeviceIdentification;
   private final ChannelElementValuesDto channelElementValues;
 
   public DecoupleMbusDeviceResponseDto(final ChannelElementValuesDto channelElementValues) {
-    super("Decouple Mbus Device was successful");
+    super(SUCCESSFUL_RESULT);
     this.channelElementValues = channelElementValues;
   }
 
@@ -23,7 +27,7 @@ public class DecoupleMbusDeviceResponseDto extends ActionResponseDto {
       final boolean success, final ChannelElementValuesDto channelElementValues) {
     super(
         success
-            ? "Decouple Mbus Device was successful."
+            ? SUCCESSFUL_RESULT
             : "Channel information could not be correctly interpreted. "
                 + "Mbus Device was successful decoupled anyway.");
     this.channelElementValues = channelElementValues;
@@ -31,14 +35,8 @@ public class DecoupleMbusDeviceResponseDto extends ActionResponseDto {
 
   @Override
   public String toString() {
-    return "DecoupleMbusDeviceResponseDto [channel="
-        + this.channelElementValues.getChannel()
-        + ", mbusDeviceIdentification="
-        + this.mbusDeviceIdentification
-        + "]";
-  }
-
-  public void setMbusDeviceIdentification(final String mbusDeviceIdentification) {
-    this.mbusDeviceIdentification = mbusDeviceIdentification;
+    return String.format(
+        "DecoupleMbusDeviceResponseDto [channel=%d, mbusDeviceIdentification=%s]",
+        this.channelElementValues.getChannel(), this.mbusDeviceIdentification);
   }
 }
