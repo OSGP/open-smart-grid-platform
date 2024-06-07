01e6eab75c53b30e2d40f5b5f2d8a06d53e1b967
Sander Verbruggen <sander.verbruggen@alliander.com>
2024-06-07 09:27:06 +0200

    FDP-942: removed unused variables, rewrote Javadoc with config instructions

--- osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/AbstractPersistenceConfig.java
+++ osgp/shared/shared/src/main/java/org/opensmartgridplatform/shared/application/config/AbstractPersistenceConfig.java
@@ -5,8 +5,6 @@
 package org.opensmartgridplatform.shared.application.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import java.util.Properties;
-import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationVersion;
 import org.hibernate.jpa.HibernatePersistenceProvider;
@@ -14,229 +12,228 @@ import org.opensmartgridplatform.shared.exceptionhandling.DeprecatedPropertyExce
 import org.opensmartgridplatform.shared.infra.db.DefaultConnectionPoolFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.support.AbstractPlatformTransactionManager;
 
-/** This class provides the basic components used for persistence. */
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * This class provides the basic components used for persistence.
+ * Configuring the datasource URL can be done in two ways:
+ * <ul>
+ *     <li><code>db.url</code> with the complete URL. If this property is present it will be used</li>
+ *     <li>Separate properties for <code>db.protocol</code>, <code>db.host</code>, <code>db.port</code> and <code>db.name</code></li>
+ * </ul>
+ * You can also combine it when using several databases. Define a global property e.g. <code>db.url=jdbc:postgres://host1:5432,host2:5432/${db.name}</code>.
+ * This way you can define <code>db.host</code> per app, but have a global host config.
+ */
 public abstract class AbstractPersistenceConfig extends AbstractConfig {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AbstractPersistenceConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractPersistenceConfig.class);
 
-  private static final String REGEX_COMMA_WITH_OPTIONAL_WHITESPACE = "\\s*+,\\s*+";
+    private static final String REGEX_COMMA_WITH_OPTIONAL_WHITESPACE = "\\s*+,\\s*+";
 
-  @Value("${db.username}")
-  private String username;
+    @Value("${db.username}")
+    private String username;
 
-  @Value("${db.password}")
-  private String password;
+    @Value("${db.password}")
+    private String password;
 
-  @Value("${db.driver}")
-  private String driverClassName;
+    @Value("${db.driver}")
+    private String driverClassName;
 
-  // Accept a direct property db.url or else construct it from the individual db properties
+    // Accept a direct property db.url or else construct it from the individual db properties
 
-  /**
-   * The JDBC URL for the database. This can be constructed in two ways:
-   *
-   * <ul>
-   *   <li>a property {@code db.url} with the whole JDBC URL in it (e.g. for db host failover)
-   *   <li>- four properties: {@code db.protocol}, {@code db.host}, {@code db.port} (defaults to
-   *       5432) and {@code db.name}
-   * </ul>
-   */
-  @Value("${db.url:${db.protocol}${db.host}:${db.port:5432}/${db.name}}")
-  private String databaseUrl;
+    /**
+     * The JDBC URL for the database. This can be constructed in two ways, see the class Javadoc for details.
+     */
+    @Value("${db.url:${db.protocol}${db.host}:${db.port:5432}/${db.name}}")
+    private String databaseUrl;
 
-  @Value("${db.min_pool_size:1}")
-  private int minPoolSize;
+    @Value("${db.min_pool_size:1}")
+    private int minPoolSize;
 
-  @Value("${db.max_pool_size:5}")
-  private int maxPoolSize;
+    @Value("${db.max_pool_size:5}")
+    private int maxPoolSize;
 
-  @Value("${db.initialization_fail_timeout:300000}")
-  private long initializationFailTimeout;
+    @Value("${db.initialization_fail_timeout:300000}")
+    private long initializationFailTimeout;
 
-  @Value("${db.validation_timeout:5000}")
-  private long validationTimeout;
+    @Value("${db.validation_timeout:5000}")
+    private long validationTimeout;
 
-  @Value("${db.connection_timeout:30000}")
-  private long connectionTimeout;
+    @Value("${db.connection_timeout:30000}")
+    private long connectionTimeout;
 
-  @Value("${db.auto_commit:false}")
-  private boolean isAutoCommit;
+    @Value("${db.auto_commit:false}")
+    private boolean isAutoCommit;
 
-  @Value("${db.idle_timeout:120000}")
-  private int idleTimeout;
+    @Value("${db.idle_timeout:120000}")
+    private int idleTimeout;
 
-  @Value("${db.max_lifetime:1800000}")
-  private int maxLifetime;
+    @Value("${db.max_lifetime:1800000}")
+    private int maxLifetime;
 
-  @Value("${flyway.initial.version}")
-  private String flywayInitialVersion;
+    @Value("${flyway.initial.version}")
+    private String flywayInitialVersion;
 
-  @Value("${flyway.initial.description}")
-  private String flywayInitialDescription;
+    @Value("${flyway.initial.description}")
+    private String flywayInitialDescription;
 
-  @Value("${flyway.init.on.migrate:true}")
-  private boolean flywayInitOnMigrate;
+    @Value("${flyway.init.on.migrate:true}")
+    private boolean flywayInitOnMigrate;
 
-  private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
 
-  @Value("${hibernate.dialect}")
-  private String hibernateDialect;
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
 
-  private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
+    private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
 
-  @Value("${hibernate.format_sql}")
-  private String hibernateFormatSql;
+    @Value("${hibernate.format_sql}")
+    private String hibernateFormatSql;
 
-  /**
-   * This is the old property of Hibernate 4.X, replaced by hibernate.physical_naming_strategy in
-   * Hibernate 5.x.
-   */
-  private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY_DEPRECATED =
-      "hibernate.ejb.naming_strategy";
+    /**
+     * This is the old property of Hibernate 4.X, replaced by hibernate.physical_naming_strategy in
+     * Hibernate 5.x.
+     */
+    private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY_DEPRECATED =
+            "hibernate.ejb.naming_strategy";
 
-  @Value("${hibernate.ejb.naming_strategy}")
-  private String hibernateNamingStrategyDeprecated;
+    private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY =
+            "hibernate.physical_naming_strategy";
 
-  private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY =
-      "hibernate.physical_naming_strategy";
+    @Value("${hibernate.physical_naming_strategy}")
+    private String hibernateNamingStrategy;
 
-  @Value("${hibernate.physical_naming_strategy}")
-  private String hibernateNamingStrategy;
+    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
 
-  private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+    @Value("${hibernate.show_sql}")
+    private String hibernateShowSql;
 
-  @Value("${hibernate.show_sql}")
-  private String hibernateShowSql;
+    @Value("${entitymanager.packages.to.scan}")
+    private String entitymanagerPackagesToScan;
 
-  @Value("${entitymanager.packages.to.scan}")
-  private String entitymanagerPackagesToScan;
+    private HikariDataSource dataSource;
 
-  @Autowired private Environment env;
+    protected DataSource getDataSource() {
+        if (this.dataSource == null) {
+            final DefaultConnectionPoolFactory factory = this.builder().build();
+            this.dataSource = factory.getDefaultConnectionPool();
+        }
 
-  private HikariDataSource dataSource;
-
-  protected DataSource getDataSource() {
-    if (this.dataSource == null) {
-      final DefaultConnectionPoolFactory factory = this.builder().build();
-      this.dataSource = factory.getDefaultConnectionPool();
+        return this.dataSource;
     }
 
-    return this.dataSource;
-  }
+    protected JpaTransactionManager transactionManager() {
+        final JpaTransactionManager transactionManager = new JpaTransactionManager();
 
-  protected JpaTransactionManager transactionManager() {
-    final JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(this.entityManagerFactory().getObject());
+        transactionManager.setTransactionSynchronization(
+                AbstractPlatformTransactionManager.SYNCHRONIZATION_ALWAYS);
 
-    transactionManager.setEntityManagerFactory(this.entityManagerFactory().getObject());
-    transactionManager.setTransactionSynchronization(
-        AbstractPlatformTransactionManager.SYNCHRONIZATION_ALWAYS);
-
-    return transactionManager;
-  }
-
-  protected abstract LocalContainerEntityManagerFactoryBean entityManagerFactory();
-
-  protected Flyway createFlyway() {
-    return this.createFlyway(this.getDataSource());
-  }
-
-  protected Flyway createFlyway(final DataSource dataSource) {
-    return Flyway.configure()
-        .baselineVersion(MigrationVersion.fromVersion(this.flywayInitialVersion))
-        .baselineDescription(this.flywayInitialDescription)
-        .baselineOnMigrate(this.flywayInitOnMigrate)
-        .outOfOrder(true)
-        .table("schema_version")
-        .dataSource(dataSource)
-        .load();
-  }
-
-  protected LocalContainerEntityManagerFactoryBean entityManagerFactory(
-      final String persistenceUnitName) {
-    return this.entityManagerFactory(
-        persistenceUnitName, this.getDataSource(), this.entitymanagerPackagesToScan);
-  }
-
-  protected LocalContainerEntityManagerFactoryBean entityManagerFactory(
-      final String persistenceUnitName, final DataSource dataSource) {
-    return this.entityManagerFactory(
-        persistenceUnitName, dataSource, this.entitymanagerPackagesToScan);
-  }
-
-  protected LocalContainerEntityManagerFactoryBean entityManagerFactory(
-      final String persistenceUnitName,
-      final DataSource dataSource,
-      final String entitymanagerPackagesToScan) {
-    this.checkForDeprecatedHibernateNamingStrategyConfiguration();
-
-    final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
-        new LocalContainerEntityManagerFactoryBean();
-
-    entityManagerFactoryBean.setPersistenceUnitName(persistenceUnitName);
-    entityManagerFactoryBean.setDataSource(dataSource);
-    if (entitymanagerPackagesToScan.contains(",")) {
-      entityManagerFactoryBean.setPackagesToScan(
-          entitymanagerPackagesToScan.split(REGEX_COMMA_WITH_OPTIONAL_WHITESPACE));
-    } else {
-      entityManagerFactoryBean.setPackagesToScan(entitymanagerPackagesToScan);
+        return transactionManager;
     }
-    entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
 
-    final Properties jpaProperties = new Properties();
-    jpaProperties.put(PROPERTY_NAME_HIBERNATE_DIALECT, this.hibernateDialect);
-    jpaProperties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, this.hibernateFormatSql);
-    jpaProperties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, this.hibernateNamingStrategy);
-    jpaProperties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, this.hibernateShowSql);
+    protected abstract LocalContainerEntityManagerFactoryBean entityManagerFactory();
 
-    entityManagerFactoryBean.setJpaProperties(jpaProperties);
-
-    return entityManagerFactoryBean;
-  }
-
-  private void checkForDeprecatedHibernateNamingStrategyConfiguration() {
-    final String deprecatedProperty =
-        this.environment.getProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY_DEPRECATED);
-
-    if (deprecatedProperty != null) {
-      final String message =
-          String.format(
-              "Using '%s=%s' is deprecated and no longer works with Hibernate 5.X! Use '%s=%s' instead!",
-              PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY_DEPRECATED,
-              deprecatedProperty,
-              PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY,
-              HibernateNamingStrategy.class.getName());
-
-      LOGGER.error(message);
-      throw new DeprecatedPropertyException(message);
+    protected Flyway createFlyway() {
+        return this.createFlyway(this.getDataSource());
     }
-  }
 
-  protected void destroyDataSource() {
-    if (this.dataSource != null) {
-      this.dataSource.close();
+    protected Flyway createFlyway(final DataSource dataSource) {
+        return Flyway.configure()
+                .baselineVersion(MigrationVersion.fromVersion(this.flywayInitialVersion))
+                .baselineDescription(this.flywayInitialDescription)
+                .baselineOnMigrate(this.flywayInitOnMigrate)
+                .outOfOrder(true)
+                .table("schema_version")
+                .dataSource(dataSource)
+                .load();
     }
-  }
 
-  protected DefaultConnectionPoolFactory.Builder builder() {
-    return new DefaultConnectionPoolFactory.Builder()
-        .withDriverClassName(this.driverClassName)
-        .withDatabaseUrl(this.databaseUrl)
-        .withMinPoolSize(this.minPoolSize)
-        .withMaxPoolSize(this.maxPoolSize)
-        .withAutoCommit(this.isAutoCommit)
-        .withIdleTimeout(this.idleTimeout)
-        .withMaxLifetime(this.maxLifetime)
-        .withInitializationFailTimeout(this.initializationFailTimeout)
-        .withValidationTimeout(this.validationTimeout)
-        .withConnectionTimeout(this.connectionTimeout)
-        .withUsername(this.username)
-        .withPassword(this.password);
-  }
+    protected LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            final String persistenceUnitName) {
+        return this.entityManagerFactory(
+                persistenceUnitName, this.getDataSource(), this.entitymanagerPackagesToScan);
+    }
+
+    protected LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            final String persistenceUnitName, final DataSource dataSource) {
+        return this.entityManagerFactory(
+                persistenceUnitName, dataSource, this.entitymanagerPackagesToScan);
+    }
+
+    protected LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            final String persistenceUnitName,
+            final DataSource dataSource,
+            final String entitymanagerPackagesToScan) {
+        this.checkForDeprecatedHibernateNamingStrategyConfiguration();
+
+        final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setPersistenceUnitName(persistenceUnitName);
+        entityManagerFactoryBean.setDataSource(dataSource);
+        if (entitymanagerPackagesToScan.contains(",")) {
+            entityManagerFactoryBean.setPackagesToScan(
+                    entitymanagerPackagesToScan.split(REGEX_COMMA_WITH_OPTIONAL_WHITESPACE));
+        } else {
+            entityManagerFactoryBean.setPackagesToScan(entitymanagerPackagesToScan);
+        }
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+
+        final Properties jpaProperties = new Properties();
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_DIALECT, this.hibernateDialect);
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, this.hibernateFormatSql);
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, this.hibernateNamingStrategy);
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, this.hibernateShowSql);
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    private void checkForDeprecatedHibernateNamingStrategyConfiguration() {
+        final String deprecatedProperty =
+                this.environment.getProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY_DEPRECATED);
+
+        if (deprecatedProperty != null) {
+            final String message =
+                    String.format(
+                            "Using '%s=%s' is deprecated and no longer works with Hibernate 5.X! Use '%s=%s' instead!",
+                            PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY_DEPRECATED,
+                            deprecatedProperty,
+                            PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY,
+                            HibernateNamingStrategy.class.getName());
+
+            LOGGER.error(message);
+            throw new DeprecatedPropertyException(message);
+        }
+    }
+
+    protected void destroyDataSource() {
+        if (this.dataSource != null) {
+            this.dataSource.close();
+        }
+    }
+
+    protected DefaultConnectionPoolFactory.Builder builder() {
+        return new DefaultConnectionPoolFactory.Builder()
+                .withDriverClassName(this.driverClassName)
+                .withDatabaseUrl(this.databaseUrl)
+                .withMinPoolSize(this.minPoolSize)
+                .withMaxPoolSize(this.maxPoolSize)
+                .withAutoCommit(this.isAutoCommit)
+                .withIdleTimeout(this.idleTimeout)
+                .withMaxLifetime(this.maxLifetime)
+                .withInitializationFailTimeout(this.initializationFailTimeout)
+                .withValidationTimeout(this.validationTimeout)
+                .withConnectionTimeout(this.connectionTimeout)
+                .withUsername(this.username)
+                .withPassword(this.password);
+    }
 }
