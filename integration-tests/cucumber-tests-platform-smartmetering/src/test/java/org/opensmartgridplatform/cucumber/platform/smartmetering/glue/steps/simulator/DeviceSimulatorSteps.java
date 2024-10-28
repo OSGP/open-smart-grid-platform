26f9d074986a4a0d2a75ecd9243b024e6c7e7792
Natasja Nortier <natasja.nortier@alliander.com>
2024-10-28 14:40:14 +0100

    SMHE-2299 update cxf version
    Signed-off-by: Natasja Nortier <natasja.nortier@alliander.com>

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/simulator/DeviceSimulatorSteps.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/glue/steps/simulator/DeviceSimulatorSteps.java
@@ -22,7 +22,6 @@ import org.opensmartgridplatform.dlms.interfaceclass.InterfaceClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 public class DeviceSimulatorSteps {
 
@@ -35,12 +34,8 @@ public class DeviceSimulatorSteps {
 
   @Autowired private JsonObjectCreator jsonObjectCreator;
 
-  @Value("${dynamic.properties.base.url}")
-  private String dynamicPropertiesBaseUrl;
-
   public void clearDlmsAttributeValues() {
     try {
-      this.simulatorTriggerClient = new SimulatorTriggerClient(this.dynamicPropertiesBaseUrl);
       this.simulatorTriggerClient.clearDlmsAttributeValues();
     } catch (final SimulatorTriggerClientException stce) {
       LOGGER.error("Error calling simulatorTriggerClient.clearDlmsAttributeValues()", stce);
