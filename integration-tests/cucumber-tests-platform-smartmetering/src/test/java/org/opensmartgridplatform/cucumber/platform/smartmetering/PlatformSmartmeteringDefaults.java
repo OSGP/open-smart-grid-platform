0371480238d19e10fa925e1d5fce9993dc5f8daa
paulus <paulus@nunnink.dev>
2025-05-06 12:43:38 +0200

    Revert "Fixed formatting for cucumber integration test."
    This reverts commit c6fce72217cde4029c3dde7b702383eecb49793a.

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/PlatformSmartmeteringDefaults.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/PlatformSmartmeteringDefaults.java
@@ -10,6 +10,7 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.opensmartgridplatform.cucumber.platform.PlatformDefaults;
 import org.opensmartgridplatform.domain.core.entities.DeviceModel;
 import org.opensmartgridplatform.domain.core.entities.ProtocolInfo;
 
