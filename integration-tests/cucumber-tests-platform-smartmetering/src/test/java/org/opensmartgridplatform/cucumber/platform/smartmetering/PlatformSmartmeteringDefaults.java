e7dbb0592cd5164ffa1f31afddb3b51ef9f718bb
paulus <paulus@nunnink.dev>
2025-05-06 13:06:16 +0200

    Revert "Added constant for postgres host string."
    This reverts commit 5c231c13776f48baf705190e1912ac2ae1b7f8d6.

--- integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/PlatformSmartmeteringDefaults.java
+++ integration-tests/cucumber-tests-platform-smartmetering/src/test/java/org/opensmartgridplatform/cucumber/platform/smartmetering/PlatformSmartmeteringDefaults.java
@@ -42,7 +42,6 @@ public class PlatformSmartmeteringDefaults
   public static final boolean HLS3ACTIVE = false;
   public static final boolean HLS4ACTIVE = false;
   public static final boolean HLS5ACTIVE = true;
-  public static final String HOST = "postgres";
   public static final String ICC_ID = "iccid";
   public static final boolean LLS1_ACTIVE = false;
   public static final boolean USE_HDLC = false;
@@ -109,7 +108,7 @@ public class PlatformSmartmeteringDefaults
   static {
     InetAddress localhost;
     try {
-      localhost = InetAddress.getByName(PlatformSmartmeteringDefaults.HOST);
+      localhost = InetAddress.getByName("postgres");
     } catch (final UnknownHostException e) {
       localhost = null;
     }
