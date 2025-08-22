28e0c30310b8893f2a4e0a9aef32196f446a4238
stefan.ermens <stefan.ermens@alliander.com>
2025-08-22 13:20:58 +0200

    Fix formatting
    Signed-off-by: stefan.ermens <stefan.ermens@alliander.com>

--- osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/periodicmeterreads/GetPeriodicMeterReadsGasCommandExecutor.java
+++ osgp/protocol-adapter-dlms/osgp-protocol-adapter-dlms/src/main/java/org/opensmartgridplatform/adapter/protocol/dlms/domain/commands/periodicmeterreads/GetPeriodicMeterReadsGasCommandExecutor.java
@@ -147,9 +147,9 @@ public class GetPeriodicMeterReadsGasCommandExecutor
           logTime = this.readClock(periodType, previousLogTime, intervalTime, bufferedObject);
         }
         case AMR_PROFILE_STATUS,
-                AMR_PROFILE_STATUS_HOURLY_G,
-                AMR_PROFILE_STATUS_DAILY_G,
-                AMR_PROFILE_STATUS_MONTHLY_G ->
+            AMR_PROFILE_STATUS_HOURLY_G,
+            AMR_PROFILE_STATUS_DAILY_G,
+            AMR_PROFILE_STATUS_MONTHLY_G ->
             // The status is used in most profiles. But for some it is not used. In that case, the
             // selectedObjects will not contain a status object and readStatus will return null.
             status = this.readAmrProfileStatusCode(bufferedObject);
