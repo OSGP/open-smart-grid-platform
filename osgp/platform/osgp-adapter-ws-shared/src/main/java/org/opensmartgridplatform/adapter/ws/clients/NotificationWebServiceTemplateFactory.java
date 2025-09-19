e97060d69d0f8134cc61b6602837d52cd5c1b730
Harry Middelburg <harry.middelburg@alliander.com>
2025-09-19 09:34:35 +0200

    REmoved password from logging
    Signed-off-by: Harry Middelburg <harry.middelburg@alliander.com>

--- osgp/platform/osgp-adapter-ws-shared/src/main/java/org/opensmartgridplatform/adapter/ws/clients/NotificationWebServiceTemplateFactory.java
+++ osgp/platform/osgp-adapter-ws-shared/src/main/java/org/opensmartgridplatform/adapter/ws/clients/NotificationWebServiceTemplateFactory.java
@@ -173,25 +173,21 @@ public class NotificationWebServiceTemplateFactory {
     LOGGER.info(
         """
     NotificationWebServiceConfiguration
-    targetUri: {},
-    keystore:   {},
-                {},
-                {},
-                {},
-    truststore: {},
-                {},
-                {},
-                {}
+    targetUri: {}
+    keystore:   use:      {}
+                type:     {}
+                location: {}
+    truststore: use:      {}
+                type:     {}
+                location: {}
     """,
         config.getTargetUri(),
         config.isUseKeyStore(),
         config.getKeyStoreType(),
         config.getKeyStoreLocation(),
-        config.getKeyStorePassword(),
         config.isUseTrustStore(),
         config.getTrustStoreType(),
-        config.getTrustStoreLocation(),
-        config.getTrustStorePassword());
+        config.getTrustStoreLocation());
     if (config.isUseKeyStore() || config.isUseTrustStore()) {
       clientBuilder.setSSLSocketFactory(this.createSslConnectionSocketFactory(config));
     }
